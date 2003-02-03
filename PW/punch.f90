18c747f55cf0b3cbcb52c96bbd384f0c67c378b0
cavazzon <cavazzon@c92efa57-630b-4861-b058-cf58834340f0>
2003-02-03 09:03:53 +0000

    - back to the previous punch carlo
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@49 c92efa57-630b-4861-b058-cf58834340f0

--- PW/punch.f90
+++ PW/punch.f90
@@ -6,6 +6,9 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !
+
+#if defined __NEW_PUNCH
+
 !----------------------------------------------------------------------
 subroutine punch  
   !-----------------------------------------------------------------------
@@ -15,8 +18,7 @@ subroutine punch
   !
   !
   use pwcom, only: nks, filpun, reduce_io, evc, nwordwfc, iunwfc, lscf, &
-    rho, nspin, iunpun, et, wg, nbnd, nkstot, nbndx, tau, ityp, irt, xk,&
-    wk, isk, lforce, force, ltetra, tetra
+    rho, nspin, iunpun, et, wg, nbnd, nkstot, nbndx
   use io, only: prefix
 #ifdef PARA
   use para
@@ -26,14 +28,10 @@ subroutine punch
   implicit none
   !
   integer :: ik, i, ibnd, kunittmp  
-  logical :: exst
-  logical :: new_punch = .true.  
+  logical :: exst  
   !
-  if (new_punch) then
-     filpun = trim(prefix)//'.save'
-  else
-     filpun = trim(prefix)//'.pun'
-  end if
+  filpun = trim(prefix)//'.save'
+  write (6, '(/,5x,"Writing file ",a14,"for program phonon")') filpun
   !
   kunittmp = 1
   !
@@ -62,6 +60,8 @@ subroutine punch
   call poolrecover (et, nbndx, nkstot, nks)
   call poolrecover (wg, nbnd , nkstot, nks)
   !
+  ! In parallel execution, only the first node writes this file
+  !
   kunittmp = kunit
   !
 #endif
@@ -69,46 +69,102 @@ subroutine punch
   !  Write the charge density on a separate file
   !
   call io_pot ( + 1, trim(prefix)//'.rho', rho, nspin)  
-  
+
   iunpun = 4  
-  if (new_punch) then
-     !
-     ! write one single file with all wavefunctions
-     !
-     call writefile_new( 'all', iunpun, et, wg, kunittmp )
-     !
-  else
-     !
-     ! write old-fashioned file, leave wavefunctions where they are
-     !
-#ifdef PARA
-     ! In parallel execution, only the first node writes this file
-     if (me.eq.1.and.mypool.eq.1) then
-#endif
-     call seqopn (iunpun, filpun, 'unformatted', exst)  
-     !
-     call saveall (iunpun, 1)  
-     write (iunpun) tau  
-     write (iunpun) ityp  
-     write (iunpun) irt  
-     if (lforce) write (iunpun) force  
-     if (ltetra) write (iunpun) tetra  
-     write (iunpun) ( (xk (i, ik), i = 1, 3), ik = 1, nkstot)  
-     write (iunpun) (  wk (ik), ik = 1, nkstot)  
-     write (iunpun) ( isk (ik), ik = 1, nkstot)
-#ifdef PARA
-     write (iunpun) kunit
-#endif
-     write (iunpun) ( (et (ibnd, ik), ibnd = 1, nbnd), ik = 1, nkstot)  
-     write (iunpun) ( (wg (ibnd, ik), ibnd = 1, nbnd), ik = 1, nkstot)
-     close (unit = iunpun)  
-#ifdef PARA
-     end if
-#endif
-     !
-     call writefile_new( 'config', iunpun, et, wg, kunittmp )
-     !
-  end if
+  call writefile_new( 'all', iunpun, et, wg, kunittmp )
 
   return  
 end subroutine punch
+
+
+#else
+
+
+!----------------------------------------------------------------------
+subroutine punch  
+  !-----------------------------------------------------------------------
+  !
+  !     This routine is called at the end of the run to save on a file
+  !     the informations needed to the phonon program.
+  !
+  !
+  use pwcom  
+  use io, only: prefix
+#ifdef PARA
+  use para
+#endif
+  use restart_module, only: writefile_new
+  !
+  implicit none
+  !
+  integer :: ik, i, ibnd, kunittmp  
+  logical :: exst  
+  !
+  filpun = trim(prefix)//'.pun'
+  write (6, '(/,5x,"Writing file ",a14,"for program phonon")') filpun
+  !
+  kunittmp = 1
+  !
+  ! if the wavefunction has not been written on file, do it now
+  !
+  if (nks.eq.1.and.reduce_io) call davcio (evc, nwordwfc, iunwfc, 1, + 1)
+  !
+  ! The following instruction is used  when more k-points are needed
+  ! for finite-q phonon calculations (on fine q-grid) then those needed
+  ! for self-consistency. In such a case, a self-consistent calculation
+  ! with few k-points is followed by a non-self-consistent one with added
+  ! k-points, whose weight is set to zero.
+  !
+  if (.not.lscf) call sum_band  
+  !
+  !  Write: general variables (including dimensions of the arrays),
+  !  atomic positions, forces, k-points, eigenvalues
+  !
+#ifdef PARA
+  !
+  ! xk, wk, isk, et, wg are distributed across pools
+  ! the first node has a complete copy of xk, wk, isk,
+  ! while eigenvalues et and weights wg must be
+  ! explicitely collected to the first node
+  !
+  call poolrecover (et, nbndx, nkstot, nks)
+  call poolrecover (wg, nbnd , nkstot, nks)
+  !
+  ! In parallel execution, only the first node writes this file
+  !
+  if (me.eq.1.and.mypool.eq.1) then
+#endif
+  iunpun = 4  
+  call seqopn (iunpun, filpun, 'unformatted', exst)  
+  !
+  call saveall (iunpun, 1)  
+  write (iunpun) tau  
+  write (iunpun) ityp  
+  write (iunpun) irt  
+  if (lforce) write (iunpun) force  
+  if (ltetra) write (iunpun) tetra  
+  write (iunpun) ( (xk (i, ik), i = 1, 3), ik = 1, nkstot)  
+  write (iunpun) (  wk (ik), ik = 1, nkstot)  
+  write (iunpun) ( isk (ik), ik = 1, nkstot)
+#ifdef PARA
+  write (iunpun) kunit
+  kunittmp = kunit
+#endif
+  write (iunpun) ( (et (ibnd, ik), ibnd = 1, nbnd), ik = 1, nkstot)  
+  write (iunpun) ( (wg (ibnd, ik), ibnd = 1, nbnd), ik = 1, nkstot)
+  close (unit = iunpun)  
+#ifdef PARA
+  end if
+#endif
+  !
+  !  Write the charge density on a separate file
+  !
+  call io_pot ( + 1, trim(prefix)//'.rho', rho, nspin)  
+
+  call writefile_new( 'config', iunpun, et, wg, kunittmp )
+
+  return  
+end subroutine punch
+
+#endif
+
