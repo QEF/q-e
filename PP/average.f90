d4ac74ca37a02020ba758267fe1de088ba126103
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2003-06-30 08:11:42 +0000

    Generalizzata average.f90 in modo da poter fare medie planari sui piani che si ottengono tenendo fisso uno qualunque degli indici della mesh di fft.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@251 c92efa57-630b-4861-b058-cf58834340f0

--- PP/average.f90
+++ PP/average.f90
@@ -10,26 +10,32 @@
 subroutine average
   !-----------------------------------------------------------------------
   !
-  !      This program is used to compute the macroscopic averages of the
-  !      charge density along a given direction.
-  !      It read the charge density, or more than one charge from
-  !      input and adds them with appropriate weights.
-  !      Then it computes the planar averages of this charge
-  !      along the xy planes. As a third step it interpolates
-  !      the averages on a mesh with a given number of points.
-  !      At the end it computes a macroscopic average with a
-  !      window of fixed dimension.
+  !      This program calculates planar and macroscopic averages
+  !      of a quantity defined on a 3D-FFT mesh. 
+  !      The planar average is done on FFT mesh planes. 
+  !      It reads the quantity to average, or several quantities, from
+  !      one or several files and adds them with the given weights.
+  !      It computes the planar average of the resulting quantity
+  !      averaging on planes defined by the FFT mesh points and by one
+  !      direction perpendicular to the planes.
+  !      The planar average can be interpolated on a  
+  !      1D-mesh with an arbitrary number of points.
+  !      Finally, it computes the macroscopic average. The size
+  !      of the averaging window is given as input.
   !
-  !      It receive as input the following file:
+  !      It receive as input the following variables:
   !
-  !      nfile        ! the number of charge files
-  !      filename     ! the name of the charge file
-  !      weight       ! the weight of this charge
+  !      nfile        ! the number of 3D-FFT files
+  ! for each file:
+  !      filename     ! the name of the 3D-FFT file
+  !      weight       ! the weight of the quantity in this file
   !      .
   !      .
+  ! end
   !      npt          ! the number of points of the thick mesh
-  !      awin         ! the lenght of window which computes the averages.
-  !      ionflag      ! if true the ionic charge is added
+  !      idir         ! 1,2 or 3. It is the fixed index which defines
+  !                   ! the planes of the planar average
+  !      awin         ! the size of the window for macroscopic averages.
   !
 #include "machine.h"
   use parameters, only: DP
@@ -56,7 +62,7 @@ subroutine average
   ! points in the window
   ! counter on atoms
   ! counter on mesh points
-  ! counter on directions
+  ! counters on directions
 
   real(kind=DP) :: rhodum, awin, deltaz, weight (nfilemax), gre(npixmax), &
        gim(npixmax), macros(npixmax)
@@ -71,12 +77,12 @@ subroutine average
   ! the function to average (im. part)
 
   real(kind=DP) :: celldms (6), gcutmsa, duals, ecuts, zvs (ntypx), ats(3,3)
+  real(kind=DP) :: leng
   real(kind=DP), allocatable :: taus (:,:)
   integer, allocatable :: ityps (:)
   character (len=3) :: atms(ntypx)
 
-  logical :: ionflag
-  ! if true the ionic charge is added
+  integer :: nfft, nfftx, idir
 
   character (len=80) :: filename (nfilemax)
   ! names of the files with the charge
@@ -92,21 +98,44 @@ subroutine average
   read (inunit, *, err = 1100, iostat = ios) npt
 
   if (npt.lt.0.or.npt.gt.npixmax) call errore ('average', ' wrong npt', 1)
+  read (inunit, *, err = 1100, iostat = ios) idir
   read (inunit, *, err = 1100, iostat = ios) awin
-  read (inunit, *, err = 1100, iostat = ios) ionflag
 
 1100 call errore ('average', 'readin input', abs (ios) )
 
   call plot_io (filename (1), title, nrx1, nrx2, nrx3, nr1, nr2, &
        nr3, nat, ntyp, ibrav, celldm, at, gcutm, dual, ecutwfc, &
        plot_num, atm, ityp, zv, tau, rhodum, 0)
-  if (npt.le.nr3) call errore ('average', 'npt smaller than nr3', 1)
 
-  allocate(tau (3, nat) )
-  allocate(ityp(nat) )
-  alat = celldm (1)
+  nspin = 1
+  if (ibrav.gt.0) call latgen (ibrav, celldm, at (1, 1), &
+                                              at (1, 2), at (1, 3) )
+  call recips (at (1, 1), at (1, 2), at (1, 3), bg (1, 1), bg (1, 2) &
+       , bg (1, 3) )
+
+  alat = celldm(1)
   tpiba = 2.d0 * pi / alat
   tpiba2 = tpiba**2
+
+  if (idir.eq.1) then
+     nfft=nr1
+     nfftx=nrx1
+     leng=alat*sqrt(at(1,1)**2+at(2,1)**2+at(3,1)**2)
+  elseif (idir.eq.2) then
+     nfft=nr2
+     nfftx=nrx2
+     leng=alat*sqrt(at(1,2)**2+at(2,2)**2+at(3,2)**2)
+  elseif (idir.eq.3) then
+     nfft=nr3
+     nfftx=nrx3
+     leng=alat*sqrt(at(1,3)**2+at(2,3)**2+at(3,3)**2)
+  else
+     call errore('average','idir is wrong',1)
+  endif
+  if (npt.lt.nfft) call errore ('average', 'npt smaller than nfft', 1)
+
+  allocate(tau (3, nat))
+  allocate(ityp(nat))
   doublegrid = dual.gt.4.d0
   if (doublegrid) then
      gcutms = 4.d0 * ecutwfc / tpiba2
@@ -114,14 +143,6 @@ subroutine average
      gcutms = gcutm
   endif
 
-  nspin = 1
-  if (ibrav.gt.0) then
-    call latgen (ibrav, celldm, at (1, 1), at (1, 2), at (1, 3), omega )
-    at = at / alat  !  bring at in units of alat
-  end if
-
-  call recips (at (1, 1), at (1, 2), at (1, 3), bg (1, 1), bg (1, 2) &
-       , bg (1, 3) )
 
   call volume (alat, at (1, 1), at (1, 2), at (1, 3), omega)
   call set_fft_dim
@@ -140,7 +161,7 @@ subroutine average
      psic (ir) = weight (1) * cmplx (rho (ir, 1),0.d0)
   enddo
   !
-  !       Now we open the input file and we read what has been written
+  !       Now we open all the other files
   !
   iunpun = 4
   !
@@ -177,52 +198,77 @@ subroutine average
   !
   !   compute the direct and reciprocal lattices
   !
-  allocate (funcr(nrx3))    
-  allocate (funci(nrx3))    
+  allocate (funcr(nfftx))    
+  allocate (funci(nfftx))    
   !
   !     At this point we start the calculations, first we compute the
   !     planar averages
   !
-  do k = 1, nr3
-     funcr (k) = 0.d0
-     funci (k) = 0.d0
-     do j = 1, nr2
-        do i = 1, nr1
-           ir = i + (j - 1) * nrx1 + (k - 1) * nrx1 * nrx2
-           funcr (k) = funcr (k) + real (psic (ir) )
+  if (idir.eq.1) then
+     do i = 1, nr1
+        funcr (i) = 0.d0
+        funci (i) = 0.d0
+        do j = 1, nr2
+           do k = 1, nr3
+              ir = i + (j - 1) * nrx1 + (k - 1) * nrx1 * nrx2
+              funcr (i) = funcr (i) + real (psic(ir))
+           enddo
         enddo
+        funcr (i) = funcr (i) / (float (nr2 * nr3))
      enddo
-     funcr (k) = funcr (k) / (float (nr1 * nr2) )
-  enddo
-  do k = 1, nr3
-     write (6, * ) k, funcr (k)
-  enddo
-  !
-  !     add more points to compute the macroscopic average
-  !
-  call cft (funcr, funci, nr3, nr3, nr3, - 1)
-  call DSCAL (nr3, 1.d0 / nr3, funcr, 1)
-  call DSCAL (nr3, 1.d0 / nr3, funci, 1)
+  elseif (idir.eq.2) then
+     do j = 1, nr2
+        funcr (j) = 0.d0
+        funci (j) = 0.d0
+        do i = 1, nr1
+           do k = 1, nr3
+              ir = i + (j - 1) * nrx1 + (k - 1) * nrx1 * nrx2
+              funcr (j) = funcr (j) + real (psic (ir) )
+           enddo
+        enddo
+        funcr (j) = funcr (j) / (float (nr1 * nr3) )
+     enddo
+  elseif (idir.eq.3) then
+     do k = 1, nr3
+        funcr (k) = 0.d0
+        funci (k) = 0.d0
+        do j = 1, nr2
+           do i = 1, nr1
+              ir = i + (j - 1) * nrx1 + (k - 1) * nrx1 * nrx2
+              funcr (k) = funcr (k) + real (psic (ir) )
+           enddo
+        enddo
+        funcr (k) = funcr (k) / (float (nr1 * nr2) )
+     enddo
+  else
+     call errore('average','wrong idir',1)
+  endif
+!
+!     add more points to compute the macroscopic average
+!
+  call cft (funcr, funci, nfft, nfft, nfft, - 1)
+  call DSCAL (nfft, 1.d0 / nfft, funcr, 1)
+  call DSCAL (nfft, 1.d0 / nfft, funci, 1)
   do k = 1, npt
-     if (k.le.nr3 / 2) then
+     if (k.le.nfft / 2) then
         gre (k) = funcr (k)
         gim (k) = funci (k)
-     elseif (k.gt.npt - nr3 / 2) then
-        gre (k) = funcr (k - npt + nr3)
-        gim (k) = funci (k - npt + nr3)
+     elseif (k.gt.npt - nfft / 2) then
+        gre (k) = funcr (k - npt + nfft)
+        gim (k) = funci (k - npt + nfft)
      else
         gre (k) = 0.d0
         gim (k) = 0.d0
      endif
   enddo
-  if (mod (nr3, 2) .eq.0) then
-     gre (nr3 / 2 + 1) = 0.5d0 * funcr (nr3 / 2 + 1)
-     gim (nr3 / 2 + 1) = 0.5d0 * funci (nr3 / 2 + 1)
-     gre (npt - nr3 / 2 + 1) = gre (nr3 / 2 + 1)
-     gim (npt - nr3 / 2 + 1) = - gim (nr3 / 2 + 1)
+  if (mod (nfft, 2) .eq.0) then
+     gre (nfft / 2 + 1) = 0.5d0 * funcr (nfft / 2 + 1)
+     gim (nfft / 2 + 1) = 0.5d0 * funci (nfft / 2 + 1)
+     gre (npt - nfft / 2 + 1) = gre (nfft / 2 + 1)
+     gim (npt - nfft / 2 + 1) = - gim (nfft / 2 + 1)
   else
-     gre (nr3 / 2 + 1) = funcr (nr3 / 2 + 1)
-     gim (nr3 / 2 + 1) = funci (nr3 / 2 + 1)
+     gre (nfft / 2 + 1) = funcr (nfft / 2 + 1)
+     gim (nfft / 2 + 1) = funci (nfft / 2 + 1)
   endif
 
 
@@ -230,7 +276,7 @@ subroutine average
   !
   !     compute the macroscopic average
   !
-  nmacro = npt * (awin / (alat * celldm (3) ) )
+  nmacro = npt * (awin / leng )
   if (nmacro.le.0) call errore ('average ', 'nmacro is too small ', 1)
   do i = 1, npt
      macros (i) = 0.d0
@@ -249,7 +295,7 @@ subroutine average
   !
   !     print the results on output
   !
-  deltaz = alat * celldm (3) / float (npt - 1)
+  deltaz = leng / float (npt)
 
 
   write (6, '(3f15.9)') (deltaz * (i - 1) , gre (i) , macros (i) , &
