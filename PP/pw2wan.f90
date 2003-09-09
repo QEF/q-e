296b7de4c7c96f6dbb5729a56f87a630f517c985
cavazzon <cavazzon@c92efa57-630b-4861-b058-cf58834340f0>
2003-09-09 13:35:18 +0000

    interface for wannier function code
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@307 c92efa57-630b-4861-b058-cf58834340f0

--- PP/pw2wan.f90
+++ PP/pw2wan.f90
@@ -33,13 +33,12 @@ subroutine do_wannier(nodenumber)
   real(kind=8), dimension(3):: s0
   integer :: ik, i
 
-  namelist / inputpp / tmp_dir, filpun, nk, s0, prefix
+  namelist / inputpp / tmp_dir, nk, s0, prefix
   !
   nd_nmbr = nodenumber  
   !
   !   set default values for variables in namelist
   !
-  filpun = ' '  
   tmp_dir = './'
   prefix = ' '
   nk = 0
@@ -51,17 +50,11 @@ subroutine do_wannier(nodenumber)
   !
   !     Check of namelist variables
   !
-  if (filpun.eq.' ') &
-       call errore ('wannier', 'Missing input file name', 1)
-  !
   !   Now allocate space for pwscf variables, read and check them.
   !
   call read_file  
   call openfil  
 
-  do ik = 1, nks
-    write(6,fmt="(3F9.6)" ) xk(1,ik), xk(2,ik), xk(3,ik)
-  end do
   !
   if (nk(1)==0 .and. nk(2)==0 .and. nk(3) == 0) then
      nk(1) = nk1
@@ -71,17 +64,20 @@ subroutine do_wannier(nodenumber)
      s0(2) = k2/2d0
      s0(3) = k3/2d0
   end if
+
   call write_wannier (nk, s0)
 
-  call cryst_to_cart (nks,xk,at,-1)
+  write( 6, * ) 'K-points (reciprocal lattice coordinates) '
   do ik = 1, nks
-    write(6,fmt="(' ik = ',I3,3F10.6)" ) ik,xk(1,ik), xk(2,ik), xk(3,ik)
+    write(6,fmt="(' ik = ',I3,3F10.6)" ) ik, xk(1,ik), xk(2,ik), xk(3,ik)
   end do
-  write(6,*)
-  do i = 1, 3
-    write(6,fmt="(' a(',I1,')',3F10.6)" ) i,at(1,i), at(2,i), at(3,i)
+  !
+  call cryst_to_cart ( nks, xk, at, -1)
+  !
+  write( 6, * ) 'K-points in unit of 2PI/alat (cartesian coordinates) '
+  do ik = 1, nks
+    write(6,fmt="(' ik = ',I3,3F10.6)" ) ik, xk(1,ik), xk(2,ik), xk(3,ik)
   end do
-
   !
   return  
 end subroutine do_wannier
@@ -98,55 +94,89 @@ subroutine write_wannier (nk, s0)
   integer , dimension(3):: nk
   real(kind=8), dimension(3):: s0
 
-  integer :: i,j,k, ig, ik, ibnd
+  integer :: i,j,k, ig, ik, ibnd, na
   integer, allocatable :: kisort(:,:)
   real(kind=8), allocatable :: ei_k(:,:)
+  real(kind=8), allocatable :: rat(:,:,:)
+  integer, allocatable :: natom(:)
 
-  open (unit=4, file='KG_GRIDS.DAT', &
-       form='formatted', status='unknown') 
-  write(4,'(3i4,3f12.6,i8)') nk, s0, ngm
-  write(4,*) (ig1(ig), ig2(ig), ig3(ig), ig=1,ngm)
-  close(unit=4)
+  allocate ( kisort( npwx, nkstot ) )
+  allocate (ei_k(npwx,nkstot))
+  allocate ( rat( 3, nat, ntyp ) )
+  allocate ( natom( ntyp ) )
+  kisort = 0
+  ei_k = 0.0
+  natom = 0 
 
-  open (unit=40, file='vec.in', &
-       form='unformatted', status='unknown')
+  do i = 1, nat
+    if( ityp(i) > ntyp ) &
+      call errore( ' write_wannier ', ' type index out of range ', 1 )
+    natom( ityp( i ) ) = natom( ityp( i ) ) + 1
+    rat( :, natom( ityp( i ) ), ityp( i ) ) = tau( :, i )
+    call cryst_to_cart ( 1, rat( :, natom( ityp( i ) ), ityp( i ) ), at, -1)
+  end do
+
+  !  Open file launch.dat
+
+  open (unit=40, file='launch.dat', form='unformatted', status='unknown')
+
+  write(6,*) 'Crystal basis vectors, in unit of alat = ', alat
+  do i = 1, 3
+    write(6,fmt="(' a(',I1,')',3F10.6)" ) i,at(1,i), at(2,i), at(3,i)
+  end do
+  write( 40 ) alat
+  write( 40 ) ( at( i, 1 ), i = 1, 3 )  ! save A1
+  write( 40 ) ( at( i, 2 ), i = 1, 3 )  ! save A2
+  write( 40 ) ( at( i, 3 ), i = 1, 3 )  ! save A3
+
+  write(6,*) 'Atomic positions ( lattice coordinates )'
+  write( 40 ) ntyp
+  do i = 1, ntyp
+    write( 40 ) natom(i), atm(i)
+    write( 40 ) ( ( rat( k, j, i ), k = 1, 3 ), j = 1, natom(i) )
+    write(6,*) 'Specie ', atm(i), ' atoms = ', natom(i)
+    do j = 1, natom( i )
+      write(6,fmt="(' tau(',I1,')',3F10.6)" ) j, ( rat( k, j, i ), k = 1, 3 )
+    end do
+  end do
+
+  write (40) ecutwfc, nbnd
+  write (40) ( nk(i), i = 1, 3 ), ( s0(j), j = 1, 3 ), ngm
+  write (40)( ig1(ig), ig2(ig), ig3(ig), ig = 1, ngm )
+
+  do ik = 1, nkstot
+     npw = npwx
+     call gk_sort (xk (1, ik), ngm, g, ecutwfc / tpiba2, npw, kisort(1,ik), g2kin)
+     ngk (ik) = npw
+     write (6,fmt="(' k ',2I4,4F12.6)") ik, npw, ecutwfc / tpiba2,  xk(1,ik), xk(2,ik), xk(3,ik)
+  end do
+  write (40) npwx, nbnd, nkstot
+  write (6,*) 'Wave function dimensions ( npwx, nbnd, nkstot) = ', npwx, nbnd, nkstot
+  write (40)( ( kisort(ig,ik), ig = 1, npwx ), ik = 1, nkstot )
+
+  do i = 1, npwx
+     ! write(*,'(1(3x,f12.6),4i7)') dreal(evc(i,1)*conjg(evc(i,1))),kisort(i,1), &
+     !     ig1(kisort(i,1)), ig2(kisort(i,1)), ig3(kisort(i,1))
+  end do
+ 
+  ei_k(1:nbnd,:) = et(1:nbnd,:) / 2.0d0  !  Rydberg to Hartree conversion
+
+  write (40) ( ( ei_k( ig, ik ), ig = 1, npwx ), ik = 1, nkstot )
+  write (40) ( ngk( ik ), ik = 1, nkstot )
+  write (40) ( nbnd, ik = 1, nkstot )
+  write (40) nr1, nr2, nr3, ngm
 
   do ik = 1, nkstot
      call davcio (evc, nwordwfc, iunwfc, ik, - 1)  
      write (40) ((DREAL(evc(ig,ibnd)), ig=1,npwx),ibnd=1,nbnd)
      write (40) ((1.0*DIMAG(evc(ig,ibnd)), ig=1,npwx),ibnd=1,nbnd)
   end do
-  close(unit=40)
 
-  allocate (kisort(npwx,nkstot))
-  kisort = 0
-  open (unit=41, file='val.in', &
-       form='unformatted', status='unknown')
-  do ik = 1, nkstot
-     npw = npwx
-     call gk_sort (xk (1, ik), ngm, g, ecutwfc / tpiba2, npw, &
-          kisort(1,ik), g2kin)
-     ngk (ik) = npw
-     write (6,fmt="(' k ',I4,3F12.6)") ik, xk(1,ik), xk(2,ik), xk(3,ik)
-  end do
-  write (41) npwx, nbnd, nkstot
-  write (6,*) ' *** DEBUG 1 = ', npwx, nbnd, nkstot
-  write (41)((kisort(ig,ik),ig=1,npwx),ik=1,nkstot)
-
-  do i=1,npwx
-     write(*,'(1(3x,f12.6),4i7)') dreal(evc(i,1)*conjg(evc(i,1))),kisort(i,1), &
-          ig1(kisort(i,1)), ig2(kisort(i,1)), ig3(kisort(i,1))
-  end do
- 
   deallocate (kisort)
-  allocate (ei_k(npwx,nkstot))
-  ei_k = 0.0
-  ei_k(1:nbnd,:) = et(1:nbnd,:) / 2.0d0  !  Rydberg to Hartree conversion
-  write (41) ((ei_k(ig,ik),ig=1,npwx),ik=1,nkstot)
   deallocate (ei_k)
-  write (41) (ngk(ik), ik=1,nkstot)
-  write (41) (nbnd, ik=1,nkstot)
-  write (41) nr1, nr2, nr3, ngm
-  close (unit=41)
+  deallocate ( rat )
+  deallocate ( natom )
+
+  close ( unit = 40 )
 
 end subroutine write_wannier
