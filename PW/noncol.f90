a4bc8d02bdc57c87fde4a552f2e7da6a55b77ea4
moscac <moscac@c92efa57-630b-4861-b058-cf58834340f0>
2003-10-30 13:40:10 +0000

    *** empty log message ***
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@358 c92efa57-630b-4861-b058-cf58834340f0

---
+++ PW/noncol.f90
@@ -0,0 +1,47 @@
+!
+! Copyright (C) 2001-2003 PWSCF group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!----------------------------------------------------------------------------
+!
+MODULE noncollin
+  USE parameters, ONLY :  DP, ntypx
+  !
+  SAVE
+  !
+  INTEGER :: &
+      npol,               & !  number of coordinates of wfc
+      report,             & !  print the local quantities (magnet. and rho)
+                            !  every #report iterations
+      i_cons                !  indicator for contrained local quantities
+  !
+  INTEGER, ALLOCATABLE :: &
+      pointlist(:,:),     & !  points in the integration volume
+                            !  around atom na
+      pointnum(:)           !  number of such points
+  !
+  LOGICAL :: &
+      noncolin              !  true if noncollinear magnetism is allowed
+  !
+  REAL (KIND=DP) :: &
+      angle1(ntypx),       &!  Define the polar coordinates of the starting
+      angle2(ntypx),       &!  magnetization's direction for each atom
+      mcons(3,ntypx),      &!  constrained values for local variables
+      vtcon,               &!  contribution of the constraining fields to
+                            !  the total energy
+      degspin,             &!  spin degeneration (2.0 in LDA case,
+                            !  1.0 in the other cases)
+      r_m,                 &!  Radius for local integrations
+      lambda                !  prefactor in the penalty functional 
+                            !  for constraints
+  !
+  REAL (KIND=DP), ALLOCATABLE :: &
+      factlist(:,:),       &! weightenig factors for local integrations
+      r_loc(:),            &! local integrated charge 
+      m_loc(:,:)            ! local integrated magnetization
+     ! mcons(:,:),           ! constrained values for local variables
+  !
+END MODULE noncollin
