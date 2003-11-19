8e19250404dd8354f4aa9bc9e8a3710759b9dc10
moscac <moscac@c92efa57-630b-4861-b058-cf58834340f0>
2003-11-19 13:08:23 +0000

    *** empty log message ***
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@404 c92efa57-630b-4861-b058-cf58834340f0

--- PW/delta_e.f90
+++ PW/delta_e.f90
@@ -7,53 +7,56 @@
 !
 
 subroutine delta_e (nr1, nr2, nr3, nrxx, rho, v_in, v_out, omega, &
-     de, deband, nspin)
-  use parameters
-  implicit none
+ de, deband, nspin)
+use parameters
+implicit none
 
-  integer :: nspin, nr1, nr2, nr3, nrxx, i
-  ! nspin=1 if LDA, nspin=2 if LSDA
-  ! fft grid dimensions
-  ! counter
+integer :: nspin, nr1, nr2, nr3, nrxx, i, ipol
+                             ! nspin=1 if LDA, nspin=2 if LSDA
+                             ! fft grid dimensions
+                             ! counters
 
-  real(kind=DP) :: rho (nrxx, nspin), v_in (nrxx, nspin), v_out (nrxx, &
-       nspin), omega, de, deband
-  ! charge density
-  ! in and ...
-  ! ... out potentials from potential mixing
-  ! cell volume
-  ! total energy and band energy corrections
 
-  de = 0.d0
-  deband = 0.d0
-  if (nspin.eq.1) then
-     !
-     ! spin-unpolarized case
-     !
+real(kind=DP) :: rho (nrxx, nspin), v_in (nrxx, nspin), v_out (nrxx, &
+ nspin), omega, de, deband
+                             ! charge density
+                             ! in and ...
+                             ! ... out potentials from potential mixing
+                             ! cell volume
+                             ! total energy and band energy corrections
+de = 0.d0
+deband = 0.d0
+
+if (nspin.ne.2) then
+!
+! LDA case
+!
+  do ipol=1,nspin
      do i = 1, nrxx
-        de     = de + rho(i,nspin) * ( v_out(i,1) - v_in(i,1) )
-        deband = deband - rho(i,nspin) * v_in(i,1)
+        de     = de + rho(i,ipol) * ( v_out(i,ipol) - v_in(i,ipol) )
+        deband = deband - rho(i,ipol) * v_in(i,ipol)
      enddo
-  else
-     !
-     ! spin-polarized case
-     !
-     do i = 1, nrxx
-        de     = de + rho(i,1) * ( v_out(i,1) - v_in(i,1) ) + &
-             rho(i,2) * ( v_out(i,2) - v_in(i,2) )
-        deband = deband - rho(i,1) * v_in(i,1) - rho(i,2) * v_in(i,2)
-     enddo
+  enddo
+else
+!
+! LSDA case
+!
+   do i = 1, nrxx
+      de     = de + rho(i,1) * ( v_out(i,1) - v_in(i,1) ) + &
+                    rho(i,2) * ( v_out(i,2) - v_in(i,2) )
+      deband = deband - rho(i,1) * v_in(i,1) - rho(i,2) * v_in(i,2)
+   enddo
 
-  endif
+endif
 
-  de = omega * de / (nr1 * nr2 * nr3)
-  deband = omega * deband / (nr1 * nr2 * nr3)
+de = omega * de / (nr1 * nr2 * nr3)
+deband = omega * deband / (nr1 * nr2 * nr3)
 
 #ifdef __PARA
-  call reduce (1, de)
-  call reduce (1, deband)
+call reduce (1, de)
+call reduce (1, deband)
 #endif
 
-  return
+return
 
 end subroutine delta_e
