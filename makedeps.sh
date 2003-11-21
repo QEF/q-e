6596e1983b278e63b536e0f766f3806347fad908
ballabio <ballabio@c92efa57-630b-4861-b058-cf58834340f0>
2003-11-21 16:39:48 +0000

    cleanup [Gerardo]
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@427 c92efa57-630b-4861-b058-cf58834340f0

--- makedeps.sh
+++ makedeps.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # compute dependencies
 
+# run from directory where this script is
+cd `echo $0 | sed 's/\(.*\)\/.*/\1/'` # extract pathname
 TOPDIR=`pwd`
 
 # directory that only depends on itself
-cd $TOPDIR/Modules
-$TOPDIR/moduledep.sh > .dependencies
+DIR="Modules"
+DEPENDS=""
+if test -d $TOPDIR/$DIR ; then
+  cd $TOPDIR/$DIR
+  $TOPDIR/moduledep.sh $DEPENDS > .dependencies
+fi
 
 # directories that depend on Modules/
 DIRS="PW FPMD CPV flib"
