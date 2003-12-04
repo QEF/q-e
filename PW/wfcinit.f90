7649da5ee6511ca123d8d610d5f9181807d11aa3
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2003-12-04 09:51:26 +0000

    NEB at gamma (NEB/pwgneb.x) is now available. C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@442 c92efa57-630b-4861-b058-cf58834340f0

--- PW/wfcinit.f90
+++ PW/wfcinit.f90
@@ -2,156 +2,180 @@
 ! Copyright (C) 2001-2003 PWSCF group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
-! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-!
-!-----------------------------------------------------------------------
-subroutine wfcinit
-  !-----------------------------------------------------------------------
-  !
-  ! This routine computes an estimate of the starting wavefunctions
-  ! from superposition of atomic wavefunctions.
-  !
 #include "machine.h"
-  USE io_global,        ONLY : stdout
-  USE io_files, ONLY: nwordatwfc, iunat, iunigk, nwordwfc, iunwfc
-
-  use pwcom
-  USE wavefunctions_module,    ONLY : evc
-  implicit none
+!
+!----------------------------------------------------------------------------
+SUBROUTINE wfcinit()
+  !----------------------------------------------------------------------------
   !
-  integer :: ik, ibnd, ig, ipol, n_starting_wfc
-  ! counter on k points
-  !    "     "   bands
-  !    "     "  plane waves
-  !    "     "  polarization
-  ! number of starting wavefunctions
-  complex(kind=DP), allocatable  ::  wfcatom(:,:)
-  ! atomic wfcs for initialization
-  real(kind=DP) ::rr, arg
-  real(kind=DP), external :: rndm
-  ! random function generation
+  !     This routine computes an estimate of the starting wavefunctions
+  !     from superposition of atomic wavefunctions.
   !
-  ! state what is going to happen
+  USE parameters,            ONLY :  DP
+  USE io_global,             ONLY :  stdout
+  USE constants,             ONLY :  tpi, rytoev
+  USE brilz,                 ONLY :  tpiba2
+  USE basis,                 ONLY :  natomwfc, startingwfc
+  USE gvect,                 ONLY :  g
+  USE klist,                 ONLY :  xk, nks, nkstot
+  USE lsda_mod,              ONLY :  lsda, current_spin, isk
+  USE varie,                 ONLY :  isolve, iprint, reduce_io
+  USE wvfct,                 ONLY :  nbnd, npw, npwx, igk, g2kin, et
+  USE us,                    ONLY :  vkb, okvan
+  USE ldaU,                  ONLY :  swfcatom, lda_plus_u
+  USE io_files,              ONLY :  iunat, nwordwfc, iunwfc, iunigk, &
+                                     nwordatwfc
+  USE wavefunctions_module,  ONLY :  evc
   !
-  if (startingwfc == 'file') then
-     WRITE( stdout, '(5x,a)') 'Starting wfc from file'
+  IMPLICIT NONE
+  !
+  INTEGER :: &
+      ik,               &!  counter on k points
+      ibnd,             &!     "     "   bands
+      ig,               &!     "     "   plane waves
+      ipol,             &!     "     "   polarization
+      n_starting_wfc     !  number of starting wavefunctions
+  COMPLEX(KIND=DP), ALLOCATABLE :: & 
+      wfcatom(:,:)       !  atomic wfcs for initialization
+  REAL (KIND=DP) :: &
+      rr,               &!
+      arg                !
+  REAL (KIND=DP), EXTERNAL :: &
+      rndm               !  random function generation
+  !
+  !
+  ! ... state what is going to happen
+  !
+  IF ( startingwfc == 'file' ) THEN
      !
-     ! read the wavefunction into memory (if it is not done in c_bands)
+     WRITE( stdout, '(5X,A)' ) 'Starting wfc from file'
      !
-     if (nks.eq.1.and.reduce_io) call davcio(evc,nwordwfc,iunwfc,1,-1)
-     return
-  endif
-
-  call start_clock ('wfcinit')
-  if (startingwfc == 'atomic') then
-     if (natomwfc >= nbnd) then
-        WRITE( stdout, '(5x,a)') 'Starting wfc are atomic'
-     else
-        WRITE( stdout, '(5x,a,i3,a)') 'Starting wfc are atomic + ',&
-             nbnd-natomwfc, ' random wfc'
-     endif
-     n_starting_wfc = max (natomwfc, nbnd)
-  else
-     WRITE( stdout, '(5x,a)') 'Starting wfc are random'
+     ! ... read the wavefunction into memory (if it is not done in c_bands)
+     !
+     IF ( nks == 1 .AND. reduce_io ) &
+        CALL davcio( evc, nwordwfc, iunwfc, 1, -1 )
+     !
+     RETURN
+     !
+  END IF
+  !
+  CALL start_clock( 'wfcinit' )
+  !
+  IF ( startingwfc == 'atomic' ) THEN
+     IF ( natomwfc >= nbnd ) THEN
+        WRITE( stdout, '(5X,A)' ) 'Starting wfc are atomic'
+     ELSE
+        WRITE( stdout, '(5X,A,I3,A)') &
+            'Starting wfc are atomic + ',nbnd-natomwfc, ' random wfc'
+     END IF
+     n_starting_wfc = MAX( natomwfc, nbnd )
+  ELSE
+     WRITE( stdout, '(5X,A)' ) 'Starting wfc are random'
      n_starting_wfc = nbnd
-  endif
+  END IF
   !
-  ! Needed for LDA+U
+  ! ... Needed for LDA+U
   !
-  if (lda_plus_u) call orthoatwfc
-  if (nks > 1) rewind (iunigk)
+  IF ( lda_plus_u ) CALL orthoatwfc
+  IF ( nks > 1 ) REWIND( iunigk )
   !
-  !    we start a loop on k points
+  ! ... we start a loop on k points
   !
-  allocate (wfcatom( npwx, n_starting_wfc))
+  ALLOCATE( wfcatom(npwx,n_starting_wfc) )
   !
-  do ik = 1, nks
-     if (lsda) current_spin = isk (ik)
-     if (nks > 1) read (iunigk) npw, igk
+  DO ik = 1, nks
+     IF ( lsda ) current_spin = isk(ik)
+     IF ( nks > 1 ) READ( UNIT = iunigk ) npw, igk
      !
-     !     here we compute the kinetic energy
+     ! ... here we compute the kinetic energy
      !
-     do ig = 1, npw
-        g2kin (ig) = (xk (1, ik) + g (1, igk (ig) ) ) **2 + &
-                     (xk (2, ik) + g (2, igk (ig) ) ) **2 + &
-                     (xk (3, ik) + g (3, igk (ig) ) ) **2
-     enddo
+     DO ig = 1, npw
+        g2kin(ig) = ( xk(1,ik) + g(1,igk(ig)) )**2 + &
+                    ( xk(2,ik) + g(2,igk(ig)) )**2 + &
+                    ( xk(3,ik) + g(3,igk(ig)) )**2
+     END DO
      !
-     ! Put the correct units on the kinetic energy
+     ! ... Put the correct units on the kinetic energy
      !
-
-     g2kin(:) = g2kin(:)*tpiba2
-
-     if (lda_plus_u) call davcio (swfcatom, nwordatwfc, iunat, ik, - 1)
-
-     if (startingwfc == 'atomic') then
-        call atomic_wfc (ik, wfcatom)
-
+     g2kin(:) = g2kin(:) * tpiba2
+     !
+     IF ( lda_plus_u ) CALL davcio( swfcatom, nwordatwfc, iunat, ik, - 1 )
+     !
+     IF ( startingwfc == 'atomic' ) THEN
         !
-        !  if not enough atomic wfc are available, complete with random wfcs
+        CALL atomic_wfc( ik, wfcatom )
         !
-        do ibnd = natomwfc + 1, nbnd
-           do ig = 1, npw
-              rr = rndm ()
-              arg = tpi * rndm ()
-              wfcatom (ig, ibnd) = DCMPLX (rr * cos (arg), rr * sin (arg) )&
-                   / ( (xk (1, ik) + g (1, igk (ig) ) ) **2 + &
-                   (xk (2, ik) + g (2, igk (ig) ) ) **2 + &
-                   (xk (3, ik) + g (3, igk (ig) ) ) **2 + 1.0d0)
-           enddo
-        enddo
-
-     else
-        do ibnd = 1, nbnd
-           do ig = 1, npw
-              rr = rndm ()
-              arg = tpi * rndm ()
-              wfcatom (ig, ibnd) = DCMPLX (rr * cos (arg), rr * sin (arg) ) &
-                   / ( (xk (1, ik) + g (1, igk (ig) ) ) **2 + &
-                       (xk (2, ik) + g (2, igk (ig) ) ) **2 + &
-                       (xk (3, ik) + g (3, igk (ig) ) ) **2 + 1.0d0)
-           enddo
-        enddo
-
-     endif
-     call init_us_2 (npw, igk, xk (1, ik), vkb)
+        ! ... if not enough atomic wfc are available, complete with random wfcs
+        !
+        DO ibnd = ( natomwfc + 1 ), nbnd
+           DO ig = 1, npw
+              rr = rndm()
+              arg = tpi * rndm()
+              wfcatom(ig,ibnd) = CMPLX( rr * COS( arg ), rr * SIN( arg ) ) / &
+                                 ( ( xk(1,ik) + g(1,igk(ig)) )**2 + &
+                                   ( xk(2,ik) + g(2,igk(ig)) )**2 + &
+                                   ( xk(3,ik) + g(3,igk(ig)) )**2 + 1.0D0 )
+           END DO
+        END DO
+        !
+     ELSE
+        DO ibnd = 1, nbnd
+           DO ig = 1, npw
+              rr = rndm()
+              arg = tpi * rndm()
+              wfcatom(ig,ibnd) = CMPLX( rr * COS( arg ), rr * SIN( arg ) ) / &
+                                 ( ( xk(1,ik) + g(1,igk(ig)) )**2 + &
+                                   ( xk(2,ik) + g(2,igk(ig)) )**2 + &
+                                   ( xk(3,ik) + g(3,igk(ig)) )**2 + 1.0D0 )
+           END DO
+        END DO
+        !
+     END IF
      !
-     !   Diagonalize the Hamiltonian on the basis of atomic wfcs
+     CALL init_us_2( npw, igk, xk(1,ik), vkb )
      !
-     if (isolve.eq.1) then
-        call cinitcgg &
-             (npwx, npw, n_starting_wfc, nbnd, wfcatom, evc, et (1, ik))
-     else
-        call rotate_wfc &
-             (npwx, npw, n_starting_wfc, nbnd, wfcatom, okvan, evc, et (1, ik))
-     endif
-
-     do ibnd = 1, nbnd
-        do ig = npw + 1, npwx
-           evc (ig, ibnd) = (0.d0, 0.d0)
-        enddo
-     enddo
-     if (nks.gt.1.or..not.reduce_io) call davcio (evc, nwordwfc, iunwfc, ik, 1)
-
-  enddo
-  deallocate (wfcatom)
-  if (iprint.eq.1) then
+     ! ... Diagonalize the Hamiltonian on the basis of atomic wfcs
+     !
+     IF ( isolve == 1 ) THEN
+        CALL cinitcgg( npwx, npw, n_starting_wfc, nbnd, &
+                       wfcatom, evc, et(1,ik) )
+     ELSE
+        CALL rotate_wfc( npwx, npw, n_starting_wfc, nbnd, wfcatom, &
+                         okvan, evc, et(1,ik) )
+     END IF
+     !
+     DO ibnd = 1, nbnd
+        DO ig = npw + 1, npwx
+           evc(ig,ibnd) = ( 0.D0, 0.D0 )
+        END DO
+     END DO
+     !
+     IF ( nks > 1 .OR. .NOT. reduce_io ) &
+        CALL davcio( evc, nwordwfc, iunwfc, ik, 1 )
+     !
+  END DO
+  !
+  DEALLOCATE( wfcatom )
+  !
+  IF ( iprint == 1 ) THEN
 #ifdef __PARA
-     call poolrecover (et, nbnd, nkstot, nks)
+     CALL poolrecover( et, nbnd, nkstot, nks )
 #endif
-     do ik = 1, nkstot
-        WRITE( stdout, 9010) (xk (ipol, ik), ipol = 1, 3)
-        WRITE( stdout, '(2x,8f9.4)') (et (ibnd, ik) * rytoev, ibnd = 1, nbnd)
-     enddo
-  endif
+     DO ik = 1, nkstot
+        WRITE( stdout, 9010 ) ( xk(ipol,ik), ipol = 1, 3 )
+        WRITE( stdout, '(2X,8F9.4)' ) ( et(ibnd,ik) * rytoev, ibnd = 1, nbnd )
+     END DO
+  END IF
 #ifdef FLUSH
-  call flush (6)
+  CALL flush( 6 )
 #endif
-  call stop_clock ('wfcinit')
-
-  return
-9010 format (/'          k =',3f7.4,'     band energies (ev):'/)
-end subroutine wfcinit
+  CALL stop_clock( 'wfcinit' )
+  !
+  RETURN
+  !
+9010 FORMAT(/'          k =',3F7.4,'     band energies (ev):'/)
+  !
+END SUBROUTINE wfcinit
 
