ae61b66ca278c516600ee286ecb339b7fc56b203
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2003-12-10 13:53:29 +0000

    Againg a bug in c_mkdir (a string terminator was missing). This should be the final fix. C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@448 c92efa57-630b-4861-b058-cf58834340f0

--- clib/c_mkdir.c
+++ clib/c_mkdir.c
@@ -7,13 +7,62 @@
 */
 
 #include <stdio.h>
-#include <time.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <time.h>
 #include "cp.h"
 
-int C_MKDIR( char * dirname )
+static void fatal ( const char * msg )
 {
+
+   fprintf( stderr , "fatal: %s" , *msg ? msg : "Oops!" ) ;
+   exit( -1 ) ;
+
+} /* fatal */
+
+
+static void * xcmalloc ( size_t size )
+{
+
+  register void * ptr = malloc( size ) ;
+
+  if ( ptr == NULL )
+    fatal( "c_mkdir: virtual memory exhausted" ) ;
+  else
+    memset( ptr , 0 , size ) ;
+
+  return ptr ;
+
+} /* xcmalloc */
+
+
+int C_MKDIR( const char * dirname , const int * length )
+{
+
+   int retval = -1 ;
+
    mode_t mode = 0777 ;
-   return mkdir( dirname, mode );
-}
+
+   char * ldir = ( char * ) xcmalloc( (*length) + 1 ) ;
+
+   memcpy( ldir , dirname , *length ) ;
+
+   ldir[*length] = '\0' ;	/* memset() in xcmalloc() already do this */
+
+   retval = mkdir( ldir , mode ) ;
+
+   if ( retval == -1  && errno != EEXIST )
+     fprintf( stderr , "mkdir fail: [%d] %s\n" , errno , strerror( errno ) ) ;
+
+   free( ldir ) ;
+
+   return retval ;
+
+} /* c_mkdir_ */
+
+
+/* EOF */
