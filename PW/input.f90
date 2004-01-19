c5baf9fdae0723f019a7ee6a5a075720edb9f822
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2004-01-19 10:53:55 +0000

    Additional check for fixed occupations.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@490 c92efa57-630b-4861-b058-cf58834340f0

--- PW/input.f90
+++ PW/input.f90
@@ -288,6 +288,8 @@ SUBROUTINE iosys()
   IF( nbnd < 1 ) THEN
      CALL errore( ' iosys ', ' nbnd less than 1 ', nbnd )
   END IF
+  IF (tfixed_occ .and. nbnd.eq.0) &
+     call errore( 'iosys','set the number of bands',1)
   IF( nelec < 0 ) THEN
      CALL errore( ' iosys ', ' nelec less than 0 ', nelec )
   END IF
@@ -988,7 +990,7 @@ SUBROUTINE iosys()
      lstres    = .TRUE.
      IF ( cell_factor_ <= 0.D0 ) cell_factor_ = 1.2D0
      IF ( cmass <= 0.D0 ) &
-        CALL errore( 'readin', &
+        CALL errore( 'iosys', &
                    & 'vcsmd: a positive value for cell mass is required', 1 )
   ELSE
      cell_factor_ = 1.D0
