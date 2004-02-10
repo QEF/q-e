3b740001e124b43bcc843e5aab40a0a51a47a940
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2004-02-10 08:43:32 +0000

    Cleanup. Only one auxiliary vector is used (instead of both hpsi and spsi). C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@579 c92efa57-630b-4861-b058-cf58834340f0

--- PW/rotate_wfc.f90
+++ PW/rotate_wfc.f90
@@ -1,94 +1,106 @@
 !
-! Copyright (C) 2001-2003 PWSCF group
+! Copyright (C) 2001-2004 PWSCF group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-!
-!----------------------------------------------------------------------
-subroutine rotate_wfc (npwx, npw, nstart, nbnd, psi, overlap, evc, e)
-  !----------------------------------------------------------------------
-  !
-  !   Hamiltonian diagonalization in the subspace spanned
-  !   by nstart states psi (atomic or random wavefunctions).
-  !   Produces on output nbnd eigenvectors (nbnd <= nstart) in evc.
-  !   Calls h_psi, s_psi to calculate H|psi> ans S|psi>
-  !
 #include "machine.h"
-  USE kinds
-  implicit none
+!
+!----------------------------------------------------------------------------
+SUBROUTINE rotate_wfc( npwx, npw, nstart, nbnd, psi, overlap, evc, e )
+  !----------------------------------------------------------------------------
   !
-
-  integer :: npw, npwx, nstart, nbnd
-  ! dimension of the matrix to be diagonalized
-  ! leading dimension of matrix psi, as declared in the calling pgm unit
-  ! input number of states
-  ! output number of states
-  logical :: overlap
-  ! if .false. : S|psi> not needed
-
-  complex(kind=DP) :: psi (npwx, nstart), evc (npwx,nbnd)
-  ! input and output eigenvectors (may overlap)
-
-  real(kind=DP) :: e (nbnd)
-  ! eigenvalues
-
-  ! auxiliary variables:
-  complex(kind=DP), allocatable :: hpsi (:,:), spsi (:,:), &
-       hc (:,:), sc (:,:), vc (:,:)
-  real(kind=DP), allocatable :: en (:)
+  ! ... Hamiltonian diagonalization in the subspace spanned
+  ! ... by nstart states psi ( atomic or random wavefunctions ).
+  ! ... produces on output nbnd eigenvectors ( nbnd <= nstart ) in evc.
+  ! ... Calls h_psi, s_psi to calculate H|psi> ans S|psi>
+  ! ... It only uses an auxiliary array of the same size as psi.
+  !
+  USE kinds, ONLY : DP
+  !
+  IMPLICIT NONE
+  !
+  ! ... I/O variables
+  !
+  INTEGER :: npw, npwx, nstart, nbnd
+    ! dimension of the matrix to be diagonalized
+    ! leading dimension of matrix psi, as declared in the calling pgm unit
+    ! input number of states
+    ! output number of states
+  LOGICAL :: overlap
+    ! if .FALSE. : S|psi> not needed
+  COMPLEX(KIND=DP) :: psi(npwx,nstart), evc(npwx,nbnd)
+    ! input and output eigenvectors (may overlap)
+  REAL(KIND=DP) :: e(nbnd)
+    ! eigenvalues
+  !
+  ! ... local variables
+  !
+  COMPLEX(KIND=DP), ALLOCATABLE :: aux(:,:), hc(:,:), sc(:,:), vc(:,:)
+  REAL(KIND=DP),    ALLOCATABLE :: en(:)
   !
   !
-  call start_clock ('wfcrot')
-  allocate (hpsi(  npwx , nstart))    
-  if (overlap) allocate (spsi(  npwx , nstart))    
-  allocate (hc( nstart , nstart))    
-  allocate (sc( nstart , nstart))    
-  allocate (vc( nstart , nstart))    
-  allocate (en( nstart ))
+  CALL start_clock( 'wfcrot' )
   !
-  ! Set up the Hamiltonian and Overlap matrix
+  ALLOCATE( aux(  npwx, nstart ) )    
+  ALLOCATE( hc( nstart, nstart) )    
+  ALLOCATE( sc( nstart, nstart) )    
+  ALLOCATE( vc( nstart, nstart) )    
+  ALLOCATE( en( nstart ) )
   !
-  call h_psi (npwx, npw, nstart, psi, hpsi)
-  if (overlap) call s_psi (npwx, npw, nstart, psi, spsi)
-
-  call ZGEMM ('c', 'n', nstart, nstart, npw, (1.d0, 0.d0) , psi, npwx, &
-       hpsi, npwx, (0.d0, 0.d0) , hc, nstart)
-#ifdef __PARA
-  call reduce (2 * nstart * nstart, hc)
+  ! ... Set up the Hamiltonian and Overlap matrix on the subspace :
+  !
+  ! ...      H_ij = <psi_i| H |psi_j>     S_ij = <psi_i| S |psi_j>
+  !
+  CALL h_psi( npwx, npw, nstart, psi, aux )
+  !
+  call ZGEMM( 'C', 'N', nstart, nstart, npw, ( 1.D0, 0.D0 ), psi, npwx, &
+              aux, npwx, ( 0.D0, 0.D0 ), hc, nstart )
+  !            
+#if defined (__PARA)
+  CALL reduce( 2 * nstart * nstart, hc )
 #endif
-  if (overlap) then
-     call ZGEMM ('c', 'n', nstart, nstart, npw, (1.d0, 0.d0) , psi, npwx, &
-       spsi, npwx, (0.d0, 0.d0) , sc, nstart)
-  else
-     call ZGEMM ('c', 'n', nstart, nstart, npw, (1.d0, 0.d0) , psi, npwx, &
-       psi, npwx, (0.d0, 0.d0) , sc, nstart)
-  end if
-#ifdef __PARA
-  call reduce (2 * nstart * nstart, sc)
+  !
+  IF ( overlap ) THEN
+     !
+     CALL s_psi( npwx, npw, nstart, psi, aux )
+     !
+     CALL ZGEMM( 'C', 'N', nstart, nstart, npw, ( 1.D0, 0.D0 ), psi, npwx, &
+                 aux, npwx, ( 0.D0, 0.D0 ), sc, nstart )
+     !
+  ELSE
+     !
+     CALL ZGEMM( 'C', 'N', nstart, nstart, npw, ( 1.D0, 0.D0 ), psi, npwx, &
+                 psi, npwx, ( 0.D0, 0.D0 ), sc, nstart )
+     !  
+  END IF
+  !
+#if defined (__PARA)
+  CALL reduce( 2 * nstart * nstart, sc )
 #endif
   !
-  ! Diagonalize
+  ! ... Diagonalize
   !
-  call cdiaghg (nstart, nbnd, hc, sc, nstart, en, vc)
+  CALL cdiaghg( nstart, nbnd, hc, sc, nstart, en, vc )
   !
-  e (:) = en(1:nbnd)
+  e(:) = en(1:nbnd)
   !
-  !   update the basis set
+  ! ...  update the basis set
   !  
-  call ZGEMM ('n', 'n', npw, nbnd, nstart, (1.d0, 0.d0) , psi, npwx, &
-       vc, nstart, (0.d0, 0.d0) , hpsi, npwx)
-  evc(:, :) = hpsi(:, 1:nbnd)
-
-  deallocate (en)
-  deallocate (vc)
-  deallocate (sc)
-  deallocate (hc)
-  if (overlap) deallocate (spsi)
-  deallocate (hpsi)
-
-  call stop_clock ('wfcrot')
-  return
-end subroutine rotate_wfc
-
+  CALL ZGEMM( 'N', 'N', npw, nbnd, nstart, ( 1.D0, 0.D0 ), psi, npwx, &
+              vc, nstart, ( 0.D0, 0.D0 ), aux, npwx ) 
+  !     
+  evc(:,:) = aux(:,1:nbnd)
+  !
+  DEALLOCATE( en )
+  DEALLOCATE( vc )
+  DEALLOCATE( sc )
+  DEALLOCATE( hc )
+  DEALLOCATE( aux )
+  !
+  CALL stop_clock( 'wfcrot' )
+  !
+  RETURN
+  !
+END SUBROUTINE rotate_wfc
--- PW/rotate_wfc_gamma.f90
+++ PW/rotate_wfc_gamma.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2003 PWSCF group
+! Copyright (C) 2003-2004 PWSCF group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -17,70 +17,79 @@ SUBROUTINE rotate_wfc_gamma( npwx, npw, nstart, gstart, nbnd, &
   ! ... (nbnd <= nvec). Calls h_psi, s_psi to calculate H|psi> and S|psi>.
   ! ... This version assumes real wavefunctions (k=0) with only
   ! ... half plane waves stored: psi(-G)=psi*(G), except G=0
+  ! ... It only uses an auxiliary array of the same size as psi.
   !
   USE kinds, ONLY : DP
-  USE wvfct,      ONLY : gamma_only 
+  USE wvfct, ONLY : gamma_only 
   !
   IMPLICIT NONE
   !
-  INTEGER          :: npw, npwx, nstart, nbnd, gstart
-  ! dimension of the matrix to be diagonalized
-  ! leading dimension of matrix psi, as declared in the calling pgm unit
-  ! input number of states
-  ! output number of states
-  LOGICAL          :: overlap
-  ! if .FALSE. : S|psi> not needed
+  ! ... I/O variables
+  !
+  INTEGER :: npw, npwx, nstart, nbnd, gstart
+    ! dimension of the matrix to be diagonalized
+    ! leading dimension of matrix psi, as declared in the calling pgm unit
+    ! input number of states
+    ! output number of states
+  LOGICAL :: overlap
+    ! if .FALSE. : S|psi> not needed
   COMPLEX(KIND=DP) :: psi(npwx,nstart), evc(npwx,nbnd)
-  ! input and output eigenvectors (may overlap)
-  REAL(KIND=DP)    :: e(nbnd)
-  ! eigenvalues
+    ! input and output eigenvectors (may overlap)
+  REAL(KIND=DP) :: e(nbnd)
+    ! eigenvalues
   !
   ! ... auxiliary variables:
   !
-  COMPLEX(KIND=DP), ALLOCATABLE :: hpsi(:,:), spsi(:,:)
-  REAL(KIND=8), ALLOCATABLE     :: hr(:,:), sr(:,:), vr(:,:), en(:)
+  COMPLEX(KIND=DP), ALLOCATABLE :: aux(:,:)
+  REAL(KIND=DP),    ALLOCATABLE :: hr(:,:), sr(:,:), vr(:,:), en(:)
   !
   !
-  ALLOCATE( hpsi(  npwx, nstart ) )    
-  IF ( overlap ) &
-       ALLOCATE( spsi(  npwx, nstart ) )    
+  ALLOCATE( aux(  npwx, nstart ) )    
   ALLOCATE( hr( nstart, nstart ) )    
   ALLOCATE( sr( nstart, nstart ) )    
   ALLOCATE( vr( nstart, nstart ) )    
   ALLOCATE( en( nstart ) )
   !
-  ! ... Set up the Hamiltonian and Overlap matrix
+  ! ... Set up the Hamiltonian and Overlap matrix on the subspace :
   !
-  CALL h_psi( npwx, npw, nstart, psi, hpsi )
+  ! ...      H_ij = <psi_i| H |psi_j>     S_ij = <psi_i| S |psi_j>
   !
-  IF ( overlap ) CALL s_psi( npwx, npw, nstart, psi, spsi )
+  CALL h_psi( npwx, npw, nstart, psi, aux )
   !
   CALL DGEMM( 'T', 'N', nstart, nstart, 2 * npw, 2.D0 , psi, &
-              2 * npwx, hpsi, 2 * npwx, 0.D0, hr, nstart )
+              2 * npwx, aux, 2 * npwx, 0.D0, hr, nstart )
   !  
   IF ( gstart == 2 ) &
-     call DGER( nstart, nstart, -1.D0, psi, 2 * npwx, hpsi, &
+     call DGER( nstart, nstart, -1.D0, psi, 2 * npwx, aux, &
                 2 * npwx, hr, nstart )
   !     
-#ifdef __PARA
+#if defined (__PARA)
   CALL reduce( nstart * nstart, hr )
 #endif
   !
   IF ( overlap ) THEN
+     !
+     CALL s_psi( npwx, npw, nstart, psi, aux )
+     !
      CALL DGEMM( 'T', 'N', nstart, nstart, 2 * npw, 2.D0 , psi, &
-                 2 * npwx, spsi, 2 * npwx, 0.D0, sr, nstart )
+                 2 * npwx, aux, 2 * npwx, 0.D0, sr, nstart )
+     !            
      IF ( gstart == 2 ) &
         CALL DGER( nstart, nstart, -1.D0, psi, 2 * npwx, &
-                   spsi, 2 * npwx, sr, nstart )
+                   aux, 2 * npwx, sr, nstart )
+     !              
   ELSE
+     !
      CALL DGEMM( 'T', 'N', nstart, nstart, 2 * npw, 2.D0, psi, &
                  2 * npwx, psi, 2 * npwx, 0.D0, sr, nstart )
+     !
      IF ( gstart == 2 ) &
         CALL DGER( nstart, nstart, -1.D0, psi, 2 * npwx, &
                    psi, 2 * npwx, sr, nstart )
+     !
   END IF
   !
-#ifdef __PARA
+#if defined (__PARA)
   CALL reduce( nstart * nstart, sr )
 #endif
   !
@@ -93,19 +102,16 @@ SUBROUTINE rotate_wfc_gamma( npwx, npw, nstart, gstart, nbnd, &
   ! ... update the basis set
   !
   CALL DGEMM( 'N', 'N', 2 * npw, nbnd, nstart, 1.D0, psi, 2 * npwx, &
-              vr, nstart, 0.D0, hpsi, 2 * npwx )
+              vr, nstart, 0.D0, aux, 2 * npwx )
   !   
-  evc(:,:) = hpsi(:,1:nbnd)
+  evc(:,:) = aux(:,1:nbnd)
   !
   DEALLOCATE( en )
   DEALLOCATE( vr )
   DEALLOCATE( sr )
   DEALLOCATE( hr )
-  IF ( overlap ) &
-     DEALLOCATE( spsi )
-  DEALLOCATE( hpsi )
+  DEALLOCATE( aux )
   !
   RETURN
   !
-END SUBROUTINE rotate_wfc_gamma  
-
+END SUBROUTINE rotate_wfc_gamma
