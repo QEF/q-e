10df2cbdc34070050c51128e5b24a17d1ea9e973
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2004-02-23 12:13:07 +0000

    improving file
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@641 c92efa57-630b-4861-b058-cf58834340f0

--- GUI/PWgui/pwgui.bat
+++ GUI/PWgui/pwgui.bat
@@ -5,6 +5,8 @@ rem *** This is a template Batch file for MS-Windows (edit to suit your needs)
 rem ***
 rem *** EDIT: set the PWGUI variable to point to correct directory !!!
 
-set PWGUI=c:\Tone\GUI\PWgui
+set initdir=%HOMEPATH%
+set PWGUI=C:\Tone\PWgui-0.6.1
 
-wish %PWGUI%\pwgui.tcl
+cd %initdir%
+start wish %PWGUI%\pwgui.tcl
