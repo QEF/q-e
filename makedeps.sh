01c6c91f112876f3fa8f175efdf572a393a2e291
ballabio <ballabio@c92efa57-630b-4861-b058-cf58834340f0>
2004-02-26 14:15:10 +0000

    cleanup, added check for unresolved dependencies [Gerardo]
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@678 c92efa57-630b-4861-b058-cf58834340f0

--- makedeps.sh
+++ makedeps.sh
@@ -1,44 +1,30 @@
 #!/bin/sh
-# compute dependencies
+# compute dependencies for the PWscf directory tree
 
 # run from directory where this script is
 cd `echo $0 | sed 's/\(.*\)\/.*/\1/'` # extract pathname
 TOPDIR=`pwd`
 
-# directory that only depends on itself
-DIR="Modules"
-DEPENDS=""
-if test -d $TOPDIR/$DIR ; then
-  cd $TOPDIR/$DIR
-  $TOPDIR/moduledep.sh $DEPENDS > .dependencies
-fi
+for DIR in Modules PW FPMD CPV flib PP PWCOND PWNC Gamma PH D3
+do
+    # set inter-directory dependencies
+    case $DIR in
+	Modules )                         DEPENDS=""                       ;;
+	PW | FPMD | CPV | flib )          DEPENDS="../Modules"             ;;
+	PP | PWCOND | PWNC | Gamma | PH ) DEPENDS="../Modules ../PW"       ;;
+	D3 )                              DEPENDS="../Modules ../PW ../PH" ;;
+    esac
 
-# directories that depend on Modules/
-DIRS="PW FPMD CPV flib"
-DEPENDS="../Modules"
-for DIR in $DIRS ; do
-  if test -d $TOPDIR/$DIR ; then
-    cd $TOPDIR/$DIR
-    $TOPDIR/moduledep.sh $DEPENDS > .dependencies
-  fi
-done
+    # generate dependencies file
+    if test -d $TOPDIR/$DIR
+    then
+	cd $TOPDIR/$DIR
+	$TOPDIR/moduledep.sh $DEPENDS > .dependencies
+    fi
 
-# directories that depend on Modules/, PW/
-DIRS="PP PWCOND PWNC Gamma PH"
-DEPENDS="../Modules ../PW"
-for DIR in $DIRS ; do
-  if test -d $TOPDIR/$DIR ; then
-    cd $TOPDIR/$DIR
-    $TOPDIR/moduledep.sh $DEPENDS > .dependencies
-  fi
-done
-
-# directories that depend on Modules/, PW/, PH/
-DIRS="D3"
-DEPENDS="../Modules ../PW ../PH"
-for DIR in $DIRS ; do
-  if test -d $TOPDIR/$DIR ; then
-    cd $TOPDIR/$DIR
-    $TOPDIR/moduledep.sh $DEPENDS > .dependencies
-  fi
+    # check for missing dependencies
+    if grep @ .dependencies
+    then
+	echo WARNING: modules not found in directory $DIR
+    fi
 done
