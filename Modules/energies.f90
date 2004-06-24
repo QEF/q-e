20b7d4642e49a79d690cc459e78bab4da78a8c2f
cavazzon <cavazzon@c92efa57-630b-4861-b058-cf58834340f0>
2004-06-24 10:13:56 +0000

    more energy components added for SIC
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@994 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/energies.f90
+++ Modules/energies.f90
@@ -15,25 +15,34 @@
 
         TYPE dft_energy_type
           REAL(dbl)  :: ETOT
-          REAL(dbl)  :: EKIN
+          REAL(dbl)  :: SKIN
           REAL(dbl)  :: EMKIN
+          REAL(dbl)  :: SELF_SXC
+          REAL(dbl)  :: SXC
           REAL(dbl)  :: EHT
+          REAL(dbl)  :: EH
+          REAL(dbl)  :: SELF_EHTE
           REAL(dbl)  :: EHTE
           REAL(dbl)  :: EHTI
           REAL(dbl)  :: EPSEU
           REAL(dbl)  :: ENL
           REAL(dbl)  :: ENT
-          REAL(dbl)  :: EXC
           REAL(dbl)  :: VXC
+          REAL(dbl)  :: EXC
           REAL(dbl)  :: ESELF
           REAL(dbl)  :: ESR
           REAL(dbl)  :: EVDW
           REAL(dbl)  :: EBAND
+          REAL(dbl)  :: EKIN
         END TYPE
 
         REAL(dbl)  :: EHTE = 0.0_dbl
+        REAL(dbl)  :: SELF_EHTE = 0.0_dbl
         REAL(dbl)  :: EHTI = 0.0_dbl
+        REAL(dbl)  :: EH = 0.0_dbl
         REAL(dbl)  :: EHT = 0.0_dbl
+        REAL(dbl)  :: SXC = 0.0_dbl
+        REAL(dbl)  :: SELF_SXC = 0.0_dbl
         REAL(dbl)  :: EKIN = 0.0_dbl
         REAL(dbl)  :: ESELF = 0.0_dbl
         REAL(dbl)  :: EVDW = 0.0_dbl
@@ -44,25 +53,24 @@
         REAL(dbl)  :: ESR = 0.0_dbl
         REAL(dbl)  :: EXC = 0.0_dbl
         REAL(dbl)  :: VXC = 0.0_dbl
+        REAL(dbl)  :: SELF_VXC = 0.0_dbl
         REAL(dbl)  :: EBAND = 0.0_dbl
-        REAL(dbl)  :: self_ehte = 0.0d0
-        REAL(dbl)  :: self_sxc = 0.0d0
-        REAL(dbl)  :: self_vxc = 0.0d0
 
 
       CONTAINS
 
 ! ---------------------------------------------------------------------------- !
 
-        SUBROUTINE total_energy(edft, omega, eexc, vvxc, eh, eps, &
-          self_ehte_in, self_sxc_in, self_vxc_in, nnr)
+!        SUBROUTINE total_energy( edft, omega, eexc, vvxc, eh, eps, &
+!          self_ehte_in, self_sxc_in, self_vxc_in, nnr)
+
+        SUBROUTINE total_energy( edft, omega, vvxc, eps, self_vxc_in, nnr)
 
           TYPE (dft_energy_type) :: edft
-          REAL(dbl), INTENT(IN) :: OMEGA, EEXC, VVXC
+          REAL(dbl), INTENT(IN) :: OMEGA, VVXC
           REAL(dbl) :: VXC
-          REAL(dbl) :: self_sxc_in, self_vxc_in
-          COMPLEX(dbl) :: self_ehte_in
-          COMPLEX(dbl), INTENT(IN) :: EH, EPS
+          REAL(dbl) :: self_vxc_in
+          COMPLEX(dbl), INTENT(IN) :: EPS
           INTEGER, INTENT(IN) :: nnr 
 
           eself = edft%eself
@@ -71,20 +79,23 @@
           evdw  = edft%evdw
           esr   = edft%esr
           ekin  = edft%ekin
+          sxc   = edft%sxc
+          ehti  = edft%ehte
+          ehte  = edft%ehte
+          self_ehte  = edft%ehte
+          self_sxc   = edft%self_sxc
 
-          self_ehte = REAL( self_ehte_in )
-          self_sxc = self_sxc_in
           self_vxc = self_vxc_in
 
-          EXC   = EEXC * omega / REAL(NNR)
+          EXC   = edft%sxc * omega / REAL(NNR) !EEXC * omega / REAL(NNR)
           VXC   = VVXC * omega / REAL(NNR)
+
           edft%exc  = exc
           edft%vxc  = vxc
 
-          EHT   = REAL(eh) + esr - eself
-          edft%eht  = eht
-          ehte = edft%ehte
-          ehti = edft%ehti
+          !EHT   = REAL( eh ) + esr - eself
+          edft%eht =  edft%eh + esr - eself ! = eht
+          EHT = edft%eht
 
           EPSEU = REAL(eps)
           edft%epseu = epseu
@@ -119,45 +130,74 @@
 
 ! ---------------------------------------------------------------------------- !
 
-        SUBROUTINE print_energies( edft )
+        SUBROUTINE print_energies( tsic, edft )
+          LOGICAL, INTENT(IN) :: tsic
           TYPE (dft_energy_type), OPTIONAL, INTENT(IN) :: edft
+          REAL ( dbl ) :: EHT
+
           IF( PRESENT ( edft ) ) THEN
-            WRITE( stdout,1) edft%ETOT, edft%EKIN, edft%EHT, edft%ESELF, edft%ESR, &
-              edft%EPSEU, edft%ENL, edft%EXC, edft%EVDW, edft%emkin, &
-              self_ehte, self_sxc, self_vxc, edft%ehti
+            IF( tsic ) THEN
+              WRITE( stdout,1) edft%ETOT, edft%EKIN, edft%EHT,&
+                 edft%ESELF, edft%ESR, edft%EH, &
+                 edft%ehte,  edft%self_ehte, edft%ehti, &
+                 edft%EPSEU, edft%ENL, edft%SXC,  edft%self_SXC,&
+                 edft%EVDW, edft%emkin, vxc, self_vxc
+            ELSE
+              WRITE( stdout,2) edft%ETOT, edft%EKIN, edft%EHT,&
+                 edft%ESELF, edft%ESR, edft%EH, &
+                 edft%ehte,  edft%ehti, edft%EPSEU, edft%ENL, edft%SXC, &
+                 edft%EVDW, edft%emkin
+            END IF
           ELSE
             WRITE( stdout,1) ETOT, EKIN, EHT, ESELF, ESR, EPSEU, ENL, EXC, EVDW
           END IF
-1         FORMAT(/,/,6X,'                TOTAL ENERGY = ',F18.10,' A.U.'/ &
+2         FORMAT(/,/,6X,'                TOTAL ENERGY = ',F18.10,' A.U.'/ &
                     ,6X,'              KINETIC ENERGY = ',F18.10,' A.U.'/ &
                     ,6X,'        ELECTROSTATIC ENERGY = ',F18.10,' A.U.'/ &
-                    ,6X,'        ESELF                = ',F18.10,' A.U.'/ &
-                    ,6X,'        ESR                  = ',F18.10,' A.U.'/ &
+                    ,6X,'                       ESELF = ',F18.10,' A.U.'/ &
+                    ,6X,'                         ESR = ',F18.10,' A.U.'/ &
+                    ,6X,'              HARTREE ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'                HARTREE EHTE = ',F18.10,' A.U.'/ &
+                    ,6X,'                HARTREE EHTI = ',F18.10,' A.U.'/ &
                     ,6X,'      PSEUDOPOTENTIAL ENERGY = ',F18.10,' A.U.'/ &
                     ,6X,'  N-L PSEUDOPOTENTIAL ENERGY = ',F18.10,' A.U.'/ &
                     ,6X,' EXCHANGE-CORRELATION ENERGY = ',F18.10,' A.U.'/ &
                     ,6X,'        VAN DER WAALS ENERGY = ',F18.10,' A.U.'/ &
-                    ,6X,'        EMASS KINETIC ENERGY = ',F18.10,' A.U.'/ &
-                    ,6X,' SIC HARTREE ELECTRON ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'        EMASS KINETIC ENERGY = ',F18.10,' A.U.'/,/)
+1         FORMAT(/,/,6X,'                TOTAL ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'              KINETIC ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'        ELECTROSTATIC ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'                       ESELF = ',F18.10,' A.U.'/ &
+                    ,6X,'                         ESR = ',F18.10,' A.U.'/ &
+                    ,6X,'              HARTREE ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'                HARTREE EHTE = ',F18.10,' A.U.'/ &
+                    ,6X,'            HARTREE SIC_EHTE = ',F18.10,' A.U.'/ &
+                    ,6X,'                HARTREE EHTI = ',F18.10,' A.U.'/ &
+                    ,6X,'      PSEUDOPOTENTIAL ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'  N-L PSEUDOPOTENTIAL ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,' EXCHANGE-CORRELATION ENERGY = ',F18.10,' A.U.'/ &
                     ,6X,' SIC EXCHANGE-CORRELA ENERGY = ',F18.10,' A.U.'/ &
-                    ,6X,' SIC EXCHANGE-CORRELA POTENT = ',F18.10,' A.U.'/ &
-                    ,6X,' IONIC ELECTROSTATIC  ENERGY = ',F18.10,' A.U.'/,/)
-
+                    ,6X,'        VAN DER WAALS ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'        EMASS KINETIC ENERGY = ',F18.10,' A.U.'/ &
+                    ,6X,'     EXCHANGE-CORRELA POTENT = ',F18.10,' A.U.'/ &
+                    ,6X,' SIC EXCHANGE-CORRELA POTENT = ',F18.10,' A.U.'/,/)
           RETURN
         END SUBROUTINE print_energies
+
 
 ! ---------------------------------------------------------------------------- !
 
         SUBROUTINE debug_energies( edft )
           TYPE (dft_energy_type), OPTIONAL, INTENT(IN) :: edft
           IF( PRESENT ( edft ) ) THEN
-            WRITE( stdout,2) edft%ETOT, edft%EKIN, edft%EHT, edft%ESELF, edft%ESR, &
-              edft%EPSEU, edft%ENL, edft%EXC, edft%VXC, edft%EVDW, edft%EHTE, &
-              edft%EHTI, edft%ENT, edft%EBAND, (edft%EXC-edft%VXC), &
+            WRITE( stdout,2) edft%ETOT, edft%EKIN, edft%EHT, &
+              edft%ESELF, edft%ESR, edft%EH, &
+              edft%EPSEU, edft%ENL, edft%SXC, edft%VXC, edft%EVDW, edft%EHTE, &
+              edft%EHTI, edft%ENT, edft%EBAND, (edft%SXC-edft%VXC), &
               (edft%EHTI+edft%ESR-edft%ESELF), &
-              edft%EBAND-edft%EHTE+(edft%EXC-edft%VXC)+(edft%EHTI+edft%ESR-edft%ESELF)
+              edft%EBAND-edft%EHTE+(edft%SXC-edft%VXC)+(edft%EHTI+edft%ESR-edft%ESELF)
           ELSE
-            WRITE( stdout,2) ETOT, EKIN, EHT, ESELF, ESR, EPSEU, ENL, EXC, VXC, &
+            WRITE( stdout,2) ETOT, EKIN, EHT, ESELF, ESR, EH, EPSEU, ENL, EXC, VXC, &
               EVDW, EHTE, EHTI, ENT, EBAND, (EXC-VXC), (EHTI+ESR-ESELF), &
               EBAND-EHTE+(EXC-VXC)+(EHTI+ESR-ESELF)
           END IF
@@ -167,6 +207,7 @@
             ,6X,' EHT ..... = ',F18.10,/ &
             ,6X,' ESELF ... = ',F18.10,/ &
             ,6X,' ESR ..... = ',F18.10,/ &
+            ,6X,' EH ...... = ',F18.10,/ &
             ,6X,' EPSEU ... = ',F18.10,/ &
             ,6X,' ENL ..... = ',F18.10,/ &
             ,6X,' EXC ..... = ',F18.10,/ &
