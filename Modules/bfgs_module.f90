5c4ced7c2c8aa9bc72004bcec63f858d4ec3fad5
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2004-08-18 10:36:47 +0000

    Interpolation of the "accepted" bfgs step disabled (not yet safe). C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1115 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/bfgs_module.f90
+++ Modules/bfgs_module.f90
@@ -5,7 +5,7 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-#define NEW_INTERPOLATION
+!#define NEW_INTERPOLATION
 !
 !----------------------------------------------------------------------------
 MODULE bfgs_module
