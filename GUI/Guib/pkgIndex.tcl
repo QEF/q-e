05d3d930cb1cdc8984e43f69e62dcba3b2739ec0
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2004-09-13 08:59:46 +0000

    *** empty log message ***
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1262 c92efa57-630b-4861-b058-cf58834340f0

--- GUI/Guib/pkgIndex.tcl
+++ GUI/Guib/pkgIndex.tcl
@@ -1,4 +1,6 @@
 # Tcl package index file, version 1.0
 
-set version [gets [open [file join $dir VERSION] r]]
+set channel [open [file join $dir VERSION] r]
+set version [gets $channel]
+close $channel
 package ifneeded Guib $version [list source [file join $dir init.tcl]]
