9669d9890092808e0b4d84c9b86154f5d748acad
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2004-10-14 11:06:32 +0000

    newd.f90 in the PWNC moved to PW. The merging was already done.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1380 c92efa57-630b-4861-b058-cf58834340f0

--- PW/newd.f90
+++ PW/newd.f90
@@ -19,19 +19,21 @@ subroutine newd
   USE ions_base,            ONLY : nat, ntyp => nsp, ityp
   USE cell_base,            ONLY : omega
   USE gvect,                ONLY : nr1, nr2, nr3, nrx1, nrx2, nrx3, g, gg, &
-                                   ngm, gstart, ig1, ig2, ig3, eigts1, eigts2, &
-                                   eigts3, nl
+                                   ngm, gstart, ig1, ig2, ig3, &
+                                   eigts1, eigts2, eigts3, nl
   USE lsda_mod,             ONLY : nspin
+  USE spin_orb,             ONLY : lspinorb
+  USE noncollin_module,     ONLY : noncolin
   USE scf,                  ONLY : vr, vltot
   USE us,                   ONLY : okvan
-  USE uspp,                 ONLY : deeq, dvan
+  USE uspp,                 ONLY : deeq, dvan, deeq_nc, dvan_so
   USE uspp_param,           ONLY : lmaxq, nh, nhm, tvanp
   USE wvfct,                ONLY : gamma_only
   USE wavefunctions_module, ONLY : psic
   !
   implicit none
   !
-  integer :: ig, nt, ih, jh, na, is
+  integer :: ig, nt, ih, jh, na, is, nht
   ! counters on g vectors, atom type, beta functions x 2, atoms, spin
   complex(kind=DP), allocatable :: aux (:,:), qgm(:), qgm_na (:)
   ! work space
@@ -43,15 +45,21 @@ subroutine newd
   if (.not.okvan) then
      ! no ultrasoft potentials: use bare coefficients for projectors
      do na = 1, nat
-        nt = ityp (na)
-        do is = 1, nspin
-           do ih = 1, nh (nt)
-              do jh = ih, nh (nt)
-                 deeq (ih, jh, na, is) = dvan (ih, jh, nt)
-                 deeq (jh, ih, na, is) = deeq (ih, jh, na, is)
-              enddo
-           enddo
-        enddo
+        nt=ityp(na)
+        nht = nh (nt)
+        if (lspinorb) then
+           deeq_nc (1:nht,1:nht, na, 1:nspin) = &
+                dvan_so (1:nht, 1:nht, 1:nspin, nt)
+        else if (noncolin) then
+           deeq_nc (1:nht,1:nht, na, 1) = dvan (1:nht, 1:nht, nt)
+           deeq_nc (1:nht,1:nht, na, 2) = (0.d0, 0.d0)
+           deeq_nc (1:nht,1:nht, na, 3) = (0.d0, 0.d0)
+           deeq_nc (1:nht,1:nht, na, 4) = dvan (1:nht, 1:nht, nt)
+        else
+           do is=1, nspin
+              deeq (1:nht,1:nht, na, is) = dvan (1:nht, 1:nht, nt)
+           end do
+        end if
      end do
      return
   end if
@@ -74,19 +82,17 @@ subroutine newd
   !
   ! fourier transform of the total effective potential
   !
-#ifdef DEBUG_NEWD
-   call start_clock ('newd:fftvg')
-#endif
   do is = 1, nspin
-     psic (:) = vltot (:) + vr (:, is)
+     if (nspin == 4 .and. is /= 1) then 
+        psic (:) = vr (:, is)
+     else
+        psic (:) = vltot (:) + vr (:, is)
+     endif
      call cft3 (psic, nr1, nr2, nr3, nrx1, nrx2, nrx3, - 1)
      do ig = 1, ngm
         aux (ig, is) = psic (nl (ig) )
      enddo
   enddo
-#ifdef DEBUG_NEWD
-   call stop_clock ('newd:fftvg')
-#endif
   !
   ! here we compute the integral Q*V for each atom,
   !       I = sum_G exp(-iR.G) Q_nm v^*
@@ -95,21 +101,13 @@ subroutine newd
      if (tvanp (nt) ) then
         do ih = 1, nh (nt)
            do jh = ih, nh (nt)
-#ifdef DEBUG_NEWD
-   call start_clock ('newd:qvan2')
-#endif
               !
               ! The Q(r) for this atomic species without structure factor
               !
               call qvan2 (ngm, ih, jh, nt, qmod, qgm, ylmk0)
-#ifdef DEBUG_NEWD
-   call stop_clock ('newd:qvan2')
-#endif
+              !
               do na = 1, nat
-                 if (ityp (na) .eq.nt) then
-#ifdef DEBUG_NEWD
-   call start_clock ('newd:int1')
-#endif
+                 if (ityp (na) == nt) then
                     !
                     ! The Q(r) for this specific atom
                     !
@@ -117,13 +115,7 @@ subroutine newd
                        qgm_na (ig) = qgm(ig) * eigts1 (ig1(ig), na) &
                                              * eigts2 (ig2(ig), na) &
                                              * eigts3 (ig3(ig), na) 
-                     enddo
-#ifdef DEBUG_NEWD
-   call stop_clock ('newd:int1')
-#endif
-#ifdef DEBUG_NEWD
-   call start_clock ('newd:int2')
-#endif
+                    enddo
                     !
                     !  and the product with the Q functions
                     !
@@ -133,42 +125,111 @@ subroutine newd
                        if (gamma_only .and. gstart==2) &
                            deeq (ih, jh, na, is) = deeq (ih, jh, na, is) - &
                                              omega*real(aux(1,is)*qgm_na(1))
+                       deeq (jh,ih,na,is) = deeq (ih,jh,na,is)
                     enddo
-#ifdef DEBUG_NEWD
-   call stop_clock ('newd:int2')
-#endif
                  endif
               enddo
            enddo
         enddo
      endif
   enddo
+
+  deallocate (aux, qgm_na, qgm, qmod, ylmk0)
+
 #ifdef __PARA
   call reduce (nhm * nhm * nat * nspin, deeq)
 #endif
-  do na = 1, nat
-     nt = ityp (na)
-     do is = 1, nspin
+
+  if (lspinorb) then
+     call newd_so ( )
+  else if (noncolin) then 
+     call newd_nc ( )
+  else
+      do na = 1, nat
+        nt = ityp (na)
+        do is = 1, nspin
         !           WRITE( stdout,'( "dmatrix atom ",i4, " spin",i4)') na,is
         !           do ih = 1, nh(nt)
         !              WRITE( stdout,'(8f9.4)') (deeq(ih,jh,na,is),jh=1,nh(nt))
         !           end do
-        do ih = 1, nh (nt)
-           do jh = ih, nh (nt)
-              deeq (ih, jh, na, is) = deeq (ih, jh, na, is) + dvan (ih,jh,nt)
-              deeq (jh, ih, na, is) = deeq (ih, jh, na, is)
+           do ih = 1, nh (nt)
+              do jh = 1, nh (nt)
+                   deeq (ih, jh, na, is) = deeq (ih, jh, na, is) &
+                &                      + dvan (ih, jh, nt)
+              enddo
            enddo
         enddo
-     enddo
-     !        WRITE( stdout,'( "dion pseudo ",i4)') nt
-     !        do ih = 1, nh(nt)
-     !           WRITE( stdout,'(8f9.4)') (dvan(ih,jh,nt),jh=1,nh(nt))
-     !        end do
+     end do
+  end if
 
-  enddo
-  deallocate ( aux, qgm_na, qgm, qmod, ylmk0 )
   call stop_clock ('newd')
 
   return
-end subroutine newd
 
+CONTAINS
+  !
+  SUBROUTINE newd_so ( )
+    !
+    USE spin_orb, ONLY : fcoef
+    implicit none
+    integer :: ijs, is1, is2, kh, lh
+    
+    do na = 1, nat
+       nt = ityp (na)
+       ijs=0
+       do is1=1,2
+          do is2=1,2
+             ijs=ijs+1
+             do ih = 1, nh(nt)
+                do jh = 1, nh(nt)
+                   deeq_nc(ih,jh,na,ijs) = dvan_so(ih,jh,ijs,nt)
+                   do kh= 1, nh(nt)
+                      do lh= 1, nh(nt)
+                         deeq_nc(ih,jh,na,ijs) = deeq_nc(ih,jh,na,ijs) +   &
+                                             deeq (kh,lh,na,1)*            &
+                           (fcoef(ih,kh,is1,1,nt)*fcoef(lh,jh,1,is2,nt)  + &
+                            fcoef(ih,kh,is1,2,nt)*fcoef(lh,jh,2,is2,nt)) + &
+                                             deeq (kh,lh,na,2)*            &
+                           (fcoef(ih,kh,is1,1,nt)*fcoef(lh,jh,2,is2,nt)  + &
+                            fcoef(ih,kh,is1,2,nt)*fcoef(lh,jh,1,is2,nt)) + &
+                                (0.d0,-1.d0)*deeq (kh,lh,na,3)*            &
+                           (fcoef(ih,kh,is1,1,nt)*fcoef(lh,jh,2,is2,nt)  - &
+                            fcoef(ih,kh,is1,2,nt)*fcoef(lh,jh,1,is2,nt)) + &
+                                             deeq (kh,lh,na,4)*            &
+                           (fcoef(ih,kh,is1,1,nt)*fcoef(lh,jh,1,is2,nt)  - &
+                            fcoef(ih,kh,is1,2,nt)*fcoef(lh,jh,2,is2,nt))   
+                      enddo
+                   enddo
+                enddo
+             enddo
+          enddo
+       enddo
+    enddo
+
+    return
+  end subroutine newd_so
+  !
+  SUBROUTINE newd_nc ( )
+    !
+    implicit none
+
+    do na = 1, nat
+       nt = ityp (na)
+       do is = 1, nspin
+          do ih = 1, nh (nt)
+             do jh = 1, nh (nt)
+                deeq_nc(ih, jh, na, 1) = dvan(ih,jh,nt) + &
+                     deeq (ih,jh,na,1) + deeq (ih,jh,na,4)
+                deeq_nc(ih, jh, na, 2) = &
+                     deeq (ih,jh,na,2) - (0.d0,1.d0)*deeq (ih,jh,na,3)
+                deeq_nc(ih, jh, na, 3) = &
+                     deeq (ih,jh,na,2) + (0.d0,1.d0)*deeq (ih,jh,na,3)
+                deeq_nc(ih, jh, na, 4) = dvan(ih,jh,nt) + &
+                     deeq (ih,jh,na,1) - deeq (ih,jh,na,4)
+             enddo
+          enddo
+       enddo
+    enddo
+  end subroutine newd_nc
+
+end subroutine newd
