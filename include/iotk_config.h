8e09ef80ca2cc9a565ce4f047921c352661f80e2
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2004-11-24 13:45:54 +0000

    Added definitions for lahey compiler. C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1473 c92efa57-630b-4861-b058-cf58834340f0

--- include/iotk_config.h
+++ include/iotk_config.h
@@ -5,44 +5,61 @@
 ! Here you can choose one of the preset configurations
 ! Commenting all these lines, you can drive the configuration using -D options.
 
-#ifdef __AIX
+#if defined (__AIX)
 
 @PROCESS OPTIMIZE(0)
 
-#define __IOTK_BINARY_FORMAT "IBM-SP/XLF"
-#define __IOTK_LOGICAL1 1
-#define __IOTK_LOGICAL2 2
-#define __IOTK_LOGICAL3 4
-#define __IOTK_LOGICAL4 8
-#define __IOTK_INTEGER1 4
-#define __IOTK_INTEGER2 2
-#define __IOTK_INTEGER3 1
-#define __IOTK_INTEGER4 8
-#define __IOTK_REAL1    8
-#define __IOTK_REAL2    4
-#undef  __IOTK_REAL3 
-#undef  __IOTK_REAL4
+#  define __IOTK_BINARY_FORMAT "IBM-SP/XLF"
+#  define __IOTK_LOGICAL1 1
+#  define __IOTK_LOGICAL2 2
+#  define __IOTK_LOGICAL3 4
+#  define __IOTK_LOGICAL4 8
+#  define __IOTK_INTEGER1 4
+#  define __IOTK_INTEGER2 2
+#  define __IOTK_INTEGER3 1
+#  define __IOTK_INTEGER4 8
+#  define __IOTK_REAL1    8
+#  define __IOTK_REAL2    4
+#  undef  __IOTK_REAL3 
+#  undef  __IOTK_REAL4
 
-#define __IOTK_MAXRANK 5
+#  define __IOTK_MAXRANK 5
 
 #else
 
-#define __IOTK_UNKNOWN_PROCESS
-#define __IOTK_BINARY_FORMAT "UNKNOWN"
-#define __IOTK_LOGICAL1 1
-#define __IOTK_LOGICAL2 2
-#define __IOTK_LOGICAL3 4
-#define __IOTK_LOGICAL4 8
-#define __IOTK_INTEGER1 4
-#define __IOTK_INTEGER2 2
-#define __IOTK_INTEGER3 1
-#define __IOTK_INTEGER4 8
-#define __IOTK_REAL1    8
-#define __IOTK_REAL2    4
-#undef  __IOTK_REAL3 
-#undef  __IOTK_REAL4
+#  if defined (__LAHEY)
 
-#define __IOTK_MAXRANK 5
+#    define __IOTK_UNKNOWN_PROCESS
+#    define __IOTK_BINARY_FORMAT "UNKNOWN"
+#    define __IOTK_LOGICAL1 1
+#    define __IOTK_LOGICAL2 2
+#    define __IOTK_LOGICAL3 4
+#    define __IOTK_LOGICAL4 8
+#    define __IOTK_INTEGER1 4
+#    define __IOTK_REAL1    8
+
+#    define __IOTK_MAXRANK 5
+
+#  else
+
+#    define __IOTK_UNKNOWN_PROCESS
+#    define __IOTK_BINARY_FORMAT "UNKNOWN"
+#    define __IOTK_LOGICAL1 1
+#    define __IOTK_LOGICAL2 2
+#    define __IOTK_LOGICAL3 4
+#    define __IOTK_LOGICAL4 8
+#    define __IOTK_INTEGER1 4
+#    define __IOTK_INTEGER2 2
+#    define __IOTK_INTEGER3 1
+#    define __IOTK_INTEGER4 8
+#    define __IOTK_REAL1    8
+#    define __IOTK_REAL2    4
+#    undef  __IOTK_REAL3 
+#    undef  __IOTK_REAL4
+
+#    define __IOTK_MAXRANK 5
+
+#  endif
 
 #endif
 
