cf2199ac8b8cbcbb76a7d4d0b785052fef019ff3
cavazzon <cavazzon@c92efa57-630b-4861-b058-cf58834340f0>
2004-12-31 14:19:26 +0000

    - addition of a new file (forgotten in previous commit)
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1534 c92efa57-630b-4861-b058-cf58834340f0

---
+++ CPV/mainvar.f90
@@ -0,0 +1,121 @@
+!
+! Copyright (C) 2002-2004 CP90 group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+
+#include "f_defs.h"
+
+MODULE cp_main_variables
+
+  USE kinds, ONLY: dbl
+  USE parameters, ONLY: natx, nsx, nacx
+  
+  IMPLICIT NONE
+  SAVE
+
+!
+! structure factors e^{-ig*R}
+!
+      complex(dbl), allocatable:: ei1(:,:,:),  ei2(:,:,:),  ei3(:,:,:)
+      complex(dbl), allocatable:: eigr(:,:,:)
+!
+! structure factors (summed over atoms of the same kind)
+!
+      complex(dbl), allocatable:: sfac(:,:)
+!
+! indexes, positions, and structure factors for the box grid
+!
+      integer       :: irb(3,natx,nsx)
+      real(dbl)  :: taub(3,natx)
+      complex(dbl), allocatable:: eigrb(:,:,:)
+! 
+! charge densities and potentials
+!     rhog  = charge density in g space
+!     rhor  = charge density in r space (dense grid)
+!     rhos  = charge density in r space (smooth grid)
+!     rhoc  = core charge density in real space (dense grid)
+!
+      complex(dbl), allocatable:: rhog(:,:)
+      real(dbl),    allocatable:: rhor(:,:), rhos(:,:), rhoc(:)
+!
+! nonlocal projectors:
+!     bec   = scalar product of projectors and wave functions
+!     betae = nonlocal projectors in g space = beta x e^(-ig.R) 
+!     becdr = <betae|g|psi> used in force calculation
+!     rhovan= \sum_i f(i) <psi(i)|beta_l><beta_m|psi(i)>
+!     deeq  = \int V_eff(r) q_lm(r) dr
+!
+      real(dbl), allocatable:: bec(:,:), becdr(:,:,:)
+      real(dbl), allocatable:: bephi(:,:), becp(:,:)
+!
+!  mass preconditioning
+!
+      real(dbl), allocatable:: ema0bg(:)
+!
+!  constraints (lambda at t, lambdam at t-dt, lambdap at t+dt)
+!
+      real(dbl), allocatable:: lambda(:,:), lambdam(:,:), lambdap(:,:)
+!
+      real(dbl) acc(nacx)
+
+
+CONTAINS
+
+
+   SUBROUTINE allocate_mainvar &
+      ( ngw, ngb, ngs, ng, nr1, nr2, nr3, nnr, nnrsx, nax, nsp, nspin, n, nx, nhsa, nlcc_any )
+      INTEGER, INTENT(IN) :: ngw, ngb, ngs, ng, nr1, nr2, nr3, nnr, nnrsx, nax, nsp, nspin, n, nx, nhsa
+      LOGICAL, INTENT(IN) :: nlcc_any
+      !
+      !     allocation of all arrays not already allocated in init and nlinit
+      !
+      allocate(eigr(ngw,nax,nsp))
+      allocate(eigrb(ngb,nax,nsp))
+      allocate(sfac(ngs,nsp))
+      allocate(rhor(nnr,nspin))
+      allocate(rhos(nnrsx,nspin))
+      allocate(rhog(ng,nspin))
+      if ( nlcc_any ) allocate(rhoc(nnr))
+      allocate(ema0bg(ngw))
+      allocate(lambda(nx,nx))
+      allocate(lambdam(nx,nx))
+      allocate(lambdap(nx,nx))
+      allocate(ei1(-nr1:nr1,nax,nsp))
+      allocate(ei2(-nr2:nr2,nax,nsp))
+      allocate(ei3(-nr3:nr3,nax,nsp))
+      allocate(becdr(nhsa,n,3))
+      allocate(bec  (nhsa,n))
+      allocate(bephi(nhsa,n))
+      allocate(becp (nhsa,n))
+      RETURN
+   END SUBROUTINE
+
+
+   SUBROUTINE deallocate_mainvar( )
+
+      IF( ALLOCATED( ei1 ) ) DEALLOCATE( ei1 )
+      IF( ALLOCATED( ei2 ) ) DEALLOCATE( ei2 )
+      IF( ALLOCATED( ei3 ) ) DEALLOCATE( ei3 )
+      IF( ALLOCATED( eigr ) ) DEALLOCATE( eigr )
+      IF( ALLOCATED( sfac ) ) DEALLOCATE( sfac )
+      IF( ALLOCATED( eigrb ) ) DEALLOCATE( eigrb )
+      IF( ALLOCATED( rhor ) ) DEALLOCATE( rhor )
+      IF( ALLOCATED( rhos ) ) DEALLOCATE( rhos )
+      IF( ALLOCATED( rhog ) ) DEALLOCATE( rhog )
+      IF( ALLOCATED( rhoc ) ) DEALLOCATE( rhoc )
+      IF( ALLOCATED( bec ) ) DEALLOCATE( bec )
+      IF( ALLOCATED( becdr ) ) DEALLOCATE( becdr )
+      IF( ALLOCATED( bephi ) ) DEALLOCATE( bephi )
+      IF( ALLOCATED( becp ) ) DEALLOCATE( becp )
+      IF( ALLOCATED( ema0bg ) ) DEALLOCATE( ema0bg )
+      IF( ALLOCATED( lambda ) ) DEALLOCATE( lambda )
+      IF( ALLOCATED( lambdam ) ) DEALLOCATE( lambdam )
+      IF( ALLOCATED( lambdap ) ) DEALLOCATE( lambdap )
+
+      RETURN
+   END SUBROUTINE
+
+END MODULE
