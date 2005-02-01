4f80a6acd79e5d71d59e7d156b150e2a9c940557
ballabio <ballabio@c92efa57-630b-4861-b058-cf58834340f0>
2005-02-01 11:37:35 +0000

    another X -> 1X [Gerardo]
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1605 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/path_formats.f90
+++ Modules/path_formats.f90
@@ -44,7 +44,7 @@ MODULE path_formats
     run_output = "(5X,I5,4X,F15.7,10X,F10.6,12X,L)"
   !    
   CHARACTER (LEN=*), PARAMETER :: &
-    summary_fmt = "(5X,A,T35,' = ',X,A)"
+    summary_fmt = "(5X,A,T35,' = ',1X,A)"
   !
   CHARACTER (LEN=*), PARAMETER :: &
     final_fmt = "(/,5X,75('-'),/)"
