ee366d19b7a903aa58ac931538cd1452667e3502
ballabio <ballabio@c92efa57-630b-4861-b058-cf58834340f0>
2005-03-08 16:57:25 +0000

    cleanup [Gerardo]
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1689 c92efa57-630b-4861-b058-cf58834340f0

--- install/Rules.cpp
+++ install/Rules.cpp
@@ -3,11 +3,10 @@
 
 .f90.o:
 	$(CPP) $(CPPFLAGS) $*.f90 $*.F90
-	$(F90) $(F90FLAGS) $(MODULEFLAG) -c $*.F90 -o $*.o
-#	rm $*.F90
+	$(F90) $(F90FLAGS) -c $*.F90 -o $*.o
 
 .f.o:
 	$(F77) $(F77FLAGS) -c $<
 
 .c.o:
-	$(CC) $(CCFLAGS) -c $<
+	$(CC) $(CFLAGS) -c $<
