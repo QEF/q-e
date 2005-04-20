9dc94c8315bc354f5f5d2a138809ca835810166f
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2005-04-20 17:31:14 +0000

    oops 2...
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1821 c92efa57-630b-4861-b058-cf58834340f0

--- release.sh
+++ release.sh
@@ -1,85 +1,68 @@
 #!/bin/sh -x
 
-VERSION=2.1.3
-GUI_VERSION=2.1.3
+# BEWARE: 
+# in order to build the GUI tarball from CVS sources the following
+# software is needed:
+#   1. pdflatex
+#   2. convert (from Image-Magick)
+#   3. latex2html
+
+dir=pw-2-1
+cd $HOME
+if test -d $dir.save; then /bin/rm -r $dir.save; fi
+if test -d $dir; then mv $dir $dir.save; fi
+mkdir $dir
+
+if test -d espresso ; then
+    cd espresso
+else
+    echo "
+   Oops. espresso/ does not exist. Aborting ...
+"
+    exit 1
+fi
+
+GUI_VERSION=`cat GUI/PWgui/VERSION`
 GUI=PWgui-$GUI_VERSION
+make veryclean
+find . -type f -name *~ -exec /bin/rm {} \;
+find . -type f -name .#* -exec /bin/rm {} \;
+if test -f pw.tar.gz ; then /bin/rm pw.tar.gz ; fi
+make tar tar-gui
+cd  ../$dir
 
-TMPDIR=./espresso-$VERSION
+mkdir bin
+tar -xzf ../O-sesame/pw.tar.gz
+tar -xzf ../O-sesame/$GUI.tgz
+find $GUI -name CVS -exec /bin/rm -r {} \;
 
-# check that $TMPDIR does not exist
-
-if test -d $TMPDIR ; then (echo "$TMPDIR exists, stopping"; exit) ; fi
-mkdir $TMPDIR
-
-# make a copy of everything in $TMPDIR
-
-tar -cf - bin/ config* README* Make* make* \
+tar -czf ../cp.tar.gz bin/ config* README* Make* make*           \
                       install-sh install/ moduledep.sh License upftools/ \
                       include/ Doc/ Modules/ clib/ flib/ \
-                      PW/ PP/ PH/ Gamma/ PWNC/ PWCOND/ D3/ Raman/ pwtools/ \
-		      CPV/ FPMD/ atomic/ atomic_doc/ examples/ pseudo/ $GUI \
-         | (cd $TMPDIR ; tar -xf -) 
+                      CPV/
 
-tar -czf $TMPDIR/cp-$VERSION.tar.gz \
-                            $TMPDIR/bin/  $TMPDIR/config* $TMPDIR/README* \
-                            $TMPDIR/Make* $TMPDIR/make*   $TMPDIR/install-sh \
-                            $TMPDIR/install/ $TMPDIR/moduledep.sh \
-                            $TMPDIR/License  $TMPDIR/upftools/     \
-                            $TMPDIR/include/ $TMPDIR/Doc/ $TMPDIR/Modules/ \
-                            $TMPDIR/clib/ $TMPDIR/flib/ \
-                            $TMPDIR/CPV/ 
+tar -czf ../fpmd.tar.gz bin/ config* README* Make* make*        \
+                      install-sh install/ moduledep.sh License upftools/ \
+                      include/ Doc/ Modules/ clib/ flib/ \
+                      CPV/
 
-tar -czf $TMPDIR/fpmd-$VERSION.tar.gz \
-                            $TMPDIR/bin/  $TMPDIR/config* $TMPDIR/README* \
-                            $TMPDIR/Make* $TMPDIR/make*   $TMPDIR/install-sh \
-                            $TMPDIR/install/ $TMPDIR/moduledep.sh \
-                            $TMPDIR/License  $TMPDIR/upftools/     \
-                            $TMPDIR/include/ $TMPDIR/Doc/ $TMPDIR/Modules/ \
-                            $TMPDIR/clib/ $TMPDIR/flib/ \
-                            $TMPDIR/FPMD/
+tar -czf ../$GUI.tar.gz $GUI
 
-tar -czf $TMPDIR/$GUI.tar.gz $TMPDIR/$GUI
+tar -czf ../pw.tar.gz bin/ config* README* Make* make* \
+                      install-sh install/ moduledep.sh License upftools/ \
+                      include/ Doc/ Modules/ clib/ flib/ \
+                      PW/ PP/ PH/ Gamma/ PWCOND/ D3/ Raman/ pwtools/
 
-tar -czf $TMPDIR/pw_src-$VERSION.tar.gz \
-                            $TMPDIR/bin/  $TMPDIR/config* $TMPDIR/README* \
-                            $TMPDIR/Make* $TMPDIR/make*   $TMPDIR/install-sh \
-                            $TMPDIR/install/ $TMPDIR/moduledep.sh \
-                            $TMPDIR/License  $TMPDIR/upftools/     \
-                            $TMPDIR/include/ $TMPDIR/Doc/ $TMPDIR/Modules/ \
-                            $TMPDIR/clib/ $TMPDIR/flib/ \
-                            $TMPDIR/PW/   $TMPDIR/PP/ $TMPDIR/PH/ \
-                            $TMPDIR/Gamma/ $TMPDIR/PWNC/ $TMPDIR/PWCOND/ \
-                            $TMPDIR/D3/   $TMPDIR/Raman/ $TMPDIR/pwtools/
+tar -czf ../examples.tar.gz examples/ pseudo/
 
-tar -czf $TMPDIR/examples-$VERSION.tar.gz $TMPDIR/examples/ $TMPDIR/pseudo/
+tar -czf ../espresso.tar.gz  bin/ config* README* Make* make* \
+                      install-sh install/ moduledep.sh License upftools/ \
+                      include/ Doc/ Modules/ clib/ flib/ \
+                      PW/ PP/ PH/ Gamma/ PWCOND/ D3/ Raman/ pwtools/ \
+		      CPV/ atomic/ atomic_doc/ examples/ pseudo/ $GUI
 
-tar -czf $TMPDIR/espresso-$VERSION.tar.gz \
-                            $TMPDIR/bin/  $TMPDIR/config* $TMPDIR/README* \
-                            $TMPDIR/Make* $TMPDIR/make*   $TMPDIR/install-sh \
-                            $TMPDIR/install/ $TMPDIR/moduledep.sh \
-                            $TMPDIR/License  $TMPDIR/upftools/     \
-                            $TMPDIR/include/ $TMPDIR/Doc/ $TMPDIR/Modules/ \
-                            $TMPDIR/clib/ $TMPDIR/flib/ \
-                            $TMPDIR/PW/   $TMPDIR/PP/ $TMPDIR/PH/ \
-                            $TMPDIR/Gamma/ $TMPDIR/PWNC/ $TMPDIR/PWCOND/ \
-                            $TMPDIR/D3/   $TMPDIR/Raman/ $TMPDIR/pwtools/ \
-                            $TMPDIR/CPV/ $TMPDIR/FPMD/ \
-		            $TMPDIR/atomic/ $TMPDIR/atomic_doc/ \
-                            $TMPDIR/examples/ $TMPDIR/pseudo/ $TMPDIR/$GUI
+scp Doc/README Doc/ChangeLog Doc/BUGS Doc/manual.tex \
+    Doc/*.png  Doc/manual.pdf ../pw.tar.gz ../espresso.tar.gz \
+    ../examples.tar.gz ../$GUI.tar.gz ../fpmd.tar.gz ../cp.tar.gz \
+    cibs:public_html/pw
 
-cd $TMPDIR
-
-cp README     README-$VERSION
-cp Doc/ChangeLog  ChangeLog-$VERSION
-cp Doc/BUGS       BUGS-$VERSION
-cp Doc/manual.tex manual-$VERSION.tex
-cp Doc/manual.pdf manual-$VERSION.pdf
-
-scp README-$VERSION ChangeLog-$VERSION BUGS-$VERSION \
-    manual-$VERSION.tex manual-$VERSION.pdf \
-    espresso-$VERSION.tar.gz examples-$VERSION.tar.gz $GUI.tar.gz  \
-    pw_src-$VERSION.tar.gz cp-$VERSION.tar.gz fpmd-$VERSION.tar.gz \
-    cibs.sns.it:public_html/pw
-
-cd ../
-/bin/rm -r $TMPDIR
