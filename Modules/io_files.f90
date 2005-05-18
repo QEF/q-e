ff0b81c10485de71d293b5c3ac77363c2bc52671
silviu <silviu@c92efa57-630b-4861-b058-cf58834340f0>
2005-05-18 20:44:10 +0000

    a stupid comma was missing.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1879 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/io_files.f90
+++ Modules/io_files.f90
@@ -61,7 +61,7 @@ MODULE io_files
   CHARACTER (LEN=261) :: &
     exit_file = "os.EXIT"    ! file required for a soft exit  
   CHARACTER (LEN=11), PARAMETER :: xmlpun = 'restart.xml'
-  CHARACTER(LEN=256) :: vib_out_file  = 'vibrations.out' & ! output of phrozen phonon vibrational calculation
+  CHARACTER(LEN=256) :: vib_out_file  = 'vibrations.out', & ! output of phrozen phonon vibrational calculation
                         vib_mass_file = 'mass.vib'         ! isotope masses used for diagonalizing the
                                                       ! ...dynamical matrix
   !
