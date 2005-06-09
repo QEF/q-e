e9929db5747e36fd602d8509fc67fe03663cb6e5
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2005-06-09 09:27:51 +0000

    re-enable ASR on Z*, indent
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@1950 c92efa57-630b-4861-b058-cf58834340f0

--- pwtools/dynmat.f90
+++ pwtools/dynmat.f90
@@ -424,369 +424,364 @@ subroutine set_asr(asr,dynr,zeu,nat,nax,tau,axis)
   ! ASR on effective charges
   !
   if(asr.eq.'simple') then
-           do i=1,3
-               do j=1,3
-                  sum=0.0
-                  do na=1,nat
-                     sum = sum + zeu(i,j,na)
-                  end do
-                  do na=1,nat
-                     !zeu(i,j,na) = zeu(i,j,na) - sum/nat
-                  end do
-               end do
-            end do
-     else
-      ! generating the vectors of the orthogonal of the subspace to project 
-      ! the effective charges matrix on
-      !
-      zeu_u(:,:,:,:)=0.0d0
-      do i=1,3
+     do i=1,3
         do j=1,3
-          do na=1,nat
-            zeu_new(i,j,na)=zeu(i,j,na)
-          enddo
-        enddo
-      enddo
-      !
-      p=0
-      do i=1,3
+           sum=0.0
+           do na=1,nat
+              sum = sum + zeu(i,j,na)
+           end do
+           do na=1,nat
+              zeu(i,j,na) = zeu(i,j,na) - sum/nat
+           end do
+        end do
+     end do
+  else
+     ! generating the vectors of the orthogonal of the subspace to project 
+     ! the effective charges matrix on
+     !
+     zeu_u(:,:,:,:)=0.0d0
+     do i=1,3
         do j=1,3
-          ! These are the 3*3 vectors associated with the 
-          ! translational acoustic sum rules
-          p=p+1
-          zeu_u(p,i,j,:)=1.0d0
-          !
+           do na=1,nat
+              zeu_new(i,j,na)=zeu(i,j,na)
+           enddo
         enddo
-      enddo
-      !
-      if (n.eq.4) then
-         do i=1,3
+     enddo
+     !
+     p=0
+     do i=1,3
+        do j=1,3
+           ! These are the 3*3 vectors associated with the 
+           ! translational acoustic sum rules
+           p=p+1
+           zeu_u(p,i,j,:)=1.0d0
+           !
+        enddo
+     enddo
+     !
+     if (n.eq.4) then
+        do i=1,3
            ! These are the 3 vectors associated with the 
            ! single rotational sum rule (1D system)
            p=p+1
            do na=1,nat
-             zeu_u(p,i,MOD(axis,3)+1,na)=-tau(MOD(axis+1,3)+1,na)
-             zeu_u(p,i,MOD(axis+1,3)+1,na)=tau(MOD(axis,3)+1,na)
+              zeu_u(p,i,MOD(axis,3)+1,na)=-tau(MOD(axis+1,3)+1,na)
+              zeu_u(p,i,MOD(axis+1,3)+1,na)=tau(MOD(axis,3)+1,na)
            enddo
            !
-         enddo
-      endif
-      !
-      if (n.eq.6) then
-         do i=1,3
+        enddo
+     endif
+     !
+     if (n.eq.6) then
+        do i=1,3
            do j=1,3
-             ! These are the 3*3 vectors associated with the 
-             ! three rotational sum rules (0D system - typ. molecule)
-             p=p+1
-             do na=1,nat
-               zeu_u(p,i,MOD(j,3)+1,na)=-tau(MOD(j+1,3)+1,na)
-               zeu_u(p,i,MOD(j+1,3)+1,na)=tau(MOD(j,3)+1,na)
-             enddo
-             !
+              ! These are the 3*3 vectors associated with the 
+              ! three rotational sum rules (0D system - typ. molecule)
+              p=p+1
+              do na=1,nat
+                 zeu_u(p,i,MOD(j,3)+1,na)=-tau(MOD(j+1,3)+1,na)
+                 zeu_u(p,i,MOD(j+1,3)+1,na)=tau(MOD(j,3)+1,na)
+              enddo
+              !
            enddo
-         enddo
-      endif
-      !
-      ! Gram-Schmidt orthonormalization of the set of vectors created.
-      !
-      nzeu_less=0
-      do k=1,p
+        enddo
+     endif
+     !
+     ! Gram-Schmidt orthonormalization of the set of vectors created.
+     !
+     nzeu_less=0
+     do k=1,p
         zeu_w(:,:,:)=zeu_u(k,:,:,:)
         zeu_x(:,:,:)=zeu_u(k,:,:,:)
         do q=1,k-1
-          r=1
-          do izeu_less=1,nzeu_less
-            if (zeu_less(izeu_less).eq.q) r=0
-          enddo
-          if (r.ne.0) then
+           r=1
+           do izeu_less=1,nzeu_less
+              if (zeu_less(izeu_less).eq.q) r=0
+           enddo
+           if (r.ne.0) then
               call sp_zeu(zeu_x,zeu_u(q,:,:,:),nat,scal)
               zeu_w(:,:,:) = zeu_w(:,:,:) - scal* zeu_u(q,:,:,:)
-          endif
+           endif
         enddo
         call sp_zeu(zeu_w,zeu_w,nat,norm2)
         if (norm2.gt.1.0d-16) then
            zeu_u(k,:,:,:) = zeu_w(:,:,:) / DSQRT(norm2)
-          else
+        else
            nzeu_less=nzeu_less+1
            zeu_less(nzeu_less)=k
         endif
-      enddo
-      !
-      !
-      ! Projection of the effective charge "vector" on the orthogonal of the 
-      ! subspace of the vectors verifying the sum rules
-      !
-      zeu_w(:,:,:)=0.0d0
-      do k=1,p
+     enddo
+     !
+     !
+     ! Projection of the effective charge "vector" on the orthogonal of the 
+     ! subspace of the vectors verifying the sum rules
+     !
+     zeu_w(:,:,:)=0.0d0
+     do k=1,p
         r=1
         do izeu_less=1,nzeu_less
-          if (zeu_less(izeu_less).eq.k) r=0
+           if (zeu_less(izeu_less).eq.k) r=0
         enddo
         if (r.ne.0) then
-            zeu_x(:,:,:)=zeu_u(k,:,:,:)
-            call sp_zeu(zeu_x,zeu_new,nat,scal)
-            zeu_w(:,:,:) = zeu_w(:,:,:) + scal*zeu_u(k,:,:,:)
+           zeu_x(:,:,:)=zeu_u(k,:,:,:)
+           call sp_zeu(zeu_x,zeu_new,nat,scal)
+           zeu_w(:,:,:) = zeu_w(:,:,:) + scal*zeu_u(k,:,:,:)
         endif
-      enddo
-      !
-      ! Final substraction of the former projection to the initial zeu, to get
-      ! the new "projected" zeu
-      !
-      zeu_new(:,:,:)=zeu_new(:,:,:) - zeu_w(:,:,:)
-      call sp_zeu(zeu_w,zeu_w,nat,norm2)
-      write(6,'("Norm of the difference between old and new effectives charges: " &
-                        ,F25.20)') DSQRT(norm2)
-      !
-      ! Check projection
-      !
-      !write(6,'("Check projection of zeu")')
-      !do k=1,p
-      !  zeu_x(:,:,:)=zeu_u(k,:,:,:)
-      !  call sp_zeu(zeu_x,zeu_new,nat,scal)
-      !  if (DABS(scal).gt.1d-10) write(6,'("k= ",I8," zeu_new|zeu_u(k)= ",F15.10)') k,scal
-      !enddo
-      !
-      do i=1,3
+     enddo
+     !
+     ! Final substraction of the former projection to the initial zeu, to get
+     ! the new "projected" zeu
+     !
+     zeu_new(:,:,:)=zeu_new(:,:,:) - zeu_w(:,:,:)
+     call sp_zeu(zeu_w,zeu_w,nat,norm2)
+     write(6,'("Norm of the difference between old and new effectives charges: " &
+          ,F25.20)') DSQRT(norm2)
+     !
+     ! Check projection
+     !
+     !write(6,'("Check projection of zeu")')
+     !do k=1,p
+     !  zeu_x(:,:,:)=zeu_u(k,:,:,:)
+     !  call sp_zeu(zeu_x,zeu_new,nat,scal)
+     !  if (DABS(scal).gt.1d-10) write(6,'("k= ",I8," zeu_new|zeu_u(k)= ",F15.10)') k,scal
+     !enddo
+     !
+     do i=1,3
         do j=1,3
-          do na=1,nat
-            zeu(i,j,na)=zeu_new(i,j,na)
-          enddo
+           do na=1,nat
+              zeu(i,j,na)=zeu_new(i,j,na)
+           enddo
         enddo
-      enddo
+     enddo
   endif
-  !
-  !
-  !
-  !
   !
   ! ASR on dynamical matrix
   !
   if(asr.eq.'simple') then
      do i=1,3
-       do j=1,3
-         do na=1,nat
-           sum=0.0
-           do nb=1,nat
-             if (na.ne.nb) sum=sum+dynr(1,i,j,na,nb)
-           end do
-           dynr(1,i,j,na,na) = -sum
-         end do
-       end do
-     end do
-  !
-    else
-      ! generating the vectors of the orthogonal of the subspace to project 
-      ! the dyn. matrix on
-      !
-      u(:,:,:,:,:)=0.0d0
-      do r=1,2
-        do i=1,3
-          do j=1,3
-            do na=1,nat
-              do nb=1,nat
-                dynr_new(r,i,j,na,nb)=dynr(r,i,j,na,nb)
-              enddo
-            enddo
-          enddo
-        enddo
-      enddo
-      !
-      p=0
-      do i=1,3
         do j=1,3
-          do na=1,nat
-            ! These are the 3*3*nat vectors associated with the 
-            ! translational acoustic sum rules
-            p=p+1
-            do nb=1,nat
-              u(p,i,j,na,nb)=1.0d0
-            enddo
-            !
-          enddo
-        enddo
-      enddo
-      !
-      if (n.eq.4) then
-         do i=1,3
            do na=1,nat
-             ! These are the 3*nat vectors associated with the 
-             ! single rotational sum rule (1D system)
-             p=p+1
-             do nb=1,nat
-               u(p,i,axis,na,nb)=0.0d0
-               u(p,i,MOD(axis,3)+1,na,nb)=-tau(MOD(axis+1,3)+1,nb)
-               u(p,i,MOD(axis+1,3)+1,na,nb)=tau(MOD(axis,3)+1,nb)
-             enddo
-             !
-           enddo
-         enddo
-      endif
-      !
-      if (n.eq.6) then
-         do i=1,3
+              sum=0.0
+              do nb=1,nat
+                 if (na.ne.nb) sum=sum+dynr(1,i,j,na,nb)
+              end do
+              dynr(1,i,j,na,na) = -sum
+           end do
+        end do
+     end do
+     !
+  else
+     ! generating the vectors of the orthogonal of the subspace to project 
+     ! the dyn. matrix on
+     !
+     u(:,:,:,:,:)=0.0d0
+     do r=1,2
+        do i=1,3
            do j=1,3
-             do na=1,nat
-               ! These are the 3*3*nat vectors associated with the 
-               ! three rotational sum rules (0D system - typ. molecule)
-               p=p+1
-               do nb=1,nat
-                 u(p,i,j,na,nb)=0.0d0
-                 u(p,i,MOD(j,3)+1,na,nb)=-tau(MOD(j+1,3)+1,nb)
-                 u(p,i,MOD(j+1,3)+1,na,nb)=tau(MOD(j,3)+1,nb)
-               enddo
-               !
-             enddo
-           enddo
-         enddo
-      endif
-      !
-      m=0
-      do i=1,3
-        do j=1,3
-          do na=1,nat
-            do nb=1,nat
-              ! These are the vectors associated with the symmetry constraints
-              q=1
-              l=1
-              do while((l.le.m).and.(q.ne.0))
-                if ((ind_v(l,1,1).eq.i).and.(ind_v(l,1,2).eq.j).and. &
-                        (ind_v(l,1,3).eq.na).and.(ind_v(l,1,4).eq.nb)) q=0
-                if ((ind_v(l,2,1).eq.i).and.(ind_v(l,2,2).eq.j).and. &
-                        (ind_v(l,2,3).eq.na).and.(ind_v(l,2,4).eq.nb)) q=0
-                      l=l+1
+              do na=1,nat
+                 do nb=1,nat
+                    dynr_new(r,i,j,na,nb)=dynr(r,i,j,na,nb)
+                 enddo
               enddo
-              if ((i.eq.j).and.(na.eq.nb)) q=0
-              if (q.ne.0) then
-                  m=m+1
-                  ind_v(m,1,1)=i
-                  ind_v(m,1,2)=j
-                  ind_v(m,1,3)=na
-                  ind_v(m,1,4)=nb
-                  v(m,1)=1.0d0/DSQRT(2.0d0)
-                  ind_v(m,2,1)=j
-                  ind_v(m,2,2)=i
-                  ind_v(m,2,3)=nb
-                  ind_v(m,2,4)=na
-                  v(m,2)=-1.0d0/DSQRT(2.0d0)
-              endif
-            enddo
-          enddo
+           enddo
         enddo
-      enddo
-      !
-      ! Gram-Schmidt orthonormalization of the set of vectors created.
-      ! Note that the vectors corresponding to symmetry constraints are already
-      ! orthonormalized by construction.
-      !
-      n_less=0
-      do k=1,p
+     enddo
+     !
+     p=0
+     do i=1,3
+        do j=1,3
+           do na=1,nat
+              ! These are the 3*3*nat vectors associated with the 
+              ! translational acoustic sum rules
+              p=p+1
+              do nb=1,nat
+                 u(p,i,j,na,nb)=1.0d0
+              enddo
+              !
+           enddo
+        enddo
+     enddo
+     !
+     if (n.eq.4) then
+        do i=1,3
+           do na=1,nat
+              ! These are the 3*nat vectors associated with the 
+              ! single rotational sum rule (1D system)
+              p=p+1
+              do nb=1,nat
+                 u(p,i,axis,na,nb)=0.0d0
+                 u(p,i,MOD(axis,3)+1,na,nb)=-tau(MOD(axis+1,3)+1,nb)
+                 u(p,i,MOD(axis+1,3)+1,na,nb)=tau(MOD(axis,3)+1,nb)
+              enddo
+              !
+           enddo
+        enddo
+     endif
+     !
+     if (n.eq.6) then
+        do i=1,3
+           do j=1,3
+              do na=1,nat
+                 ! These are the 3*3*nat vectors associated with the 
+                 ! three rotational sum rules (0D system - typ. molecule)
+                 p=p+1
+                 do nb=1,nat
+                    u(p,i,j,na,nb)=0.0d0
+                    u(p,i,MOD(j,3)+1,na,nb)=-tau(MOD(j+1,3)+1,nb)
+                    u(p,i,MOD(j+1,3)+1,na,nb)=tau(MOD(j,3)+1,nb)
+                 enddo
+                 !
+              enddo
+           enddo
+        enddo
+     endif
+     !
+     m=0
+     do i=1,3
+        do j=1,3
+           do na=1,nat
+              do nb=1,nat
+                 ! These are the vectors associated with the symmetry constraints
+                 q=1
+                 l=1
+                 do while((l.le.m).and.(q.ne.0))
+                    if ((ind_v(l,1,1).eq.i).and.(ind_v(l,1,2).eq.j).and. &
+                         (ind_v(l,1,3).eq.na).and.(ind_v(l,1,4).eq.nb)) q=0
+                    if ((ind_v(l,2,1).eq.i).and.(ind_v(l,2,2).eq.j).and. &
+                         (ind_v(l,2,3).eq.na).and.(ind_v(l,2,4).eq.nb)) q=0
+                    l=l+1
+                 enddo
+                 if ((i.eq.j).and.(na.eq.nb)) q=0
+                 if (q.ne.0) then
+                    m=m+1
+                    ind_v(m,1,1)=i
+                    ind_v(m,1,2)=j
+                    ind_v(m,1,3)=na
+                    ind_v(m,1,4)=nb
+                    v(m,1)=1.0d0/DSQRT(2.0d0)
+                    ind_v(m,2,1)=j
+                    ind_v(m,2,2)=i
+                    ind_v(m,2,3)=nb
+                    ind_v(m,2,4)=na
+                    v(m,2)=-1.0d0/DSQRT(2.0d0)
+                 endif
+              enddo
+           enddo
+        enddo
+     enddo
+     !
+     ! Gram-Schmidt orthonormalization of the set of vectors created.
+     ! Note that the vectors corresponding to symmetry constraints are already
+     ! orthonormalized by construction.
+     !
+     n_less=0
+     do k=1,p
         w(:,:,:,:)=u(k,:,:,:,:)
         x(:,:,:,:)=u(k,:,:,:,:)
         do l=1,m
-          !
-          call sp2(x,v(l,:),ind_v(l,:,:),nat,scal)
-          do r=1,2
-            i=ind_v(l,r,1)
-            j=ind_v(l,r,2)
-            na=ind_v(l,r,3)
-            nb=ind_v(l,r,4)
-            w(i,j,na,nb)=w(i,j,na,nb)-scal*v(l,r)
-          enddo
+           !
+           call sp2(x,v(l,:),ind_v(l,:,:),nat,scal)
+           do r=1,2
+              i=ind_v(l,r,1)
+              j=ind_v(l,r,2)
+              na=ind_v(l,r,3)
+              nb=ind_v(l,r,4)
+              w(i,j,na,nb)=w(i,j,na,nb)-scal*v(l,r)
+           enddo
         enddo
         if (k.le.(9*nat)) then
-            na1=MOD(k,nat)
-            if (na1.eq.0) na1=nat
-            j1=MOD((k-na1)/nat,3)+1
-            i1=MOD((((k-na1)/nat)-j1+1)/3,3)+1
-          else
-            q=k-9*nat
-            if (n.eq.4) then
-                na1=MOD(q,nat)
-                if (na1.eq.0) na1=nat
-                i1=MOD((q-na1)/nat,3)+1
-              else
-                na1=MOD(q,nat)
-                if (na1.eq.0) na1=nat
-                j1=MOD((q-na1)/nat,3)+1
-                i1=MOD((((q-na1)/nat)-j1+1)/3,3)+1
-            endif
+           na1=MOD(k,nat)
+           if (na1.eq.0) na1=nat
+           j1=MOD((k-na1)/nat,3)+1
+           i1=MOD((((k-na1)/nat)-j1+1)/3,3)+1
+        else
+           q=k-9*nat
+           if (n.eq.4) then
+              na1=MOD(q,nat)
+              if (na1.eq.0) na1=nat
+              i1=MOD((q-na1)/nat,3)+1
+           else
+              na1=MOD(q,nat)
+              if (na1.eq.0) na1=nat
+              j1=MOD((q-na1)/nat,3)+1
+              i1=MOD((((q-na1)/nat)-j1+1)/3,3)+1
+           endif
         endif
         do q=1,k-1
-          r=1
-          do i_less=1,n_less
-            if (u_less(i_less).eq.q) r=0
-          enddo
-          if (r.ne.0) then
+           r=1
+           do i_less=1,n_less
+              if (u_less(i_less).eq.q) r=0
+           enddo
+           if (r.ne.0) then
               call sp3(x,u(q,:,:,:,:),i1,na1,nat,scal)
               w(:,:,:,:) = w(:,:,:,:) - scal* u(q,:,:,:,:)
-          endif
+           endif
         enddo
         call sp1(w,w,nat,norm2)
         if (norm2.gt.1.0d-16) then
            u(k,:,:,:,:) = w(:,:,:,:) / DSQRT(norm2)
-          else
+        else
            n_less=n_less+1
            u_less(n_less)=k
         endif
-      enddo
-      !
-      ! Projection of the dyn. "vector" on the orthogonal of the 
-      ! subspace of the vectors verifying the sum rules and symmetry contraints
-      !
-      w(:,:,:,:)=0.0d0
-      do l=1,m
+     enddo
+     !
+     ! Projection of the dyn. "vector" on the orthogonal of the 
+     ! subspace of the vectors verifying the sum rules and symmetry contraints
+     !
+     w(:,:,:,:)=0.0d0
+     do l=1,m
         call sp2(dynr_new(1,:,:,:,:),v(l,:),ind_v(l,:,:),nat,scal)
         do r=1,2
-          i=ind_v(l,r,1)
-          j=ind_v(l,r,2)
-          na=ind_v(l,r,3)
-          nb=ind_v(l,r,4)
-          w(i,j,na,nb)=w(i,j,na,nb)+scal*v(l,r)
+           i=ind_v(l,r,1)
+           j=ind_v(l,r,2)
+           na=ind_v(l,r,3)
+           nb=ind_v(l,r,4)
+           w(i,j,na,nb)=w(i,j,na,nb)+scal*v(l,r)
         enddo
-      enddo
-      do k=1,p
+     enddo
+     do k=1,p
         r=1
         do i_less=1,n_less
-          if (u_less(i_less).eq.k) r=0
+           if (u_less(i_less).eq.k) r=0
         enddo
         if (r.ne.0) then
-            x(:,:,:,:)=u(k,:,:,:,:)
-            call sp1(x,dynr_new(1,:,:,:,:),nat,scal)
-            w(:,:,:,:) = w(:,:,:,:) + scal* u(k,:,:,:,:)
+           x(:,:,:,:)=u(k,:,:,:,:)
+           call sp1(x,dynr_new(1,:,:,:,:),nat,scal)
+           w(:,:,:,:) = w(:,:,:,:) + scal* u(k,:,:,:,:)
         endif
-      enddo
-      !
-      ! Final substraction of the former projection to the initial dynr, to get
-      ! the new "projected" dynr
-      !
-      dynr_new(1,:,:,:,:)=dynr_new(1,:,:,:,:) - w(:,:,:,:)
-      call sp1(w,w,nat,norm2)
-      write(6,'("norm w= ",F25.20)') DSQRT(norm2)
-      !
-      ! Check projection
-      !
-      !write(6,'("Check projection")')
-      !do l=1,m
-      !  call sp2(dynr_new(1,:,:,:,:),v(l,:),ind_v(l,:,:),nat,scal)
-      !  if (DABS(scal).gt.1d-10) write(6,'("l= ",I8," dyn|v(l)= ",F15.10)') l,scal
-      !enddo
-      !do k=1,p
-      !  x(:,:,:,:)=u(k,:,:,:,:)
-      !  call sp1(x,dynr_new(1,:,:,:,:),nat,scal)
-      !  if (DABS(scal).gt.1d-10) write(6,'("k= ",I8," dyn|u(k)= ",F15.10)') k,scal
-      !enddo
-      !
-      do i=1,3
+     enddo
+     !
+     ! Final substraction of the former projection to the initial dynr, to get
+     ! the new "projected" dynr
+     !
+     dynr_new(1,:,:,:,:)=dynr_new(1,:,:,:,:) - w(:,:,:,:)
+     call sp1(w,w,nat,norm2)
+     write(6,'("norm w= ",F25.20)') DSQRT(norm2)
+     !
+     ! Check projection
+     !
+     !write(6,'("Check projection")')
+     !do l=1,m
+     !  call sp2(dynr_new(1,:,:,:,:),v(l,:),ind_v(l,:,:),nat,scal)
+     !  if (DABS(scal).gt.1d-10) write(6,'("l= ",I8," dyn|v(l)= ",F15.10)') l,scal
+     !enddo
+     !do k=1,p
+     !  x(:,:,:,:)=u(k,:,:,:,:)
+     !  call sp1(x,dynr_new(1,:,:,:,:),nat,scal)
+     !  if (DABS(scal).gt.1d-10) write(6,'("k= ",I8," dyn|u(k)= ",F15.10)') k,scal
+     !enddo
+     !
+     do i=1,3
         do j=1,3
-          do na=1,nat
-            do nb=1,nat
-              dynr(1,i,j,na,nb)=dynr_new(1,i,j,na,nb)
-            enddo
-          enddo
+           do na=1,nat
+              do nb=1,nat
+                 dynr(1,i,j,na,nb)=dynr_new(1,i,j,na,nb)
+              enddo
+           enddo
         enddo
-      enddo
+     enddo
   endif
-
   !
   return
 end subroutine set_asr
