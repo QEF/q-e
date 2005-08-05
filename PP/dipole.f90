abe6c16f6bd0658a801f975348690a3eab2edd8e
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2005-08-05 08:25:46 +0000

    More cleanup of the dipole/MAakovPayne stuff
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@2090 c92efa57-630b-4861-b058-cf58834340f0

--- PP/dipole.f90
+++ PP/dipole.f90
@@ -9,29 +9,25 @@
 program dipole
   !-----------------------------------------------------------------------
   !
-  !      DESCRIPTION of the INPUT: see file INPUT_CHDENS in pwdocs/
-  !      (only for variables: nfile filepp weight x0)
-  ! 
-  !      Calculation of the dipole moment:
-  !          to be used for an isolated molecule in a box;
-  !          the molecule must be at the center of the box.
-  !          The code computes the dipole on the Wigner-Seitz cell of
-  !          the Bravais lattice. The 3d box must contain this cell 
-  !          otherwise meaningless numbers are obtained
-  !
-  !      Calculation of Makov-Payne correction for charged supercells:
-  !          - not thoroughly tested
-  !          - works only for clusters embedded within a cubic supercell
-  !          - the cluster (and the plotting box) MUST be CENTERED 
-  !            around (0,0,0), otherwise meaningless results are printed
-  !          - always check that the printed total charge is the right one
-  !          - for impurities in bulk crystals the correction should work
-  !            as well, but the Madelung constant of the considered lattice
-  !            must be used and the correction has to be divided by the
-  !             crystal dielectric constant.
-  !      Ref.: G. Makov and M.C. Payne, PRB 51, 4014 (1995)
+  !      For isolated systems, e.g. molecules in a supercell
+  !      The molecule should be centered around the origin. If not,
+  !      use variable x0 to re-center the molecule: the results
+  !      should not depend on the exact choice of the origin
+  !      if the cell is sufficiently large
+  !      Calculation of Makov-Payne correction for charged supercells
+  !      as in : G. Makov and M.C. Payne, PRB 51, 4014 (1995)
   !      (note that Eq. 15 has the wrong sign for the quadrupole term)
-  !      Contributed by Giovanni Cantele
+  !      Contributed by Giovanni Cantele and Paolo Cazzato
+  !
+  !      DESCRIPTION of the INPUT:
+  !
+  !-&input           Namelist &input; contains
+  !
+  !      filepp      file containing the 3D charge (produced by pp.x)
+  !                  (REQUIRED)
+  !      x0(3)       center the box around point x0 (in alat units)
+  !                  ( default: (0,0,0) )
+  !
   !
 #include "f_defs.h"
   USE io_global,  ONLY : stdout
@@ -48,34 +44,20 @@ program dipole
   USE io_files, ONLY: nd_nmbr
 
   implicit none
-  integer, parameter :: nfilemax = 7
-  ! maximum number of files with charge
 
-  integer :: nfile, plot_num
+  real(kind=DP) :: x0 (3), dipol(0:3), quadrupol
+  real(kind=DP), allocatable :: rhor(:)
+  integer :: ios, plot_num
+  character (len=256) :: filepp
 
-  real(kind=DP) :: x0 (3), weight (nfilemax), dipol(0:3), quadrupol
-
-  character (len=256) :: filename (nfilemax)
-
-  real(kind=DP) :: celldms (6), gcutmsa, duals, ecuts, zvs(ntypx), ats(3,3)
-  real(kind=DP), allocatable :: taus (:,:), rhor(:)
-  integer :: ibravs, nrx1sa, nrx2sa, nrx3sa, nr1sa, nr2sa, nr3sa, &
-       ntyps, nats
-  integer, allocatable :: ityps (:)
-  integer :: ios, ipol, ifile, na, ir, i, j
-  character (len=3) :: atms(ntypx)
-  character (len=256) :: filepp(nfilemax)
-
-  namelist /input/  nfile, filepp, weight, x0
+  namelist /input/  filepp, x0
   !
   call start_postproc (nd_nmbr)
   !
   !   set the DEFAULT values
   !
-  nfile         = 1
-  filepp(1)   = ' '
-  weight(1)     = 1.0d0
-  x0(:)         = 0.d0
+  filepp   = ' '
+  x0(:)    = 0.d0
   !
   !    read and check input data
   !
@@ -86,14 +68,10 @@ program dipole
   read (5, input, err = 200, iostat = ios)
 200 call errore ('dipole', 'reading input namelist', abs (ios) )
 
-  ! check for number of files
-  if (nfile < 1 .or. nfile > nfilemax) &
-       call errore ('dipole ', 'nfile is wrong ', 1)
   !
   ! Read the header and allocate objects
   !
-
-  call read_io_header(filepp (1), title, nrx1, nrx2, nrx3, nr1, nr2, nr3, &
+  call read_io_header(filepp, title, nrx1, nrx2, nrx3, nr1, nr2, nr3, &
                 nat, ntyp, ibrav, celldm, at, gcutm, dual, ecutwfc, plot_num )
   !
   allocate(tau (3, nat))
@@ -109,73 +87,45 @@ program dipole
   else
      gcutms = gcutm
   endif
-
+  !
   nspin = 1
   if (ibrav > 0) then
     call latgen (ibrav, celldm, at(1,1), at(1,2), at(1,3), omega )
     at = at / alat !  bring at in units of alat
   end if
-
+  !
   call recips (at(1,1), at(1,2), at(1,3), bg(1,1), bg(1,2), bg(1,3) )
   call volume (alat, at(1,1), at(1,2), at(1,3), omega)
-
+  !
   call set_fft_dim ( )
-
+  !
   allocate (rho (nrx1*nrx2*nrx3,1))
   !
-  ! Read first file
+  ! Read file
   !
-  call plot_io (filepp (1), title, nrx1, nrx2, nrx3, nr1, nr2, nr3, &
+  call plot_io (filepp, title, nrx1, nrx2, nrx3, nr1, nr2, nr3, &
                 nat, ntyp, ibrav, celldm, at, gcutm, dual, ecutwfc, &
                 plot_num, atm, ityp, zv, tau, rho(1,1), -1)
   !
-  rhor (:) = weight (1) * rho (:,1)
+  rhor (:) = rho (:,1)
   !
-  ! Read following files (if any), verify consistency
-  ! Note that only rho is read; all other quantities are discarded
-  !
-  do ifile = 2, nfile
-     allocate  (taus( 3 , nat))    
-     allocate  (ityps( nat))    
-     !
-     call plot_io (filepp (ifile), title, nrx1sa, nrx2sa, nrx3sa, &
-          nr1sa, nr2sa, nr3sa, nats, ntyps, ibravs, celldms, ats, gcutmsa, &
-          duals, ecuts, plot_num, atms, ityps, zvs, taus, rho(1,1), - 1)
-     !
-     deallocate (ityps)
-     deallocate (taus)
-     !
-     if (nats.gt.nat) call errore ('dipole', 'wrong file order? ', 1)
-     if (nrx1.ne.nrx1sa.or.nrx2.ne.nrx2sa) call &
-          errore ('dipole', 'incompatible nrx1 or nrx2', 1)
-     if (nr1.ne.nr1sa.or.nr2.ne.nr2sa.or.nr3.ne.nr3sa) call &
-          errore ('dipole', 'incompatible nr1 or nr2 or nr3', 1)
-     if (ibravs.ne.ibrav) call errore ('dipole', 'incompatible ibrav', 1)
-     if (gcutmsa.ne.gcutm.or.duals.ne.dual.or.ecuts.ne.ecutwfc ) &
-          call errore ('dipole', 'incompatible gcutm or dual or ecut', 1)
-     do i = 1, 6
-        if (abs( celldm (i)-celldms (i) ) .gt. 1.0e-7 ) call errore &
-             ('dipole', 'incompatible celldm', 1)
-     enddo
-     !
-     rhor (:) = rhor (:) + weight (ifile) * rho (:,1)
-  enddo
-  !
-  call dipole_fast (celldm (1), at, nat, omega, tau, &
+  call dipole_fast (celldm (1), at, bg, omega, nat, tau, &
        nrx1, nrx2, nrx3, nr1, nr2, nr3, rhor, &
        x0, dipol, quadrupol)
-
-  call write_dipol(dipol,quadrupol,tau,nat,alat,zv,ntyp,ityp, &
+  !
+  call write_dipol(x0,dipol,quadrupol,tau,nat,alat,zv,ntyp,ityp, &
        ibrav)
- 
+  !
   deallocate(rhor)
   deallocate(tau)
   deallocate(ityp)
+  !
   call stop_pp
+  !
 end program dipole
 !
 !-----------------------------------------------------------------------
-subroutine dipole_fast (alat, at, nat, omega, tau, nrx1, nrx2, nrx3, &
+subroutine dipole_fast (alat, at, bg, omega, nat, tau, nrx1, nrx2, nrx3, &
      nr1, nr2, nr3, rhor, x0, dipol, quadrupol)
   !-----------------------------------------------------------------------
   !
@@ -183,33 +133,85 @@ subroutine dipole_fast (alat, at, nat, omega, tau, nrx1, nrx2, nrx3, &
   USE kinds, only : DP
   implicit none
   integer :: nat, nrx1, nrx2, nrx3, nr1, nr2, nr3
-  real(kind=DP) :: alat, omega, tau (3, nat), at (3, 3), &
+  real(kind=DP) :: alat, omega, tau (3, nat), at (3, 3), bg (3, 3), &
        rhor(nrx1,nrx2,nrx3), x0 (3), dipol(0:3), quadrupol
 
-  real(kind=DP) :: r(3)
-  integer :: i,j,k, i1,j1,k1, ipol
+  real(kind=DP) :: r(3), w1, w2, w3
+  integer :: i,j,k, i0,j0,k0, i1,j1,k1, nr1m,nr2m,nr3m, ipol
 
   dipol=0.d0
   quadrupol=0.d0
-  do k1 = -nr3/2, nr3/2
-     k = k1 + 1
+
+  ! find FFT grid point closer to the desired center, X0
+  ! add 1 because r=0 corresponds to (1,1,1)
+
+  i0 = mod( nint( (x0(1)*bg(1,1)+x0(2)*bg(2,1)+x0(3)*bg(3,1))*nr1), nr1) + 1
+  j0 = mod( nint( (x0(1)*bg(1,2)+x0(2)*bg(2,2)+x0(3)*bg(3,2))*nr2), nr2) + 1
+  k0 = mod( nint( (x0(1)*bg(1,3)+x0(2)*bg(2,3)+x0(3)*bg(3,3))*nr3), nr3) + 1
+  !
+  ! redefine X0 so that what is used in the ionic term is consistent with
+  ! what is used for the dipole term
+  !
+  x0(1)=(i0-1)*at(1,1)/ nr1 +(j0-1)*at(1,2)/ nr2 +(k0-1)*at(1,3)/ nr3
+  x0(2)=(i0-1)*at(2,1)/ nr1 +(j0-1)*at(2,2)/ nr2 +(k0-1)*at(2,3)/ nr3
+  x0(3)=(i0-1)*at(3,1)/ nr1 +(j0-1)*at(3,2)/ nr2 +(k0-1)*at(3,3)/ nr3
+  !
+  WRITE( stdout,'(5x,"Origin translated by: ",3f8.4," (alat units)")') x0
+
+  nr1m = nr1/2
+  nr2m = nr2/2
+  nr3m = nr3/2
+  do k1 = -nr3m, nr3m
+     k = mod (k1 + k0, nr3)
      if (k < 1) k = k + nr3
-     do j1 = -nr2/2, nr2/2
-        j = j1 + 1
+     !
+     ! correct weight at boundaries : if nr is even, we are summing both
+     ! points at -nr/2 and nr/2, and they are the same because of periodicity
+     !
+     IF ( 2*nr3m == nr3 .AND. ABS(k1) == nr3m ) THEN
+        w3 = 0.5d0
+     else
+        w3 = 1.d0
+     end if
+     do j1 = -nr2m, nr2m
+        j = mod (j1 + j0, nr2)
         if (j < 1) j = j + nr2
-        do i1 = -nr1/2, nr1/2  
-           i = i1 + 1
+        !
+        ! correct weight at boundaries
+        !
+        IF ( 2*nr2m == nr2 .AND. ABS(j1) == nr2m ) THEN
+           w2 = 0.5d0 * w3
+        else
+           w2 = 1.d0 * w3
+        end if
+        do i1 = -nr1m, nr1m
+           i = mod(i1 + i0, nr1)
            if (i < 1) i = i + nr1
-           dipol(0) = dipol(0) + rhor (i, j, k) 
+           !
+           ! correct weight at boundaries
+           !
+           IF ( 2*nr1m == nr1 .AND. ABS(i1) == nr1m ) THEN
+              w1 = 0.5d0 * w2
+           else
+              w1 = 1.d0 * w2
+           end if
+           !
+           ! dipol(0) = charge density
+           !
+           dipol(0) = dipol(0) + w1*rhor (i, j, k) 
+           !
+           ! r-vectors are centered around r=0
+           ! it is the FFT cell that is centered around x0
+           !
            do ipol=1,3
-!!! x0 ???
               r(ipol) = i1*at(ipol,1)/nr1 + &
                         j1*at(ipol,2)/nr2 + &
                         k1*at(ipol,3)/nr3
            enddo
+           !
            do ipol=1,3
-              dipol(ipol)=dipol(ipol) + r(ipol)*rhor(i,j,k)
-              quadrupol = quadrupol + rhor (i, j, k)*r(ipol)**2
+              dipol(ipol)=dipol(ipol) + r(ipol)*w1*rhor(i,j,k)
+              quadrupol = quadrupol + r(ipol)**2*w1*rhor (i, j, k)
            enddo
         enddo
      enddo
@@ -225,7 +227,7 @@ subroutine dipole_fast (alat, at, nat, omega, tau, nrx1, nrx2, nrx3, &
 end subroutine dipole_fast
 !
 !------------------------------------------------------------
-subroutine write_dipol(dipol_el,quadrupol_el,tau,nat,alat,zv, &
+subroutine write_dipol(x0,dipol_el,quadrupol_el,tau,nat,alat,zv, &
      ntyp,ityp,ibrav)
   !-----------------------------------------------------------
   USE io_global,  ONLY : stdout
@@ -235,7 +237,7 @@ subroutine write_dipol(dipol_el,quadrupol_el,tau,nat,alat,zv, &
 
   integer :: nat, ntyp, ityp(nat), ibrav
   real(kind=dp) :: dipol_el(0:3), quadrupol_el, tau(3,nat), zv(ntyp), alat
-
+  real(kind=dp) :: x0(3)
   real(kind=dp) :: debye, dipol_ion(3), quadrupol_ion, dipol(3), quadrupol
   real(kind=DP) :: rhotot, zvtot, corr1, corr2, qq, AA, BB
 
@@ -256,8 +258,8 @@ subroutine write_dipol(dipol_el,quadrupol_el,tau,nat,alat,zv, &
   do na=1,nat
      zvtot = zvtot+zv(ityp(na))
      do ipol=1,3
-        dipol_ion(ipol)=dipol_ion(ipol)+zv(ityp(na))* tau(ipol,na)*alat
-        quadrupol_ion  =quadrupol_ion  +zv(ityp(na))*(tau(ipol,na)*alat)**2
+        dipol_ion(ipol)=dipol_ion(ipol)+zv(ityp(na))* (tau(ipol,na)-x0(ipol))*alat
+        quadrupol_ion  =quadrupol_ion  +zv(ityp(na))*((tau(ipol,na)-x0(ipol))*alat)**2
      enddo
   enddo
   !
@@ -278,36 +280,21 @@ subroutine write_dipol(dipol_el,quadrupol_el,tau,nat,alat,zv, &
   if (abs(qq) < 1.d-3) then
       corr2 = 0.0d0
   endif
-
   !
-  !  Charge inside the Wigner-Seitz cell
-  !
-  WRITE( stdout, '(/4x," Charge density inside the Wigner-Seitz cell:",&
+  WRITE( stdout, '(/5x,"Charge density inside the Wigner-Seitz cell:",&
        &3f14.8," el.")')  dipol_el(0)
   !
-  !  print the electron dipole moment calculated by the plotting 3d routines
+  debye=2.54176d0
+  !
   !  A positive dipole goes from the - charge to the + charge.
   !
-  WRITE( stdout, '(/4x,"Electrons dipole moments",3f14.8," a.u.")')  &
-       (-dipol_el(ipol),ipol=1,3)
-  !
-  ! print the ionic and total dipole moment
-  !
-  WRITE( stdout, '(4x,"     Ions dipole moments",3f14.8," a.u.")') &
-       (dipol_ion(ipol),ipol=1,3)
-  WRITE( stdout,'(4x,"    Total dipole moments",3f14.8," a.u.")') &
-       (-dipol(ipol),ipol=1,3)
-  !
-  !   Print the same information in Debye
-  !
-  debye=2.54176d0
-
-  WRITE( stdout,'(/4x,"Electrons dipole moments",3f14.8," Debye")') &
-       (-dipol_el(ipol)*debye,ipol=1,3)
-  WRITE( stdout,'(4x,"     Ions dipole moments",3f14.8," Debye")') &
-       (dipol_ion(ipol)*debye,ipol=1,3)
-  WRITE( stdout,'(4x,"    Total dipole moments",3f14.8," Debye")') &
-       (-dipol(ipol)*debye,ipol=1,3)
+  WRITE( stdout, '( 5x,"Dipole moment:")')
+  WRITE( stdout, '( 5x,"Elect",3f8.4," au,  ", 3f8.4," Debye")') &
+       (-dipol_el(ipol),ipol=1,3), (-dipol_el(ipol)*debye, ipol=1,3)
+  WRITE( stdout, '( 5x,"Ionic",3f8.4," au,  ", 3f8.4," Debye")') &
+       ( dipol_ion(ipol),ipol=1,3), (dipol_ion(ipol)*debye,ipol=1,3)
+  WRITE( stdout, '( 5x,"Total",3f8.4," au,  ", 3f8.4," Debye")') &
+       (-dipol(ipol),ipol=1,3),     (-dipol(ipol)*debye,   ipol=1,3)
   !
   ! print the Makov-Payne correction
   !
@@ -319,19 +306,17 @@ subroutine write_dipol(dipol_el,quadrupol_el,tau,nat,alat,zv, &
   WRITE( stdout, '(//8x,"*********    MAKOV-PAYNE CORRECTION    ********")')
   WRITE(stdout,'(/4x,"Makov-Payne correction with Madelung constant = ",f8.4)')&
        Madelung(ibrav)
-  WRITE( stdout, '(/,"Warning: results are meaningless if the cluster is &
-       & not centered within the 3d box.")')
   WRITE( stdout, '(/4x,"Electron charge: ",f15.8," el.")') -dipol_el(0)
-  WRITE( stdout, '(4x,"   Ionic charge: ",f15.8," el.")') zvtot
-  WRITE( stdout, '(4x,"   Total charge: ",f15.8," el.")') qq
+  WRITE( stdout, '( 4x,"   Ionic charge: ",f15.8," el.")') zvtot
+  WRITE( stdout, '( 4x,"   Total charge: ",f15.8," el.")') qq
   !
   ! print the electronic,ionic and total quadrupol moment
   !
   WRITE( stdout, '(/4x,"Electrons quadrupole moment",f20.8," a.u.")')  &
        -quadrupol_el
-  WRITE( stdout, '(4x,"     Ions quadrupole moment",f20.8," a.u.")') &
+  WRITE( stdout, '( 4x,"     Ions quadrupole moment",f20.8," a.u.")') &
        quadrupol_ion
-  WRITE( stdout,'(4x,"    Total quadrupole moment",f20.8," a.u.")') &
+  WRITE( stdout, '( 4x,"    Total quadrupole moment",f20.8," a.u.")') &
        quadrupol
   !
   !  print the results
