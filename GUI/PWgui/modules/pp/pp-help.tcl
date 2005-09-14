7eb89d94fb01f007abc3fcede28b6f6863c4b16a
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2005-09-14 17:24:01 +0000

    GUI updated to reflect new input of pp.x - chdens.x input removed, documentation (marginally) updated
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@2179 c92efa57-630b-4861-b058-cf58834340f0

--- GUI/PWgui/modules/pp/pp-help.tcl
+++ GUI/PWgui/modules/pp/pp-help.tcl
@@ -8,7 +8,8 @@ help  outdir -vartype character -helpfmt txt2html -helptext {
 }
   
 help  filplot  -vartype character -helpfmt txt2html -helptext {
-     punch file, contains the quantity selected by plot_num
+    file "filplot" contains the quantity selected by plot_num
+    (can be saved for further processing)
 }
 
 help  plot_num -vartype integer -helpfmt txt2html -helptext {
@@ -84,3 +85,127 @@ set _e  {
 foreach var {emin emax} {
     help $var -vartype real -helpfmt txt2html -helptext $_e
 }
+
+help nfile -vartype integer -helpfmt txt2html -helptext {
+            the number of data files (OPTIONAL)
+<p> ( default = 1 )
+}
+
+help filepp -vartype character -helpfmt txt2html -helptext {
+    nfile=1: file containing the quantity to be plotted
+    nfile>1: see "weight"
+    (default: filepp(1)=filplot)
+}
+help weight -vartype real -helpfmt txt2html -helptext {
+            Weighing factors: assuming that rho(i) is the quantity read
+            from filepp(i), the quantity that will be plotted is:
+            weight(1)*rho(1) + weight(2)*rho(2) + weight(3)*rho(3)+...
+            (OPTIONAL: default weight(1)=1.0)
+
+            BEWARE: atomic coordinates are read from the first file;
+            if their number is different for different files,
+            the first file must have the largest number of atoms
+<p> (  default : weight(1) = 1.0)
+}
+
+
+help iflag -vartype integer -helpfmt txt2html -helptext {
+          1 1D plot of spherical average
+          1 if a 1D plot is required (DEFAULT)
+          2 if a 2D plot is required
+          3 if a 3D plot is required
+          4 if a 2D polar plot on a sphere is required
+<p> ( default = 1 )
+}
+
+help output_format -vartype integer -helpfmt txt2html -helptext {
+         (ignored on 1D plot)
+
+          0  format suitable for gnuplot   (1D) (DEFAULT)
+          1  format suitable for contour.x (2D)
+          2  format suitable for plotrho   (2D)
+          3  format suitable for XCRYSDEN  (1D, 2D, 3D)
+          4  format suitable for gOpenMol  (3D)
+             (formatted: convert to unformatted *.plt)
+          5  format suitable for XCRYSDEN  (3D)
+          6  Gaussian cube-file format (3D)
+             (can be read by many programs)
+<p> ( default = 0 )
+}
+
+set _volume {
+  IF iflag = 1
+                    REQUIRED:
+                e1  3D vector which determines the plotting line
+                x0  3D vector, origin of the line
+                nx  number of points in the line:
+                    rho(i) = rho( x0 + e1 * (i-1)/(nx-1) ), i=1, nx
+  
+  ELSEIF iflag = 2
+                    REQUIRED:
+            e1, e2  3D vectors which determine the plotting plane
+                    (must be orthogonal)
+                x0  3D vector, origin of the plane
+            nx, ny  number of points in the plane:
+                    rho(i,j) = rho( x0 + e1 * (i-1)/(nx-1)
+                                       + e2 * (j-1)/(ny-1) ), i=1,nx ; j=1,ny
+  
+  ELSEIF iflag = 3
+                    OPTIONAL:
+        e1, e2, e3  3D vectors which determine the plotting parallelepiped
+                    (if present, must be orthogonal)
+                x0  3D vector, origin of the parallelepiped
+          nx,ny,nz  number of points in the parallelepiped:
+                    rho(i,j,k) = rho( x0 + e1 * (i-1)/(nx-1)
+                                         + e2 * (j-1)/(ny-1)
+                                         + e3 * (k-1)/(nz-1) ),
+                                 i = 1, nx ; j = 1, ny ; k = 1, nz
+  
+                  - If output_format = 3 (XCRYSDEN), the above variables
+                    are used to determine the grid to plot. 
+                  - If output_format = 5 (XCRYSDEN), the above variables
+                    are ignored, the entire FFT grid is written in the
+                    XCRYSDEN format - works for any crystal axis (VERY FAST)
+                  - If e1, e2, e3, x0 are present, e1 e2 e3 are parallel
+                    to xyz and parallel to crystal axis, a subset of the
+                    FFT grid that approximately covers the parallelepiped
+                    defined by e1, e2, e3, x0, is written (presently only
+                    in gopenmol "formatted" file format) - works only
+                    if the crystal axis are parallel to xyz
+                  - Otherwise, the required 3D grid is generated from the
+                    Fourier components (may be VERY slow)
+}
+
+set _e {
+     3D vectors which determine the plotting line (e1), plane (e1,e2), or 
+     parallelepiped (e1, e2, e3)
+}
+foreach e {e1 e2 e3} {
+    help $e -vartype real -helpfmt txt2html -helptext "$_e\n\n$_volume"
+}
+
+set _x {
+    3D vector, origin of either line (1D plot), plane (2D plot), 
+    or parallelepiped (3D plot)
+}
+help x0 -vartype real -helpfmt txt2html -helptext "$_x\n\n$_volume"
+
+set _n {
+    number of points for line (nx), plane (nx,ny), or parallelepiped (nx,ny,nz)
+}
+foreach n {nx ny nz} {
+    help $n -vartype real -helpfmt txt2html -helptext "$_n\n\n$_volume"
+}
+
+help radius -vartype real -helpfmt txt2html -helptext {
+    radius    Radius of the sphere (alat units), centered at (0,0,0)
+    nx, ny    number of points in the polar plane:
+                      phi(i)  = 2 pi * (i - 1)/(nx-1), i=1, nx
+                      theta(j)=   pi * (j - 1)/(ny-1), j=1, ny
+}
+
+help fileout -vartype character -helpfmt txt2html -helptext {  
+    name of the file to which the plot is written
+<p> ( default = standard output )
+}
+
--- GUI/PWgui/modules/pp/pp.tcl
+++ GUI/PWgui/modules/pp/pp.tcl
@@ -4,113 +4,222 @@ module PP\#auto -title "PWSCF GUI: module PP.x" -script {
 
     readfilter  ::pwscf::ppReadFilter
 
-    namelist inputpp -name "INPUTPP" {
+    page extract -name "Choose Data to be plotted " {
+	namelist inputpp -name "INPUTPP" {
+	    
+	    var prefix \
+		-label    "Prefix of punch file saved by program PW.X (prefix):" \
+		-widget [list entrybutton "Prefix ..." [list ::pwscf::selectFileRoot $this prefix]] \
+		-fmt      %S
 
-	var prefix \
-	    -label    "Prefix of punch file saved by program PW.X (prefix):" \
-	    -widget [list entrybutton "Prefix ..." [list ::pwscf::selectFileRoot $this prefix]] \
-	    -fmt      %S
-
-	var outdir {
-	    -label    "Temporary directory where PW.X files resides (outdir):"
-	    -widget   entrydirselectquote
-	    -fmt      %S
-	}
-	var filplot {
-	    -label    "Output file that will contain the calculated quantity (filplot):"
-	    -fmt      %S
-	}
-	var plot_num {
-	    -label    "What to calculate (plot_num):"
-	    -widget   radiobox
-	    -textvalue {
-		"charge density"
-		"total potential (= V_bare + V_H + V_xc)"
-		"local ionic potential"
-		"local density of states at E_fermi" 
-		"local density of electronic entropy"
-		"STM images"
-		"spin polarization (= rho(up) - rho(down))"
-		"|psi|^2"
-		"electron localization function (ELF)"
-		"planar average of all |psi|^2"
-		"integrated local density of states (ILDOS)"
-		"the V_bare + V_H potential"
-		"the electric field potential"
-		"the noncolinear magnetization"
+	    var outdir {
+		-label    "Temporary directory where PW.X files resides (outdir):"
+		-widget   entrydirselectquote
+		-fmt      %S
 	    }
-	    -value { 0 1 2 3 4 5 6 7 8 9 10 11 12 13 }
-	    -fmt %d
-	}
-	var spin_component {
-	    -label    "Charge/potential/magnetization spin component (spin_component):"
-	    -widget   optionmenu
-	    -textvalue {
-		"total charge/potential"
-		"spin up charge/potential"
-		"spin down charge/potential"
-		"absolute value"
-		"x component of the magnetization"
-		"y component of the magnetization"
-		"z component of the magnetization"
+	    var filplot {
+		-label    "Output file that will contain the calculated quantity (filplot):"
+		-fmt      %S
 	    }
-	    -value { 0 1 2  0 1 2 3 }
-	}	
-
-	group stm -name "STM" {
-	    var sample_bias {
-		-label    "For STM: the bias of the sample [in Ryd] in STM images (sample_bias):"
-		-validate fortranreal
+	    var plot_num {
+		-label    "What to calculate (plot_num):"
+		-widget   radiobox
+		-textvalue {
+		    "charge density"
+		    "total potential (= V_bare + V_H + V_xc)"
+		    "local ionic potential"
+		    "local density of states at E_fermi" 
+		    "local density of electronic entropy"
+		    "STM images"
+		    "spin polarization (= rho(up) - rho(down))"
+		    "|psi|^2"
+		    "electron localization function (ELF)"
+		    "planar average of all |psi|^2"
+		    "integrated local density of states (ILDOS)"
+		    "the V_bare + V_H potential"
+		    "the electric field potential"
+		    "the noncolinear magnetization"
+		}
+		-value { 0 1 2 3 4 5 6 7 8 9 10 11 12 13 }
+		-fmt %d
 	    }
-	    var stm_wfc_matching {
-		-label     "For STM: wave-function matching (stm_wfc_matching):"
-		-widget    radiobox
-		-textvalue {Yes No}
-		-value     {.true. .false.}
-	    }
-	    var z {
-		-label    "For STM: height of matching [in celldm(3) units] (z):"
-		-validate fortranreal
-	    }
-	    var dz {
-		-label    "For STM: distance of next STM image calculation (dz):"
-		-validate fortranreal
-	    }
-	}
-
-	group psi2 -name "Psi2" {
-	    var kpoint {
-		-label    "For |psi^2|: which k-point (kpoint):"
-		-widget    spinint
-		-validate  posint
-		-fmt       %d
+	    var spin_component {
+		-label    "Charge/potential/magnetization spin component (spin_component):"
+		-widget   optionmenu
+		-textvalue {
+		    "total charge/potential"
+		    "spin up charge/potential"
+		    "spin down charge/potential"
+		    "absolute value"
+		    "x component of the magnetization"
+		    "y component of the magnetization"
+		    "z component of the magnetization"
+		}
+		-value { 0 1 2  0 1 2 3 }
 	    }	
-	    var kband {
-		-label    "For |psi^2|: which band (kband):"
-		-widget    spinint
-		-validate  posint
-		-fmt       %d
+	    
+	    group stm -name "STM" {
+		var sample_bias {
+		    -label    "For STM: the bias of the sample [in Ryd] in STM images (sample_bias):"
+		    -validate fortranreal
+		}
+		var stm_wfc_matching {
+		    -label     "For STM: wave-function matching (stm_wfc_matching):"
+		    -widget    radiobox
+		    -textvalue {Yes No}
+		    -value     {.true. .false.}
+		}
+		var z {
+		    -label    "For STM: height of matching [in celldm(3) units] (z):"
+		    -validate fortranreal
+		}
+		var dz {
+		    -label    "For STM: distance of next STM image calculation (dz):"
+		    -validate fortranreal
+		}
 	    }
-	    var lsign {
-		-label    "For |psi^2| & Gamma: save the sign(psi) (lsign):"
-		-widget    radiobox
-		-textvalue {Yes No}
-		-value     {.true. .false.}
+	    
+	    group psi2 -name "Psi2" {
+		var kpoint {
+		    -label    "For |psi^2|: which k-point (kpoint):"
+		    -widget    spinint
+		    -validate  posint
+		    -fmt       %d
+		}	
+		var kband {
+		    -label    "For |psi^2|: which band (kband):"
+		    -widget    spinint
+		    -validate  posint
+		    -fmt       %d
+		}
+		var lsign {
+		    -label    "For |psi^2| & Gamma: save the sign(psi) (lsign):"
+		    -widget    radiobox
+		    -textvalue {Yes No}
+		    -value     {.true. .false.}
+		}
 	    }
-	}
-
-	group ildos -name "ILDOS" {
-	    var emin {
-		-label    "For ILDOS: miminum energy [in eV] (emin):"
-		-validate  fortranreal
-	    }	
-	    var emax {
-		-label    "FOR ILDOS: maximum energy [in eV] (emax):"
-		-validate  fortranreal
-	    }	
+	    
+	    group ildos -name "ILDOS" {
+		var emin {
+		    -label    "For ILDOS: miminum energy [in eV] (emin):"
+		    -validate  fortranreal
+		}	
+		var emax {
+		    -label    "FOR ILDOS: maximum energy [in eV] (emax):"
+		    -validate  fortranreal
+		}	
+	    }
 	}
     }
+    page chdens -name "Specify Plot " {
 
+	namelist plot -name "PLOT" {
+	    
+	    var nfile {
+		-label    "Number of data files (nfile):"
+		-widget   spinint
+		-validate posint
+		-default  1
+	    }
+	    
+	    dimension filepp {
+		-label    "Name of the data file"
+		-start    1
+		-end      1
+		-widget   entryfileselectquote
+	    }
+	    
+	    dimension weight {
+		-label    "Weight of the charge"
+		-start    1
+		-end      1
+		-widget   entry
+		-validate fortranreal
+		-default  1.0
+	    }
+	    
+	    separator -label "--- Plot info ---"
+	    
+	    var fileout -label "Name of output file (fileout):"
+	    
+	    var iflag {
+		-label     "Dimensionality of plot (iflag):"
+		-textvalue {
+		    "1D plot, spherical average"
+		    "1D plot"
+		    "2D plot"
+		    "3D plot"
+		    "2D polar plot"
+		}
+		-value  { 0 1 2 3 4 }
+		-widget optionmenu
+	    }
+	    
+	    var output_format {
+		-label     "Format of the output (output_format):"
+		-textvalue {
+		    "XCRYSDEN's XSF format"
+		    "XCRYSDEN's XSF format (whole unit cell)"
+		    "format suitable for gnuplot"
+		    "format suitable for contour.x"
+		    "format suitable for plotrho"
+		    "format suitable for gOpenMol"
+		    "Gaussian cube-file format"
+		}
+		-value     { 3 5 0 1 2 4 6 }
+		-widget    optionmenu
+	    }
+	    
+	    separator -label "--- Spanning vectors & origin ---"
+	    
+	    dimension e1 {
+		-label    "1st spanning vector:"
+		-validate fortranreal
+		-start    1
+		-end      3
+		-pack     left
+	    }
+	    
+	    dimension e2 {
+		-label    "2nd spanning vector"
+		-validate fortranreal
+		-start    1
+		-end      3
+		-pack     left
+	    }
+	    
+	    dimension e3 {
+		-label    "3rd spanning vector"
+		-validate fortranreal
+		-start    1
+		-end      3
+		-pack     left
+	    }
+	    
+	    dimension x0 {
+		-label    "Origin of the plot"
+		-validate fortranreal
+		-start    1
+		-end      3
+		-pack     left
+	    }
+	    
+	    
+	    separator -label "--- Number of points in each direction ---" 
+	    
+	    group nxnynz -name nxnynz {
+		packwidgets left
+		var nx -label "nx:" -validate posint -widget spinint
+		var ny -label "ny:" -validate posint -widget spinint
+		var nz -label "nz:" -validate posint -widget spinint
+	    }
+	    
+	    separator -label "--- Polar plot ---"
+	    var radius -label "Radius of the sphere (radius):" -validate real
+	    
+	}
+    }
     # ----------------------------------------------------------------------
     # take care of specialties
     # ----------------------------------------------------------------------
