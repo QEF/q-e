ecf83b9f0d70b3f47f9a978755bc51c04d09e037
silviu <silviu@c92efa57-630b-4861-b058-cf58834340f0>
2005-11-24 23:28:55 +0000

    more updates to i/o path. [silviu]
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@2517 c92efa57-630b-4861-b058-cf58834340f0

--- VIB/read_input_vib.f90
+++ VIB/read_input_vib.f90
@@ -41,7 +41,8 @@ SUBROUTINE read_input_vib()
      ! Set input file name
      !
      dirlen         = INDEX(outdir,' ') - 1
-     input_filename = TRIM(prefix)//'.vib.inp'
+     input_filename = './' // TRIM(prefix) // '.vib.inp'
+     input_filename = outdir(1:dirlen) // '/' // input_filename
      WRITE (stdout,*) 'Input information is read from:', input_filename
      !
      ! ... Open input file
