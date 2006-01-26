d2513b2a6fbffde38bb322e350780b0ac256ecee
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2006-01-26 13:08:28 +0000

    Cleanup of preprocessing stuff in include files, documentation updated Who knows what the __GNU_LINK stuff was used for?
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@2731 c92efa57-630b-4861-b058-cf58834340f0

--- include/c_defs.h
+++ include/c_defs.h
@@ -6,159 +6,26 @@
   or http://www.gnu.org/copyleft/gpl.txt .
 */
 
+/* Machine-dependent Fortran to C calling convention
+   redefine C symbols so that Fortran finds them
+   Absoft:
+      capital letters, no added underscores (leave as is)
+   XLF (Aix, Mac OS-X), HP-UX:
+      lowercase, with no added underscores 
+   G95, Alpha Linux:
+      lowercase, with one added underscore if the name does
+      not contain underscores, with two if it does!
+   Most other cases: 
+      lowercase, with one added underscore 
+*/
 
-#if defined __T3E || defined __ABSOFT
+#if !defined (__ABSOFT)
 
-#  define FFTW_INPLACE_DRV_1D FFTW_INPLACE_DRV_1D
-#  define FFTW_INPLACE_DRV_2D FFTW_INPLACE_DRV_2D
-#  define FFTW_INPLACE_DRV_3D FFTW_INPLACE_DRV_3D
-#  define CREATE_PLAN_1D CREATE_PLAN_1D
-#  define CREATE_PLAN_2D CREATE_PLAN_2D
-#  define CREATE_PLAN_3D CREATE_PLAN_3D
-#  define DESTROY_PLAN_1D DESTROY_PLAN_1D
-#  define DESTROY_PLAN_2D DESTROY_PLAN_2D
-#  define DESTROY_PLAN_3D DESTROY_PLAN_3D
-#  define FFT_X_STICK FFT_X_STICK
-#  define FFT_Y_STICK FFT_Y_STICK
-#  define FFT_Z_STICK FFT_Z_STICK
-#  define CPFLUSH CPFLUSH
-#  define ELAPSED_SECONDS ELAPSED_SECONDS
-#  define CCLOCK CCLOCK
-#  define LN_ALLOC LN_ALLOC 
-#  define LN_DEALLOC LN_DEALLOC
-#  define LN_SET LN_SET
-#  define LN_ACTIVATE LN_ACTIVATE
-#  define LN_IND LN_IND
-#  define MEMSTAT MEMSTAT
-#  define C_MKDIR C_MKDIR
+/* Absoft: do nothing */
 
-#endif
+#  if defined (__AIX) || defined (__HP) || defined (__MAC)
 
-#if defined __SGI || defined __FUJITSU || defined __SX4 || defined __INTEL || defined __LAHEY || defined __SX6 || defined __SUN || defined __ALTIX
-
-#  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d_
-#  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d_
-#  define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d_
-#  define CREATE_PLAN_1D create_plan_1d_
-#  define CREATE_PLAN_2D create_plan_2d_
-#  define CREATE_PLAN_3D create_plan_3d_
-#  define DESTROY_PLAN_1D destroy_plan_1d_
-#  define DESTROY_PLAN_2D destroy_plan_2d_
-#  define DESTROY_PLAN_3D destroy_plan_3d_
-#  define FFT_X_STICK fft_x_stick_
-#  define FFT_Y_STICK fft_y_stick_
-#  define FFT_Z_STICK fft_z_stick_
-#  define CPFLUSH cpflush_
-#  define ELAPSED_SECONDS elapsed_seconds_
-#  define CCLOCK cclock_
-#  define LN_ALLOC ln_alloc_ 
-#  define LN_DEALLOC ln_dealloc_
-#  define LN_SET ln_set_
-#  define LN_ACTIVATE ln_activate_
-#  define LN_IND ln_ind_
-#  define MEMSTAT memstat_
-#  define C_MKDIR c_mkdir_
-
-#endif
-
-#if defined __PGI
-
-#  if defined __GNU_LINK
-
-#  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d__
-#  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d__
-#  define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d__
-#  define CREATE_PLAN_1D create_plan_1d__
-#  define CREATE_PLAN_2D create_plan_2d__
-#  define CREATE_PLAN_3D create_plan_3d__
-#  define DESTROY_PLAN_1D destroy_plan_1d__
-#  define DESTROY_PLAN_2D destroy_plan_2d__
-#  define DESTROY_PLAN_3D destroy_plan_3d__
-#  define FFT_X_STICK fft_x_stick__
-#  define FFT_Y_STICK fft_y_stick__
-#  define FFT_Z_STICK fft_z_stick__
-#  define CPFLUSH cpflush_
-#  define ELAPSED_SECONDS elapsed_seconds_
-#  define CCLOCK cclock_
-#  define LN_ALLOC ln_alloc__
-#  define LN_DEALLOC ln_dealloc__
-#  define LN_SET ln_set__
-#  define LN_ACTIVATE ln_activate__
-#  define LN_IND ln_ind__
-#  define MEMSTAT memstat_
-#  define C_MKDIR c_mkdir_
-
-#  else
-
-#  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d_
-#  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d_
-#  define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d_
-#  define CREATE_PLAN_1D create_plan_1d_
-#  define CREATE_PLAN_2D create_plan_2d_
-#  define CREATE_PLAN_3D create_plan_3d_
-#  define DESTROY_PLAN_1D destroy_plan_1d_
-#  define DESTROY_PLAN_2D destroy_plan_2d_
-#  define DESTROY_PLAN_3D destroy_plan_3d_
-#  define FFT_X_STICK fft_x_stick_
-#  define FFT_Y_STICK fft_y_stick_
-#  define FFT_Z_STICK fft_z_stick_
-#  define CPFLUSH cpflush_
-#  define ELAPSED_SECONDS elapsed_seconds_
-#  define CCLOCK cclock_
-#  define LN_ALLOC ln_alloc_
-#  define LN_DEALLOC ln_dealloc_
-#  define LN_SET ln_set_
-#  define LN_ACTIVATE ln_activate_
-#  define LN_IND ln_ind_
-#  define MEMSTAT memstat_
-#  define C_MKDIR c_mkdir_
-
-#  endif
-
-#endif
-
-#if defined __G95
-
-#  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d__
-#  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d__
-#  define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d__
-#  define CREATE_PLAN_1D create_plan_1d__
-#  define CREATE_PLAN_2D create_plan_2d__
-#  define CREATE_PLAN_3D create_plan_3d__
-#  define DESTROY_PLAN_1D destroy_plan_1d__
-#  define DESTROY_PLAN_2D destroy_plan_2d__
-#  define DESTROY_PLAN_3D destroy_plan_3d__
-#  define FFT_X_STICK fft_x_stick__
-#  define FFT_XY_STICK fft_xy_stick__
-#  define FFT_XY fft_xy__
-#  define FFT_Y_STICK fft_y_stick__
-#  define FFT_Y_STICK2 fft_y_stick2__
-#  define FFT_Z_STICK fft_z_stick__
-#  define FFT_Z fft_z__
-#  define FFT_STICK fft_stick__
-#  define CP_GETENV cp_getenv__
-#  define CP_DATE cp_date__
-#  define CPFLUSH cpflush_
-#  define CPTIMER cptimer__
-#  define ELAPSED_SECONDS elapsed_seconds__
-#  define CCLOCK cclock_
-#  define FACTOR235 factor235__
-#  define FACTOR2 factor2__
-#  define LN_ALLOC ln_alloc__
-#  define LN_DEALLOC ln_dealloc__
-#  define LN_SET ln_set__
-#  define LN_ACTIVATE ln_activate__
-#  define LN_IND ln_ind__
-#  define MEMSTAT memstat_
-#  define READOCC readocc_
-#  define ROUND2 round2_
-#  define MYUNITNAME myunitname_
-#  define CP_ITOA cp_itoa_
-#  define C_MKDIR c_mkdir__
-
-#endif
-
-#if defined __AIX || defined __HP || defined __MAC
+/* convert to lowercase */
 
 #  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d
 #  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d
@@ -172,70 +39,68 @@
 #  define FFT_X_STICK fft_x_stick
 #  define FFT_Y_STICK fft_y_stick
 #  define FFT_Z_STICK fft_z_stick
-#  define CPFLUSH cpflush
-#  define ELAPSED_SECONDS elapsed_seconds
-#  define CCLOCK cclock
 #  define LN_ALLOC ln_alloc 
 #  define LN_DEALLOC ln_dealloc
 #  define LN_SET ln_set
 #  define LN_ACTIVATE ln_activate
 #  define LN_IND ln_ind
-#  define MEMSTAT memstat
+#  define CCLOCK cclock
+#  define ELAPSED_SECONDS elapsed_seconds
 #  define C_MKDIR c_mkdir
+#  define MEMSTAT memstat
 
-#endif
+#  elif defined (__G95) || (defined __ALPHA && defined __LINUX64)
 
+/* convert to lowercase, add one or two underscores */
 
-#if defined __ALPHA && !defined __LINUX64
+#  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d__
+#  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d__
+#  define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d__
+#  define CREATE_PLAN_1D create_plan_1d__
+#  define CREATE_PLAN_2D create_plan_2d__
+#  define CREATE_PLAN_3D create_plan_3d__
+#  define DESTROY_PLAN_1D destroy_plan_1d__
+#  define DESTROY_PLAN_2D destroy_plan_2d__
+#  define DESTROY_PLAN_3D destroy_plan_3d__
+#  define FFT_X_STICK fft_x_stick__
+#  define FFT_Y_STICK fft_y_stick__
+#  define FFT_Z_STICK fft_z_stick__
+#  define LN_ALLOC ln_alloc__
+#  define LN_DEALLOC ln_dealloc__
+#  define LN_SET ln_set__
+#  define LN_ACTIVATE ln_activate__
+#  define LN_IND ln_ind__
+#  define CCLOCK cclock_
+#  define ELAPSED_SECONDS elapsed_seconds__
+#  define C_MKDIR c_mkdir__
+#  define MEMSTAT memstat_
+
+#  else
+
+/* convert to lowercase, add one underscore */
 
 #  define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d_
 #  define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d_
 #  define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d_
+#  define CREATE_PLAN_1D create_plan_1d_
 #  define CREATE_PLAN_2D create_plan_2d_
 #  define CREATE_PLAN_3D create_plan_3d_
-#  define CREATE_PLAN_1D create_plan_1d_
 #  define DESTROY_PLAN_1D destroy_plan_1d_
 #  define DESTROY_PLAN_2D destroy_plan_2d_
 #  define DESTROY_PLAN_3D destroy_plan_3d_
 #  define FFT_X_STICK fft_x_stick_
 #  define FFT_Y_STICK fft_y_stick_
 #  define FFT_Z_STICK fft_z_stick_
-#  define CPFLUSH cpflush_
-#  define ELAPSED_SECONDS elapsed_seconds_
-#  define CCLOCK cclock_
-#  define LN_ALLOC ln_alloc_
+#  define LN_ALLOC ln_alloc_ 
 #  define LN_DEALLOC ln_dealloc_
 #  define LN_SET ln_set_
 #  define LN_ACTIVATE ln_activate_
 #  define LN_IND ln_ind_
-#  define MEMSTAT memstat_
+#  define CCLOCK cclock_
+#  define ELAPSED_SECONDS elapsed_seconds_
 #  define C_MKDIR c_mkdir_
+#  define MEMSTAT memstat_
 
-#endif
-
-#if defined __ALPHA && defined __LINUX64
-
-#    define FFTW_INPLACE_DRV_1D fftw_inplace_drv_1d__
-#    define FFTW_INPLACE_DRV_2D fftw_inplace_drv_2d__
-#    define FFTW_INPLACE_DRV_3D fftw_inplace_drv_3d__
-#    define CREATE_PLAN_1D create_plan_1d__
-#    define DESTROY_PLAN_1D destroy_plan_1d__
-#    define CREATE_PLAN_2D create_plan_2d__
-#    define CREATE_PLAN_3D create_plan_3d__
-#    define DESTROY_PLAN_2D destroy_plan_2d__
-#    define DESTROY_PLAN_3D destroy_plan_3d__
-#    define FFT_X_STICK fft_x_stick__
-#    define FFT_Y_STICK fft_y_stick__
-#    define FFT_Z_STICK fft_z_stick__
-#    define CPFLUSH cpflush_
-#    define ELAPSED_SECONDS elapsed_seconds__
-#    define CCLOCK cclock_
-#    define LN_ALLOC ln_alloc__
-#    define LN_DEALLOC ln_dealloc__
-#    define LN_SET ln_set__
-#    define LN_ACTIVATE ln_activate__
-#    define LN_IND ln_ind__
-#    define MEMSTAT memstat_
-#    define C_MKDIR c_mkdir__
+#  endif
 
 #endif
--- include/f_defs.h
+++ include/f_defs.h
@@ -6,53 +6,30 @@
 !  or http://www.gnu.org/copyleft/gpl.txt .
 !
 
-
-#if defined(CRAYY) || defined(__LINUX) || defined(HITACHI) || defined(__SUN)
+#if defined(__AIX) || defined (__MAC64) || defined (FUJ64)|| defined (__ALPHA) || defined (__SX6) || defined(__LINUX64) || defined (__HP64) || defined (__SGI64)
+#  define C_POINTER  integer(kind=8)
+#else
 #  define C_POINTER  integer(kind=4)
 #endif
 
-#if defined(__AIX)
-#  define C_POINTER  integer(kind=8)
-#endif
-
-#if defined __MAC
-#  if defined __MAC64
-#    define C_POINTER  integer(kind=8)
-#  else
-#    define C_POINTER  integer(kind=4)
-#  endif
-#endif
-
-#if defined (FUJ64)|| defined (__ALPHA) || defined (__SX6) || defined(__LINUX64) || defined(__T3E)
-#  define C_POINTER  integer(kind=8)
-#endif
-
-#if defined __HP
-#  if defined __HP64
-#    define C_POINTER  integer(kind=8)
-#  else
-#    define C_POINTER  integer(kind=4)
-#  endif
-#endif
-
-#if defined __SGI || defined __ORIGIN
-#  if defined __SGI64
-#    define C_POINTER  integer(kind=8)
-#  else
-#    define C_POINTER  integer(kind=4)
-#  endif
-#endif
-
-#if defined(__SX4)
-#  define C_POINTER  real(kind=8)
-#endif
-
-#if defined(__SX4)
-#  define DIRECT_IO_FACTOR 1
-#elif defined(__ALPHA)
+#if defined(__ALPHA)
 #  define DIRECT_IO_FACTOR 2
 #else
 #  define DIRECT_IO_FACTOR 8 
+#endif
+
+#if defined (__AIX) || defined (__MAC) || defined(__ABSOFT)
+#  define flush  flush_
+#endif
+
+#if defined(__ABSOFT)
+#  define getenv getenv_
+#  define getarg getarg_
+#  define iargc  iargc_
+#endif
+
+#if defined(__LAM) && ( defined (__LINUX) || defined (__LINUX64) )
+#  define MPI_REAL8 MPI_DOUBLE_PRECISION
 #endif
 
 #  define DREAL       @@_use_DBLE_instead@@
@@ -60,248 +37,132 @@
 #  define DCMPLX      @@_use_CMPLX_instead@@
 #  define CMPLX(a,b)  cmplx(a,b,kind=DP)
 
-#if defined(CRAYY) || defined (__SX4) || defined (__T3E)
+#if defined(ADD_BLAS_TWO_UNDERSCORES)
 
-#  define DAXPY       saxpy
-#  define DCOPY       scopy
-#  define DDOT        sdot
-#  define DGETRF      sgetrf
-#  define DGETRI      sgetri
-#  define DGEMV       sgemv
-#  define DGEMM       sgemm
-#  define DNRM2       snrm2
-#  define DSCAL       sscal
-#  define DSPEV       sspev
-#  define DSYTRF      ssytrf
-#  define DSYTRI      ssytri
-#  define DSYEV       ssyev
-#  define DSYGV       ssygv
-#  define DSYGVX      ssygvx
-#  define DSWAP       sswap
-#  define ILAENV      ilaenv
-#  define ZAXPY       caxpy
-#  define ZCOPY       ccopy
-#  define ZDOTC       cdotc
-#  define ZDOTU       cdotu
-#  define ZGEMM       cgemm
-#  define ZGEMV       cgemv
-#  define ZGESV       cgesv
-#  define ZGESVD      cgesvd
-#  define ZGGEV       cggev
-#  define ZHEEV       cheev
-#  define ZHEEVX      cheevx
-#  define ZHEGV       chegv
-#  define ZHEGVX      chegvx
-#  define ZHETRD      CHETRD
-#  define ZHPEV       chpev
-#  define ZSCAL       cscal
-
-#  define IZAMAX   icamax
-#  define DYAX     syax
-#  define ZSWAP    cswap
-#  define ZDSCAL   csscal
-#  define IDAMAX   isamax
-#  define DGEMUL   sgemul
-#  define DGESUB   sgesub
-#  define DGER     sger
-#  define DGEMTX   sgemtx
-#  define DGEMX    sgemx
-
-#  define DLAMCH   slamch
-#  define DLAPY3   slapy3
-#  define DZNRM2   scnrm2
-#  define ZLADIV   cladiv
-#  define DLAE2    slae2
-#  define DLAEV2   slaev2
-#  define DLANST   slanst
-#  define DLAPY2   slapy2
-#  define DLARTG   slartg
-#  define DLASCL   slascl
-#  define DLASRT   slasrt
-#  define ZLASET   claset
-#  define ZLASR    clasr
-
-#  define DPOTRF   spotrf
-#  define DPOTRS   spotrs
-
-#else
-
-#  if defined(ADD_BLAS_TWO_UNDERSCORES)
-
-#    define DAXPY       daxpy__
-#    define DCOPY       dcopy__
-#    define DDOT        ddot__
-#    define DGETRF      dgetrf__
-#    define DGETRI      dgetri__
-#    define DGEMV       dgemv__
-#    define DGEMM       dgemm__
-#    define DNRM2       dnrm2__
-#    define DSCAL       dscal__
-#    define DSPEV       dspev__
-#    define DSYTRF      dsytrf__
-#    define DSYTRI      dsytri__
-#    define DSYEV       dsyev__
-#    define DSYGV       dsygv__
-#    define DSYGVX      dsygvx__
-#    define DSWAP       dswap__
-#    define ILAENV      ilaenv__
-#    define ZAXPY       zaxpy__
-#    define ZCOPY       zcopy__
-#    define ZDOTC       zdotc__
-#    define ZDOTU       zdotu__
-#    define ZGEMM       zgemm__
-#    define ZGEMV       zgemv__
-#    define ZGESV       zgesv__
-#    define ZGESVD      zgesvd__
-#    define ZGGEV       zggev__
-#    define ZHEEV       zheev__
-#    define ZHEEVX      zheevx__
-#    define ZHEGV       zhegv__
-#    define ZHEGVX      zhegvx__
-#    define ZHPEV       zhpev__
-#    define ZSCAL       zscal__
-
-#    define IZAMAX   izamax__
-#    define DYAX     dyax__
-#    define ZSWAP    zswap__
-#    define ZDSCAL   zdscal__
-#    define IDAMAX   idamax__
-#    define DGEMUL   dgemul__
-#    define DGESUB   dgesub__
+#    define DAXPY    daxpy__
+#    define DCOPY    dcopy__
+#    define DDOT     ddot__
+#    define DGETRF   dgetrf__
+#    define DGETRI   dgetri__
+#    define DGEMV    dgemv__
+#    define DGEMM    dgemm__
 #    define DGER     dger__
-#    define DGEMTX   dgemtx__
-#    define DGEMX    dgemx__
-
+#    define DNRM2    dnrm2__
 #    define DPOTRF   dpotrf__
 #    define DPOTRS   dpotrs__
+#    define DSCAL    dscal__
+#    define DSPEV    dspev__
+#    define DSYTRF   dsytrf__
+#    define DSYTRI   dsytri__
+#    define DSYEV    dsyev__
+#    define DSYGV    dsygv__
+#    define DSYGVX   dsygvx__
+#    define DSWAP    dswap__
+#    define ILAENV   ilaenv__
+#    define IDAMAX   idamax__
+#    define IZAMAX   izamax__
+#    define ZAXPY    zaxpy__
+#    define ZCOPY    zcopy__
+#    define ZDOTC    zdotc__
+#    define ZDOTU    zdotu__
+#    define ZGEMM    zgemm__
+#    define ZGEMV    zgemv__
+#    define ZGESV    zgesv__
+#    define ZGESVD   zgesvd__
+#    define ZGGEV    zggev__
+#    define ZHEEV    zheev__
+#    define ZHEEVX   zheevx__
+#    define ZHEGV    zhegv__
+#    define ZHEGVX   zhegvx__
+#    define ZHPEV    zhpev__
+#    define ZSCAL    zscal__
+#    define ZSWAP    zswap__
+#    define ZDSCAL   zdscal__
 
 #  elif defined(ADD_BLAS_ONE_UNDERSCORE)
 
-#    define DAXPY       daxpy_
-#    define DCOPY       dcopy_
-#    define DDOT        ddot_
-#    define DGETRF      dgetrf_
-#    define DGETRI      dgetri_
-#    define DGEMV       dgemv_
-#    define DGEMM       dgemm_
-#    define DNRM2       dnrm2_
-#    define DSCAL       dscal_
-#    define DSPEV       dspev_
-#    define DSYTRF      dsytrf_
-#    define DSYTRI      dsytri_
-#    define DSYEV       dsyev_
-#    define DSYGV       dsygv_
-#    define DSYGVX      dsygvx_
-#    define DSWAP       dswap_
-#    define ILAENV      ilaenv_
-#    define ZAXPY       zaxpy_
-#    define ZCOPY       zcopy_
-#    define ZDOTC       zdotc_
-#    define ZDOTU       zdotu_
-#    define ZGEMM       zgemm_
-#    define ZGEMV       zgemv_
-#    define ZGESV       zgesv_
-#    define ZGESVD      zgesvd_
-#    define ZGGEV       zggev_
-#    define ZHEEV       zheev_
-#    define ZHEEVX      zheevx_
-#    define ZHEGV       zhegv_
-#    define ZHEGVX      zhegvx_
-#    define ZHPEV       zhpev_
-#    define ZSCAL       zscal_
-
-#    define IZAMAX   izamax_
-#    define DYAX     dyax_
-#    define ZSWAP    zswap_
-#    define ZDSCAL   zdscal_
-#    define IDAMAX   idamax_
-#    define DGEMUL   dgemul_
-#    define DGESUB   dgesub_
+#    define DAXPY    daxpy_
+#    define DCOPY    dcopy_
+#    define DDOT     ddot_
+#    define DGETRF   dgetrf_
+#    define DGETRI   dgetri_
+#    define DGEMV    dgemv_
+#    define DGEMM    dgemm_
 #    define DGER     dger_
-#    define DGEMTX   dgemtx_
-#    define DGEMX    dgemx_
-
+#    define DNRM2    dnrm2_
 #    define DPOTRF   dpotrf_
 #    define DPOTRS   dpotrs_
+#    define DSCAL    dscal_
+#    define DSPEV    dspev_
+#    define DSYTRF   dsytrf_
+#    define DSYTRI   dsytri_
+#    define DSYEV    dsyev_
+#    define DSYGV    dsygv_
+#    define DSYGVX   dsygvx_
+#    define DSWAP    dswap_
+#    define ILAENV   ilaenv_
+#    define IDAMAX   idamax_
+#    define IZAMAX   izamax_
+#    define ZAXPY    zaxpy_
+#    define ZCOPY    zcopy_
+#    define ZDOTC    zdotc_
+#    define ZDOTU    zdotu_
+#    define ZGEMM    zgemm_
+#    define ZGEMV    zgemv_
+#    define ZGESV    zgesv_
+#    define ZGESVD   zgesvd_
+#    define ZGGEV    zggev_
+#    define ZHEEV    zheev_
+#    define ZHEEVX   zheevx_
+#    define ZHEGV    zhegv_
+#    define ZHEGVX   zhegvx_
+#    define ZHPEV    zhpev_
+#    define ZSCAL    zscal_
+#    define ZSWAP    zswap_
+#    define ZDSCAL   zdscal_
 
 #  else
 
-#    define DAXPY       daxpy
-#    define DCOPY       dcopy
-#    define DDOT        ddot
-#    define DGETRF      dgetrf
-#    define DGETRI      dgetri
-#    define DGEMV       dgemv
-#    define DGEMM       dgemm
-#    define DNRM2       dnrm2
-#    define DSCAL       dscal
-#    define DSPEV       dspev
-#    define DSYTRF      dsytrf
-#    define DSYTRI      dsytri
-#    define DSYEV       dsyev
-#    define DSYGV       dsygv
-#    define DSYGVX      dsygvx
-#    define DSWAP       dswap
-#    define ILAENV      ilaenv
-#    define ZAXPY       zaxpy
-#    define ZCOPY       zcopy
-#    define ZDOTC       zdotc
-#    define ZDOTU       zdotu
-#    define ZGEMM       zgemm
-#    define ZGEMV       zgemv
-#    define ZGESV       zgesv
-#    define ZGESVD      zgesvd
-#    define ZGGEV       zggev
-#    define ZHEEV       zheev
-#    define ZHEEVX      zheevx
-#    define ZHEGV       zhegv
-#    define ZHEGV       zhegv
-#    define ZHEGVX      zhegvx
-#    define ZHPEV       zhpev
-#    define ZSCAL       zscal
-
-#    define IZAMAX   izamax
-#    define DYAX     dyax
-#    define ZSWAP    zswap
-#    define ZDSCAL   zdscal
-#    define IDAMAX   idamax
-#    define DGEMUL   dgemul
-#    define DGESUB   dgesub
+#    define DAXPY    daxpy
+#    define DCOPY    dcopy
+#    define DDOT     ddot
+#    define DGETRF   dgetrf
+#    define DGETRI   dgetri
+#    define DGEMV    dgemv
+#    define DGEMM    dgemm
 #    define DGER     dger
-#    define DGEMTX   dgemtx
-#    define DGEMX    dgemx
-
+#    define DNRM2    dnrm2
 #    define DPOTRF   dpotrf
 #    define DPOTRS   dpotrs
+#    define DSCAL    dscal
+#    define DSPEV    dspev
+#    define DSYTRF   dsytrf
+#    define DSYTRI   dsytri
+#    define DSYEV    dsyev
+#    define DSYGV    dsygv
+#    define DSYGVX   dsygvx
+#    define DSWAP    dswap
+#    define ILAENV   ilaenv
+#    define IDAMAX   idamax
+#    define IZAMAX   izamax
+#    define ZAXPY    zaxpy
+#    define ZCOPY    zcopy
+#    define ZDOTC    zdotc
+#    define ZDOTU    zdotu
+#    define ZGEMM    zgemm
+#    define ZGEMV    zgemv
+#    define ZGESV    zgesv
+#    define ZGESVD   zgesvd
+#    define ZGGEV    zggev
+#    define ZHEEV    zheev
+#    define ZHEEVX   zheevx
+#    define ZHEGV    zhegv
+#    define ZHEGVX   zhegvx
+#    define ZHPEV    zhpev
+#    define ZSCAL    zscal
+#    define ZSWAP    zswap
+#    define ZDSCAL   zdscal
 
-#  endif
 
-#endif
-
-#if defined (__AIX) || defined (__MAC)
-#  define flush  flush_
-#endif
-
-#if defined(__ABSOFT)
-#  define flush  flush_
-#  define getenv getenv_
-#  define getarg getarg_
-#  define iargc  iargc_
-#endif
-
-#ifdef __T3E
-#  define iargc        ipxfargc
-#  define getarg(x,y)  pxfgetarg(x,y,  ilen,ierr)
-#  define getenv(x,y)  pxfgetenv(x,0,y,ilen,ierr)
-#endif
-
-#if defined(__BENCHLIB)
-#  define DCOPY       scopy_t3e
-#  define DGEMM       s_gemm
-#  define ZGEMM       c_gemm
-#endif
-
-#if defined(__LAM) && ( defined (__LINUX) || defined (__LINUX64) )
-#define MPI_REAL8 MPI_DOUBLE_PRECISION
 #endif
 
