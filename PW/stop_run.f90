831001d1b694c621f9ed0caf166ddfa09dbefa5c
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2006-03-09 13:49:13 +0000

    oops, punch has not to be called here at convergence. C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@2899 c92efa57-630b-4861-b058-cf58834340f0

--- PW/stop_run.f90
+++ PW/stop_run.f90
@@ -32,7 +32,7 @@ SUBROUTINE stop_run( flag )
   !
   ! ... here we write all the data required to restart
   !
-  CALL punch()
+  IF ( .NOT. flag ) CALL punch()
   !
   IF ( lpath ) CALL io_path_stop()
   !
