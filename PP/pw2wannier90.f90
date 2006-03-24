261b8d992bf014792c223f042cae3f081093990a
degironc <degironc@c92efa57-630b-4861-b058-cf58834340f0>
2006-03-24 16:14:34 +0000

    corrected (maybe correct) version of radial part
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@2957 c92efa57-630b-4861-b058-cf58834340f0

--- PP/pw2wannier90.f90
+++ PP/pw2wannier90.f90
@@ -1138,7 +1138,7 @@ subroutine radialpart(ng, q, alfa, rvalue, lmax, radial)
      do ig=1,ng
        call sph_bes (mesh_r, r(1), q(ig), l, bes)
        aux(:) = bes(ir) * func_r(:) * r(:)
-       call simpson (mesh_r, func_r, rij(1), rad_int)
+       call simpson (mesh_r, aux, rij(1), rad_int)
        radial(ig,l) = rad_int * pref
      enddo
   enddo
