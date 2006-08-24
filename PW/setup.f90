7a72e7836be494f35cf64d59bdfff45b9c80ed39
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2006-08-24 08:51:07 +0000

    Most of the spin-orbit-related code in setup goes to a separate subroutine. This should be a purely aestethic change with no side effects.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3334 c92efa57-630b-4861-b058-cf58834340f0

--- PW/setup.f90
+++ PW/setup.f90
@@ -40,7 +40,7 @@ SUBROUTINE setup()
   !
   USE kinds,              ONLY : DP
   USE constants,          ONLY : eps8
-  USE parameters,         ONLY : npsx, nchix, npk
+  USE parameters,         ONLY : npk
   USE io_global,          ONLY : stdout
   USE io_files,           ONLY : tmp_dir, prefix, delete_if_present
   USE constants,          ONLY : pi, degspin
@@ -60,7 +60,6 @@ SUBROUTINE setup()
   USE ktetra,             ONLY : nk1, nk2, nk3, k1, k2, k3, &
                                  tetra, ntetra, ltetra
   USE symme,              ONLY : s, t_rev, irt, ftau, nsym, invsym
-  USE atom,               ONLY : oc, chi, nchi, lchi, jchi, mesh
   USE pseud,              ONLY : zp
   USE wvfct,              ONLY : nbnd, nbndx, gamma_only
   USE control_flags,      ONLY : tr2, ethr, alpha0, beta0, lscf, lmd, lpath, &
@@ -68,7 +67,7 @@ SUBROUTINE setup()
                                  modenum, lbands
   USE relax,              ONLY : starting_diag_threshold
   USE cellmd,             ONLY : calc
-  USE uspp_param,         ONLY : psd, betar, nbeta, dion, jjj, lll, tvanp
+  USE uspp_param,         ONLY : psd, nbeta, jjj, tvanp
   USE uspp,               ONLY : okvan
   USE ldaU,               ONLY : d1, d2, d3, lda_plus_u, Hubbard_U, Hubbard_l, &
                                  Hubbard_alpha, Hubbard_lmax
@@ -100,16 +99,12 @@ SUBROUTINE setup()
       tipo,           &!
       is,             &!
       nb,             &!
-      nbe,            &!
-      ind, ind1,      &!
       ierr,           &!
-      l,              &!
       ibnd             !
   LOGICAL :: &
       minus_q,        &!
       ltest            !
   REAL(DP) :: &
-      vionl,          &!
       iocc             !
   INTEGER, EXTERNAL :: &
       n_atom_wfc,     &!
@@ -411,135 +406,11 @@ SUBROUTINE setup()
      !
   END DO
   !
-  IF ( .NOT. lspinorb ) THEN
-     !
-     DO nt = 1, ntyp
-        !
-        IF ( so(nt) ) THEN
-           !
-           IF ( tvanp(nt) ) &
-                CALL errore( 'setup', 'US j-average not yet implemented', 1 )
-           !
-           nbe = 0
-           !
-           DO nb = 1, nbeta(nt)
-              !
-              nbe = nbe + 1
-              !
-              IF ( lll(nb,nt) /= 0 .AND. &
-                   ABS( jjj(nb,nt) - lll(nb,nt) - 0.5D0 ) < 1.D-7 ) nbe = nbe - 1
-           END DO
-           !
-           nbeta(nt) = nbe
-           !
-           nbe = 0
-           !
-           DO nb = 1, nbeta(nt)
-              !
-              nbe = nbe + 1
-              !
-              l = lll(nbe,nt)
-              !
-              IF ( l /= 0 ) THEN
-                 !
-                 IF (ABS(jjj(nbe,nt)-lll(nbe,nt)+0.5d0).LT.1.d-7) THEN
-                    IF (ABS(jjj(nbe+1,nt)-lll(nbe+1,nt)-0.5d0).GT.1.d-7) &
-                       call errore('setup','wrong beta functions',1)
-                    ind=nbe+1
-                    ind1=nbe
-                 ELSE
-                    IF (ABS(jjj(nbe+1,nt)-lll(nbe+1,nt)+0.5d0).GT.1.d-7) &
-                       call errore('setup','wrong beta functions',1)
-                    ind=nbe
-                    ind1=nbe+1
-                 ENDIF
-                 !
-                 vionl = ( ( l + 1.D0 ) * dion(ind,ind,nt) + &
-                      l * dion(ind1,ind1,nt) ) / ( 2.D0 * l + 1.D0 )
-                 !
-                 betar(1:mesh(nt),nb,nt) = 1.D0 / ( 2.D0 * l + 1.D0 ) * &
-                      ( ( l + 1.D0 ) * SQRT( dion(ind,ind,nt) / vionl ) * &
-                      betar(1:mesh(nt),ind,nt) + &
-                      l * SQRT( dion(ind1,ind1,nt) / vionl ) * &
-                      betar(1:mesh(nt),ind1,nt) )
-                 !
-                 dion(nb,nb,nt) = vionl
-                 !
-                 nbe = nbe + 1
-                 !
-              ELSE
-                 !
-                 betar(1:mesh(nt),nb,nt) = betar(1:mesh(nt),nbe,nt)
-                 !
-                 dion(nb,nb,nt) = dion(nbe,nbe,nt)
-                 !
-              END IF
-              !
-              lll(nb,nt)=lll(nbe,nt)
-              !
-           END DO
-           !
-           nbe = 0
-           !
-           DO nb = 1, nchi(nt)
-              !
-              nbe = nbe + 1
-              !
-              IF ( lchi(nb,nt) /= 0 .AND. &
-                   ABS(jchi(nb,nt)-lchi(nb,nt)-0.5D0 ) < 1.D-7 ) nbe = nbe - 1
-              !
-           END DO
-           !
-           nchi(nt) = nbe
-           ! 
-           nbe = 0
-           !
-           do nb = 1, nchi(nt)
-              !
-              nbe = nbe + 1
-              !
-              l = lchi(nbe,nt)
-              !
-              IF ( l /= 0 ) THEN
-                 !
-                 IF (ABS(jchi(nbe,nt)-lchi(nbe,nt)+0.5d0).LT.1.d-7) THEN
-                    IF (ABS(jchi(nbe+1,nt)-lchi(nbe+1,nt)-0.5d0).GT.1.d-7) &
-                       call errore('setup','wrong chi functions',1)
-                    ind=nbe+1
-                    ind1=nbe
-                 ELSE
-                    IF (ABS(jchi(nbe+1,nt)-lchi(nbe+1,nt)+0.5d0).GT.1.d-7) &
-                       call errore('setup','wrong chi functions',1)
-                    ind=nbe
-                    ind1=nbe+1
-                 END IF
-                 !
-                 chi(1:mesh(nt),nb,nt)=((l+1.D0) * chi(1:mesh(nt),ind,nt)+ &
-                      l * chi(1:mesh(nt),ind1,nt)) / ( 2.D0 * l + 1.D0 )
-
-                 nbe = nbe + 1
-                 !
-              ELSE
-                 !
-                 chi(1:mesh(nt),nb,nt) = chi(1:mesh(nt),nbe,nt)
-                 !
-              END IF
-              !
-              lchi(nb,nt)= lchi(nbe,nt)
-              !
-           END DO
-           !
-        END IF
-        !
-        so(nt) = .FALSE.
-        !
-     END DO
-     !
-  END IF
+  IF ( .NOT. lspinorb ) CALL average_pp ( ntyp )
   !
   ! ... set number of atomic wavefunctions
   !
-  natomwfc = n_atom_wfc( nat, npsx, ityp, nchix, nchi, oc, lchi, jchi )
+  natomwfc = n_atom_wfc( nat, ityp )
   !
   ! ... set the max number of bands used in iterative diagonalization
   !
@@ -854,20 +725,21 @@ SUBROUTINE setup()
 END SUBROUTINE setup
 !
 !----------------------------------------------------------------------------
-FUNCTION n_atom_wfc( nat, npsx, ityp, nchix, nchi, oc, lchi, jchi )
+FUNCTION n_atom_wfc( nat, ityp )
   !----------------------------------------------------------------------------
   !
-  ! ... Find max number of bands needed
+  ! ... Find number of starting atomic orbitals
   !
-  USE kinds,            ONLY : DP
-  use noncollin_module, ONLY : noncolin
-  use spin_orb,         ONLY : so
+  USE atom,             ONLY : oc, chi, nchi, lchi, jchi
+  USE noncollin_module, ONLY : noncolin
+  USE spin_orb,         ONLY : so
   !
   IMPLICIT NONE
   !
+  INTEGER, INTENT(IN)  :: nat, ityp(nat)
+  !
   INTEGER  :: n_atom_wfc
-  INTEGER  :: nat, npsx, ityp(nat), nchix, nchi(npsx), lchi(nchix,npsx)
-  REAL(DP) :: oc(nchix,npsx), jchi(nchix,npsx)
+  !
   INTEGER  :: na, nt, n
   !
   !
@@ -1106,3 +978,144 @@ SUBROUTINE check_para_diag_efficiency()
   RETURN
   !
 END SUBROUTINE check_para_diag_efficiency
+
+!----------------------------------------------------------------------------
+SUBROUTINE average_pp ( ntyp ) 
+  !----------------------------------------------------------------------------
+  !
+  USE kinds,            ONLY : DP
+  USE atom,             ONLY : chi, nchi, lchi, jchi, mesh
+  USE spin_orb,         ONLY : so
+  USE uspp_param,       ONLY : betar, dion, jjj, lll, nbeta, tvanp
+  !
+  IMPLICIT NONE
+  !
+  INTEGER, INTENT(IN) :: ntyp
+  !
+  INTEGER  :: nt, nb, nbe, ind, ind1, l
+  REAL(DP) :: vionl
+  !
+  !
+  DO nt = 1, ntyp
+     !
+     IF ( so(nt) ) THEN
+        !
+        IF ( tvanp(nt) ) &
+             CALL errore( 'setup', 'US j-average not yet implemented', 1 )
+        !
+        nbe = 0
+        !
+        DO nb = 1, nbeta(nt)
+           !
+           nbe = nbe + 1
+           !
+           IF ( lll(nb,nt) /= 0 .AND. &
+                ABS( jjj(nb,nt) - lll(nb,nt) - 0.5D0 ) < 1.D-7 ) nbe = nbe - 1
+        END DO
+        !
+        nbeta(nt) = nbe
+        !
+        nbe = 0
+        !
+        DO nb = 1, nbeta(nt)
+           !
+           nbe = nbe + 1
+           !
+           l = lll(nbe,nt)
+           !
+           IF ( l /= 0 ) THEN
+              !
+              IF (ABS(jjj(nbe,nt)-lll(nbe,nt)+0.5d0).LT.1.d-7) THEN
+                 IF (ABS(jjj(nbe+1,nt)-lll(nbe+1,nt)-0.5d0).GT.1.d-7) &
+                      call errore('setup','wrong beta functions',1)
+                 ind=nbe+1
+                 ind1=nbe
+              ELSE
+                 IF (ABS(jjj(nbe+1,nt)-lll(nbe+1,nt)+0.5d0).GT.1.d-7) &
+                      call errore('setup','wrong beta functions',1)
+                 ind=nbe
+                 ind1=nbe+1
+              ENDIF
+              !
+              vionl = ( ( l + 1.D0 ) * dion(ind,ind,nt) + &
+                   l * dion(ind1,ind1,nt) ) / ( 2.D0 * l + 1.D0 )
+              !
+              betar(1:mesh(nt),nb,nt) = 1.D0 / ( 2.D0 * l + 1.D0 ) * &
+                   ( ( l + 1.D0 ) * SQRT( dion(ind,ind,nt) / vionl ) * &
+                   betar(1:mesh(nt),ind,nt) + &
+                   l * SQRT( dion(ind1,ind1,nt) / vionl ) * &
+                   betar(1:mesh(nt),ind1,nt) )
+              !
+              dion(nb,nb,nt) = vionl
+              !
+              nbe = nbe + 1
+                 !
+           ELSE
+              !
+              betar(1:mesh(nt),nb,nt) = betar(1:mesh(nt),nbe,nt)
+              !
+              dion(nb,nb,nt) = dion(nbe,nbe,nt)
+              !
+           END IF
+           !
+           lll(nb,nt)=lll(nbe,nt)
+           !
+        END DO
+        !
+        nbe = 0
+        !
+        DO nb = 1, nchi(nt)
+           !
+           nbe = nbe + 1
+           !
+           IF ( lchi(nb,nt) /= 0 .AND. &
+                ABS(jchi(nb,nt)-lchi(nb,nt)-0.5D0 ) < 1.D-7 ) nbe = nbe - 1
+           !
+        END DO
+        !
+        nchi(nt) = nbe
+        ! 
+        nbe = 0
+        !
+        do nb = 1, nchi(nt)
+           !
+           nbe = nbe + 1
+           !
+           l = lchi(nbe,nt)
+           !
+           IF ( l /= 0 ) THEN
+              !
+              IF (ABS(jchi(nbe,nt)-lchi(nbe,nt)+0.5d0).LT.1.d-7) THEN
+                 IF (ABS(jchi(nbe+1,nt)-lchi(nbe+1,nt)-0.5d0).GT.1.d-7) &
+                      call errore('setup','wrong chi functions',1)
+                 ind=nbe+1
+                 ind1=nbe
+              ELSE
+                 IF (ABS(jchi(nbe+1,nt)-lchi(nbe+1,nt)+0.5d0).GT.1.d-7) &
+                      call errore('setup','wrong chi functions',1)
+                 ind=nbe
+                 ind1=nbe+1
+              END IF
+              !
+              chi(1:mesh(nt),nb,nt)=((l+1.D0) * chi(1:mesh(nt),ind,nt)+ &
+                   l * chi(1:mesh(nt),ind1,nt)) / ( 2.D0 * l + 1.D0 )
+              !   
+              nbe = nbe + 1
+              !
+           ELSE
+              !
+              chi(1:mesh(nt),nb,nt) = chi(1:mesh(nt),nbe,nt)
+              !
+           END IF
+           !
+           lchi(nb,nt)= lchi(nbe,nt)
+           !
+        END DO
+        !
+     END IF
+     !
+     so(nt) = .FALSE.
+     !
+  END DO
+  !
+END SUBROUTINE average_pp
