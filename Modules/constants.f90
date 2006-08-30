2524864ad7cd954c4068a55a02d68968b9253620
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2006-08-30 16:32:10 +0000

    Cleanup of physical constants and conversion factors (to be completed)
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3356 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/constants.f90
+++ Modules/constants.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2002-2003 FPMD & PWSCF group
+! Copyright (C) 2002-2006 Quantum-Espresso group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -26,48 +26,54 @@ MODULE constants
   REAL(DP), PARAMETER :: sqrtpm1= 1.0_DP / sqrtpi
   REAL(DP), PARAMETER :: sqrt2  = 1.41421356237309504880_DP
   !
-  ! ... Physical constants
+  ! ... Physical constants, SI (NIST CODATA 2002)
   !
-  REAL(DP), PARAMETER :: K_BOLTZMAN_SI    = 1.38066D-23  ! J K^-1 
-  REAL(DP), PARAMETER :: K_BOLTZMAN_AU    = 3.1667D-6    ! Hartree K^-1 
-  REAL(DP), PARAMETER :: K_BOLTZMAN_M1_AU = 315795.26D0  ! Hartree^-1 K 
-  REAL(DP), PARAMETER :: FACTEM           = 315795.26D0  ! 27.212d0*11605.d0 Hartree^-1 K 
+  REAL(DP), PARAMETER :: H_PLANCK_SI      = 6.6260693D-34    ! J s
+  REAL(DP), PARAMETER :: K_BOLTZMANN_SI   = 1.3806505D-23    ! J K^-1 
+  REAL(DP), PARAMETER :: ELECTRON_SI      = 1.60217653D-19   ! C
+  REAL(DP), PARAMETER :: ELECTRONVOLT_SI  = 1.60217653D-19   ! J  
+  REAL(DP), PARAMETER :: ELECTRONMASS_SI  = 9.1093826D-31    ! Kg
+  REAL(DP), PARAMETER :: HARTREE_SI       = 4.35974417D-18   ! J
+  REAL(DP), PARAMETER :: RYDBERG_SI       = HARTREE_SI/2.0_DP! J
+  REAL(DP), PARAMETER :: BOHR_RADIUS_SI   = 0.5291772108D-10 ! m
+  REAL(DP), PARAMETER :: AMU_SI           = 1.66053886D-27   ! Kg
   !
-  ! ... Physical constants defining the Atomic Units System
+  ! ... Physical constants, atomic units:
+  ! ... AU for "Hartree" atomic units (e = m = hbar = 1)
+  ! ... RY for "Rydberg" atomic units (e^2=2, m=1/2, hbar=1)
   !
-  REAL(DP), PARAMETER :: BOHR_RADIUS_SI   = 0.529177D-10 ! m
-  REAL(DP), PARAMETER :: BOHR_RADIUS_CM   = 0.529177D-8  ! cm
-  REAL(DP), PARAMETER :: BOHR_RADIUS_ANGS = 0.529177D0   ! angstrom
-  REAL(DP), PARAMETER :: ELECTRONMASS_SI  = 9.10953D-31  ! Kg
-  REAL(DP), PARAMETER :: ELECTRONMASS_AMU = 5.4858D-4    ! amu
+  REAL(DP), PARAMETER :: K_BOLTZMANN_AU   = K_BOLTZMANN_SI / HARTREE_SI
+  REAL(DP), PARAMETER :: K_BOLTZMANN_RY   = K_BOLTZMANN_SI / RYDBERG_SI
   !
-  ! ... Units conversion factors
+  ! ... Unit conversion factors: energy and masses
   !
-  REAL(DP), PARAMETER :: ELECTRONVOLT_SI  = 1.6021892D-19  ! J  
-  REAL(DP), PARAMETER :: AMU_SI           = 1.66057D-27    ! Kg
-  REAL(DP), PARAMETER :: ANGSTROM_AU      = 1.889727D0     ! au
-  REAL(DP), PARAMETER :: AU_TO_OHMCMM1    = 46000.0D0      ! (ohm cm)^-1
-  REAL(DP), PARAMETER :: AU_KB            = 294210.0D0     ! Kbar
-  REAL(DP), PARAMETER :: KB_AU            = 1.0D0/294210.0D0 ! au
-  REAL(DP), PARAMETER :: AU_GPA           = 29421.0D0      ! GPa
-  REAL(DP), PARAMETER :: AUTOEV           = 27.211652D0    ! eV
-  REAL(DP), PARAMETER :: RYTOEV           = 13.605826D0    ! eV
-  REAL(DP), PARAMETER :: AMU_AU           = 1822.89D0   ! (mass of a proton)
-  REAL(DP), PARAMETER :: AU_TERAHERTZ     = 2.418D-5    ! from au to THz
-  REAL(DP), PARAMETER :: AU_SEC           = 2.4189D-17  ! sec
-  REAL(DP), PARAMETER :: AU_PS            = 2.4189D-5   ! sec
-  REAL(DP), PARAMETER :: DIP_DEBYE        = 2.54168     ! hartree atomic units dipole to Debye
-  !     
-  REAL(DP), PARAMETER :: eV_to_kelvin = 1.1604D4            ! from eV to Kelvin
-  REAL(DP), PARAMETER :: ry_to_kelvin = 315642.28D0 * 0.5D0 ! from Ry to Kelvin
+  REAL(DP), PARAMETER :: AUTOEV           = HARTREE_SI / ELECTRONVOLT_SI
+  REAL(DP), PARAMETER :: RYTOEV           = AUTOEV / 2.0_DP
+  REAL(DP), PARAMETER :: AMU_AU           = AMU_SI / ELECTRONMASS_SI
+  REAL(DP), PARAMETER :: AMU_RY           = AMU_AU / 2.0_DP
   !
-  ! ... mass conversion: a.m.u to a.u. (Ry)
+  ! ... Unit conversion factors: atomic unit of time, in s and ps
   !
-  REAL(DP), PARAMETER :: amconv= 1.66042D-24 / 9.1095D-28 * 0.5D0 
+  REAL(DP), PARAMETER :: AU_SEC           = H_PLANCK_SI/tpi/HARTREE_SI
+  REAL(DP), PARAMETER :: AU_PS            = AU_SEC * 1.0D+12
   !
-  ! ... pressure conversion from Ry/(a.u)^3 to K
+  ! ... Unit conversion factors: pressure (1 Pa = 1 J/m^3, 1GPa = 10 Kbar )
   !
-  REAL(DP), PARAMETER :: uakbar= 147105.d0
+  REAL(DP), PARAMETER :: AU_GPA           = HARTREE_SI / BOHR_RADIUS_SI ** 3 &
+                                            / 1.0D+9 
+  REAL(DP), PARAMETER :: RY_KBAR          = 10.0_dp * AU_GPA / 2.0_dp
+  !
+  ! ... Unit conversion factors: 1 debye = 10^-18 esu*cm 
+  ! ...                                  = 3.3356409519*10^-30 C*m 
+  ! ...                                  = 0.208194346 e*A
+  ! ... ( 1 esu = (0.1/c) Am, c=299792458 m/s)
+  !
+  REAL(DP), PARAMETER :: DEBYE_SI         = 3.3356409519 * 10D-30 ! C*m 
+  REAL(DP), PARAMETER :: AU_DEBYE         = ELECTRON_SI * BOHR_RADIUS_SI / &
+	                                       DEBYE_SI
+  !
+  REAL(DP), PARAMETER :: eV_to_kelvin = ELECTRONVOLT_SI / K_BOLTZMANN_SI
+  REAL(DP), PARAMETER :: ry_to_kelvin = RYDBERG_SI / K_BOLTZMANN_SI
   !
   ! ... zero up to a given accuracy
   !
@@ -78,10 +84,23 @@ MODULE constants
   REAL(DP), PARAMETER :: eps16 = 1.0D-16
   REAL(DP), PARAMETER :: eps32 = 1.0D-32
   !
-  REAL(DP), PARAMETER :: rhothr = 1.0e-5_DP ! tolerance
   REAL(DP), PARAMETER :: gsmall = 1.0d-12
   !
   REAL(DP), PARAMETER :: e2 = 2.D0      ! the square of the electron charge
   REAL(DP), PARAMETER :: degspin = 2.D0 ! the number of spins per level
   !
+  !!!!!! COMPATIBIILITY
+  !
+  REAL(DP), PARAMETER :: FACTEM = 1.0_DP / K_BOLTZMANN_AU
+  REAL(DP), PARAMETER :: bohr_radius_cm = bohr_radius_si * 100.0
+  REAL(DP), PARAMETER :: amconv = AMU_RY
+  REAL(DP), PARAMETER :: uakbar = RY_KBAR
+  REAL(DP), PARAMETER :: BOHR_RADIUS_ANGS = 0.5291772108
+  REAL(DP), PARAMETER :: ANGSTROM_AU = 1.0/BOHR_RADIUS_ANGS
+  REAL(DP), PARAMETER :: DIP_DEBYE = AU_DEBYE
+  REAL(DP), PARAMETER :: AU_TERAHERTZ  = AU_PS
+  REAL(DP), PARAMETER :: AU_TO_OHMCMM1 = 46000.0D0 ! (ohm cm)^-1
+  REAL(DP), PARAMETER :: k_boltzman_au = 3.1667D-6  ! Hartree K^-1 
+  !
+
 END MODULE constants
