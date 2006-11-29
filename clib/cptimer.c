6c2b6abe1cf2c431db66e44a7e6d6bb7657f7916
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2006-11-29 09:05:35 +0000

    Axel's patch for times - should hopefully work on all machines
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3584 c92efa57-630b-4861-b058-cf58834340f0

--- clib/cptimer.c
+++ clib/cptimer.c
@@ -6,18 +6,11 @@
   or http://www.gnu.org/copyleft/gpl.txt .
 */
 
-#include <time.h>
 #include <sys/time.h>
-#include <sys/times.h>
+#include <sys/resource.h>
+#include <unistd.h>
 
 #include "c_defs.h"
-
-/* This should catch systems that do not define the older CLK_TCK symbol, 
-   but do define the newer CLOCKS_PER_SEC constant */
-
-#ifndef CLK_TCK
-#define CLK_TCK CLOCKS_PER_SEC
-#endif
 
 double F77_FUNC(cclock,CCLOCK)()
 
@@ -34,49 +27,16 @@ double F77_FUNC(cclock,CCLOCK)()
 
 }
 
-#ifdef __QK_USER__
-#include <catamount/dclock.h>
-double F77_FUNC(scnds,SCNDS) ( )
-
-/* cray xt3 has no times() on the compute nodes.
- * Return the wall time associated to the current process.
-*/
-{
-	static int first = 1;
-	static double init_cputime = 0.0;
-	double cputime;
-
-        cputime   = dclock();
-
-	if( first ) {
-		first = 0;
-		init_cputime = cputime;
-	}
-
-	return cputime - init_cputime;
-}
-#else
-
 double F77_FUNC(scnds,SCNDS) ( )
 
 /* Return the cpu time associated to the current process 
 */
+
 {
-	static struct tms T;
-	static int first = 1;
-	static double init_cputime = 0.0;
-	double cputime;
+        static struct rusage T;
 
-        times(&T);
+        getrusage(RUSAGE_SELF, &T);
 
-        cputime   = (double)(T.tms_utime);
-        cputime  /= (double)CLK_TCK ;
-
-	if( first ) {
-		first = 0;
-		init_cputime = cputime;
-	}
-
-	return cputime - init_cputime;
+        return ((double)T.ru_utime.tv_sec + ((double)T.ru_utime.tv_usec)/1000000.0);
 }
-#endif
+
