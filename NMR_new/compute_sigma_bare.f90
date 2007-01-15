b686e515378ea5ebc4e6d77d8f3a84645a607363
seitsonen <seitsonen@c92efa57-630b-4861-b058-cf58834340f0>
2007-01-15 10:27:40 +0000

    New version, many changes due to the GIPAW reconstruction.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3658 c92efa57-630b-4861-b058-cf58834340f0

--- NMR_new/compute_sigma_bare.f90
+++ NMR_new/compute_sigma_bare.f90
@@ -5,7 +5,7 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !-----------------------------------------------------------------------
-SUBROUTINE compute_sigma_bare(chi_bare)
+SUBROUTINE compute_sigma_bare(chi_bare, sigma_bare)
   !-----------------------------------------------------------------------
   !
   ! ... Compute the bare contribution to the chemical shift at the
@@ -18,49 +18,52 @@ SUBROUTINE compute_sigma_bare(chi_bare)
   USE ions_base,            ONLY : nat, tau, atm, ityp
   USE io_global,       ONLY : stdout
   USE symme,     ONLY : s, nsym, irt
-  USE mp_global, ONLY : me_pool
   USE pwcom
   USE nmr_module
 
+  ! Arguments
   REAL(DP), INTENT(IN) :: chi_bare(3,3)
-  REAL(DP) :: macroscopic_shape(3,3)
-  real(dp) :: sigma_bare(3,3,nat)
-  complex(dp) :: tmp_sigma(3,3)
-  real(dp) :: arg, tr_sigma
+  real(dp), intent(out) :: sigma_bare(3,3,nat)
+  
+  ! Local
   integer :: na, ig
+  real(dp) :: macroscopic_shape(3,3)
+  real(dp) :: arg, tr_sigma
+  complex(dp) :: tmp_sigma(3,3)
 
-  macroscopic_shape(:,:) = 2.d0/3.d0
+  macroscopic_shape(:,:) = 2.0_dp/3.0_dp
   ! like in paratec:
-  macroscopic_shape(:,:) = 0.d0
+  macroscopic_shape(:,:) = 0.0_dp
   do na = 1, 3 
-    macroscopic_shape(na,na) = 2.d0/3.d0
+    macroscopic_shape(na,na) = 2.0_dp/3.0_dp
   enddo
-
-  write(stdout,'(5X,''NMR chemical shifts in ppm:'')')
+  
+  write(stdout,'(5X,''NMR chemical bare shifts in ppm:'')')
   write(stdout,*)
   
-
   do na = 1, nat
-    tmp_sigma(:,:) = 0.d0
-
+    tmp_sigma(:,:) = 0.0_dp
+    
     do ig = gstart, ngm              
       arg = (g(1,ig)*tau(1,na) + g(2,ig)*tau(2,na) + g(3,ig)*tau(3,na)) * tpi
-      tmp_sigma(:,:) = tmp_sigma(:,:) + b_ind(ig,:,:) * cmplx(cos(arg),sin(arg))
+      tmp_sigma(:,:) = tmp_sigma(:,:) &
+           + b_ind(ig,:,:) * cmplx(cos(arg),sin(arg))
     enddo
-
-#if 0
+    
+#if 1
     ! this is the G = 0 term
-    if (gstart == 2) &
-      tmp_sigma(:,:) = tmp_sigma(:,:) + &
-        (4.d0*pi) * macroscopic_shape(:,:) * chi_bare(:,:)
+    if (gstart == 2) then
+       tmp_sigma(:,:) = tmp_sigma(:,:) &
+            - (4.0_dp*pi) * macroscopic_shape(:,:) * chi_bare(:,:) !*TMPTMPTMP
+    end if
 #endif
-
+    
     sigma_bare(:,:,na) = real(tmp_sigma(:,:))
   enddo
 #ifdef __PARA
   call reduce(9*na, sigma_bare)
 #endif
-
+  
 #if 0
   ! symmetrize tensors ??
   do na = 1, nat
@@ -71,15 +74,105 @@ SUBROUTINE compute_sigma_bare(chi_bare)
     call trntns (sigma_bare(1,1,na), at, bg, 1)
   enddo
 #endif
-
+  
   do na = 1, nat
-    tr_sigma = (sigma_bare(1,1,na)+sigma_bare(2,2,na)+sigma_bare(3,3,na))/3.d0
-    write(stdout,'(5X,''Atom'',I3,2X,A3,'' pos: ('',3(F10.6),'') &
-       &  sigma: '',F14.4)') na, atm(ityp(na)), tau(:,na), tr_sigma*1d6
-    write(stdout, tens_fmt) sigma_bare(:,:,na) * 1d6
+    tr_sigma = (sigma_bare(1,1,na)+sigma_bare(2,2,na)+sigma_bare(3,3,na))/3.0_dp
+    write(stdout,'(5X,''Atom'',I3,2X,A3,'' pos: ('',3(F10.6),&
+          '')  sigma: '',F14.4)') na, atm(ityp(na)), tau(:,na), tr_sigma*1e6_dp
+    write(stdout, tens_fmt) sigma_bare(:,:,na) * 1e6_dp
   enddo
-
+  
 end subroutine compute_sigma_bare
 
+!-----------------------------------------------------------------------
+SUBROUTINE compute_sigma_diamagnetic( sigma_diamagnetic )
+  !-----------------------------------------------------------------------
+  !
+  ! ... Compute the diamagnetic contribution to the chemical shift at the
+  ! ... position of the nuclei, given the induced field
+  USE kinds,                ONLY : DP
+  USE klist,                ONLY : xk
+  USE wvfct,                ONLY : nbnd, npwx, npw, igk  
+  USE gvect,                ONLY : ngm, gstart, nr1, nr2, nr3, nrx1, nrx2, &
+                                   nrx3, nrxx, nl, nlm, g, gg, ecutwfc, gcutm
+  USE ions_base,            ONLY : nat, tau, atm, ityp
+  USE io_global,       ONLY : stdout
+  USE symme,     ONLY : s, nsym, irt
+  USE pwcom
+  USE nmr_module
 
+  ! Arguments
+  real(dp), intent(inout) :: sigma_diamagnetic(3,3,nat)
+  
+  ! Local
+  integer :: na
+  real(dp) :: tr_sigma
+  
 
+  write(stdout,'(5X,''NMR chemical diamagnetic shifts in ppm:'')')
+  write(stdout,*)
+  
+  ! symmetrize tensors
+  do na = 1, nat
+    call trntns (sigma_diamagnetic(1,1,na), at, bg, -1)
+  enddo
+  call symz(sigma_diamagnetic, nsym, s, nat, irt)
+  do na = 1, nat
+    call trntns (sigma_diamagnetic(1,1,na), at, bg, 1)
+  enddo
+  
+  do na = 1, nat
+    tr_sigma = (sigma_diamagnetic(1,1,na)+sigma_diamagnetic(2,2,na) &
+         +sigma_diamagnetic(3,3,na))/3.0_dp
+    write(stdout,'(5X,''Atom'',I3,2X,A3,'' pos: ('',3(F10.6),&
+          '')  sigma: '',F14.4)') na, atm(ityp(na)), tau(:,na), tr_sigma*1e6_dp
+    write(stdout, tens_fmt) sigma_diamagnetic(:,:,na) * 1e6_dp
+  enddo
+
+end subroutine compute_sigma_diamagnetic
+
+!-----------------------------------------------------------------------
+SUBROUTINE compute_sigma_paramagnetic( sigma_paramagnetic )
+  !-----------------------------------------------------------------------
+  !
+  ! ... Compute the paramagnetic contribution to the chemical shift at the
+  ! ... position of the nuclei, given the induced field
+  USE kinds,                ONLY : DP
+  USE klist,                ONLY : xk
+  USE wvfct,                ONLY : nbnd, npwx, npw, igk  
+  USE gvect,                ONLY : ngm, gstart, nr1, nr2, nr3, nrx1, nrx2, &
+                                   nrx3, nrxx, nl, nlm, g, gg, ecutwfc, gcutm
+  USE ions_base,            ONLY : nat, tau, atm, ityp
+  USE io_global,       ONLY : stdout
+  USE symme,     ONLY : s, nsym, irt
+  USE pwcom
+  USE nmr_module
+
+  ! Arguments
+  real(dp), intent(inout) :: sigma_paramagnetic(3,3,nat)
+  
+  ! Local
+  integer :: na
+  real(dp) :: tr_sigma
+
+  write(stdout,'(5X,''NMR chemical paramagnetic shifts in ppm:'')')
+  write(stdout,*)
+  
+  ! symmetrize tensors
+  do na = 1, nat
+    call trntns (sigma_paramagnetic(1,1,na), at, bg, -1)
+  enddo
+  call symz(sigma_paramagnetic, nsym, s, nat, irt)
+  do na = 1, nat
+    call trntns (sigma_paramagnetic(1,1,na), at, bg, 1)
+  enddo
+  
+  do na = 1, nat
+    tr_sigma = (sigma_paramagnetic(1,1,na)+sigma_paramagnetic(2,2,na) &
+         +sigma_paramagnetic(3,3,na))/3.0_dp
+    write(stdout,'(5X,''Atom'',I3,2X,A3,'' pos: ('',3(F10.6),&
+          '')  sigma: '',F14.4)') na, atm(ityp(na)), tau(:,na), tr_sigma*1e6_dp
+    write(stdout, tens_fmt) sigma_paramagnetic(:,:,na) * 1e6_dp
+  enddo
+
+end subroutine compute_sigma_paramagnetic
