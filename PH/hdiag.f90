d3245f932588a41ed8a012e1848cc14743d90337
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2007-01-22 10:54:24 +0000

    Fixed recently introduced compilation error + bad call to cinitcgg
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3704 c92efa57-630b-4861-b058-cf58834340f0

--- PH/hdiag.f90
+++ PH/hdiag.f90
@@ -16,7 +16,6 @@ subroutine hdiag( max_iter, avg_iter, xk_, et_ )
   use kinds, only : DP
   use pwcom, ONLY: g, g2kin, tpiba2, ecfixed, qcutz, q2sigma, nbnd, &
         npwx, vkb, igk, npw
-  use g_psi_mod
   USE wavefunctions_module,  ONLY: evc
   USE ramanm, ONLY: eth_ns
   implicit none
@@ -33,19 +32,22 @@ subroutine hdiag( max_iter, avg_iter, xk_, et_ )
   !
   !     Local variables:
   !
-  real(DP) :: cg_iter, erf
+  REAL(DP) :: cg_iter
   ! number of iteration in CG
+  REAL(DP), EXTERNAL :: erf
   ! error function
-  integer :: ig, ntry, notconv
+  INTEGER  :: ig, ntry, notconv
   ! counter on G vectors
   ! number or repeated call to diagonalization in case of non convergence
   ! number of notconverged elements
   INTEGER, ALLOCATABLE :: btype(:)
     ! type of band: valence (1) or conduction (0)  
+  REAl(DP), ALLOCATABLE :: h_prec(:)
+    ! preconditioning matrix (diagonal)
 
   call start_clock ('hdiag')
 
-  allocate (h_diag( npwx), btype(nbnd))
+  allocate (h_prec( npwx), btype(nbnd))
   btype(:) = 1
   !
   !   various initializations
@@ -69,25 +71,24 @@ subroutine hdiag( max_iter, avg_iter, xk_, et_ )
 
   !
   ! Conjugate-Gradient diagonalization
-  ! h_diag is the precondition matrix
   !
   do ig = 1, npw
-     h_diag (ig) = max (1.d0, g2kin (ig) )
+     h_prec (ig) = max (1.d0, g2kin (ig) )
   enddo
   ntry = 0
 10 continue
   if (ntry > 0) then
-     call cinitcgg (npwx, npw, nbnd, nbnd, evc, evc, et_ )
+     call cinitcgg (npwx, npw, nbnd, nbnd, evc, evc, et_, .FALSE. )
      avg_iter = avg_iter + 1.d0
   endif
-  call ccgdiagg (npwx, npw, nbnd, evc, et_, btype, h_diag, eth_ns, &
+  call ccgdiagg (npwx, npw, nbnd, evc, et_, btype, h_prec, eth_ns, &
        max_iter, .true., notconv, cg_iter)
   avg_iter = avg_iter + cg_iter
   ntry = ntry + 1
 
   if (ntry.le.5.and.notconv.gt.0) goto 10
 
-  deallocate (btype, h_diag)
+  deallocate (btype, h_prec)
   call stop_clock ('hdiag')
 
   return
