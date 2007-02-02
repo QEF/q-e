4708082eb9a892ef84d5b2cd08cb248977d63dc4
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2007-02-02 17:53:13 +0000

    Added function "c_rename" to rename a file - not sure how portable it is and whether it is really useful
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3741 c92efa57-630b-4861-b058-cf58834340f0

--- clib/c_mkdir.c
+++ clib/c_mkdir.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2003 PWSCF group
+  Copyright (C) 2003-2007 Quantum-Espresso group
   This file is distributed under the terms of the
   GNU General Public License. See the file `License'
   in the root directory of the present distribution,
@@ -61,7 +61,41 @@ int F77_FUNC_(c_mkdir,C_MKDIR)( const char * dirname , const int * length )
 
    return retval ;
 
-} /* c_mkdir_ */
+} /* c_mkdir */
 
+/* call from fortran as
+   ios = c_remame ( TRIM(old-file-name), TRIM_LEN(old-file-name), &
+                    TRIM(new-file-name), TRIM_LEN(new-file-name) )
+   renames file old-file-name into new-file-name (don't try this on
+   open files!)
+   ios should return 0 if everything is ok, -1 otherwise.
+   Written by PG by imitating "c_mkdir" without really understanding it */
+
+int F77_FUNC_(c_rename,C_RENAME)( const char * oldname, const int * oldlength ,
+                                  const char * newname, const int * newlength )
+{
+
+   int retval = -1 ;
+
+   char * oldname_ = ( char * ) xcmalloc( (*oldlength) + 1 ) ;
+   char * newname_ = ( char * ) xcmalloc( (*newlength) + 1 ) ;
+
+   memcpy( oldname_ , oldname , *oldlength ) ;
+   memcpy( newname_ , newname , *newlength ) ;
+
+   oldname_[*oldlength] = '\0' ;
+   newname_[*newlength] = '\0' ;
+
+   retval = rename( oldname_, newname_ ) ;
+
+   if ( retval == -1 )
+     fprintf( stderr , "mv fail: [%d] %s\n" , errno , strerror( errno ) ) ;
+
+   free( oldname_ ) ;
+   free( newname_ ) ;
+
+   return retval ;
+
+} /* c_rename */
 
 /* EOF */
