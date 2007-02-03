db27c0db1c802e5dc369b914f834a01c62638f00
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2007-02-03 15:10:21 +0000

    Tra_write_matrix (used only for debug) now symmetrizes the matrix before writing it.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3744 c92efa57-630b-4861-b058-cf58834340f0

--- PH/tra_write_matrix.f90
+++ PH/tra_write_matrix.f90
@@ -10,44 +10,34 @@ subroutine tra_write_matrix (alpha, adyn, u, nat)
 #include "f_defs.h"
   USE io_global,  ONLY : stdout
   USE kinds, only : DP
+  USE cell_base, only : at, bg
+  USE symme, only : s, irt
+  USE modes, only : invs, rtau, irgq, nsymq, irotmq, minus_q
+  USE qpoint, only : xq
   implicit none
   !
-  !    This routine writes on output the dynamical matrix in the cartesian
-  !    It transform it from the basis of the modes but on output adyn is
-  !    unchanged
+  !    This routine writes on output the symmetrized dynamical matrix in 
+  !    cartesian coordinates. The input matrix adyn is in the basis of 
+  !    the modes.
+  !    On output adyn is unchanged
   !
   integer :: i, j, na, nb, nat
-  integer :: icart, jcart, mu, nu
   complex(DP) :: adyn (3 * nat, 3 * nat), u (3 * nat, 3 * nat)
-  complex(DP) :: wdyn (3, 3, nat, nat), work
+  complex(DP) :: auxdyn (3*nat, 3*nat)
   character (len=*) :: alpha
-  WRITE( stdout, * ) nat
-  do i = 1, 3 * nat
-     na = (i - 1) / 3 + 1
-     icart = i - 3 * (na - 1)
-     do j = 1, 3 * nat
-        nb = (j - 1) / 3 + 1
-        jcart = j - 3 * (nb - 1)
-        work = (0.d0, 0.d0)
-        do mu = 1, 3 * nat
-           do nu = 1, 3 * nat
-              work = work + u (i, mu) * adyn (mu, nu) * CONJG(u (j, nu) )
-           enddo
-        enddo
-        wdyn (icart, jcart, na, nb) = work
-     enddo
 
+  auxdyn=adyn
+  CALL symdyn_munu (auxdyn, u, xq, s, invs, rtau, irt, irgq, at, bg, &
+          nsymq, nat, irotmq, minus_q)
 
-  enddo
   WRITE( stdout, '(a)') alpha
   do na = 1, nat
      do nb = 1, nat
         WRITE( stdout, '(2i4)') na, nb
         do i = 1, 3
-           WRITE( stdout, '(6f10.5)') (wdyn (i, j, na, nb) , j = 1, 3)
+           WRITE( stdout, '(6f10.5)') (auxdyn(3*(na-1)+i, 3*(nb-1)+j),j=1,3)
         enddo
      enddo
-
   enddo
   return
 end subroutine tra_write_matrix
