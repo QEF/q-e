107f6552c043c2ee1441a2cdc9942bd20edd43f3
sbraccia <sbraccia@c92efa57-630b-4861-b058-cf58834340f0>
2007-03-19 02:40:53 +0000

    Added function to test the exit status of a job. C.S.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3853 c92efa57-630b-4861-b058-cf58834340f0

---
+++ examples/check_failure.sh
@@ -0,0 +1,9 @@
+# function to test the exit status of a job
+function check_failure() {
+    # usage: check_failure $?
+    if test $1 != 0; then
+        $ECHO "Error condition encountered during test: exit status = $1"
+        $ECHO "Aborting"
+        exit 1
+    fi
+}
