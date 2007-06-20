1b326069d0b4c4e30c143165a517cd093f942489
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2007-06-20 14:38:46 +0000

    erf: cleanup, all constants brought to 16 significant digits
    Does anybody see any good reason to keep an internal erf instead
    of using the f90 intrinsic one? It should be fortran standard - PG
    
    
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@3975 c92efa57-630b-4861-b058-cf58834340f0

--- flib/erf.f90
+++ flib/erf.f90
@@ -1,12 +1,12 @@
 !
-! Copyright (C) 2002-2003 PWSCF+CP group
+! Copyright (C) 2002-2007 Quantum-Espresso group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !---------------------------------------------------------------------
-real(8) function erf (x)  
+function erf (x)  
   !---------------------------------------------------------------------
   !
   !     Error function - computed from the rational approximations of
@@ -15,27 +15,29 @@ real(8) function erf (x)
   !     for abs(x) le 0.47 erf is calculated directly
   !     for abs(x) gt 0.47 erf is calculated via erf(x)=1-erfc(x)
   !
+  use kinds, only : DP
   implicit none  
-  real(8) :: x, x2, p1 (4), q1 (4)
-  real(8), external :: erfc  
-  data p1 / 2.42667955230532d2, 2.19792616182942d1, &
-       6.99638348861914d0, -3.56098437018154d-2 /
-  data q1 / 2.15058875869861d2, 9.11649054045149d1, &
-       1.50827976304078d1, 1.00000000000000d0 /
+  real(DP), intent(in) :: x
+  real(DP) :: x2, p1 (4), q1 (4)
+  real(DP), external :: erfc  
+  real(DP) :: erf
+  data p1 / 2.426679552305318E2_DP, 2.197926161829415E1_DP, &
+            6.996383488619136_DP,  -3.560984370181538E-2_DP /
+  data q1 / 2.150588758698612E2_DP, 9.116490540451490E1_DP, &
+            1.508279763040779E1_DP, 1.000000000000000_DP /
   !
-  if (abs (x) .gt.6.d0) then  
+  if (abs (x) > 6.0_DP) then  
      !
-     !  erf(6)=1-10^(-17) cannot be distinguished from 1 with 16-byte words
+     !  erf(6)=1-10^(-17) cannot be distinguished from 1
      !
-     erf = sign (1.d0, x)  
+     erf = sign (1.0_DP, x)  
   else  
-     if (abs (x) .le.0.47d0) then  
+     if (abs (x)  <= 0.47_DP) then  
         x2 = x**2  
-        erf = x * (p1 (1) + x2 * (p1 (2) + x2 * (p1 (3) + x2 * p1 ( &
-             4) ) ) ) / (q1 (1) + x2 * (q1 (2) + x2 * (q1 (3) + x2 * q1 ( &
-             4) ) ) )
+        erf = x * (p1 (1) + x2 * (p1 (2) + x2 * (p1 (3) + x2 * p1 (4) ) ) ) &
+                / (q1 (1) + x2 * (q1 (2) + x2 * (q1 (3) + x2 * q1 (4) ) ) )
      else  
-        erf = 1.d0 - erfc (x)  
+        erf = 1.0_DP - erfc (x)  
      endif
   endif
   !
@@ -43,42 +45,48 @@ real(8) function erf (x)
 end function erf
 !
 !---------------------------------------------------------------------
-real(8) function erfc (x)  
+function erfc (x)  
   !---------------------------------------------------------------------
   !
   !     erfc(x) = 1-erf(x)  - See comments in erf
   !
+  use kinds, only : DP
   implicit none  
-  real(8) :: x, ax, x2, xm2, p2 (8), q2 (8), p3 (5), q3 (5), pim1
-  real(8), external :: erf  
-  data p2 / 3.00459261020162d2, 4.51918953711873d2, &
-       3.39320816734344d2, 1.52989285046940d2, 4.31622272220567d1, &
-       7.21175825088309d0, 5.64195517478974d-1, -1.36864857382717d-7 /
-  data q2 / 3.00459260956983d2, 7.90950925327898d2, &
-       9.31354094850610d2, 6.38980264465631d2, 2.77585444743988d2, &
-       7.70001529352295d1, 1.27827273196294d1, 1.00000000000000d0 /
-  data p3 / -2.99610707703542d-3, -4.94730910623251d-2, &
-       -2.26956593539687d-1, -2.78661308609648d-1, -2.23192459734185d-2 &
-       /
-  data q3 / 1.06209230528468d-2, 1.91308926107830d-1, &
-       1.05167510706793d0, 1.98733201817135d0, 1.00000000000000d0 /
+  real(DP),intent(in) :: x
+  real(DP)            :: erfc
+  real(DP) :: ax, x2, xm2, p2 (8), q2 (8), p3 (5), q3 (5), pim1
+  real(DP), external :: erf  
+  data p2 / 3.004592610201616E2_DP,  4.519189537118719E2_DP, &
+            3.393208167343437E2_DP,  1.529892850469404E2_DP, &
+            4.316222722205674E1_DP,  7.211758250883094_DP,   &
+            5.641955174789740E-1_DP,-1.368648573827167E-7_DP /
+  data q2 / 3.004592609569833E2_DP,  7.909509253278980E2_DP, &
+            9.313540948506096E2_DP,  6.389802644656312E2_DP, &
+            2.775854447439876E2_DP,  7.700015293522947E1_DP, &
+            1.278272731962942E1_DP,  1.000000000000000_DP /
+  data p3 /-2.996107077035422E-3_DP,-4.947309106232507E-2_DP, &
+           -2.269565935396869E-1_DP,-2.786613086096478E-1_DP, &
+           -2.231924597341847E-2_DP /
+  data q3 / 1.062092305284679E-2_DP, 1.913089261078298E-1_DP, &
+            1.051675107067932_DP,    1.987332018171353_DP,    &
+            1.000000000000000_DP /
 
-  data pim1 / 0.564189583547756d0 /  
+  data pim1 / 0.56418958354775629_DP /  
   !        ( pim1= sqrt(1/pi) )
   ax = abs (x)  
-  if (ax.gt.26.d0) then  
+  if (ax > 26.0_DP) then  
      !
      !  erfc(26.0)=10^(-296); erfc( 9.0)=10^(-37);
      !
-     erfc = 0.d0  
-  elseif (ax.gt.4.d0) then  
+     erfc = 0.0_DP  
+  elseif (ax > 4.0_DP) then  
      x2 = x**2  
-     xm2 = (1.d0 / ax) **2  
-     erfc = (1.d0 / ax) * exp ( - x2) * (pim1 + xm2 * (p3 (1) &
+     xm2 = (1.0_DP / ax) **2  
+     erfc = (1.0_DP / ax) * exp ( - x2) * (pim1 + xm2 * (p3 (1) &
           + xm2 * (p3 (2) + xm2 * (p3 (3) + xm2 * (p3 (4) + xm2 * p3 (5) &
           ) ) ) ) / (q3 (1) + xm2 * (q3 (2) + xm2 * (q3 (3) + xm2 * &
           (q3 (4) + xm2 * q3 (5) ) ) ) ) )
-  elseif (ax.gt.0.47d0) then  
+  elseif (ax > 0.47_DP) then  
      x2 = x**2  
      erfc = exp ( - x2) * (p2 (1) + ax * (p2 (2) + ax * (p2 (3) &
           + ax * (p2 (4) + ax * (p2 (5) + ax * (p2 (6) + ax * (p2 (7) &
@@ -86,29 +94,32 @@ real(8) function erfc (x)
           (q2 (3) + ax * (q2 (4) + ax * (q2 (5) + ax * (q2 (6) + ax * &
           (q2 (7) + ax * q2 (8) ) ) ) ) ) ) )
   else  
-     erfc = 1.d0 - erf (ax)  
+     erfc = 1.0_DP - erf (ax)  
   endif
   !
   ! erf(-x)=-erf(x)  =>  erfc(-x) = 2-erfc(x)
   !
-  if (x.lt.0.d0) erfc = 2.d0 - erfc  
+  if (x < 0.0_DP) erfc = 2.0_DP - erfc  
   !
   return  
 end function erfc
 !
 !---------------------------------------------------------------------
-real(8) function gauss_freq (x)
+function gauss_freq (x)
   !---------------------------------------------------------------------
   !
   !     gauss_freq(x) = (1+erf(x/sqrt(2)))/2 = erfc(-x/sqrt(2))/2
   !             - See comments in erf
   !
-  real(8) :: x
-  real(8), parameter :: c =  0.707106781186548d0
+  use kinds, only : DP
+  implicit none
+  real(DP),intent(in) :: x
+  real(DP)            :: gauss_freq
+  real(DP), parameter :: c =  0.7071067811865475_DP
   !        ( c= sqrt(1/2) )
-  real(8), external :: erfc
+  real(DP), external :: erfc
   !
-  gauss_freq = 0.5d0 * erfc ( - x * c)
+  gauss_freq = 0.5_DP * erfc ( - x * c)
   !
   return
 end function gauss_freq
