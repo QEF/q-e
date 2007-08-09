6a9e50bf564155cfd1798c41477c39b67323f622
cavazzon <cavazzon@c92efa57-630b-4861-b058-cf58834340f0>
2007-08-09 20:29:41 +0000

    - small changes for block-like parallelization of diagonalization
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@4102 c92efa57-630b-4861-b058-cf58834340f0

--- flib/gridsetup.f90
+++ flib/gridsetup.f90
@@ -10,40 +10,84 @@
 !-----------------------------------------------------------------------
 !
 
-SUBROUTINE GRID2D_DIMS( nproc, nprow, npcol )
+SUBROUTINE GRID2D_DIMS( grid_shape, nproc, nprow, npcol )
    !
    ! This subroutine factorizes the number of processors (NPROC)
-   ! into NPROW and NPCOL,  that are the sizes of the 2D processors mesh.
+   ! into NPROW and NPCOL according to the shape
    !
    !    Written by Carlo Cavazzoni
    !
    IMPLICIT NONE
+   CHARACTER, INTENT(IN) :: grid_shape
    INTEGER, INTENT(IN)  :: nproc
    INTEGER, INTENT(OUT) :: nprow, npcol
-   integer sqrtnp,i
-   sqrtnp = int( sqrt( dble(nproc) ) + 1 )
-   do i=1,sqrtnp
-      if(mod(nproc,i).eq.0) nprow = i
-   end do
-   npcol = nproc/nprow
+   INTEGER :: sqrtnp, i
+   !
+   sqrtnp = INT( SQRT( REAL( nproc ) + 0.1 ) )
+   !
+   IF( grid_shape == 'S' ) THEN
+      ! Square grid
+      nprow = sqrtnp
+      npcol = sqrtnp
+   ELSE
+      ! Rectangular grid
+      DO i = 1, sqrtnp + 1
+         IF( MOD( nproc, i ) == 0 ) nprow = i
+      end do
+      npcol = nproc / nprow
+   END IF
    RETURN
 END SUBROUTINE
 
-SUBROUTINE GRID2D_COORDS( rank, nprow, npcol, row, col )
+SUBROUTINE GRID2D_COORDS( order, rank, nprow, npcol, row, col )
+   !
+   !  this subroutine compute the cartesian coordinetes "row" and "col"
+   !  of the processor whose MPI task id is "rank". 
+   !  Note that if the rank is larger that the grid size
+   !  all processors whose MPI task id is greather or equal 
+   !  than nprow * npcol are placed on the diagonal extension of the grid itself
+   !
    IMPLICIT NONE
+   CHARACTER, INTENT(IN) :: order
    INTEGER, INTENT(IN)  ::  rank          ! process index starting from 0
    INTEGER, INTENT(IN)  ::  nprow, npcol  ! dimensions of the processor grid
    INTEGER, INTENT(OUT) ::  row, col
-   row = MOD( rank, nprow )
-   col = rank / nprow
+   IF( rank >= 0 .AND. rank < nprow * npcol ) THEN
+      IF( order == 'C' .OR. order == 'c' ) THEN
+         !  grid in COLUMN MAJOR ORDER
+         row = MOD( rank, nprow )
+         col = rank / nprow
+      ELSE
+         !  grid in ROW MAJOR ORDER
+         row = rank / npcol
+         col = MOD( rank, npcol )
+      END IF
+   ELSE
+      row = rank
+      col = rank
+   END IF
    RETURN
 END SUBROUTINE
 
-SUBROUTINE GRID2D_RANK( nprow, npcol, row, col, rank )
+SUBROUTINE GRID2D_RANK( order, nprow, npcol, row, col, rank )
+   !
+   !  this subroutine compute the processor MPI task id "rank" of the processor  
+   !  whose cartesian coordinate are "row" and "col".
+   !  Note that the subroutine assume cyclic indexing ( row = nprow = 0 )
+   !
    IMPLICIT NONE
+   CHARACTER, INTENT(IN) :: order
    INTEGER, INTENT(OUT) ::  rank         ! process index starting from 0
    INTEGER, INTENT(IN)  ::  nprow, npcol  ! dimensions of the processor grid
    INTEGER, INTENT(IN)  ::  row, col
-   rank = row + col * nprow
+   
+   IF( order == 'C' .OR. order == 'c' ) THEN
+     !  grid in COLUMN MAJOR ORDER
+     rank = MOD( row + nprow, nprow ) + MOD( col + npcol, npcol ) * nprow
+   ELSE
+     !  grid in ROW MAJOR ORDER
+     rank = MOD( col + npcol, npcol ) + MOD( row + nprow, nprow ) * npcol
+   END IF
+   !
    RETURN
 END SUBROUTINE
