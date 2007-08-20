c573204d728f7112e6a3e1a33b4e024ef05b919c
cavazzon <cavazzon@c92efa57-630b-4861-b058-cf58834340f0>
2007-08-20 22:03:48 +0000

    - added a fully distributed memory subroutine pcdiaghg
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@4170 c92efa57-630b-4861-b058-cf58834340f0

--- PW/cdiaghg.f90
+++ PW/cdiaghg.f90
@@ -321,3 +321,148 @@ SUBROUTINE cdiaghg( n, m, h, s, ldh, e, v )
   RETURN
   !
 END SUBROUTINE cdiaghg
+!
+! fully data distributed subroutine
+!
+!----------------------------------------------------------------------------
+SUBROUTINE pcdiaghg( n, h, s, ldh, e, v, desc )
+  !----------------------------------------------------------------------------
+  !
+  ! ... calculates eigenvalues and eigenvectors of the generalized problem
+  ! ... Hv=eSv, with H hermitean matrix, S overlap matrix.
+  ! ... On output both matrix are unchanged
+  !
+  ! ... LAPACK version - uses both ZHEGV and ZHEGVX
+  ! ... the parallel version is also implemented
+  !
+  USE kinds,            ONLY : DP
+  USE mp,               ONLY : mp_bcast
+  USE mp_global,        ONLY : root_pool, intra_pool_comm
+  USE zhpev_module,     ONLY : pzhpev_drv
+  USE descriptors,      ONLY : descla_siz_ , lambda_node_ , nlax_ , la_nrl_ , la_nrlx_ , &
+                               la_npc_ , la_npr_ , la_me_ , la_comm_
+  !
+  IMPLICIT NONE
+  !
+  INTEGER, INTENT(IN) :: n, ldh
+    ! dimension of the matrix to be diagonalized
+    ! leading dimension of h, as declared in the calling pgm unit
+  COMPLEX(DP), INTENT(INOUT) :: h(ldh,ldh), s(ldh,ldh)
+    ! actually intent(in) but compilers don't know and complain
+    ! matrix to be diagonalized
+    ! overlap matrix
+  REAL(DP), INTENT(OUT) :: e(n)
+    ! eigenvalues
+  COMPLEX(DP), INTENT(OUT) :: v(ldh,ldh)
+    ! eigenvectors (column-wise)
+  INTEGER, INTENT(IN) :: desc( descla_siz_ )
+  !
+  INTEGER             :: nx, nrl, nrlx
+    ! local block size
+  COMPLEX(DP), ALLOCATABLE :: ss(:,:), hh(:,:)
+  COMPLEX(DP), ALLOCATABLE :: diag(:,:), vv(:,:)
+    ! work space used only in parallel diagonalization
+  !
+  ! ... input s and h are copied so that they are not destroyed
+  !
+  IF( desc( lambda_node_ ) > 0 ) THEN
+     !
+     nx   = desc( nlax_ )
+     nrl  = desc( la_nrl_ )
+     nrlx = desc( la_nrlx_ )
+     !
+     IF( nx /= ldh ) &
+        CALL errore(" pcdiaghg ", " inconsistent leading dimension ", ldh )
+     !
+     ALLOCATE( hh( nx, nx ) )
+     ALLOCATE( ss( nx, nx ) )
+     !
+     hh(1:nx,1:nx) = h(1:nx,1:nx)
+     ss(1:nx,1:nx) = s(1:nx,1:nx)
+     !
+  END IF
+
+  CALL start_clock( 'choldc' )
+  !
+  ! ... Cholesky decomposition of sl ( L is stored in sl )
+  !
+  IF( desc( lambda_node_ ) > 0 ) THEN
+     !
+     CALL pzpotf( ss, nx, n, desc )
+     !
+  END IF
+  !
+  CALL stop_clock( 'choldc' )
+  !
+  ! ... L is inverted ( sl = L^-1 )
+  !
+  CALL start_clock( 'inversion' )
+  !
+  IF( desc( lambda_node_ ) > 0 ) THEN
+     !
+     CALL pztrtri( ss, nx, n, desc )
+     !
+  END IF
+  !
+  CALL stop_clock( 'inversion' )
+  !
+  ! ... vl = L^-1*H
+  !
+  CALL start_clock( 'paragemm' )
+  !
+  IF( desc( lambda_node_ ) > 0 ) THEN
+     !
+     CALL sqr_zmm_cannon( 'N', 'N', n, ONE, ss, nx, hh, nx, ZERO, v, nx, desc )
+     !
+  END IF
+  !
+  ! ... hl = ( L^-1*H )*(L^-1)^T
+  !
+  IF( desc( lambda_node_ ) > 0 ) THEN
+     !
+     CALL sqr_zmm_cannon( 'N', 'C', n, ONE, v, nx, ss, nx, ZERO, hh, nx, desc )
+     !
+  END IF
+  !
+  CALL stop_clock( 'paragemm' )
+  !
+  IF ( desc( lambda_node_ ) > 0 ) THEN
+     ! 
+     !  Compute local dimension of the cyclically distributed matrix
+     !
+     ALLOCATE( diag( nrlx, n ) )
+     ALLOCATE( vv( nrlx, n ) )
+     !
+     CALL blk2cyc_zredist( n, diag, nrlx, hh, nx, desc )
+     !
+     CALL pzhpev_drv( 'V', diag, nrlx, e, vv, nrlx, nrl, n, &
+          desc( la_npc_ ) * desc( la_npr_ ), desc( la_me_ ), desc( la_comm_ ) )
+     !
+     CALL cyc2blk_zredist( n, vv, nrlx, hh, nx, desc )
+     !
+     DEALLOCATE( vv )
+     DEALLOCATE( diag )
+     !
+  END IF
+  !
+  ! ... v = (L^T)^-1 v
+  !
+  CALL start_clock( 'paragemm' )
+  !
+  IF ( desc( lambda_node_ ) > 0 ) THEN
+     !
+     CALL sqr_zmm_cannon( 'C', 'N', n, ONE, ss, nx, hh, nx, ZERO, v, nx, desc )
+     !
+  END IF
+  !
+  CALL mp_bcast( e, root_pool, intra_pool_comm )
+  !
+  CALL stop_clock( 'paragemm' )
+  !
+  IF ( desc( lambda_node_ ) > 0 ) THEN
+     DEALLOCATE( ss, hh )
+  END IF
+  !
+  RETURN
+  !
+END SUBROUTINE pcdiaghg
