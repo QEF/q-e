576deec9137432dbb9c894aa8177babf4c1808b2
paulatto <paulatto@c92efa57-630b-4861-b058-cf58834340f0>
2007-09-21 12:49:52 +0000

    Previous fix was breaking some init routines
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@4284 c92efa57-630b-4861-b058-cf58834340f0

--- PW/hinit0.f90
+++ PW/hinit0.f90
@@ -22,7 +22,8 @@ SUBROUTINE hinit0()
   USE wvfct,     ONLY : npw, g2kin, igk
   USE io_files,  ONLY : iunigk
   USE realus,    ONLY : tqr, qpointlist
-  USE grid_paw_routines, ONLY : init_prad, set_paw_rhoc, init_paw_vloc, paw_grid_setlocal
+  USE grid_paw_routines,  ONLY : init_prad, set_paw_rhoc, init_paw_vloc, paw_grid_setlocal
+  USE grid_paw_variables, ONLY : okpaw
   !
   IMPLICIT NONE
   !
@@ -32,12 +33,12 @@ SUBROUTINE hinit0()
   ! ... calculate the local part of the pseudopotentials
   !
   CALL init_vloc()
-  CALL init_paw_vloc() !!PAW!!
+  IF (okpaw) CALL init_paw_vloc() !!PAW!!
   !
   ! ... k-point independent parameters of non-local pseudopotentials
   !
   CALL init_us_1()
-  CALL init_prad() !!PAW!!
+  IF (okpaw) CALL init_prad() !!PAW!!
   CALL init_at_1()
   !
   REWIND( iunigk )
@@ -82,12 +83,12 @@ SUBROUTINE hinit0()
   ! ... calculate the total local potential
   !
   CALL setlocal()
-  CALL paw_grid_setlocal() !!PAW!!
+  IF (okpaw) CALL paw_grid_setlocal() !!PAW!!
   !
   ! ... calculate the core charge (if any) for the nonlinear core correction
   !
   CALL set_rhoc()
-  CALL set_paw_rhoc() !!PAW!!
+  IF (okpaw) CALL set_paw_rhoc() !!PAW!!
   !
   IF ( tqr ) CALL qpointlist()
   !
