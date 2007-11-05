0cd0c1b939bc3111865cc27f11a37a36bf4e5af4
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2007-11-05 17:38:31 +0000

    Makefile update and small correction to previous commit.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@4396 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/read_paw.f90
+++ Modules/read_paw.f90
@@ -329,7 +329,7 @@ subroutine set_pseudo_paw (is, pawset)
   ! PWSCF modules
   !
   USE atom,  ONLY: rgrid, msh
-  USE uspp_param, ONLY: upf
+  USE uspp_param, ONLY: upf, tvanp
   USE funct, ONLY: set_dft_from_name, dft_is_meta, dft_is_hybrid
   !
   USE pseudo_types
