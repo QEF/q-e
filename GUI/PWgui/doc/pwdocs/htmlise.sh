e1c6bca90fde57f8a5d370c7082ee890b44b52c6
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2008-02-15 18:19:43 +0000

    Synchronising(updating) PWgui with QE 4.0cvs. For changes see files: GUI/PWgui/ChangeLog and NEWS.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@4703 c92efa57-630b-4861-b058-cf58834340f0

--- GUI/PWgui/doc/pwdocs/htmlise.sh
+++ GUI/PWgui/doc/pwdocs/htmlise.sh
@@ -10,7 +10,7 @@ docfile=$3
 #version=$(ls $docfile-*)
 #version=${version#$docfile-}
 
-version=`grep def users-guide.tex | grep version | awk '{split($1,v,"[{}]"); print v[2]}'`
+version=`grep def users-guide.tex | grep '\\\def\\\version' | awk '{split($1,v,"[{}]"); print v[2]}'`
 
 echo "
 <html>
