c83af91293cafdef927a656a39303ef8b807139e
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2008-05-14 14:20:41 +0000

    adopting makefile accordingly to users-guide.tex to user_guide.html transition.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@4924 c92efa57-630b-4861-b058-cf58834340f0

--- GUI/PWgui/doc/pwdocs/htmlise.sh
+++ GUI/PWgui/doc/pwdocs/htmlise.sh
@@ -10,7 +10,8 @@ docfile=$3
 #version=$(ls $docfile-*)
 #version=${version#$docfile-}
 
-version=`grep def users-guide.tex | grep '\\\def\\\version' | awk '{split($1,v,"[{}]"); print v[2]}'`
+#version=`grep def users-guide.tex | grep '\\\def\\\version' | awk '{split($1,v,"[{}]"); print v[2]}'`
+version=4.0
 
 echo "
 <html>
