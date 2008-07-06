c946fc6b2da33d7c3d5db143ad32081b74ae5716
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2008-07-06 20:11:10 +0000

    Changed according to da Silva's suggestions
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@5044 c92efa57-630b-4861-b058-cf58834340f0

--- clib/stack.c
+++ clib/stack.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2007 PWSCF group
+  Copyright (C) 2007-2008 Quantum-Espressogroup
   This file is distributed under the terms of the
   GNU General Public License. See the file `License'
   in the root directory of the present distribution,
@@ -11,17 +11,19 @@
 #include <sys/resource.h>
 #include "c_defs.h"
 
-void F77_FUNC(remove_stack_limit,REMOVE_STACK_LIMIT) (void) {
+void F77_FUNC_(remove_stack_limit,REMOVE_STACK_LIMIT) (void) {
  
   struct rlimit rlim = { RLIM_INFINITY, RLIM_INFINITY };
 
-  /* Axel's version - sets default to 1 GB
-    if (0 == getrlimit(RLIMIT_STACK, &rlim)) {
-        rlim.rlim_cur=1<<31;
-        setrlimit(RLIMIT_STACK, &rlim);
-  */
+  /* Modified according to Cesar Da Silva suggestions */
   if ( setrlimit(RLIMIT_STACK, &rlim) == -1 ) {
-    perror("Cannot set stack size to infinity");
-    /* exit(1); */
+    if ( getrlimit(RLIMIT_STACK, &rlim) == 0 ) {
+      rlim.rlim_cur = rlim.rlim_max;
+      if ( setrlimit(RLIMIT_STACK, &rlim) == 0 ) {
+        getrlimit(RLIMIT_STACK, &rlim);
+      } else {
+        perror("  Cannot set stack size to new value");
+      }
+    }
   }
 }
