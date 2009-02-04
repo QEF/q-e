44ea523f337457579d20d1ac00d36882deaf8cbf
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2009-02-04 13:44:47 +0000

    Bug fix: problem with effective charges d Force / d E  in the noncollinear+NLCC case.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@5375 c92efa57-630b-4861-b058-cf58834340f0

--- PH/addnlcc_zstar_eu_us.f90
+++ PH/addnlcc_zstar_eu_us.f90
@@ -63,7 +63,7 @@ SUBROUTINE addnlcc_zstar_eu_us( drhoscf )
         !  compute the exchange and correlation potential for this mode
         !
         nrtot = nr1 * nr2 * nr3
-        fac = 1.d0 / DBLE (nspin)
+        fac = 1.d0 / DBLE (nspin0)
         DO ipert = 1, npe
            mode = imode0 + ipert
            
