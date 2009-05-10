61408183c7a550b6a5fd071155a9ddca3fb678a5
ccavazzoni <ccavazzoni@c92efa57-630b-4861-b058-cf58834340f0>
2009-05-10 09:16:57 +0000

    - int_to_char without WRITE statements.   In FORTRAN you cannot embed an I/O statement into an I/O statement.   So the FORTRAN statement:     WRITE(...) something // int_to_char(..)   where int_to_char contains another WRITE it is an ERROR.   In fact with some compiler QE was hanging on that function.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@5558 c92efa57-630b-4861-b058-cf58834340f0

--- flib/int_to_char.f90
+++ flib/int_to_char.f90
@@ -1,3 +1,4 @@
+
   !-----------------------------------------------------------------------
   FUNCTION int_to_char( i )
     !-----------------------------------------------------------------------
@@ -6,29 +7,31 @@
     !
     INTEGER, INTENT(IN) :: i
     CHARACTER (LEN=6)   :: int_to_char
-    !
+    CHARACTER :: c
+    INTEGER   :: n, j
     !   
-    IF ( i < 10 ) THEN
-       !
-       WRITE( UNIT = int_to_char , FMT = "(I1)" ) i
-       !
-    ELSE IF ( i < 100 ) THEN
-       !
-       WRITE( UNIT = int_to_char , FMT = "(I2)" ) i
-       !
-    ELSE IF ( i < 1000 ) THEN
-       !
-       WRITE( UNIT = int_to_char , FMT = "(I3)" ) i
-       !
-    ELSE IF ( i < 10000 ) THEN
-       !
-       WRITE( UNIT = int_to_char , FMT = "(I4)" ) i
-       !
-    ELSE      
-       ! 
-       WRITE( UNIT = int_to_char , FMT = "(I5)" ) i     
-       !
-    END IF    
+    IF( i < 0 ) &
+       CALL errore( ' int_to_char ', ' negative input not allowed ', 1 )
+    !   
+    n = i
+    j = 1
+    DO WHILE( j < 7 ) 
+       int_to_char(j:j) = CHAR( MOD( n, 10 ) + ICHAR( '0' ) )
+       n = n / 10
+       IF( n == 0 ) EXIT
+       j = j + 1
+    END DO
+    !
+    IF( j < 7 ) THEN
+       DO n = 1, j/2
+          c = int_to_char( n : n )
+          int_to_char( n : n ) = int_to_char( j-n+1 : j-n+1 )
+          int_to_char( j-n+1 : j-n+1 ) = c
+       END DO
+       IF( j < 6 ) int_to_char(j+1:6) = ' '
+    ELSE
+       int_to_char(1:6) = '*'
+    END IF
     !
     RETURN
     !
