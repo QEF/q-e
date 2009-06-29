c4ef18388bc5d89117974b1809f9abc55e3b2e35
paulatto <paulatto@c92efa57-630b-4861-b058-cf58834340f0>
2009-06-29 16:11:58 +0000

    Added f_defs.h: it may become necessary to add some workaround for erfc on some versions of the xlf compiler, better to keep the standard fortran definitions in all files
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@5634 c92efa57-630b-4861-b058-cf58834340f0

--- flib/erf.f90
+++ flib/erf.f90
@@ -5,6 +5,7 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
+#include "f_defs.h"
 !---------------------------------------------------------------------
 function erf (x)  
   !---------------------------------------------------------------------
