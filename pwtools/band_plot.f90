2f97fd5ee527a60cd954975ee57ff24a5873947b
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2009-07-16 12:37:44 +0000

    band_plot.x couldn't read the bands file created by bands.x: mismatch in the format. Fixing it.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@5693 c92efa57-630b-4861-b058-cf58834340f0

--- pwtools/band_plot.f90
+++ pwtools/band_plot.f90
@@ -6,39 +6,48 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 program prog
+  implicit none
   real, allocatable :: e(:,:)
   real, allocatable :: k(:,:)
   real, dimension(3) ::k0,a
+  integer nbnd, nbnd2, nks, i, n, j
+  real ef, dk
   character(len=32):: input, output
+  namelist/plot/ nbnd, nks
 
-  write(6,*) 'number of calculated bands' 
-  read(5,*) nbands
-  write(6,*) 'number of bands to be plotted'
-  read(5,*) nbands2
-  write(6,*) 'number of k-points'
-  read(5,*) nk
-  write(6,*) 'fermi level (eV)'
+  write(6,*) 'Number of bands to be plotted:'
+  read(5,*) nbnd2
+  write(6,*) 'Fermi level (eV):'
   read(5,*) ef
-  write(6,*) 'nome bande.in e bande.out'
-  read(5,*) input,output
+  write(6,*) 'Name of the bands file (produced by band.x):'
+  read(5,*) input
+  write(6,*) 'Name of the output file:'
+  read(5,*) output
 
-  allocate(e(nk,nbands))
-  allocate(k(nk,3))
   open(10,file=input, status='old')
-  open(11,file=output, status='new')
-  do i=1,nk
-     !  read(10,*) 
-     read(10,9020)(k(i,j),j=1,3)
-     write(6,*)(k(i,j),j=1,3)
-     !  read(10,*)
-     read(10,9030) (e(i,n),n=1,nbands)
-     write(13,9030) (e(i,n),n=1,nbands)
+  open(11,file=output, status='unknown')
+
+  read(10,plot)
+
+  if (nbnd2.gt.nbnd) nbnd2=nbnd
+
+  allocate(e(nks,nbnd))
+  allocate(k(nks,3))
+
+  do i=1,nks
+     read(10,*)(k(i,j),j=1,3)
+     write(6,9020)(k(i,j),j=1,3)
+
+     read(10,*) (e(i,n),n=1,nbnd)
+     write(6,9030) (e(i,n),n=1,nbnd)
+
 9020 format (14x,3f7.4)
 9030 format (8f9.4)  
   enddo
-  do j=1,nbands2
-     dk=0
-     do i=1,nk
+
+  do j=1,nbnd2
+     dk=0.0
+     do i=1,nks
         if (i.eq.1) then
            k0=k(i,:)
         endif
