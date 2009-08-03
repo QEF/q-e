34ab8d8084e7736689aaad2ff05546bf4727fb08
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2009-08-03 11:24:18 +0000

    simplifying the makefile: majority of work is now done by toplevel "make doc" target and Doc/Makefile and doc-def/Makefile.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@5807 c92efa57-630b-4861-b058-cf58834340f0

--- GUI/PWgui/doc/pwdocs/htmlise.sh
+++ GUI/PWgui/doc/pwdocs/htmlise.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 NAME program_name txt_docfile"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 txt_docfile"
     exit 1
 fi
 
-docfile=$3
-
-#version=$(ls $docfile-*)
-#version=${version#$docfile-}
-
-#version=`grep def users-guide.tex | grep '\\\def\\\version' | awk '{split($1,v,"[{}]"); print v[2]}'`
-version=4.0
+docfile=$1
 
 echo "
 <html>
@@ -24,7 +18,7 @@ echo "
 </html>" > tail.$$
 
 # filter out line: "*** FILE AUTOMATICALLY CREATED ..."
-awk '$0 !~ /FILE AUTOMATICALLY CREATED: DO NOT EDIT/ { print; }' $docfile > body.$$
+awk '$0 !~ /FILE AUTOMATICALLY CREATED: DO NOT EDIT, CHANGES WILL BE LOST/ { print; }' $docfile > body.$$
 sed 's/</\&lt\;/g' body.$$ | sed "s/>/\&gt\;/g" - > body2.$$
 
 cat head.$$ body2.$$ tail.$$
