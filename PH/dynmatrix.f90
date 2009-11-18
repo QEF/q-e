4cca843b9b5a639f67a6fb4655702cf0722476ee
obm <obm@c92efa57-630b-4861-b058-cf58834340f0>
2009-11-18 15:51:47 +0000

    A very small bug preventing compilation: lgamma was not declared from the corresponding unit
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@6175 c92efa57-630b-4861-b058-cf58834340f0

--- PH/dynmatrix.f90
+++ PH/dynmatrix.f90
@@ -30,7 +30,7 @@ subroutine dynmatrix
   USE gamma_gamma,   ONLY : nasr, asr, equiv_atoms, has_equivalent, &
                             n_diff_sites
   USE efield_mod,    ONLY : epsilon, zstareu, zstarue0, zstarue
-  USE control_ph,    ONLY : epsil, zue, lgamma_gamma, search_sym, ldisp, &
+  USE control_ph,    ONLY : epsil, zue, lgamma, lgamma_gamma, search_sym, ldisp, &
                             start_irr, last_irr, done_zue, where_rec, &
                             rec_code
   USE ph_restart,    ONLY : ph_writefile
