b56b03ed3f01db2ec572d4db4f7546e13bdbbcfc
nn245 <nn245@c92efa57-630b-4861-b058-cf58834340f0>
2010-03-02 15:15:51 +0000

    Updated pw2blip.f90 to latest version (arbitrary FFT grid)
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@6472 c92efa57-630b-4861-b058-cf58834340f0

--- PP/pw2blip.f90
+++ PP/pw2blip.f90
@@ -1,55 +1,84 @@
 MODULE pw2blip
    USE kinds, ONLY: DP
 
-   ! choose dense fft grid
-   USE fft_base, ONLY: dfft => dfftp
-   USE gvect, ONLY: map_igk_to_fft => nl,&
-                    NGvec => ngm
-
-   ! choose smooth fft grid
-!   USE fft_base, ONLY: dfft => dffts
-!   USE gsmooth, ONLY: map_igk_to_fft => nls,&
-!                      NGvec => ngms
-
-   ! choose coarse fft grid
-!   USE gcoarse, ONLY: dfft => dfftc
-!   USE gcoarse, ONLY: map_igk_to_fft => nlc
-
    USE io_global, ONLY: ionode, ionode_id
+   USE mp_global, ONLY: nproc_pool
 
    PRIVATE
-   PUBLIC pw2blip_init,pw2blip_cleanup,pw2blip_transform,pw2blip_gather
+   PUBLIC pw2blip_init,pw2blip_cleanup,pw2blip_transform
    PUBLIC blipgrid,cavc
 
+   INTEGER :: ngtot
    COMPLEX(dp),ALLOCATABLE :: psic(:),cavc_flat(:)
-   INTEGER :: blipgrid(3)
+   INTEGER :: blipgrid(3),ld_bg(3)
    REAL(dp),ALLOCATABLE :: gamma(:)
 
    INTEGER,PARAMETER :: gamma_approx = 1
    REAL(dp),PARAMETER :: pi = 3.14159265358979324d0
 
+   INTEGER,ALLOCATABLE :: map_igk_to_fft(:)
+   INTEGER,ALLOCATABLE :: do_fft_x(:),do_fft_y(:)
+
 CONTAINS
 
-   SUBROUTINE pw2blip_init
-      USE gvect, ONLY: ig1,ig2,ig3
+   SUBROUTINE pw2blip_init(ngtot_in,g_vec,multiplicity)
+      USE cell_base, ONLY: at
+      USE fft_scalar, ONLY: allowed, good_fft_dimension
 
+      INTEGER,INTENT(in) :: ngtot_in
+      REAL(dp),INTENT(in) :: g_vec(3,ngtot_in)
+      REAL(dp),INTENT(in) :: multiplicity
       REAL(dp) :: da(3),k,k2,k4,cosk
-      INTEGER :: ig,d,g_int(3)
+      INTEGER :: ig,d,g_int(3,ngtot_in),g_idx(3),dummy(0)
+      INTEGER,PARAMETER :: nmax = 5000
 
-      allocate(psic(dfft%nnr)) ! local FFT grid for transform
-      if(ionode)allocate(cavc_flat(dfft%nr1x*dfft%nr2x*dfft%nr3x)) ! global FFT grid for gather
-      blipgrid = (/ dfft%nr1,dfft%nr2,dfft%nr3 /)
+      ngtot = ngtot_in
+
+      do ig=1,ngtot
+         g_int(1,ig) = nint (sum(g_vec(:,ig) * at (:,1)))
+         g_int(2,ig) = nint (sum(g_vec(:,ig) * at (:,2)))
+         g_int(3,ig) = nint (sum(g_vec(:,ig) * at (:,3)))
+      enddo
+
+      ! choose size of blip grid in real space
+      do d=1,3
+         blipgrid(d) = 2*ceiling(dble(maxval(abs(g_int(d,:))))*multiplicity)+2
+         do while(.not.allowed(blipgrid(d)))
+            blipgrid(d) = blipgrid(d) + 1
+         enddo
+         if (blipgrid(d)>nmax) &
+            call errore ('set_fft_dim', 'blipgrid is unreasonably large', blipgrid(d))
+      enddo
+
+      ! set up leading dimensions of fft data array
+      ld_bg(1) = good_fft_dimension(blipgrid(1))
+      ld_bg(2) = blipgrid(2)
+      ld_bg(3) = blipgrid(3)
+
+! Set up indices to fft grid: map_igk_to_fft
+      allocate(map_igk_to_fft(ngtot))
+      allocate(do_fft_x(blipgrid(3)*ld_bg(2)),do_fft_y(blipgrid(3)))
+      do_fft_x(:)=0
+      do_fft_y(:)=0
+      do ig=1,ngtot
+         g_idx(:) = modulo(g_int(:,ig),blipgrid(:))
+         do_fft_x(1 + g_idx(2) + ld_bg(2)*g_idx(3)) = 1
+         do_fft_y(1 + g_idx(3)) = 1
+         map_igk_to_fft (ig) = 1 + g_idx(1) + ld_bg(1)*(g_idx(2) + ld_bg(2)*g_idx(3))
+      enddo
+
+! Set up blipgrid
+      allocate(psic(ld_bg(1)*ld_bg(2)*ld_bg(3))) ! local FFT grid for transform
 
 ! Calculating gamma.
-      allocate(gamma(NGvec))
+      allocate(gamma(ngtot))
       gamma(:) = 1.d0
-      da(1:3)=2.d0*pi/dble( (/ dfft%nr1,dfft%nr2,dfft%nr3 /) )
+      da(1:3)=2.d0*pi/dble( blipgrid(:) )
       if(gamma_approx==1)then
-         do ig=1,NGvec
-            g_int(:) = (/ig1(ig),ig2(ig),ig3(ig)/)
+         do ig=1,ngtot
             do d=1,3
-               if(g_int(d)/=0)then
-                  k=da(d)*dble(g_int(d)) ; cosk=cos(k) ; k2=k*k ; k4=k2*k2
+               if(g_int(d,ig)/=0)then
+                  k=da(d)*dble(g_int(d,ig)) ; cosk=cos(k) ; k2=k*k ; k4=k2*k2
                   gamma(ig)=gamma(ig)*k4/(6.d0*((cosk-2.d0)*cosk+1.d0))
                else
                   gamma(ig)=gamma(ig)*2.d0/3.d0
@@ -57,9 +86,12 @@ CONTAINS
             enddo
          enddo ! ig
       elseif(gamma_approx==2)then
-         do ig=1,NGvec
-            gamma(ig)=1.d0/((1.d0+0.5d0*cos(da(1)*g(1,ig))) &
-               &*(1.d0+0.5d0*cos(da(2)*g(2,ig)))*(1.d0+0.5d0*cos(da(3)*g(3,ig))))
+         do ig=1,ngtot
+            gamma(ig)=1.d0/(&
+               & (1.d0+0.5d0*cos(da(1)*g_vec(1,ig))) &
+               &*(1.d0+0.5d0*cos(da(2)*g_vec(2,ig))) &
+               &*(1.d0+0.5d0*cos(da(3)*g_vec(3,ig))) &
+               &)
          enddo ! ig
       else
          write(6,*)'Bug: bad gamma_approx.' ; stop
@@ -69,50 +101,25 @@ CONTAINS
 
    SUBROUTINE pw2blip_cleanup
       deallocate(psic,gamma)
-      if(ionode)deallocate(cavc_flat)
+      deallocate(map_igk_to_fft,do_fft_x,do_fft_y)
    END SUBROUTINE pw2blip_cleanup
 
    SUBROUTINE pw2blip_transform(psi)
-      USE wvfct, ONLY: npwx ,& ! allocation size of psi
-                       npw  ,& ! number of plane waves at this k point
-                       igk     ! map from plane waves in psi to k-independent indices
-      USE fft_parallel, ONLY: tg_cft3s
+      USE fft_scalar, ONLY: cfft3ds
 
-      COMPLEX(DP), INTENT(in) :: psi(npwx)
-      INTEGER :: ipw,ig
+      COMPLEX(DP), INTENT(in) :: psi(ngtot)
 
-      ! reciprocal space is distributed in x-y sticks
       psic (:) = (0.d0, 0.d0)
-      psic (map_igk_to_fft (igk(1:npw))) = psi(1:npw)*gamma(igk(1:npw))
+      psic (map_igk_to_fft (1:ngtot)) = psi(1:ngtot)*gamma(1:ngtot)
 
       ! perform the transformation
-      call tg_cft3s (psic, dfft, +2)
-      ! real space is distributed in z planes
-   END SUBROUTINE
-
-   SUBROUTINE pw2blip_gather
-      USE mp_global, ONLY: nproc_pool, intra_pool_comm
-      USE mp, ONLY: mp_gather
-
-      INTEGER :: proc, info
-      INTEGER :: displs(0:nproc_pool-1), recvcount(0:nproc_pool-1)
-
-      do proc = 0, ( nproc_pool - 1 )
-         recvcount(proc) = dfft%nnp * dfft%npp(proc+1)
-         if( proc == 0 )then
-            displs(proc) = 0
-         else
-            displs(proc) = displs(proc-1) + recvcount(proc-1)
-         endif
-      enddo
-      info = 0
-      call mp_gather( psic, cavc_flat, &
-       & recvcount, displs, ionode_id, intra_pool_comm )
+      call cfft3ds (psic,blipgrid(1),blipgrid(2),blipgrid(3),&
+       &ld_bg(1),ld_bg(2),ld_bg(3),+1,do_fft_x(:),do_fft_y(:))
    END SUBROUTINE
 
    COMPLEX(dp) FUNCTION cavc(i1,i2,i3)
       INTEGER,INTENT(in) :: i1,i2,i3
-      cavc = cavc_flat(i1+dfft%nr1x*(i2-1+dfft%nr3x*(i3-1)))
+      cavc = psic(i1+ld_bg(2)*(i2-1+ld_bg(3)*(i3-1)))
    END FUNCTION cavc
 
 END MODULE
