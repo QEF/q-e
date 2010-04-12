015ced7203fb1b1d988b258a2f1747737c85cf61
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2010-04-12 19:20:14 +0000

    More occurrences of nonstandard forms for "Quantum ESPRESSO"
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@6618 c92efa57-630b-4861-b058-cf58834340f0

--- GWW/pw4gww/pw4gww.f90
+++ GWW/pw4gww/pw4gww.f90
@@ -1,4 +1,4 @@
-! Copyright (C) 2003-2006 Andrea Ferretti and Quantum-Espresso group
+! Copyright (C) 2003-2006 Andrea Ferretti and Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
