65801e8a65e9794e9859ebef5296b87961c440a8
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2010-06-29 08:36:55 +0000

    AE wavefunctions are written together with PS wavefunctions, on the right-hand side of the wavefunction file. The file format is compatible with the previous one, but plotting of AE and PS wavefunctions is easier.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@6873 c92efa57-630b-4861-b058-cf58834340f0

--- atomic/write_resultsps.f90
+++ atomic/write_resultsps.f90
@@ -1,169 +1,167 @@
 !
-! Copyright (C) 2004-2009 Quantum ESPRESSO group
+! Copyright (C) 2004-2010 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !--------------------------------------------------------------
-subroutine write_resultsps ( )
+SUBROUTINE write_resultsps ( )
   !--------------------------------------------------------------
-  use kinds,     only : dp
-  use radial_grids, only : ndmx
-  use io_global, only : stdout, ionode, ionode_id
-  use mp,        only : mp_bcast
-  use constants, only : eps6
-  use ld1inc,    only : title, rel, zed, zval, lsd, isic, latt, beta, tr2, &
+  USE kinds,     ONLY : dp
+  USE radial_grids, ONLY : ndmx
+  USE io_global, ONLY : stdout, ionode, ionode_id
+  USE mp,        ONLY : mp_bcast
+  USE constants, ONLY : eps6
+  USE ld1inc,    ONLY : title, rel, zed, zval, lsd, isic, latt, beta, tr2, &
                   nwfts, nnts, llts, jjts, elts, octs, iswts, enlts, nstoaets, &
                   grid, enl,  eps0, iter, etot, etots, etot0, lpaw, &
                   etots0, ekin, encl, ehrt, ecxc, nlcc, ecc, evxt, epseu, &
                   dhrsic, dxcsic, file_wavefunctionsps, phits, rytoev_fact, &
                   verbosity, frozen_core, ae_fc_energy, jj
-  use ld1inc,    only : nwf, el, psi, rcut
-  use funct, only: get_dft_name
-  implicit none
+  USE ld1inc,    ONLY : nwf, el, psi, rcut
+  USE funct, ONLY: get_dft_name
+  IMPLICIT NONE
 
-  integer, parameter :: max_out_wfc=6
-  integer :: counter
-  real(DP) :: psiaux(ndmx,max_out_wfc,2), phase
-  character (len=2) :: elaux(max_out_wfc)
-  ! max_out_wfc = max number of wfcts written to file
-  ! you need to change the format as well if you increase this
+  INTEGER, PARAMETER :: max_out_wfc=6
+  ! max_out_wfc = max number of PS wfcts written to file
+  INTEGER :: counter
+  real(DP) :: psiaux(ndmx,2*max_out_wfc), phase
+  CHARACTER (len=2) :: elaux(2*max_out_wfc)
 
-  integer :: i, j, n, ios
-  character (len=20) :: dft_name
+  INTEGER :: i, j, n, ios
+  CHARACTER (len=20) :: dft_name
   !
   !
   dft_name = get_dft_name()
-  write(stdout,110)
-110 format (/,5x,22('-'),' Testing the pseudopotential ',22('-'),/)
-  write(stdout,1150) title
-  if(rel.eq.1) write(stdout,'(5x,''scalar relativistic calculation'')')
-  if(rel.eq.2) write(stdout,'(5x,''dirac relativistic calculation'')')
-1150 format(5x,a75)
-  write(stdout,1250) zed, zval
-1250 format(/5x,'atomic number is',f6.2,'   valence charge is',f6.2)
-  write(stdout,2300) dft_name(1:len_trim(dft_name)),lsd,isic,latt,beta,tr2
-2300 format(5x,'dft =',a,'   lsd =',i1,' sic =',i1,' latt =',i1, &
+  WRITE(stdout,"(/,5x,22('-'),' Testing the pseudopotential ',22('-'),/)")
+  WRITE(stdout,'(5x,a75)') title
+  IF(rel==1) WRITE(stdout,'(5x,''scalar relativistic calculation'')')
+  IF(rel==2) WRITE(stdout,'(5x,''dirac relativistic calculation'')')
+  WRITE(stdout,"(/5x,'atomic number is',f6.2,'   valence charge is',f6.2)") &
+       zed, zval
+  WRITE(stdout,100) dft_name(1:len_trim(dft_name)),lsd,isic,latt,beta,tr2
+100 FORMAT(5x,'dft =',a,'   lsd =',i1,' sic =',i1,' latt =',i1, &
        '  beta=',f4.2,' tr2=',1pe7.1)
-  write(stdout,1270) grid%mesh,grid%r(grid%mesh),grid%xmin,grid%dx
-1270 format(5x,'mesh =',i4,' r(mesh) =',f10.5,' xmin =',f6.2,' dx =',f8.5)
-  if (rel.lt.2) then
-     write(stdout,1000)
-1000 format(/5x,'n l     nl             e AE (Ry) ',  &
+  WRITE(stdout,200) grid%mesh,grid%r(grid%mesh),grid%xmin,grid%dx
+200 FORMAT(5x,'mesh =',i4,' r(mesh) =',f10.5,' xmin =',f6.2,' dx =',f8.5)
+  IF (rel<2) THEN
+     WRITE(stdout,300)
+300 FORMAT(/5x,'n l     nl             e AE (Ry) ',  &
           '       e PS (Ry)    De AE-PS (Ry) ')
-     do n=1,nwfts
+     DO n=1,nwfts
         IF (verbosity=='high') THEN
-           if (octs(n)>-eps6) write(stdout,1104) &
+           IF (octs(n)>-eps6) WRITE(stdout,401) &
                 nnts(n),llts(n),elts(n),iswts(n),octs(n), &
                 enl(nstoaets(n)),enlts(n), &
                 enl(nstoaets(n))-enlts(n)
         ELSE
-           if (octs(n)>-eps6) write(stdout,1100) &
+           IF (octs(n)>-eps6) WRITE(stdout,400) &
                 nnts(n),llts(n),elts(n),iswts(n),octs(n), &
                 enl(nstoaets(n)),enlts(n), &
                 enl(nstoaets(n))-enlts(n)
         ENDIF
-     enddo
-     if (ionode) write(13,1100)  &
+     ENDDO
+     IF (ionode) WRITE(13,400)  &
           (nnts(n),llts(n),elts(n),iswts(n),octs(n), &
           enl(nstoaets(n)),enlts(n),  &
           enl(nstoaets(n))-enlts(n),  n=1,nwfts)
-1100 format(4x,2i2,5x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.5)
-1104 format(4x,2i2,5x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.8)
-  else
-     write(stdout,1001)
-1001 format(/5x,'n l  j  nl             e AE (Ry)',  &
+400 FORMAT(4x,2i2,5x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.5)
+401 FORMAT(4x,2i2,5x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.8)
+  ELSE
+     WRITE(stdout,500)
+500 FORMAT(/5x,'n l  j  nl             e AE (Ry)',  &
           '       e PS (Ry)    De AE-PS (Ry) ')
-     do n=1,nwfts
+     DO n=1,nwfts
         IF (verbosity=='high') THEN
-           if(octs(n)>-eps6) write(stdout,1105) &
+           IF(octs(n)>-eps6) WRITE(stdout,601) &
                nnts(n),llts(n),jjts(n),elts(n),iswts(n),octs(n), &
                enl(nstoaets(n)),enlts(n), enl(nstoaets(n))-enlts(n)
         ELSE
-           if(octs(n)>-eps6) write(stdout,1101) &
+           IF(octs(n)>-eps6) WRITE(stdout,600) &
                 nnts(n),llts(n),jjts(n),elts(n),iswts(n),octs(n), &
                 enl(nstoaets(n)),enlts(n), enl(nstoaets(n))-enlts(n)
         ENDIF
-     enddo
-     if (ionode) write(13,1101)  &
+     ENDDO
+     IF (ionode) WRITE(13,600)  &
           (nnts(n),llts(n),jjts(n),elts(n),iswts(n),octs(n), &
           enl(nstoaets(n)),enlts(n),  &
           enl(nstoaets(n))-enlts(n),  n=1,nwfts)
-1101 format(4x,2i2,f4.1,1x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.5)
-1105 format(4x,2i2,f4.1,1x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.8)
-  endif
-  write(stdout,1200) eps0,iter
-1200 format(/5x,'eps =',1pe8.1,'  iter =',i3)
-  write(stdout,*)
-  write(stdout,1211) etot, etot*0.5_dp, etot*rytoev_fact
-1211 format (5x,'Etot =',f15.6,' Ry,',f15.6, ' Ha,',f15.6,' eV')
-  write(stdout,1221) etots, etots*0.5_dp, etots*rytoev_fact
-1221 format (5x,'Etotps =',f13.6,' Ry,',f15.6,' Ha,',f15.6,' eV') 
-  if (frozen_core.or.(verbosity=='high'.and.lpaw)) &
-       write(stdout,1222) ae_fc_energy, ae_fc_energy*0.5_dp, &
+600 FORMAT(4x,2i2,f4.1,1x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.5)
+601 FORMAT(4x,2i2,f4.1,1x,a2,i4,'(',f5.2,')',f15.5,f15.5,f15.8)
+  ENDIF
+  WRITE(stdout,"(/5x,'eps =',1pe8.1,'  iter =',i3)") eps0,iter
+  WRITE(stdout,*)
+  WRITE(stdout,700) etot, etot*0.5_dp, etot*rytoev_fact
+700 FORMAT (5x,'Etot =',f15.6,' Ry,',f15.6, ' Ha,',f15.6,' eV')
+  WRITE(stdout,800) etots, etots*0.5_dp, etots*rytoev_fact
+800 FORMAT (5x,'Etotps =',f13.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
+  IF (frozen_core.or.(verbosity=='high'.and.lpaw)) &
+       WRITE(stdout,900) ae_fc_energy, ae_fc_energy*0.5_dp, &
                                            ae_fc_energy*rytoev_fact
-1222 format (5x,'Etotfc =',f13.6,' Ry,',f15.6,' Ha,',f15.6,' eV') 
+900 FORMAT (5x,'Etotfc =',f13.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
 
-  if (abs(etot-etot0)> 1.d-9) then 
-     write(stdout,1231) etot-etot0
-1231 format (5x,'dEtot_ae =',f15.6,' Ry') 
-     write(stdout,1241) etots-etots0, etot-etot0 - (etots-etots0)
-1241 format (5x,'dEtot_ps =',f15.6,' Ry,','   Delta E=',f15.6,' Ry' )
-     if (ionode) write(13,'(5x,''dEtot_ae ='',f15.6,'' Ry'')') etot-etot0
-     if (ionode) write(13,&
+  IF (abs(etot-etot0)> 1.d-9) THEN
+     WRITE(stdout,"(5x,'dEtot_ae =',f15.6,' Ry')") etot-etot0
+     WRITE(stdout,1000) etots-etots0, etot-etot0 - (etots-etots0)
+1000 FORMAT (5x,'dEtot_ps =',f15.6,' Ry,','   Delta E=',f15.6,' Ry' )
+     IF (ionode) WRITE(13,'(5x,''dEtot_ae ='',f15.6,'' Ry'')') etot-etot0
+     IF (ionode) WRITE(13,&
        '(5x,''dEtot_ps ='',f15.6,'' Ry,'',''   Delta E='', f15.6,'' Ry'' )') &
           etots-etots0, etot-etot0-(etots-etots0)
-  else
-     if (ionode) write(13,1211) etot, etot*0.5_dp, etot*rytoev_fact
-     if (ionode) write(13,1221) etots, etots*0.5_dp, etots*rytoev_fact
-  endif
-  write(stdout,1251) ekin, ekin*0.5_dp, ekin*rytoev_fact
-1251 format (/,5x,'Ekin =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
+  ELSE
+     IF (ionode) WRITE(13,700) etot, etot*0.5_dp, etot*rytoev_fact
+     IF (ionode) WRITE(13,800) etots, etots*0.5_dp, etots*rytoev_fact
+  ENDIF
+  WRITE(stdout,1100) ekin, ekin*0.5_dp, ekin*rytoev_fact
+1100 FORMAT (/,5x,'Ekin =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
 
-  write(stdout,1261) encl, encl*0.5_dp, encl*rytoev_fact
-1261 format (5x,'Encl =',f15.6,' Ry,',f15.6, ' Ha,',f15.6,' eV') 
-  write(stdout,1271) ehrt, ehrt*0.5_dp, ehrt*rytoev_fact
-1271 format (5x,'Ehrt =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV') 
-  write(stdout,1281) ecxc, ecxc*0.5_dp, ecxc*rytoev_fact
-1281 format (5x,'Ecxc =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
-  if (nlcc) write(stdout,1282) ecc, ecc*0.5_dp, ecc*rytoev_fact
-1282 format (5x,'(Ecc =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV)')
-  if (ABS(evxt)>0.0_DP) &
-     write(stdout,1291) evxt, evxt*0.5_dp, evxt*rytoev_fact
-1291 format(5x,'Evxt =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
-  if (ABS(epseu)>0.0_DP) &
-     write(stdout,1292) epseu, epseu*0.5_dp, epseu*rytoev_fact
-1292 format (5x,'Epseu=',f15.6,' Ry,',f15.6, ' Ha,',f15.6,' eV') 
-  if(isic.ne.0) write(stdout,1300) dhrsic+dxcsic, dhrsic, dxcsic
-1300 format(5x,'desic:'/5x,0pf12.4,24x,2(0pf12.4))
-  write(stdout,120)
-120 format (/,5x,22('-'), ' End of pseudopotential test ',22('-'),/)
+  WRITE(stdout,1200) encl, encl*0.5_dp, encl*rytoev_fact
+1200 FORMAT (5x,'Encl =',f15.6,' Ry,',f15.6, ' Ha,',f15.6,' eV')
+  WRITE(stdout,1271) ehrt, ehrt*0.5_dp, ehrt*rytoev_fact
+1271 FORMAT (5x,'Ehrt =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
+  WRITE(stdout,1281) ecxc, ecxc*0.5_dp, ecxc*rytoev_fact
+1281 FORMAT (5x,'Ecxc =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
+  IF (nlcc) WRITE(stdout,1282) ecc, ecc*0.5_dp, ecc*rytoev_fact
+1282 FORMAT (5x,'(Ecc =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV)')
+  IF (abs(evxt)>0.0_DP) &
+     WRITE(stdout,1291) evxt, evxt*0.5_dp, evxt*rytoev_fact
+1291 FORMAT(5x,'Evxt =',f15.6,' Ry,',f15.6,' Ha,',f15.6,' eV')
+  IF (abs(epseu)>0.0_DP) &
+     WRITE(stdout,1292) epseu, epseu*0.5_dp, epseu*rytoev_fact
+1292 FORMAT (5x,'Epseu=',f15.6,' Ry,',f15.6, ' Ha,',f15.6,' eV')
+  IF(isic/=0) WRITE(stdout,1300) dhrsic+dxcsic, dhrsic, dxcsic
+1300 FORMAT(5x,'desic:'/5x,0pf12.4,24x,2(0pf12.4))
+  WRITE(stdout,120)
+120 FORMAT (/,5x,22('-'), ' End of pseudopotential test ',22('-'),/)
   !
-  if (ionode) write(13,*)
+  IF (ionode) WRITE(13,*)
   !
-  if (file_wavefunctionsps.ne.' ') then
+  IF (file_wavefunctionsps/=' ') THEN
      counter=1
-     do i=nwfts,1,-1
+     DO i=nwfts,1,-1
+        IF (counter > max_out_wfc) exit
         elaux(counter)=elts(i)
-        psiaux(:,counter,1)=phits(:,i)
-        do j=nwf,1,-1
-           if ( elts(i) == el(j) .and. jjts(i)==jj(j) ) then
-              do n=grid%mesh,1,-1
-                 phase = psiaux(n,counter,1)*psi(n,1,j)
-                 if ( abs(phase) > 1.d-12 ) then
+        psiaux(:,counter)=phits(:,i)
+        DO j=nwf,1,-1
+           IF ( elts(i) == el(j) .and. jjts(i)==jj(j) ) THEN
+              DO n=grid%mesh,1,-1
+                 phase = psiaux(n,counter)*psi(n,1,j)
+                 IF ( abs(phase) > 1.d-12 ) THEN
                     phase = phase / abs(phase)
                     exit
-                 end if
-              end do
-              psiaux(:,counter,2)=psi(:,1,j)*phase
-           end if
-        end do
+                 ENDIF
+              ENDDO
+              psiaux(:,nwfts+counter)=psi(:,1,j)*phase
+              elaux(nwfts+counter)=el(j)
+              exit
+           ENDIF
+        ENDDO
         counter=counter+1
-        if (counter>max_out_wfc) exit
-     enddo
-     call write_wfcfile(file_wavefunctionsps,psiaux,elaux,counter-1)
-  endif
+     ENDDO
+     counter = counter - 1
+     CALL write_wfcfile(file_wavefunctionsps,psiaux,elaux,2*counter)
+  ENDIF
 
-  return
-end subroutine write_resultsps
+  RETURN
+END SUBROUTINE write_resultsps
