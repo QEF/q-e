b91c455bb252c4dcee44761d28a24ef3a65a96f1
degironc <degironc@c92efa57-630b-4861-b058-cf58834340f0>
2010-08-19 15:30:58 +0000

    clean-up of the md5_from_file routine R.Sabatini (via SdG)
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@6995 c92efa57-630b-4861-b058-cf58834340f0

--- clib/md5_from_file.c
+++ clib/md5_from_file.c
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2010 Quantum ESPRESSO group
+ Copyright (C) 2005-2008 Quantum ESPRESSO group
  This file is distributed under the terms of the
  GNU General Public License. See the file `License'
  in the root directory of the present distribution,
@@ -17,18 +17,6 @@
 
 #define MAX_BUF 1024 
 
-//char *strCopy( char * str ) 
-//{ 
-//  if( !str ) 
-//  { 
-//    return NULL; 
-//  } 
-   
-//  char *n = ( char * )malloc( strlen( str ) + 1 ); 
-//  memcpy( n, str, strlen( str ) + 1 ); 
-   
-//  return n; 
-//} 
 
 static void fatal ( const char * msg )
 {
@@ -44,7 +32,7 @@ static void * xcmalloc ( size_t size )
   register void * ptr = malloc( size ) ;
 
   if ( ptr == NULL )
-    fatal( "fatal: virtual memory exhausted" ) ;
+    fatal( "md5_from_file: virtual memory exhausted" ) ;
   else
     memset( ptr , 0 , size ) ;
 
@@ -88,47 +76,36 @@ void get_md5(const char *file, char *md5, int err)
  
      FILE *fp;
      char *data;
-     //char THISMD5[31];
      md5_state_t state;
      md5_byte_t digest[16];
      
-     //printf("     GET_MD5: check string existence -%s- ",file);
      if(file==NULL) { 
 	err = 1;
         return;
      }
-     //printf("...ok \n");
 
-     //printf("     GET_MD5: check file opening -%s- ",file);
      fp=fopen(file,"rb");
      if(fp==NULL) {
 	err = 2;
 	return;
      }
-     //printf("...ok \n");
 
-     //printf("     GET_MD5: check file reading -%s- ",file);
      data=readFile(fp);
      if(data==NULL) {
 	err = 3;
 	return;
      }
-     //printf("...ok \n");
 
-     //printf("     GET_MD5: file %s \n",data);
      md5_init(&state);
      md5_append(&state,(const md5_byte_t *)data,strlen(data));
      md5_finish(&state,digest);
 
      int i=0;
      for(i;i<16;i++){
-           //snprintf(THISMD5+i*2,sizeof(THISMD5),"%02x",digest[i]);
 	   snprintf(md5+i*2,sizeof(md5),"%02x",digest[i]);
      }
      fclose(fp);
 
-     //strcpy(md5, THISMD5);
-     //md5 = &THISMD5;
      err = 0;
      return;
 }
@@ -143,12 +120,9 @@ int F77_FUNC_(file_md5,FILE_MD5)( const int * f_name, const int * f_len, int * o
 
      f[*f_len] = '\0' ;
 
-
-     //printf("     C: md5 of file %s \n",f); 
      get_md5( f ,  md5, err) ;
-     //printf("     C: md5 status %i \n",err);
-     //printf("     C: md5 calculated %s \n",md5);     
      for( i = 0; i < 32; i++ ) out[ i ] = md5[ i ]; 
     
      return err;
 } 
+
