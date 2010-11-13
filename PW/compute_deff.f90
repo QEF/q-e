c29533b63fee94de96c397e65b5f932feaa43d77
degironc <degironc@c92efa57-630b-4861-b058-cf58834340f0>
2010-11-13 08:45:42 +0000

    adding one more copyright statement to a file
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@7212 c92efa57-630b-4861-b058-cf58834340f0

--- PW/compute_deff.f90
+++ PW/compute_deff.f90
@@ -1,3 +1,11 @@
+!
+! Copyright (C) 2009-2010 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!---------------------------------------------------------------------------
 SUBROUTINE compute_deff(deff, et)
 !
 !  This routine computes the effective value of the D-eS coefficients
