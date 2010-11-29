602cd50081950768ae2ca980618d457b0555c487
marsamos <marsamos@c92efa57-630b-4861-b058-cf58834340f0>
2010-11-29 15:59:30 +0000

    added plugin_arguments and plugin_flags file in modules
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@7263 c92efa57-630b-4861-b058-cf58834340f0

---
+++ Modules/plugin_arguments.f90
@@ -0,0 +1,53 @@
+!
+! Copyright (C) 2002-2009 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!
+!----------------------------------------------------------------------------
+SUBROUTINE plugin_arguments()
+  !-----------------------------------------------------------------------------
+  !
+  ! the name of each new plugin has to be set here. Its use or not
+  ! is controlled by its presence as argument
+  !
+  !
+  USE kinds,         ONLY : DP
+  !
+  USE io_global,     ONLY : stdout, ionode, ionode_id
+  USE mp_global,     ONLY : intra_image_comm
+  !
+  USE plugin_flags
+  !
+  USE mp,            ONLY : mp_bcast
+  !
+  IMPLICIT NONE
+  !
+  INTEGER  :: iiarg, nargs, iargc, ierr
+  CHARACTER (len=50) :: arg
+  !
+  !
+#if defined(__ABSOFT)
+#   define getarg getarg_
+#   define iargc  iargc_
+#endif
+  !
+  nargs = iargc()
+  !
+  !
+  DO iiarg = 1, ( nargs -1 )
+    CALL getarg( iiarg, plugin_name)
+write(0,*) "plugin_name: ", trim(plugin_name)
+    IF((TRIM(plugin_name)=='-plumed').or.(TRIM(plugin_name)=='-PLUMED')) THEN
+      use_plumed = .true.
+      CALL mp_bcast( use_plumed, intra_image_comm )
+write(0,*) "use_plumed= ", use_plumed
+    ENDIF
+  ENDDO
+  !
+  !
+  RETURN
+  !
+END SUBROUTINE plugin_arguments
---
+++ Modules/plugin_flags.f90
@@ -0,0 +1,31 @@
+!
+! Copyright (C) 2002-2007 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!=----------------------------------------------------------------------------=!
+MODULE plugin_flags
+  !=--------------------------------------------------------------------------=!
+  !
+  ! ... this module contains all basic variables that controls
+  ! ... the use or not of plugins.
+  !----------------------------------------------
+  !
+  USE kinds
+  USE parameters
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+  PRIVATE
+  !
+  !
+  ! ...   declare execution control variables
+  !
+  CHARACTER(LEN=256), PUBLIC :: plugin_name
+  LOGICAL, PUBLIC :: use_plumed
+  !
+END MODULE plugin_flags
