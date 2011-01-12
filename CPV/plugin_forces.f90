28851554594d617ac1be9f807ad817932ed40721
marsamos <marsamos@c92efa57-630b-4861-b058-cf58834340f0>
2011-01-12 11:13:19 +0000

    missing intra_image_comm in plugin_froces for CP
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@7399 c92efa57-630b-4861-b058-cf58834340f0

--- CPV/plugin_forces.f90
+++ CPV/plugin_forces.f90
@@ -10,6 +10,7 @@ SUBROUTINE plugin_forces()
   !----------------------------------------------------------------------------
   !
   !
+  USE mp_global,        ONLY : intra_image_comm
   USE mp,               ONLY : mp_bcast
   USE io_global,        ONLY : stdout, ionode, ionode_id
   USE kinds,            ONLY : DP
