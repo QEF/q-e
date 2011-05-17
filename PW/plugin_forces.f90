b077c491e52a0d36c1b84918d554364654e7fea2
marsamos <marsamos@c92efa57-630b-4861-b058-cf58834340f0>
2011-05-17 09:39:46 +0000

    forgot to clean patches before commit
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@7786 c92efa57-630b-4861-b058-cf58834340f0

--- PW/plugin_forces.f90
+++ PW/plugin_forces.f90
@@ -9,7 +9,7 @@
 SUBROUTINE plugin_forces()
   !----------------------------------------------------------------------------
   !
-  !  
+  !
   USE mp_global,        ONLY : intra_image_comm
   USE mp,               ONLY : mp_bcast
   USE io_global,        ONLY : stdout, ionode, ionode_id
@@ -18,25 +18,7 @@ SUBROUTINE plugin_forces()
   !
   USE plugin_flags
   !
-  USE cell_base,        ONLY : alat, at
-  USE ions_base,        ONLY : tau, nat
-  USE force_mod,        ONLY : force
-  USE control_flags,    ONLY : istep
-  !
   IMPLICIT NONE
   !
-  REAL(DP) :: at_meta(3,3)
-  REAL(DP), ALLOCATABLE :: tau_meta(:,:)
-  !
-  IF(use_plumed) then
-    IF(ionode)THEN
-      at_meta=alat*at;
-      allocate(tau_meta(3,nat))
-      tau_meta=alat*tau
-      call meta_force_calculation(at_meta,istep,tau_meta(1,1),0,0,force(1,1),0,0,0)
-      deallocate(tau_meta)
-    ENDIF
-    CALL mp_bcast(force, ionode_id, intra_image_comm)
-  ENDIF
   !
 END SUBROUTINE plugin_forces
