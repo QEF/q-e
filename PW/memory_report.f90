2d77d759315ddf463dcd5b04b84dc46da742f902
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2011-06-08 20:05:12 +0000

    Formats adjusted to very large jobs
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@7846 c92efa57-630b-4861-b058-cf58834340f0

--- PW/memory_report.f90
+++ PW/memory_report.f90
@@ -26,21 +26,20 @@ SUBROUTINE memory_report()
   !
   INTEGER, PARAMETER :: Mb=1024*1024, complex_size=16, real_size=8
   INTEGER :: g_size, nbnd_l
-  INTEGER*8 :: large_size
   !
   ! the conversions to double prevent integer overflow in very large run
   !
   WRITE( stdout, '(/5x,"Largest allocated arrays",5x,"est. size (Mb)", &
                    &5x,"dimensions")')
   WRITE( stdout, '(8x,"Kohn-Sham Wavefunctions   ",f10.2," Mb", &
-                 & 5x,"(",i7,",",i4,")")') &
+                 & 5x,"(",i7,",",i5,")")') &
      complex_size*nbnd*npol*DBLE(npwx)/Mb, npwx*npol,nbnd
   IF ( lda_plus_u ) &
      WRITE( stdout, '(8x,"Atomic wavefunctions      ",f10.2," Mb", &
-                    & 5x,"(",i7,",",i4,")")') &
+                    & 5x,"(",i7,",",i5,")")') &
      complex_size*natomwfc*npol*DBLE(npwx)/Mb, npwx*npol,natomwfc
   WRITE( stdout, '(8x,"NL pseudopotentials       ",f10.2," Mb", &
-                 & 5x,"(",i7,",",i4,")")') &
+                 & 5x,"(",i7,",",i5,")")') &
      complex_size*nkb*DBLE(npwx)/Mb, npwx, nkb
   IF ( nspin == 2 ) THEN
      WRITE( stdout, '(8x,"Each V/rho on FFT grid    ",f10.2," Mb", &
@@ -65,22 +64,22 @@ SUBROUTINE memory_report()
   !
   IF ( isolve == 0 ) THEN
      WRITE( stdout, '(8x,"Auxiliary wavefunctions   ",f10.2," Mb", &
-                 & 5x,"(",i7,",",i4,")")') &
+                 & 5x,"(",i7,",",i5,")")') &
      g_size*nbndx*npol*DBLE(npwx)/Mb, npwx*npol, nbndx
   ENDIF
   ! nbnd_l : estimated dimension of distributed matrices
   nbnd_l = nbndx/np_ortho(1)
   WRITE( stdout, '(8x,"Each subspace H/S matrix  ",f10.2," Mb", &
-                 & 5x,"(",i7,",",i4,")")') &
+                 & 5x,"(",i4,",",i4,")")') &
   DBLE(g_size*nbnd_l*nbnd_l)/Mb, nbnd_l, nbnd_l
   !
   IF ( npol > 1 ) THEN
      WRITE( stdout, '(8x,"Each <psi_i|beta_j> matrix",f10.2," Mb", &
-                 & 5x,"(",i7,",",i4,",",i4,")")') &
+                 & 5x,"(",i7,",",i4,",",i5,")")') &
      DBLE(g_size*nkb*npol*nbnd)/Mb, nkb, npol, nbnd
   ELSE
      WRITE( stdout, '(8x,"Each <psi_i|beta_j> matrix",f10.2," Mb", &
-                 & 5x,"(",i7,",",i4,")")') &
+                 & 5x,"(",i7,",",i5,")")') &
      DBLE(g_size*nkb*nbnd)/Mb, nkb, nbnd
   END IF
   !
