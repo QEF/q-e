b8a490d309c1407058ea02ea5f9b84353953c3d5
brandoncwood <brandoncwood@c92efa57-630b-4861-b058-cf58834340f0>
2011-08-23 05:47:59 +0000

    Corrected error in data extraction script for ESM example
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@8099 c92efa57-630b-4861-b058-cf58834340f0

--- examples/ESM_example/reference/esm_data.sh
+++ examples/ESM_example/reference/esm_data.sh
@@ -8,5 +8,5 @@
 echo '#   z (A)      rho (e)      Avg v_hartree        Avg v_local  Avg v_hart+v_loc'
 echo '#                                  (eV/A)             (eV/A)            (eV/A)'
 ngrid=`grep 'Dense  grid:' $1 | awk -F ',' '{print $3}' | sed 's/)//'`
-let ngrid=$ngrid+5
+let ngrid="$ngrid+5"
 grep -A${ngrid} 'ESM Charge and Potential' $1 | tail -n${ngrid} | tail -n+6
