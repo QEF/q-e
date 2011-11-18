78c80f8be5c85bf8ec8bd263898d5ae4688c6990
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2011-11-18 17:30:57 +0000

    Soruce beautification (so to speak)
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@8255 c92efa57-630b-4861-b058-cf58834340f0

--- atomic/src/run_pseudo.f90
+++ atomic/src/run_pseudo.f90
@@ -7,26 +7,26 @@
 !
 !
 !---------------------------------------------------------------
-subroutine run_pseudo
+SUBROUTINE run_pseudo
   !---------------------------------------------------------------
   !
   !     this routine is a driver to a pseudopotential calculation
   !     with the parameters given in input
   !
   !
-  use kinds, only : dp
-  use radial_grids, only : ndmx
-  use ld1_parameters, only : nwfsx
-  use ld1inc, only : enl, lpaw, nlcc, lsd, latt, pawsetup, &
+  USE kinds, ONLY : dp
+  USE radial_grids, ONLY : ndmx
+  USE ld1_parameters, ONLY : nwfsx
+  USE ld1inc, ONLY : enl, lpaw, nlcc, lsd, latt, pawsetup, &
                      nstoaets, grid, nspin, iter, rhos, rhoc, &
                      nwfts, enlts, llts, jjts, iswts, octs, phits, &
                      vxt, enne, vh, vpsloc, file_potscf, beta, tr2,  &
                      eps0, file_recon, deld, vpstot, nbeta, ddd, etots, &
                      paw_energy, iswitch, lgipaw_reconstruction, use_paw_as_gipaw
-  use atomic_paw, only : new_paw_hamiltonian
-  implicit none
+  USE atomic_paw, ONLY : new_paw_hamiltonian
+  IMPLICIT NONE
 
-  integer :: &
+  INTEGER :: &
        ns, &    ! counter on pseudowavefunctions
        n,  &    ! counter on mesh
        is       ! counter on spin
@@ -34,156 +34,156 @@ subroutine run_pseudo
   real(DP) :: &
        vnew(ndmx,2)   ! the potential
 
-  integer :: &
-       ios
+  INTEGER :: &
+       iunps, ios
 
-  logical :: &
+  LOGICAL :: &
        conv       ! if true convergence reached
 
   real(DP) :: &
        dddnew(nwfsx,nwfsx,2),   & ! the new D coefficients
        vd(2*(ndmx+nwfsx+nwfsx)), & ! Vloc and D in one array for mixing
        vdnew(2*(ndmx+nwfsx+nwfsx)) ! the new vd array
-  integer :: &
-       nerr                       ! error message 
+  INTEGER :: &
+       nerr                       ! error message
 
-  real(DP), parameter :: thresh=1.e-10_dp
-  integer, parameter :: itmax=200
-  character(len=256) :: nomefile
+  real(DP), PARAMETER :: thresh=1.e-10_dp
+  INTEGER, PARAMETER :: itmax=200
+  CHARACTER(len=256) :: nomefile
 
   !
   !     initial estimate of the eigenvalues
   !
-  do ns=1,nwfts
+  DO ns=1,nwfts
      enlts(ns)=enl(nstoaets(ns))
-  enddo
+  ENDDO
   !
   !    compute an initial estimate of the potential
   !
-  call guess_initial_wfc()
-  if (.not.lpaw) then
-     call start_potps ( )
-  else
+  CALL guess_initial_wfc()
+  IF (.not.lpaw) THEN
+     CALL start_potps ( )
+  ELSE
      CALL new_paw_hamiltonian (vpstot, ddd, etots,pawsetup, nwfts, &
                                llts, jjts, nspin, iswts, octs, phits, enlts)
-     do is=1,nspin
+     DO is=1,nspin
         vpstot(1:grid%mesh,is)=vpstot(1:grid%mesh,is)-pawsetup%psloc(1:grid%mesh)
-     enddo
-     call vdpack (grid%mesh, ndmx, nbeta, nwfsx, nspin, vpstot, ddd, vd, "PACK")
-     do is=1,nspin
+     ENDDO
+     CALL vdpack (grid%mesh, ndmx, nbeta, nwfsx, nspin, vpstot, ddd, vd, "PACK")
+     DO is=1,nspin
         vpstot(1:grid%mesh,is)=vpstot(1:grid%mesh,is)+pawsetup%psloc(1:grid%mesh)
-     enddo
-  endif
+     ENDDO
+  ENDIF
   !
   !     iterate to self-consistency
   !
-  do iter=1,itmax
-     call ascheqps_drv(vpstot, nspin, thresh, .false., nerr)
+  DO iter=1,itmax
+     CALL ascheqps_drv(vpstot, nspin, thresh, .false., nerr)
 
-     if (.not.lpaw) then
+     IF (.not.lpaw) THEN
         !
-        call chargeps(rhos,phits,nwfts,llts,jjts,octs,iswts)
-        call new_potential(ndmx,grid%mesh,grid,0.0_dp,vxt,lsd,&
+        CALL chargeps(rhos,phits,nwfts,llts,jjts,octs,iswts)
+        CALL new_potential(ndmx,grid%mesh,grid,0.0_dp,vxt,lsd,&
              nlcc,latt,enne,rhoc,rhos,vh,vnew,1)
 
-        do is=1,nspin
+        DO is=1,nspin
            vpstot(:,is)=vpstot(:,is)-vpsloc(:)
-        enddo
+        ENDDO
 
-        if (file_potscf.ne.' ') then
-           if (iter<10) then
-              write(nomefile,'(a,"_",i1)') trim(file_potscf), iter
-           elseif(iter<100) then
-              write(nomefile,'(a,"_",i2)') trim(file_potscf), iter
-           elseif(iter<1000) then
-              write(nomefile,'(a,"_",i3)') trim(file_potscf), iter
-           else
-              call errore('run_pseudo','problem with iteration',1)
-           endif
-           open(unit=18,file=trim(nomefile), status='unknown', &
+        IF (file_potscf/=' ') THEN
+           IF (iter<10) THEN
+              WRITE(nomefile,'(a,"_",i1)') trim(file_potscf), iter
+           ELSEIF(iter<100) THEN
+              WRITE(nomefile,'(a,"_",i2)') trim(file_potscf), iter
+           ELSEIF(iter<1000) THEN
+              WRITE(nomefile,'(a,"_",i3)') trim(file_potscf), iter
+           ELSE
+              CALL errore('run_pseudo','problem with iteration',1)
+           ENDIF
+           OPEN(unit=18,file=trim(nomefile), status='unknown', &
                                              err=100, iostat=ios)
-100        call errore('run_pseudo','opening file' // nomefile,abs(ios))
-           if (lsd==1) then
-              do n=1,grid%mesh
-                 write(18,'(5e20.12)') grid%r(n),vnew(n,1)-vpstot(n,1), &
+100        CALL errore('run_pseudo','opening file' // nomefile,abs(ios))
+           IF (lsd==1) THEN
+              DO n=1,grid%mesh
+                 WRITE(18,'(5e20.12)') grid%r(n),vnew(n,1)-vpstot(n,1), &
                          vnew(n,1), vnew(n,2)-vpstot(n,2), vnew(n,2)
-              enddo
-           else
-              do n=1,grid%mesh
-                 write(18,'(3e26.15)') grid%r(n),vnew(n,1)-vpstot(n,1), &
+              ENDDO
+           ELSE
+              DO n=1,grid%mesh
+                 WRITE(18,'(3e26.15)') grid%r(n),vnew(n,1)-vpstot(n,1), &
                           vnew(n,1)
-              enddo
-           endif
-           close(18)
-        endif
+              ENDDO
+           ENDIF
+           CLOSE(18)
+        ENDIF
 
-        call vpack(grid%mesh,ndmx,nspin,vnew,vpstot,1)
-        call dmixp(grid%mesh*nspin,vnew,vpstot,beta,tr2,iter,3,eps0,conv,itmax)
-        call vpack(grid%mesh,ndmx,nspin,vnew,vpstot,-1)
+        CALL vpack(grid%mesh,ndmx,nspin,vnew,vpstot,1)
+        CALL dmixp(grid%mesh*nspin,vnew,vpstot,beta,tr2,iter,3,eps0,conv,itmax)
+        CALL vpack(grid%mesh,ndmx,nspin,vnew,vpstot,-1)
 
-        do is=1,nspin
-           do n=1,grid%mesh
+        DO is=1,nspin
+           DO n=1,grid%mesh
               vpstot(n,is)=vpstot(n,is)+vpsloc(n)
-           enddo
-        enddo
-        call newd_at
+           ENDDO
+        ENDDO
+        CALL newd_at
         !
-     else
+     ELSE
         !
-        call new_paw_hamiltonian (vnew, dddnew, etots, &
+        CALL new_paw_hamiltonian (vnew, dddnew, etots, &
              pawsetup, nwfts, llts, jjts, nspin, iswts, octs, phits, enlts)
-        do is=1,nspin
+        DO is=1,nspin
            vnew(1:grid%mesh,is)=vnew(1:grid%mesh,is)-pawsetup%psloc(1:grid%mesh)
-        enddo
-        call vdpack (grid%mesh, ndmx, nbeta, nwfsx, nspin, vnew, dddnew, vdnew, "PACK")
-        call dmixp((grid%mesh+nbeta*nbeta)*nspin,vdnew,vd,beta,tr2,iter,3,eps0,conv,itmax)
-        call vdpack (grid%mesh, ndmx, nbeta, nwfsx, nspin, vpstot, ddd, vd, "UNDO")
-        do is=1,nspin
+        ENDDO
+        CALL vdpack (grid%mesh, ndmx, nbeta, nwfsx, nspin, vnew, dddnew, vdnew, "PACK")
+        CALL dmixp((grid%mesh+nbeta*nbeta)*nspin,vdnew,vd,beta,tr2,iter,3,eps0,conv,itmax)
+        CALL vdpack (grid%mesh, ndmx, nbeta, nwfsx, nspin, vpstot, ddd, vd, "UNDO")
+        DO is=1,nspin
            vpstot(1:grid%mesh,is)=vpstot(1:grid%mesh,is)+pawsetup%psloc(1:grid%mesh)
-        enddo
+        ENDDO
         !
-     endif
+     ENDIF
 
 !            write(6,*) 'iteration number',iter, eps0
-     if (conv) then
-        if (nerr /= 0) then
-           if (iswitch==2) then
-              call infomsg ('run_pseudo','Errors in PS-KS equations')
-           else
-              call errore ('run_pseudo','Errors in PS-KS equation', 1)
-           endif
-        endif
-        goto 900
-     endif
-  enddo
-  call infomsg('run_pseudo','convergence not achieved')
+     IF (conv) THEN
+        IF (nerr /= 0) THEN
+           IF (iswitch==2) THEN
+              CALL infomsg ('run_pseudo','Errors in PS-KS equations')
+           ELSE
+              CALL errore ('run_pseudo','Errors in PS-KS equation', 1)
+           ENDIF
+        ENDIF
+        GOTO 900
+     ENDIF
+  ENDDO
+  CALL infomsg('run_pseudo','convergence not achieved')
   !
   !    final calculation with all states
   !
-900 continue
+900 CONTINUE
 
-  call ascheqps_drv(vpstot, nspin, thresh, .true., nerr)
+  CALL ascheqps_drv(vpstot, nspin, thresh, .true., nerr)
 
-  if (.not.lpaw) then
-     call elsdps ( )
-  else
-     call new_paw_hamiltonian (vnew, dddnew, etots, pawsetup, nwfts, &
+  IF (.not.lpaw) THEN
+     CALL elsdps ( )
+  ELSE
+     CALL new_paw_hamiltonian (vnew, dddnew, etots, pawsetup, nwfts, &
                 llts, jjts, nspin, iswts, octs, phits, enlts, paw_energy)
-     call elsdps_paw()
-  endif
+     CALL elsdps_paw()
+  ENDIF
 
-  IF ( lgipaw_reconstruction.and.(.not.use_paw_as_gipaw) ) CALL calculate_gipaw_orbitals()
-  
-  if (file_recon.ne.' ')  call write_paw_recon ( )
+  IF ( lgipaw_reconstruction.and.(.not.use_paw_as_gipaw) ) &
+       CALL calculate_gipaw_orbitals()
 
+  IF (file_recon/=' ') CALL write_paw_recon ( )
   !
   !    compute logarithmic derivatives
   !
-  if ( deld > 0.0_dp) call lderivps ( )
+  IF ( deld > 0.0_dp) CALL lderivps ( )
   !
-  !    compute expansion in partial waves 
+  !    compute expansion in partial waves
   !
-  if ( deld > 0.0_dp) call partial_wave_expansion ( )
+  IF ( deld > 0.0_dp) CALL partial_wave_expansion ( )
 
-  return
-end subroutine run_pseudo
+  RETURN
+END SUBROUTINE run_pseudo
