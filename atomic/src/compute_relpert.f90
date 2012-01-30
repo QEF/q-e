3a464507d741c35fe6fa7431d5c999041fa8d07f
sclauzer <sclauzer@c92efa57-630b-4861-b058-cf58834340f0>
2012-01-30 14:27:08 +0000

    Added documentation to the relpert flag. Added copyright notice in source file.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@8557 c92efa57-630b-4861-b058-cf58834340f0

--- atomic/src/compute_relpert.f90
+++ atomic/src/compute_relpert.f90
@@ -1,4 +1,9 @@
-! 
+! Copyright (C) 2001-2012 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
 !--------------------------------------------
 subroutine compute_relpert( evel, edar, eso )
   !------------------------------------------
