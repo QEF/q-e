4e195472f589c09c38267f390b6047557a43be40
oliviero <oliviero@c92efa57-630b-4861-b058-cf58834340f0>
2012-02-14 16:13:18 +0000

    Some utilities developed for the Solvent module are moved into Modules because possibly of general interest. In particular: fd_gradient.f90 contains a module to perform gradients in the real space grid using finite differences. A few different schemes have been implemented, it is necessary to initialize the finite difference calculation by passing the selected scheme and order to an initialization routine. The module is parallel, but not very fancy. generate_function.f90 contains a few subroutines that generate a specific function on the real space grid. The possible functions are gaussian, exponenentials, gradient of gassians and gradient of exponentials. Also in this case the subroutines are parallel and built following the scheme in the makov_payne.f90 subroutines.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@8645 c92efa57-630b-4861-b058-cf58834340f0

---
+++ Modules/fd_gradient.f90
@@ -0,0 +1,289 @@
+!
+! Copyright (C) 2006-2010 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!
+!
+!----------------------------------------------------------------------
+! Module to finite differences gradients on dense real space grid
+! Written by Oliviero Andreussi
+!----------------------------------------------------------------------
+!
+!=----------------------------------------------------------------------=!
+   MODULE fd_gradient
+!=----------------------------------------------------------------------=!
+
+      USE kinds, ONLY: DP
+
+      IMPLICIT NONE
+ 
+  CONTAINS
+!=----------------------------------------------------------------------=!
+SUBROUTINE calc_fd_gradient( nfdpoint, icfd, ncfd, nrxx, f, grad )
+!=----------------------------------------------------------------------=!
+  USE kinds,         ONLY : DP
+  USE cell_base,     ONLY : at, bg, alat
+  USE fft_base,      ONLY : dfftp
+  USE mp,            ONLY : mp_sum
+  USE mp_global,     ONLY : me_pool, intra_pool_comm, &
+                            me_bgrp, intra_bgrp_comm
+  USE fft_base,      ONLY : grid_scatter
+
+  IMPLICIT NONE
+  !
+  INTEGER, INTENT(IN)  :: nfdpoint
+  INTEGER, INTENT(IN)  :: ncfd
+  INTEGER, INTENT(IN)  :: icfd(-nfdpoint:nfdpoint)
+  INTEGER, INTENT(IN)  :: nrxx
+  REAL( DP ), DIMENSION( nrxx ), INTENT(IN) :: f
+  REAL( DP ), DIMENSION( 3, nrxx ), INTENT(OUT) :: grad
+  
+  INTEGER :: index, index0, i, ir, ipol, in
+  INTEGER :: ix(-nfdpoint:nfdpoint),iy(-nfdpoint:nfdpoint),iz(-nfdpoint:nfdpoint)
+  INTEGER :: ixc, iyc, izc, ixp, ixm, iyp, iym, izp, izm
+  REAL( DP ), DIMENSION( :, : ), ALLOCATABLE :: gradtmp
+  !
+  grad = 0.D0
+  !
+  ALLOCATE( gradtmp( 3, dfftp%nr1x*dfftp%nr2x*dfftp%nr3x ) )
+  gradtmp = 0.D0
+  !
+  index0 = 0
+  !
+#if defined (__MPI)
+  !
+#ifdef __BANDS
+  DO i = 1, me_bgrp
+    index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+  END DO
+#else
+  DO i = 1, me_pool
+    index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+  END DO
+#endif
+  !
+#endif
+  !
+  DO ir = 1, nrxx
+    !   
+    index = index0 + ir - 1
+    iz(0) = index / (dfftp%nr1x*dfftp%nr2x)
+    index = index - (dfftp%nr1x*dfftp%nr2x)*iz(0)
+    iy(0) = index / dfftp%nr1x
+    index = index - dfftp%nr1x*iy(0)
+    ix(0) = index
+    !
+    DO in = 1, nfdpoint 
+      ix(in) = ix(in-1) + 1
+      IF( ix(in) .GT. dfftp%nr1x-1 ) ix(in) = 0     
+      ix(-in) = ix(-in+1) - 1
+      IF( ix(-in) .LT. 0 ) ix(-in) = dfftp%nr1x-1
+      iy(in) = iy(in-1) + 1
+      IF( iy(in) .GT. dfftp%nr2x-1 ) iy(in) = 0
+      iy(-in) = iy(-in+1) - 1
+      IF( iy(-in) .LT. 0 ) iy(-in) = dfftp%nr2x-1
+      iz(in) = iz(in-1) + 1
+      IF( iz(in) .GT. dfftp%nr3x-1 ) iz(in) = 0
+      iz(-in) = iz(-in+1) - 1
+      IF( iz(-in) .LT. 0 ) iz(-in) = dfftp%nr3x-1
+    ENDDO
+    !
+    DO in = -nfdpoint, nfdpoint
+      i = ix(in) + iy(0) * dfftp%nr1x + iz(0) * dfftp%nr1x * dfftp%nr2x + 1 
+      gradtmp(1,i) = gradtmp(1,i) - icfd(in)*f(ir)*dfftp%nr1x
+      i = ix(0) + iy(in) * dfftp%nr1x + iz(0) * dfftp%nr1x * dfftp%nr2x + 1 
+      gradtmp(2,i) = gradtmp(2,i) - icfd(in)*f(ir)*dfftp%nr2x 
+      i = ix(0) + iy(0) * dfftp%nr1x + iz(in) * dfftp%nr1x * dfftp%nr2x + 1 
+      gradtmp(3,i) = gradtmp(3,i) - icfd(in)*f(ir)*dfftp%nr3x 
+    ENDDO
+    !
+  ENDDO
+  !
+#if defined (__MPI)
+  !
+#ifdef __BANDS
+  DO ipol = 1, 3 
+    CALL mp_sum( gradtmp(ipol,:), intra_bgrp_comm )
+    CALL grid_scatter( gradtmp(ipol,:), grad(ipol,:) )
+  ENDDO
+#else
+  DO ipol = 1, 3 
+    CALL mp_sum( gradtmp(ipol,:), intra_pool_comm )
+    CALL grid_scatter( gradtmp(ipol,:), grad(ipol,:) )
+  ENDDO
+#endif
+  !
+#else
+  grad = gradtmp
+#endif
+  !
+  DEALLOCATE( gradtmp )
+  !
+  DO ir = 1,nrxx
+    grad(:,ir) = MATMUL( bg, grad(:,ir) )
+  ENDDO
+  grad = grad / DBLE(ncfd) / alat
+  !
+  RETURN
+
+END SUBROUTINE calc_fd_gradient
+
+SUBROUTINE init_fd_gradient( ifdtype, nfdpoint, ncfd, icfd )
+
+  USE kinds,         ONLY : DP
+
+  IMPLICIT NONE
+  !
+  INTEGER, INTENT(IN)  :: ifdtype, nfdpoint
+  INTEGER, INTENT(OUT) :: ncfd
+  INTEGER, INTENT(OUT) :: icfd(-nfdpoint:nfdpoint)
+  !
+  INTEGER :: in
+  !
+  ncfd = 0
+  icfd = 0
+  !
+  SELECT CASE ( ifdtype )
+    !
+  CASE ( 1 ) 
+    ! (2N+1)-point Central Differences
+    IF ( nfdpoint .EQ. 1 ) THEN
+      ncfd = 2
+      icfd(  1 ) =   1
+    ELSE IF ( nfdpoint .EQ. 2 ) THEN
+      ncfd = 12
+      icfd(  2 ) =  -1
+      icfd(  1 ) =   8
+    ELSE IF ( nfdpoint .EQ. 3 ) THEN
+      ncfd = 60
+      icfd(  3 ) =   1
+      icfd(  2 ) =  -9
+      icfd(  1 ) =  45
+    ELSE IF ( nfdpoint .EQ. 4 ) THEN
+      ncfd = 840
+      icfd(  4 ) =  -3
+      icfd(  3 ) =  32
+      icfd(  2 ) =-168 
+      icfd(  1 ) = 672
+    ELSE
+      WRITE(*,*)'ERROR: wrong number of points',nfdpoint,&
+               &' for finite difference type ',ifdtype
+      STOP      
+    ENDIF
+    !
+  CASE ( 2 )
+    ! Low-Noise Lanczos Differentiators ( M = 2 )
+    IF ( nfdpoint .GE. 2 ) THEN
+      ncfd = (nfdpoint)*(nfdpoint+1)*(2*nfdpoint+1)/3
+      DO in = 1,nfdpoint
+        icfd( in ) = in 
+      ENDDO
+    ELSE
+      WRITE(*,*)'ERROR: wrong number of points',nfdpoint,&
+               &' for finite difference type ',ifdtype
+      STOP      
+    END IF
+    !
+  CASE ( 3 )
+    ! Super Lanczos Low-Noise Differentiators ( M = 4 )
+    IF ( nfdpoint .EQ. 3 ) THEN
+      ncfd = 252
+      icfd(  3 ) = -22
+      icfd(  2 ) =  67
+      icfd(  1 ) =  58
+    ELSE IF ( nfdpoint .EQ. 4 ) THEN
+      ncfd = 1188
+      icfd(  4 ) = -86
+      icfd(  3 ) = 142
+      icfd(  2 ) = 193 
+      icfd(  1 ) = 126
+    ELSE IF ( nfdpoint .EQ. 5 ) THEN
+      ncfd = 5148
+      icfd(  5 ) =-300
+      icfd(  4 ) = 294
+      icfd(  3 ) = 532
+      icfd(  2 ) = 503 
+      icfd(  1 ) = 296
+    ELSE
+      WRITE(*,*)'ERROR: wrong number of points',nfdpoint,&
+               &' for finite difference type ',ifdtype
+      STOP      
+    ENDIF
+    !
+  CASE ( 4 )
+    ! Smooth Noise-Robust Differentiators  ( n = 2 )
+    IF ( nfdpoint .EQ. 2 ) THEN
+      ncfd = 8
+      icfd(  2 ) =   1
+      icfd(  1 ) =   2
+    ELSE IF ( nfdpoint .EQ. 3 ) THEN
+      ncfd = 32
+      icfd(  3 ) =   1
+      icfd(  2 ) =   4
+      icfd(  1 ) =   5
+    ELSE IF ( nfdpoint .EQ. 4 ) THEN
+      ncfd = 128
+      icfd(  4 ) =   1
+      icfd(  3 ) =   6
+      icfd(  2 ) =  14 
+      icfd(  1 ) =  14
+    ELSE IF ( nfdpoint .EQ. 5 ) THEN
+      ncfd = 512
+      icfd(  5 ) =   1
+      icfd(  4 ) =   8
+      icfd(  3 ) =  27
+      icfd(  2 ) =  48 
+      icfd(  1 ) =  42
+    ELSE
+      WRITE(*,*)'ERROR: wrong number of points',nfdpoint,&
+               &' for finite difference type ',ifdtype
+      STOP      
+    ENDIF
+    !
+  CASE ( 5 )
+    ! Smooth Noise-Robust Differentiators  ( n = 4 )
+    IF ( nfdpoint .EQ. 3 ) THEN
+      ncfd = 96
+      icfd(  3 ) =  -5
+      icfd(  2 ) =  12
+      icfd(  1 ) =  39
+    ELSE IF ( nfdpoint .EQ. 4 ) THEN
+      ncfd = 96
+      icfd(  4 ) =  -2
+      icfd(  3 ) =  -1
+      icfd(  2 ) =  16 
+      icfd(  1 ) =  27
+    ELSE IF ( nfdpoint .EQ. 5 ) THEN
+      ncfd = 1536
+      icfd(  5 ) = -11
+      icfd(  4 ) = -32
+      icfd(  3 ) =  39
+      icfd(  2 ) = 256 
+      icfd(  1 ) = 322
+    ELSE
+      WRITE(*,*)'ERROR: wrong number of points',nfdpoint,&
+               &' for finite difference type ',ifdtype
+      STOP      
+    ENDIF
+    !
+  CASE DEFAULT
+    !
+    WRITE(*,*)'ERROR: finite difference type unknown, ifdtype=',ifdtype
+    STOP
+    !
+  END SELECT
+  !
+  DO in = 1,nfdpoint
+    icfd( -in ) = - icfd( in )
+  ENDDO
+  !
+  RETURN
+  !
+END SUBROUTINE init_fd_gradient
+
+!=----------------------------------------------------------------------=!
+   END MODULE fd_gradient
+!=----------------------------------------------------------------------=!
---
+++ Modules/generate_function.f90
@@ -0,0 +1,410 @@
+!
+! Copyright (C) 2006-2010 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!
+!
+!----------------------------------------------------------------------
+! Module to generate functions on the real space dense grid
+! Written by Oliviero Andreussi
+!----------------------------------------------------------------------
+!
+!=----------------------------------------------------------------------=!
+   MODULE generate_function
+!=----------------------------------------------------------------------=!
+
+      USE kinds, ONLY: DP
+
+      IMPLICIT NONE
+ 
+  CONTAINS
+!----------------------------------------------------------------------------
+      SUBROUTINE generate_gaussian( nrxx, charge, spread, pos, rho )
+!----------------------------------------------------------------------------
+      !
+      USE kinds,            ONLY : DP
+      USE constants,        ONLY : sqrtpi
+      USE cell_base,        ONLY : at, bg, alat
+      USE fft_base,         ONLY : dfftp
+      USE mp_global,        ONLY : me_pool, intra_pool_comm, &
+                                   me_bgrp, intra_bgrp_comm
+      !
+      IMPLICIT NONE
+      !
+      ! ... Declares variables
+      !
+      INTEGER, INTENT(IN)       :: nrxx
+      REAL( DP ), INTENT(IN)    :: charge, spread
+      REAL( DP ), INTENT(IN)    :: pos( 3 )
+      REAL( DP ), INTENT(INOUT) :: rho( nrxx )
+      !
+      ! ... Local variables
+      !
+      INTEGER                   :: i, j, k, ir, ip
+      INTEGER                   :: index, index0
+      !
+      REAL( DP )                :: inv_nr1, inv_nr2, inv_nr3
+      REAL( DP )                :: scale, spr2, dist
+      REAL( DP )                :: r( 3 ), s( 3 )
+      REAL( DP ), ALLOCATABLE   :: rholocal ( : )
+      !
+      inv_nr1 = 1.D0 / DBLE( dfftp%nr1 )
+      inv_nr2 = 1.D0 / DBLE( dfftp%nr2 )
+      inv_nr3 = 1.D0 / DBLE( dfftp%nr3 )
+      !
+      index0 = 0
+      !
+#if defined (__MPI)
+      !
+#ifdef __BANDS
+      DO i = 1, me_bgrp
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#else
+      DO i = 1, me_pool
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#endif
+      !
+#endif
+      !
+      scale = charge / ( sqrtpi * spread )**3
+      spr2 = ( spread / alat )**2
+      ALLOCATE( rholocal( nrxx ) )
+      rholocal = 0.D0
+      !
+      DO ir = 1, nrxx
+         !
+         ! ... three dimensional indexes
+         !
+         index = index0 + ir - 1
+         k     = index / (dfftp%nr1x*dfftp%nr2x)
+         index = index - (dfftp%nr1x*dfftp%nr2x)*k
+         j     = index / dfftp%nr1x
+         index = index - dfftp%nr1x*j
+         i     = index
+         !
+         DO ip = 1, 3
+            r(ip) = DBLE( i )*inv_nr1*at(ip,1) + &
+                    DBLE( j )*inv_nr2*at(ip,2) + &
+                    DBLE( k )*inv_nr3*at(ip,3)
+         END DO
+         !
+         r(:) = pos(:) - r(:) 
+         !
+         ! ... minimum image convention
+         !
+         s(:) = MATMUL( r(:), bg(:,:) )
+         s(:) = s(:) - ANINT(s(:))
+         r(:) = MATMUL( at(:,:), s(:) )
+         !
+         dist = SUM( r * r ) 
+         !
+         rholocal( ir ) = scale * EXP(-dist/spr2) 
+         !      
+      END DO
+      !
+      rho = rho + rholocal
+      DEALLOCATE( rholocal )
+      !
+      RETURN
+      !
+!----------------------------------------------------------------------------
+      END SUBROUTINE generate_gaussian
+!----------------------------------------------------------------------------
+!----------------------------------------------------------------------------
+      SUBROUTINE generate_gradgaussian( nrxx, charge, spread, pos, gradrho )
+!----------------------------------------------------------------------------
+      !
+      USE kinds,            ONLY: DP
+      USE constants,        ONLY: sqrtpi
+      USE cell_base,        ONLY : at, bg, alat
+      USE fft_base,         ONLY : dfftp
+      USE mp_global,        ONLY : me_pool, intra_pool_comm, &
+                                   me_bgrp, intra_bgrp_comm
+      !
+      IMPLICIT NONE
+      !
+      ! ... Declares variables
+      !
+      INTEGER, INTENT(IN)       :: nrxx
+      REAL( DP ), INTENT(IN)    :: charge, spread
+      REAL( DP ), INTENT(IN)    :: pos( 3 )
+      REAL( DP ), INTENT(INOUT) :: gradrho( 3, nrxx )
+      !
+      ! ... Local variables
+      !
+      INTEGER                   :: i, j, k, ir, ip
+      INTEGER                   :: index, index0
+      !
+      REAL( DP )                :: inv_nr1, inv_nr2, inv_nr3
+      REAL( DP )                :: scale, spr2, dist
+      REAL( DP )                :: r( 3 ), s( 3 )
+      REAL( DP ), ALLOCATABLE   :: gradrholocal ( :, : )
+      !
+      inv_nr1 = 1.D0 / DBLE( dfftp%nr1 )
+      inv_nr2 = 1.D0 / DBLE( dfftp%nr2 )
+      inv_nr3 = 1.D0 / DBLE( dfftp%nr3 )
+      !
+      index0 = 0
+      !
+#if defined (__MPI)
+      !
+#ifdef __BANDS
+      DO i = 1, me_bgrp
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#else
+      DO i = 1, me_pool
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#endif
+      !
+#endif
+      !
+      scale = 2.d0 * charge / sqrtpi**3 / spread**5
+      spr2 = ( spread / alat )**2
+      ALLOCATE( gradrholocal( 3, nrxx ) )
+      gradrholocal = 0.D0
+      !
+      DO ir = 1, nrxx
+         !
+         ! ... three dimensional indexes
+         !
+         index = index0 + ir - 1
+         k     = index / (dfftp%nr1x*dfftp%nr2x)
+         index = index - (dfftp%nr1x*dfftp%nr2x)*k
+         j     = index / dfftp%nr1x
+         index = index - dfftp%nr1x*j
+         i     = index
+         !
+         DO ip = 1, 3
+            r(ip) = DBLE( i )*inv_nr1*at(ip,1) + &
+                    DBLE( j )*inv_nr2*at(ip,2) + &
+                    DBLE( k )*inv_nr3*at(ip,3)
+         END DO
+         !
+         r(:) = pos(:) - r(:) 
+         !
+         ! ... minimum image convention
+         !
+         s(:) = MATMUL( r(:), bg(:,:) )
+         s(:) = s(:) - ANINT(s(:))
+         r(:) = MATMUL( at(:,:), s(:) )
+         !
+         dist = SUM( r * r ) 
+         !
+         gradrholocal( :, ir ) = scale * EXP(-dist/spr2) * r(:) * alat
+         !      
+      END DO
+      !
+      gradrho = gradrho + gradrholocal
+      DEALLOCATE( gradrholocal )
+      !
+      RETURN
+      !
+!--------------------------------------------------------------------
+      END SUBROUTINE generate_gradgaussian
+!--------------------------------------------------------------------
+!----------------------------------------------------------------------------
+      SUBROUTINE generate_exponential( nrxx, spread, pos, rho )
+!----------------------------------------------------------------------------
+      !
+      USE kinds,            ONLY : DP
+      USE cell_base,        ONLY : at, bg, alat
+      USE fft_base,         ONLY : dfftp
+      USE mp_global,        ONLY : me_pool, intra_pool_comm, &
+                                   me_bgrp, intra_bgrp_comm
+      !
+      IMPLICIT NONE
+      !
+      ! ... Declares variables
+      !
+      INTEGER, INTENT(IN)       :: nrxx
+      REAL( DP ), INTENT(IN)    :: spread
+      REAL( DP ), INTENT(IN)    :: pos( 3 )
+      REAL( DP ), INTENT(INOUT) :: rho( nrxx )
+      !
+      ! ... Local variables
+      !
+      INTEGER                   :: i, j, k, ir, ip
+      INTEGER                   :: index, index0
+      !
+      REAL( DP )                :: inv_nr1, inv_nr2, inv_nr3
+      REAL( DP )                :: dist, arg
+      REAL( DP )                :: r( 3 ), s( 3 )
+      REAL( DP ), ALLOCATABLE   :: rholocal ( : )
+      REAL( DP ), PARAMETER     :: exp_arg_limit = 25.D0
+      !
+      inv_nr1 = 1.D0 / DBLE( dfftp%nr1 )
+      inv_nr2 = 1.D0 / DBLE( dfftp%nr2 )
+      inv_nr3 = 1.D0 / DBLE( dfftp%nr3 )
+      !
+      index0 = 0
+      !
+#if defined (__MPI)
+      !
+#ifdef __BANDS
+      DO i = 1, me_bgrp
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#else
+      DO i = 1, me_pool
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#endif
+      !
+#endif
+      !
+      ALLOCATE( rholocal( nrxx ) )
+      rholocal = 0.D0
+      !
+      DO ir = 1, nrxx
+         !
+         ! ... three dimensional indexes
+         !
+         index = index0 + ir - 1
+         k     = index / (dfftp%nr1x*dfftp%nr2x)
+         index = index - (dfftp%nr1x*dfftp%nr2x)*k
+         j     = index / dfftp%nr1x
+         index = index - dfftp%nr1x*j
+         i     = index
+         r = 0.D0
+         !
+         DO ip = 1, 3
+            r(ip) = DBLE( i )*inv_nr1*at(ip,1) + &
+                    DBLE( j )*inv_nr2*at(ip,2) + &
+                    DBLE( k )*inv_nr3*at(ip,3)
+         END DO
+         !
+         r(:) = pos(:) - r(:) 
+         !
+         ! ... minimum image convention
+         !
+         s(:) = MATMUL( r(:), bg(:,:) )
+         s(:) = s(:) - ANINT(s(:))
+         r(:) = MATMUL( at(:,:), s(:) )
+         !
+         dist = SQRT(SUM( r * r )) * alat
+         arg = dist - spread
+         !
+         IF( ABS( arg ) .LT. exp_arg_limit ) THEN
+           rholocal( ir ) = EXP( - arg ) 
+         ELSE 
+           rholocal( ir ) = 0.D0
+         END IF
+         !      
+      END DO
+      !
+      rho = rho + rholocal
+      DEALLOCATE( rholocal )
+      !
+      RETURN
+      !
+!--------------------------------------------------------------------
+      END SUBROUTINE generate_exponential
+!--------------------------------------------------------------------
+!----------------------------------------------------------------------------
+      SUBROUTINE generate_gradexponential( nrxx, spread, pos, gradrho )
+!----------------------------------------------------------------------------
+      !
+      USE kinds,            ONLY: DP
+      USE cell_base,        ONLY : at, bg, alat
+      USE fft_base,         ONLY : dfftp
+      USE mp_global,        ONLY : me_pool, intra_pool_comm, &
+                                   me_bgrp, intra_bgrp_comm
+      !
+      IMPLICIT NONE
+      !
+      ! ... Declares variables
+      !
+      INTEGER, INTENT(IN)       :: nrxx
+      REAL( DP ), INTENT(IN)    :: spread
+      REAL( DP ), INTENT(IN)    :: pos( 3 )
+      REAL( DP ), INTENT(INOUT) :: gradrho( 3, nrxx )
+      !
+      ! ... Local variables
+      !
+      INTEGER                   :: i, j, k, ir, ip
+      INTEGER                   :: index, index0
+      !
+      REAL( DP )                :: inv_nr1, inv_nr2, inv_nr3
+      REAL( DP )                :: dist, arg
+      REAL( DP )                :: r( 3 ), s( 3 )
+      REAL( DP ), ALLOCATABLE   :: gradrholocal ( :, : )
+      REAL( DP ), PARAMETER     :: exp_arg_limit = 25.D0
+      !
+      inv_nr1 = 1.D0 / DBLE( dfftp%nr1 )
+      inv_nr2 = 1.D0 / DBLE( dfftp%nr2 )
+      inv_nr3 = 1.D0 / DBLE( dfftp%nr3 )
+      !
+      index0 = 0
+      !
+#if defined (__MPI)
+      !
+#ifdef __BANDS
+      DO i = 1, me_bgrp
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#else
+      DO i = 1, me_pool
+        index0 = index0 + dfftp%nr1x*dfftp%nr2x*dfftp%npp(i)
+      END DO
+#endif
+      !
+#endif
+      !
+      ALLOCATE( gradrholocal( 3, nrxx ) )
+      gradrholocal = 0.D0
+      !
+      DO ir = 1, nrxx
+         !
+         ! ... three dimensional indexes
+         !
+         index = index0 + ir - 1
+         k     = index / (dfftp%nr1x*dfftp%nr2x)
+         index = index - (dfftp%nr1x*dfftp%nr2x)*k
+         j     = index / dfftp%nr1x
+         index = index - dfftp%nr1x*j
+         i     = index
+         !
+         DO ip = 1, 3
+            r(ip) = DBLE( i )*inv_nr1*at(ip,1) + &
+                    DBLE( j )*inv_nr2*at(ip,2) + &
+                    DBLE( k )*inv_nr3*at(ip,3)
+         END DO
+         !
+         r(:) = pos(:) - r(:) 
+         !
+         ! ... minimum image convention
+         !
+         s(:) = MATMUL( r(:), bg(:,:) )
+         s(:) = s(:) - ANINT(s(:))
+         r(:) = MATMUL( at(:,:), s(:) )
+         !
+         dist = SQRT(SUM( r * r )) * alat 
+         arg = dist - spread
+         IF ( dist .GT. 1.D-6 .AND. ABS( arg ) .LT. exp_arg_limit ) THEN
+           gradrholocal( :, ir ) = r(:) * alat / dist * EXP( - arg ) 
+         ELSE
+           gradrholocal( :, ir ) = 0.D0
+         ENDIF
+         !      
+      END DO
+      !
+      gradrho = gradrho + gradrholocal
+      DEALLOCATE( gradrholocal )
+      !
+      RETURN
+      !
+!--------------------------------------------------------------------
+      END SUBROUTINE generate_gradexponential
+!--------------------------------------------------------------------
+!=----------------------------------------------------------------------=!
+   END MODULE generate_function
+!=----------------------------------------------------------------------=!
+
+
