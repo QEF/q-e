594c59fb28b65bbb9df8f9ddefe2407f4bf021c6
oliviero <oliviero@c92efa57-630b-4861-b058-cf58834340f0>
2012-02-14 16:05:33 +0000

    removed obsolete solvent related modification
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@8644 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/control_flags.f90
+++ Modules/control_flags.f90
@@ -216,11 +216,6 @@ MODULE control_flags
     tr2_init,         &! initial value of tr2 for adaptive thresholds
     tr2_multi          ! the dexx multiplier for adaptive thresholds
                        ! tr2 = tr2_multi * dexx after each V_exx update 
-#ifdef __SOLVENT
-  LOGICAL, PUBLIC :: &
-    save_vltot         ! .TRUE. if a real space correction of the potential needs 
-                       ! to be added to vltot during SCF (solvent or slab calculations)
-#endif
   !
   ! ... pw diagonalization
   !
