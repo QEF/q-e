8592ef56c4abb890e92cee7fabff513337b31562
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2012-03-23 17:31:43 +0000

    In the GGA + noncollinear + domag case rhoup and rhodw are chosen with a logic slightly different than before. This option is probably not much better than before but should not be worse.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@8816 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/compute_rho.f90
+++ PW/src/compute_rho.f90
@@ -1,125 +1,44 @@
 !
-! Copyright (C) 2005 PWSCF group
+! Copyright (C) 2005-2012 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-      subroutine compute_rho(rho,rhoout,segni,nrxx)
+   SUBROUTINE compute_rho(rho,rhoout,segni,nrxx)
 !
-!   This subroutine diagonalize the spin density matrix and gives as output
+!   This subroutine diagonalizes the spin density matrix and gives as output
 !   the spin up and spin down compotents of the charge
+!   If lsign is true up and dw are with respect to the fixed quantization
+!   axis ux, otherwise rho+|m| is always rhoup and rho-|m| is always
+!   rhodw
 !
    USE kinds, ONLY : dp
-   USE cell_base,  ONLY : at, bg
-   USE symme,      ONLY : symv
-   USE constants, ONLY: pi
-   USE io_global,  ONLY :  stdout
-
-   implicit none
-   integer :: nrxx  ! input: the dimension of the mesh
+   USE noncollin_module, ONLY : lsign, ux
+   IMPLICIT NONE
+   INTEGER :: nrxx  ! input: the dimension of the mesh
          
-   real(DP) ::  rho(nrxx,4),segni(nrxx),rhoout(nrxx,2)
+   REAL(DP), INTENT(IN) ::  rho(nrxx,4)
+   REAL(DP), INTENT(OUT) :: rhoout(nrxx,2)
+   REAL(DP), INTENT(OUT) :: segni(nrxx)
                  ! input: the four components of the charge 
-                 ! output: keep track of the spin direction
                  ! output: the spin up and spin down charge
+                 ! output: the orientation when needed
+   REAL(DP) :: amag
+   INTEGER :: ir           ! counter on mesh points
 
-   complex(DP) :: re(2,2) ! the rotated density matrix
+   segni=1.0_DP
+   IF (lsign) THEN
+      DO ir=1,nrxx
+         segni(ir)=SIGN(1.0_DP,rho(ir,2)*ux(1)+rho(ir,3)*ux(2)+rho(ir,4)*ux(3))
+      ENDDO
+   ENDIF
 
-   real(DP) :: eps,amag,aux  
-   real(DP) :: mx,my,mz         ! magnetization
-   real(DP) :: ux(3)         ! magnetization
-   real(DP) :: ux0(3)      ! magnetization
+   DO ir=1,nrxx
+      amag=SQRT(rho(ir,2)**2+rho(ir,3)**2+rho(ir,4)**2)
+      rhoout(ir,1)=0.5d0*(rho(ir,1)+segni(ir)*amag)
+      rhoout(ir,2)=0.5d0*(rho(ir,1)-segni(ir)*amag)
+   ENDDO
 
-   logical :: negative
-   integer :: ir           ! counter on mesh points
-   integer :: it,ipol,count(3)
-
-      eps=1.d-12
-     
-      ux=0.d0
-      ux0=0.d0
-      do ir=min(1,nrxx/2),nrxx
-         amag=sqrt(rho(ir,2)**2+rho(ir,3)**2+rho(ir,4)**2)
-         if (amag.gt.eps) then
-             DO ipol=1,3
-                ux(ipol)=rho(ir,ipol+1)/amag
-             ENDDO
-             ux0=ux
-             goto 120
-         endif
-      enddo
-120   continue
-      ux(1)=1.d0
-      ux(2)=2.d0
-      ux(3)=3.d0
-      ux0(1)=1.d0
-      ux0(2)=2.d0
-      ux0(3)=3.d0
-
-      count(3)=0
-
-      do it=1,3 
-         count(it)=0
-         do ir=1,nrxx
-            amag=sqrt(rho(ir,2)**2+rho(ir,3)**2+rho(ir,4)**2)
-            if (amag.gt.eps) then
-               mx=rho(ir,2)/amag
-               my=rho(ir,3)/amag
-               mz=rho(ir,4)/amag
-               if (it.eq.2) then
-                  if (abs(ux(1)*mx+ux(2)*my+ux(3)*mz).lt.1.d-3) then
-                     ux(1)=ux(1)+0.5d0*mx
-                     ux(2)=ux(2)+0.5d0*my
-                     ux(3)=ux(3)+0.5d0*mz
-                     amag=sqrt(ux(1)**2+ux(2)**2+ux(3)**2)
-                     ux=ux/amag
-                  endif
-               else
-                  if (abs(ux(1)*mx+ux(2)*my+ux(3)*mz).lt.1.d-3) &
-                                       count(it)=count(it)+1
-               endif
-            endif
-         enddo
-         if (count(1).eq.0) goto 100
-      enddo
-      if (count(1).lt.count(3)) THEN 
-         ux=ux0
-         WRITE( stdout,'(5x,"it, count:",3i5)') it,count(1),count(3)
-      ENDIF
-100   continue
-      call symv (ux)
-      aux=sqrt(ux(1)**2+ux(2)**2+ux(3)**2)
-      ux=ux/aux
-      
-      WRITE( stdout,'(/,5x, "GGA quantization axis:&
-           & (",f8.4,",",f8.4,",",f8.4,"  )")') ux(1),ux(2),ux(3)
-
-      do ir=1,nrxx
-         amag=sqrt(rho(ir,2)**2+rho(ir,3)**2+rho(ir,4)**2)
-         re(1,1)=0.5d0*(rho(ir,1)+amag)
-         re(2,2)=0.5d0*(rho(ir,1)-amag)
-         if (amag.lt.eps) then
-            negative=.false.
-         else
-            mx=rho(ir,2)/amag
-            my=rho(ir,3)/amag
-            mz=rho(ir,4)/amag
-            negative=(mx*ux(1)+my*ux(2)+mz*ux(3)).gt.0.d0
-         endif
-         
-
-         if (negative) then
-            rhoout(ir,2)= DBLE(re(1,1))
-            rhoout(ir,1)= DBLE(re(2,2))
-            segni(ir)=-1.d0
-         else
-            rhoout(ir,1)= DBLE(re(1,1))
-            rhoout(ir,2)= DBLE(re(2,2))
-            segni(ir)=1.d0
-         endif
-      enddo
-
-
-      return
-      end subroutine compute_rho
+   RETURN
+END SUBROUTINE compute_rho
--- PW/src/compute_ux.f90
+++ PW/src/compute_ux.f90
@@ -1,46 +1,62 @@
 !
-! Copyright (C) 2007 PWSCF group
+! Copyright (C) 2007-2012 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-      SUBROUTINE compute_ux(m_loc,ux,nat)
+   SUBROUTINE compute_ux(m_loc,ux,nat)
 !
-!   This subroutine diagonalizes the spin density matrix and gives as output
-!   the spin up and spin down components of the charge
+!   This subroutine determines the direction of a fixed quantization axis
+!   from the starting magnetization.
 !
    USE kinds, ONLY : dp
    USE constants, ONLY: pi, eps12
    USE io_global,  ONLY :  stdout
+   USE noncollin_module, ONLY : lsign
 
-   implicit none
+   IMPLICIT NONE
 
    INTEGER, INTENT(IN) :: nat    ! number of atoms
    REAL(DP), INTENT(OUT) :: ux(3)     ! fixed direction to calculate signs
    REAL(DP), INTENT(IN) :: m_loc(3,nat)     ! local moments
          
-   REAL(DP) :: amag,uxmod !  
-   REAL(DP) :: m(3)       ! magnetization
+   REAL(DP) :: amag, uxmod ! modulus of the magnetization and of ux 
 
-   INTEGER :: i, na        ! counter on polarizations, atoms
-   INTEGER :: it,counter(3)
+   INTEGER :: na          ! counter on atoms
+   INTEGER :: starting_na ! auxiliary variable
+   LOGICAL :: is_parallel ! external function true if two vectors are parallel
+!
+!  Do not use the sign feature in the general case
+!
+   lsign=.FALSE.
+   ux=0.0_DP
 
-   ux(:)=m_loc(:,1)
-   uxmod=SQRT(ux(1)**2+ux(2)**2+ux(3)**2)
-   if (uxmod>eps12) ux=ux/uxmod
-   do na=2,nat
-      amag=SQRT(m_loc(1,na)**2+m_loc(2,na)**2+m_loc(3,na)**2)
-      if (amag > eps12) then
-         m(:)=m_loc(:,na)/amag
-         if (ABS(ux(1)*m(1)+ux(2)*m(2)+ux(3)*m(3)).lt.1.d-3) then
-            ux=ux+0.5d0*m
-            uxmod=SQRT(ux(1)**2+ux(2)**2+ux(3)**2)
-            ux=ux/uxmod
-         endif
-      endif
-   enddo
-   WRITE( stdout,'(/,5x,"Fixed quantization axis for GGA: ", 3f12.6)') &
+   starting_na=0
+   DO na=1,nat
+      amag=m_loc(1,na)**2+m_loc(2,na)**2+m_loc(3,na)**2
+      IF (amag > eps12) THEN
+         ux(:)=m_loc(:,na)
+         starting_na=na
+         lsign=.TRUE.
+         GOTO 20
+      ENDIF
+   ENDDO
+20 CONTINUE
+!
+!  The sign feature is used only when all initial magnetizations are parallel
+!  to a fixed direction that is taken as the quantization axis.
+!
+   DO na=starting_na+1, nat
+      lsign=lsign.AND.is_parallel(ux,m_loc(:,na))
+   ENDDO
+
+   IF (lsign) THEN
+      uxmod=ux(1)**2+ux(2)**2+ux(3)**2
+      IF (uxmod<eps12) CALL errore('compute_ux','strange uxmod',1) 
+      ux=ux/SQRT(uxmod)
+      WRITE( stdout,'(/,5x,"Fixed quantization axis for GGA: ", 3f12.6)') &
                           ux(1), ux(2), ux(3)
-   return
-   end subroutine compute_ux
+   ENDIF
+   RETURN
+   END SUBROUTINE compute_ux
