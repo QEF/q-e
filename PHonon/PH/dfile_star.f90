895206eb760a2a4da8c127b45719b937af13134b
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2012-05-14 13:58:04 +0000

    Nonstandard Fortran (95 at least): allocatables non allowed in structures. Unchecked.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@9003 c92efa57-630b-4861-b058-cf58834340f0

--- PHonon/PH/dfile_star.f90
+++ PHonon/PH/dfile_star.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2001-2003 PWSCF group
+! Copyright (C) 2012 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -25,7 +25,12 @@ MODULE dfile_star
       dvscf_star   !
 
   TYPE rotated_pattern_repr
-    INTEGER,ALLOCATABLE ::  npert (:), irgq (:)
+#ifdef __STD_F95
+#define __ALLOCATABLE pointer
+#else
+#define __ALLOCATABLE allocatable
+#endif
+    INTEGER,__ALLOCATABLE ::  npert (:), irgq (:)
     ! output: the dimension of each represe
     ! output: the small group of q
     INTEGER :: nsymq, irotmq, nirr, npertx
@@ -34,19 +39,19 @@ MODULE dfile_star
     ! output: the number of irr. representa
     ! output: the max number of irreps
     !
-    REAL(DP),ALLOCATABLE :: gi (:,:), gimq (:), eigen(:)
+    REAL(DP),__ALLOCATABLE :: gi (:,:), gimq (:), eigen(:)
     ! output: [S(irotq)*q - q]
     ! output: [S(irotmq)*q + q]
     ! output: eigenvalues of the dynmat
     !
-    COMPLEX(DP),ALLOCATABLE :: u(:,:), t(:,:,:,:), tmq (:,:,:)
+    COMPLEX(DP),__ALLOCATABLE :: u(:,:), t(:,:,:,:), tmq (:,:,:)
     ! output: the pattern vectors
     ! output: the symmetry matrices
     ! output: the matrice sending q -> -q+G
     LOGICAL :: minus_q
     ! output: if true one symmetry send q -> -q+G
-    INTEGER,ALLOCATABLE :: num_rap_mode(:)
-    CHARACTER(len=15),ALLOCATABLE :: name_rap_mode(:)
+    INTEGER,__ALLOCATABLE :: num_rap_mode(:)
+    CHARACTER(len=15),__ALLOCATABLE :: name_rap_mode(:)
     ! output: the number of the representation of each mode
     ! output: the name of the representation for each group of modes
   END TYPE rotated_pattern_repr
@@ -56,6 +61,11 @@ SUBROUTINE allocate_rotated_pattern_repr(rpat, nat, npertx)
   TYPE(rotated_pattern_repr),INTENT(inout) :: rpat
   INTEGER,INTENT(in) :: nat, npertx
   !
+#ifdef __STD_F95
+   NULLIFY ( rpat%npert, rpat%irgq, rpat%gi, rpat%gimq, rpat%eigen, &
+             rpat%u, rpat%t, rpat%tmq, rpat%num_rap_mode, rpat%name_rap_mode )
+#endif
+
   ALLOCATE(rpat%npert(3*nat))
   ALLOCATE(rpat%irgq(48))
   ALLOCATE(rpat%gi(3,48))
