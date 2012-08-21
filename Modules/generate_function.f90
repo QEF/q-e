de884dd168ffade3f9a8234fb56fa015573b1ca8
oliviero <oliviero@c92efa57-630b-4861-b058-cf58834340f0>
2012-08-21 12:34:51 +0000

    small modification for consistency with other pieces of code
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@9350 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/generate_function.f90
+++ Modules/generate_function.f90
@@ -515,9 +515,11 @@ CONTAINS
      !
      ! ... minimum image convention
      !
-     s(:) = MATMUL( r(:), bg(:,:) )
-     s(:) = s(:) - ANINT(s(:))
-     r(:) = MATMUL( at(:,:), s(:) )
+     CALL cryst_to_cart( 1, r, bg, -1 )
+     !
+     r(:) = r(:) - ANINT( r(:) )
+     !
+     CALL cryst_to_cart( 1, r, at, 1 )
      !
      distance(:,ir) = r(:)
      !
