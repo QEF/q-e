d867300bdf2a225ddebbfcf9da668eead79403a8
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2012-11-10 14:16:33 +0000

    Small cleanup.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@9614 c92efa57-630b-4861-b058-cf58834340f0

--- PHonon/PH/close_phq.f90
+++ PHonon/PH/close_phq.f90
@@ -16,7 +16,6 @@ SUBROUTINE close_phq( flag )
   USE io_files,      ONLY : iunigk
   USE control_flags, ONLY : twfcollect
   USE paw_variables, ONLY : okpaw
-  USE mp_global,     ONLY : me_pool
   USE io_global,     ONLY : ionode, stdout
   USE uspp,          ONLY : okvan
   USE units_ph,      ONLY : iuwfc, iudwf, iubar, iudrhous, iuebar, iudrho, &
