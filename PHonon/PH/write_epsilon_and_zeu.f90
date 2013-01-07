ead5574820ba68127918e4d83d12ba91ec2cdbce
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2013-01-07 14:01:51 +0000

    Small cleanup.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@9752 c92efa57-630b-4861-b058-cf58834340f0

--- PHonon/PH/write_epsilon_and_zeu.f90
+++ PHonon/PH/write_epsilon_and_zeu.f90
@@ -10,13 +10,14 @@ subroutine write_epsilon_and_zeu (zstareu, epsilon, nat, iudyn)
   !-----------------------------------------------------------------------
   USE kinds, only : DP
   USE control_ph, ONLY : xmldyn
+  USE io_global, ONLY : ionode
   implicit none
   ! input variables
   integer :: iudyn, nat
   ! unit number
   ! number of atom in the unit cell
 
-  real(DP) :: zstareu (3, 3, nat), epsilon (3, 3), chi(3, 3)
+  real(DP) :: zstareu (3, 3, nat), epsilon (3, 3)
   !  the effective charges
   !  the dielectric tensor
   ! local variables
@@ -26,14 +27,14 @@ subroutine write_epsilon_and_zeu (zstareu, epsilon, nat, iudyn)
   !
   ! write dielectric tensor and Z(E,Us) effective charges on iudyn
   !
-  IF (.NOT.xmldyn) THEN
-  write (iudyn, '(/,5x,"Dielectric Tensor:",/)')
-  write (iudyn, '(3f24.12)') ((epsilon(icar,jcar), jcar=1,3), icar=1,3)
-  write (iudyn, '(/5x, "Effective Charges E-U: Z_{alpha}{s,beta}",/)')
-  do na = 1, nat
-     write (iudyn, '(5x,"atom # ",i4)') na
-     write (iudyn, '(3f24.12)') ((zstareu(icar,jcar,na), jcar=1,3), icar=1,3)
-  enddo
+  IF (.NOT.xmldyn.AND.ionode) THEN
+     write (iudyn, '(/,5x,"Dielectric Tensor:",/)')
+     write (iudyn, '(3f24.12)') ((epsilon(icar,jcar), jcar=1,3), icar=1,3)
+     write (iudyn, '(/5x, "Effective Charges E-U: Z_{alpha}{s,beta}",/)')
+     do na = 1, nat
+        write (iudyn, '(5x,"atom # ",i4)') na
+        write (iudyn, '(3f24.12)') ((zstareu(icar,jcar,na), jcar=1,3), icar=1,3)
+     enddo
   ENDIF
   !
   ! write dielectric tensor and Z(E,Us) effective charges on standard output
