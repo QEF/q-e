41be3205567d2e519da7160fc6e06876a1be0ee1
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2013-03-12 15:26:15 +0000

    NEB with images was broken by one of my previous changes to I/O
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10054 c92efa57-630b-4861-b058-cf58834340f0

--- NEB/src/compute_scf.f90
+++ NEB/src/compute_scf.f90
@@ -29,7 +29,7 @@ SUBROUTINE compute_scf( fii, lii, stat  )
   USE ions_base,        ONLY : tau, nat, nsp, ityp
   USE ener,             ONLY : etot
   USE force_mod,        ONLY : force
-  USE io_files,         ONLY : prefix, tmp_dir, iunupdate, seqopn, &
+  USE io_files,         ONLY : prefix, tmp_dir, wfc_dir,  iunupdate, seqopn, &
                                exit_file, iunexit, delete_if_present
   USE path_io_units_module, ONLY : iunpath
   USE path_formats,     ONLY : scf_fmt, scf_fmt_para
@@ -263,6 +263,7 @@ SUBROUTINE compute_scf( fii, lii, stat  )
       !
       tmp_dir = TRIM( tmp_dir_saved ) // TRIM( prefix ) // "_" // &
                 TRIM( int_to_char( image ) ) // "/"
+      wfc_dir = tmp_dir
       !
       ! ... unit stdout is connected to the appropriate file
       !
