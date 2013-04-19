b6782e3384e0c01a299fc5e172ed8f2b928e1074
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2013-04-19 10:43:51 +0000

    DAXPY/ZAXPY => DGEMM/ZGEMM (definitely faster)
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10165 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/vhpsi.f90
+++ PW/src/vhpsi.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2001-2007 Quantum ESPRESSO group
+! Copyright (C) 2001-2013 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -11,11 +11,11 @@ subroutine vhpsi (ldap, np, mps, psip, hpsi)
   !-----------------------------------------------------------------------
   !
   ! This routine computes the Hubbard potential applied to the electronic
-  ! of the current k-point, the result is added to hpsi
+  ! structure of the current k-point. The result is added to hpsi
   !
   USE kinds,     ONLY : DP
   USE becmod,    ONLY : bec_type, calbec, allocate_bec_type, deallocate_bec_type
-  USE ldaU,      ONLY : Hubbard_lmax, Hubbard_l, HUbbard_U, Hubbard_alpha, &
+  USE ldaU,      ONLY : Hubbard_lmax, Hubbard_l, Hubbard_U, Hubbard_alpha, &
                         swfcatom, oatwfc, Hubbard_J0, Hubbard_beta
   USE lsda_mod,  ONLY : current_spin
   USE scf,       ONLY : v
@@ -30,50 +30,72 @@ subroutine vhpsi (ldap, np, mps, psip, hpsi)
   complex(DP), intent(in) :: psip (ldap, mps)
   complex(DP), intent(inout) :: hpsi (ldap, mps)
   !
-  integer :: ibnd, na, nt, m1, m2
-  complex(DP) :: temp
+  integer :: ibnd, na, nt, m1, m2, ldim
+  REAL(DP), ALLOCATABLE :: rtemp(:,:)
+  COMPLEX(DP), ALLOCATABLE :: ctemp(:,:)
   type (bec_type) :: proj
 
   CALL start_clock('vhpsi')
   !
   ! Offset of atomic wavefunctions initialized in setup and stored in oatwfc
   !
-  call allocate_bec_type ( natomwfc,mps, proj )
+  CALL allocate_bec_type ( natomwfc,mps, proj )
   CALL calbec (np, swfcatom, psip, proj)
-  do ibnd = 1, mps  
-     do na = 1, nat  
-        nt = ityp (na)  
-        if (Hubbard_U(nt).ne.0.d0 .or. Hubbard_alpha(nt).ne.0.d0.or.&
-            Hubbard_J0(nt).ne.0.d0 .or. Hubbard_beta(nt).ne.0.d0 ) then  
-           do m1 = 1, 2 * Hubbard_l(nt) + 1 
-              temp = 0.d0
-              if (gamma_only) then
-                 do m2 = 1, 2 * Hubbard_l(nt) + 1 
-                    temp = temp + v%ns( m1, m2, current_spin, na) * &
-                                    proj%r(oatwfc(na)+m2, ibnd)
-                 enddo
-                 call daxpy (2*np, temp, swfcatom(1,oatwfc(na)+m1), 1, &
-                                    hpsi(1,ibnd),              1)
-              else
-                 do m2 = 1, 2 * Hubbard_l(nt) + 1 
-                    temp = temp + v%ns( m1, m2, current_spin, na) * &
-                                    proj%k(oatwfc(na)+m2, ibnd)
-                 enddo
-                 call zaxpy (np, temp, swfcatom(1,oatwfc(na)+m1), 1, &
-                                    hpsi(1,ibnd),              1)
-              endif
-           enddo
-        endif
-     enddo
-  enddo
-  call deallocate_bec_type (proj)
+  !
+  DO nt = 1, ntyp
+     IF ( Hubbard_U(nt)/= 0.0_dp .OR. Hubbard_alpha(nt) /= 0.0_dp .OR. &
+          Hubbard_J0(nt) /= 0.0_dp .OR. Hubbard_beta(nt)/= 0.0_dp ) THEN
+        ldim = 2*Hubbard_l(nt) + 1
+        IF  (gamma_only) THEN
+           ALLOCATE ( rtemp(ldim,mps) )
+        ELSE
+           ALLOCATE ( ctemp(ldim,mps) )
+        END IF
+        DO na = 1, nat  
+           IF ( nt == ityp (na) ) THEN
+              IF (gamma_only) THEN
+                 DO m1 = 1,ldim 
+                    DO ibnd = 1, mps  
+                       rtemp(m1,ibnd) = 0.0_dp
+                       DO m2 = 1,ldim 
+                          rtemp(m1,ibnd) = rtemp(m1,ibnd) + v%ns(m1,m2,current_spin,na)*&
+                                                            proj%r(oatwfc(na)+m2, ibnd)
+                       ENDDO
+                    ENDDO
+                 ENDDO
+                 CALL DGEMM ('n','n', 2*np, mps, ldim, 1.0_dp, swfcatom(1,oatwfc(na)+1),&
+                      2*ldap, rtemp, ldim, 1.0_dp, hpsi, 2*ldap)
+              ELSE
+                 DO m1 = 1,ldim 
+                    DO ibnd = 1, mps  
+                       ctemp(m1,ibnd) = (0.0_dp, 0.0_dp)
+                       DO m2 = 1,ldim 
+                          ctemp(m1,ibnd) = ctemp(m1,ibnd) + v%ns(m1,m2,current_spin,na)*&
+                               proj%k(oatwfc(na)+m2, ibnd)
+                       ENDDO
+                    ENDDO
+                 ENDDO
+                 CALL ZGEMM ('n','n', np, mps, ldim, (1.0_dp,0.0_dp), &
+                      swfcatom(1,oatwfc(na)+1), ldap, ctemp, ldim, &
+                      (1.0_dp,0.0_dp), hpsi, ldap)
+              ENDIF
+           ENDIF
+        ENDDO
+        IF (gamma_only) THEN
+           DEALLOCATE ( rtemp )
+        ELSE
+           DEALLOCATE ( ctemp )
+        ENDIF
+     ENDIF
+  ENDDO
+  !
+  CALL deallocate_bec_type (proj)
   !
   CALL stop_clock('vhpsi')
 
-  return
+  RETURN
 
-end subroutine vhpsi
-
+END subroutine vhpsi
 
 subroutine vhpsi_nc (ldap, np, mps, psip, hpsi)
   !-----------------------------------------------------------------------
@@ -81,7 +103,7 @@ subroutine vhpsi_nc (ldap, np, mps, psip, hpsi)
   ! Noncollinear version (A. Smogunov). 
   !
   USE kinds,            ONLY : DP
-  USE ldaU,             ONLY : Hubbard_lmax, Hubbard_l, HUbbard_U, swfcatom, oatwfc  
+  USE ldaU,             ONLY : Hubbard_lmax, Hubbard_l, Hubbard_U, swfcatom, oatwfc  
   USE scf,              ONLY : v
   USE ions_base,        ONLY : nat, ntyp => nsp, ityp
   USE noncollin_module, ONLY : npol
