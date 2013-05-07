77e5d06d0b7b5e6f7243e1464523b1824371d212
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2013-05-07 14:42:04 +0000

    Hubbard contribution to stress many times faster for many atoms and processors
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10230 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/stres_hub.f90
+++ PW/src/stres_hub.f90
@@ -153,6 +153,7 @@ SUBROUTINE dndepsilon ( ipol,jpol,ldim,dns )
               ibnd,  & !    "    "  bands
               is,    & !    "    "  spins
               na, nt, m1, m2, nb_s, nb_e, mykey
+   REAL(DP), ALLOCATABLE :: dns_(:,:,:,:) ! partial contribution 
    COMPLEX (DP), ALLOCATABLE :: spsi(:,:), wfcatom(:,:)
    type (bec_type) :: proj, dproj
    !
@@ -169,6 +170,7 @@ SUBROUTINE dndepsilon ( ipol,jpol,ldim,dns )
    call allocate_bec_type ( nwfcU,nbnd, proj)
    call allocate_bec_type ( nwfcU,nbnd, dproj )
    call allocate_bec_type ( nkb,nbnd, becp )
+   ALLOCATE ( dns_(ldim,ldim,nspin,nat) )
    !
    ! D_Sl for l=1 and l=2 are already initialized, for l=0 D_S0 is 1
    !
@@ -206,32 +208,36 @@ SUBROUTINE dndepsilon ( ipol,jpol,ldim,dns )
       ! epsilon(ipol,jpol)
       !
       IF ( gamma_only ) THEN
-         CALL dprojdepsilon_gamma (wfcU, spsi, ipol, jpol, dproj%r)
+         CALL dprojdepsilon_gamma (spsi, ipol, jpol, nb_s, nb_e, mykey, dproj%r)
       ELSE
-         CALL dprojdepsilon_k (wfcU, spsi, ik, ipol, jpol, dproj%k)
+         CALL dprojdepsilon_k (spsi, ik, ipol, jpol, nb_s, nb_e, mykey, dproj%k)
       END IF
       !
       ! compute the derivative of the occupation numbers (quantities dn(m1,m2))
       ! of the atomic orbitals. They are real quantities as well as n(m1,m2)
       !
+      ! band parallelization. If each band appears more than once
+      ! compute its contribution only once (i.e. when mykey=0)
+      dns_(:,:,:,:) = 0.0_dp
+      IF ( mykey /= 0 ) GO TO 10
       DO na = 1,nat
          nt = ityp(na)
          IF ( is_hubbard(nt) ) THEN        
             DO m1 = 1, 2 * Hubbard_l(nt) + 1
                DO m2 = m1, 2 * Hubbard_l(nt) + 1
                   IF ( gamma_only ) THEN
-                     DO ibnd = 1,nbnd
-                        dns(m1,m2,current_spin,na) = &
-                           dns(m1,m2,current_spin,na) + wg(ibnd,ik) *&
+                     DO ibnd = nb_s, nb_e
+                        dns_(m1,m2,current_spin,na) = &
+                           dns_(m1,m2,current_spin,na) + wg(ibnd,ik) *&
                                    ( proj%r(offsetU(na)+m1,ibnd) *      &
                                     dproj%r(offsetU(na)+m2,ibnd) +      &
                                     dproj%r(offsetU(na)+m1,ibnd) *      &
                                      proj%r(offsetU(na)+m2,ibnd))
                      END DO
                   ELSE
-                     DO ibnd = 1,nbnd
-                        dns(m1,m2,current_spin,na) = &
-                           dns(m1,m2,current_spin,na) + wg(ibnd,ik) *&
+                     DO ibnd = nb_s, nb_e
+                        dns_(m1,m2,current_spin,na) = &
+                           dns_(m1,m2,current_spin,na) + wg(ibnd,ik) *&
                                DBLE(proj%k(offsetU(na)+m1,ibnd) *      &
                               CONJG(dproj%k(offsetU(na)+m2,ibnd) ) +    &
                                     dproj%k(offsetU(na)+m1,ibnd)*       &
@@ -242,8 +248,11 @@ SUBROUTINE dndepsilon ( ipol,jpol,ldim,dns )
             END DO
          END IF
       END DO
+10    CALL mp_sum(dns_, intra_pool_comm)
+      dns(:,:,:,:) = dns(:,:,:,:) + dns_(:,:,:,:)
    END DO                 ! on k-points
    !
+   DEALLOCATE ( dns_ )
    CALL mp_sum( dns, inter_pool_comm )
    !
    ! In nspin.eq.1 k-point weight wg is normalized to 2 el/band 
@@ -274,7 +283,7 @@ SUBROUTINE dndepsilon ( ipol,jpol,ldim,dns )
 END SUBROUTINE dndepsilon
 !
 !-----------------------------------------------------------------------
-SUBROUTINE dprojdepsilon_k ( wfcU, spsi, ik, ipol, jpol, dproj )
+SUBROUTINE dprojdepsilon_k ( spsi, ik, ipol, jpol, nb_s, nb_e, mykey, dproj )
    !-----------------------------------------------------------------------
    !
    ! This routine computes the first derivative of the projection
@@ -287,7 +296,7 @@ SUBROUTINE dprojdepsilon_k ( wfcU, spsi, ik, ipol, jpol, dproj )
    USE ions_base,            ONLY : nat, ntyp => nsp, ityp
    USE gvect,                ONLY : g
    USE klist,                ONLY : nks, xk
-   USE ldaU,                 ONLY : hubbard_l, is_hubbard, nwfcU
+   USE ldaU,                 ONLY : hubbard_l, is_hubbard, nwfcU, wfcU
    USE lsda_mod,             ONLY : lsda, nspin, current_spin, isk
    USE wvfct,                ONLY : nbnd, npwx, npw, igk, wg
    USE uspp,                 ONLY : nkb, vkb, qq
@@ -301,9 +310,8 @@ SUBROUTINE dprojdepsilon_k ( wfcU, spsi, ik, ipol, jpol, dproj )
    !
    ! I/O variables first
    !
-   INTEGER, INTENT(IN) :: ik, ipol, jpol
+   INTEGER, INTENT(IN) :: ik, ipol, jpol, nb_s, nb_e, mykey
    COMPLEX (DP), INTENT(IN)  :: &
-           wfcU(npwx,nwfcU), &! the atomic wfc
            spsi(npwx,nbnd)          ! S|evc>
    COMPLEX (DP), INTENT(OUT) :: &
            dproj(nwfcU,nbnd)     ! the derivative of the projection
@@ -311,17 +319,16 @@ SUBROUTINE dprojdepsilon_k ( wfcU, spsi, ik, ipol, jpol, dproj )
    INTEGER :: i, ig, ijkb0, na, ibnd, iwf, nt, ih,jh
    REAL (DP) :: xyz(3,3), q, a1, a2
    REAL (DP), PARAMETER :: eps=1.0d-8
-   COMPLEX (DP), EXTERNAL :: zdotc
    COMPLEX (DP), ALLOCATABLE :: &
-           dwfc(:,:), aux(:,:), dbeta(:,:), aux1(:,:), &
+           dwfc(:,:), aux(:,:), dbeta(:,:), aux0(:,:), aux1(:,:), &
            betapsi(:,:), dbetapsi(:,:), wfatbeta(:,:), wfatdbeta(:,:)
 
    !       dwfc(npwx,nwfcU),   ! the derivative of the atomic d wfc
    !       aux(npwx,nwfcU),    ! auxiliary array
-   !       dbeta(npwx,nkb),       ! the derivative of the beta function
-   !       aux1(npwx,nkb),        ! auxiliary array
-   !       betapsi(nhm,nbnd),     ! <beta|evc>
-   !       dbetapsi(nhm,nbnd),    ! <dbeta|evc>
+   !       dbeta(npwx,nkb),    ! the derivative of the beta function
+   !       aux0,aux1(npwx,nkb),! auxiliary arrays
+   !       betapsi(nhm,nbnd),  ! <beta|evc>
+   !       dbetapsi(nhm,nbnd), ! <dbeta|evc>
    !       wfatbeta(nwfcU,nhm),! <wfc|beta>
    !       wfatdbeta(nwfcU,nhm)! <wfc|dbeta>
 
@@ -373,66 +380,90 @@ SUBROUTINE dprojdepsilon_k ( wfcU, spsi, ik, ipol, jpol, dproj )
    ! Now the derivatives of the beta functions: we compute the term
    ! <\fi^{at}_{I,m1}|dS/d\epsilon(ipol,jpol)|\psi_{k,v,s}>
    !
-   ALLOCATE (dbeta(npwx,nkb), aux1(npwx,nkb), &
-             dbetapsi(nhm,nbnd), betapsi(nhm,nbnd), wfatbeta(nwfcU,nhm), &
-             wfatdbeta(nwfcU,nhm) )
+   ALLOCATE (aux0(npwx,nkb), aux1(npwx,nkb) )
 
    ! here the derivative of the Bessel function
-   CALL gen_us_dj (ik,dbeta)
+   CALL gen_us_dj (ik, aux0)
    ! and here the derivative of the spherical harmonic
-   CALL gen_us_dy (ik,xyz(1,ipol),aux1)
+   CALL gen_us_dy (ik, xyz(1,ipol), aux1)
 
    ijkb0 = 0
    DO nt=1,ntyp
-      dbetapsi(:,:) = (0.0_dp, 0.0_dp)
-      wfatbeta (:,:)= (0.0_dp, 0.0_dp)
-      wfatdbeta(:,:)= (0.0_dp, 0.0_dp)
+      ALLOCATE (dbeta(npwx,nh(nt)), dbetapsi(nh(nt),nbnd), betapsi(nh(nt),nbnd), &
+                wfatbeta(nwfcU,nh(nt)), wfatdbeta(nwfcU,nh(nt)) )
       DO na=1,nat
          IF ( ityp(na) .EQ. nt ) THEN
             DO ih=1,nh(nt)
                ! now we compute the true dbeta function
                DO ig = 1,npw
-                  dbeta(ig,ijkb0+ih) = - aux1(ig,ijkb0+ih)*gk(jpol,ig) - &
-                       dbeta(ig,ijkb0+ih) * gk(ipol,ig) * gk(jpol,ig) * qm1(ig)
+                  dbeta(ig,ih) = - aux1(ig,ijkb0+ih)*gk(jpol,ig) - &
+                       aux0(ig,ijkb0+ih) * gk(ipol,ig) * gk(jpol,ig) * qm1(ig)
                   IF (ipol.EQ.jpol) &
-                       dbeta(ig,ijkb0+ih) = dbeta(ig,ijkb0+ih) - vkb(ig,ijkb0+ih)*0.5d0
-               END DO
-               DO ibnd = 1,nbnd
-                  betapsi(ih,ibnd)= becp%k(ijkb0+ih,ibnd)
+                       dbeta(ig,ih) = dbeta(ig,ih) - vkb(ig,ijkb0+ih)*0.5d0
                END DO
             END DO
+            CALL calbec(npw, dbeta, evc, dbetapsi )
+            CALL calbec(npw, wfcU, dbeta,wfatdbeta )
             !
-            CALL calbec(npw, dbeta(:,ijkb0+1:ijkb0+nh(nt)), evc, dbetapsi(1:nh(nt),:) )
-            CALL calbec(npw, wfcU, vkb(:,ijkb0+1:ijkb0+nh(nt)), wfatbeta(:,1:nh(nt)) )
-            CALL calbec(npw, wfcU, dbeta(:,ijkb0+1:ijkb0+nh(nt)),wfatdbeta(:,1:nh(nt)) )
+            ! dbeta is now used as work space to store vkb
+            DO ih=1,nh(nt)
+               DO ig = 1,npw
+                  dbeta(ig,ih) = vkb(ig,ijkb0+ih)
+               END DO
+            END DO
+            CALL calbec(npw, wfcU, dbeta, wfatbeta )
             !
-            ijkb0 = ijkb0 + nh(nt)
-            !
-            DO ibnd = 1,nbnd
-               DO ih=1,nh(nt)
+            ! here starts band parallelization
+            ! beta is here used as work space to calculate dbetapsi
+            betapsi(:,:) = (0.0_dp, 0.0_dp)
+            DO ih=1,nh(nt)
+               DO ibnd = nb_s,nb_e
                   DO jh = 1,nh(nt)
-                     DO iwf=1,nwfcU
-                        dproj(iwf,ibnd) = dproj(iwf,ibnd) +               &
-                                          qq(ih,jh,nt) *                  &
-                               ( wfatdbeta(iwf,ih)*betapsi(jh,ibnd) +     &
-                                 wfatbeta(iwf,ih)*dbetapsi(jh,ibnd) )
-                     END DO
+                     betapsi(ih,ibnd) = betapsi(ih,ibnd) + &
+                          qq(ih,jh,nt)  * dbetapsi(jh,ibnd)
                   END DO
                END DO
             END DO
+            dbetapsi (:,:) =  betapsi(:,:)
+            !
+            DO ih=1,nh(nt)
+               DO ibnd = nb_s,nb_e
+                  betapsi(ih,ibnd)= (0.0_dp, 0.0_dp)
+                  DO jh = 1,nh(nt)
+                     betapsi(ih,ibnd) = betapsi(ih,ibnd) + &
+                          qq(ih,jh,nt) * becp%k(ijkb0+jh,ibnd)
+                  END DO
+               END DO
+            END DO
+            !
+            ijkb0 = ijkb0 + nh(nt)
+            !
+            ! dproj(iwf,ibnd) = \sum_ih wfatdbeta(iwf,ih)*betapsi(ih,ibnd) +
+            !                           wfatbeta(iwf,ih)*dbetapsi(ih,ibnd) 
+            !
+            IF ( mykey == 0 ) THEN
+               CALL ZGEMM('N','N',nwfcU, nb_e-nb_s+1, nh(nt), (1.0_dp,0.0_dp), &
+                    wfatdbeta, nwfcU, betapsi(1,nb_s), nh(nt),(1.0_dp,0.0_dp), &
+                    dproj(1,nb_s), nwfcU)
+               CALL ZGEMM('N','N',nwfcU,nb_e-nb_s+1, nh(nt), (1.0_dp,0.0_dp),  &
+                    wfatbeta, nwfcU, dbetapsi(1,nb_s), nh(nt),(1.0_dp,0.0_dp), &
+                    dproj(1,nb_s), nwfcU)
+            END IF
+            ! end band parallelization - only dproj(1,nb_s:nb_e) are calculated
          END IF
       END DO
+      DEALLOCATE (dbeta, dbetapsi, betapsi, wfatbeta, wfatdbeta )
    END DO
 
 
-   DEALLOCATE (dbeta, aux1, dbetapsi, betapsi, wfatbeta, wfatdbeta )
+   DEALLOCATE ( aux0, aux1 )
    DEALLOCATE ( qm1, gk )
 
    RETURN
 END SUBROUTINE dprojdepsilon_k
 !
 !-----------------------------------------------------------------------
-SUBROUTINE dprojdepsilon_gamma ( wfcU, spsi, ipol, jpol, dproj )
+SUBROUTINE dprojdepsilon_gamma ( spsi, ipol, jpol, nb_s, nb_e, mykey, dproj )
    !-----------------------------------------------------------------------
    !
    ! This routine computes the first derivative of the projection
@@ -445,7 +476,7 @@ SUBROUTINE dprojdepsilon_gamma ( wfcU, spsi, ipol, jpol, dproj )
    USE ions_base,            ONLY : nat, ntyp => nsp, ityp
    USE gvect,                ONLY : g, gstart
    USE klist,                ONLY : nks, xk
-   USE ldaU,                 ONLY : is_hubbard, hubbard_l, nwfcU
+   USE ldaU,                 ONLY : is_hubbard, hubbard_l, nwfcU, wfcU
    USE lsda_mod,             ONLY : lsda, nspin, current_spin, isk
    USE wvfct,                ONLY : nbnd, npwx, npw, igk, wg
    USE uspp,                 ONLY : nkb, vkb, qq
@@ -459,9 +490,8 @@ SUBROUTINE dprojdepsilon_gamma ( wfcU, spsi, ipol, jpol, dproj )
    !
    ! I/O variables first
    !
-   INTEGER, INTENT(IN) :: ipol, jpol
+   INTEGER, INTENT(IN) :: ipol, jpol, nb_s, nb_e, mykey
    COMPLEX (DP), INTENT(IN)  :: &
-           wfcU(npwx,nwfcU), &! the atomic wfc
            spsi(npwx,nbnd)          ! S|evc>
    REAL (DP), INTENT(OUT) :: &
            dproj(nwfcU,nbnd)     ! the derivative of the projection
@@ -469,13 +499,12 @@ SUBROUTINE dprojdepsilon_gamma ( wfcU, spsi, ipol, jpol, dproj )
    INTEGER :: ik=1, i, ig, ijkb0, na, ibnd, iwf, nt, ih,jh
    REAL (DP) :: xyz(3,3), q, a1, a2
    REAL (DP), PARAMETER :: eps=1.0d-8
-   REAL (DP), EXTERNAL :: ddot
    COMPLEX (DP), ALLOCATABLE :: &
-           dwfc(:,:), aux(:,:), dbeta(:,:), aux1(:,:)
+           dwfc(:,:), aux(:,:), dbeta(:,:), aux0(:,:), aux1(:,:)
    !       dwfc(npwx,nwfcU),   ! the derivative of the atomic d wfc
    !       aux(npwx,nwfcU),    ! auxiliary array
-   !       dbeta(npwx,nkb),       ! the derivative of the beta function
-   !       aux1(npwx,nkb),        ! auxiliary array
+   !       dbeta(npwx,nkb),    ! the derivative of the beta function
+   !       aux0,aux1(npwx,nkb) ! auxiliary arrays
    REAL (DP), ALLOCATABLE :: &
            betapsi(:,:), dbetapsi(:,:), wfatbeta(:,:), wfatdbeta(:,:)
    !       betapsi(nhm,nbnd),     ! <beta|evc>
@@ -531,58 +560,84 @@ SUBROUTINE dprojdepsilon_gamma ( wfcU, spsi, ipol, jpol, dproj )
    ! Now the derivatives of the beta functions: we compute the term
    ! <\fi^{at}_{I,m1}|dS/d\epsilon(ipol,jpol)|\psi_{k,v,s}>
    !
-   ALLOCATE (dbeta(npwx,nkb), aux1(npwx,nkb), &
-             dbetapsi(nhm,nbnd), betapsi(nhm,nbnd), &
-             wfatbeta(nwfcU,nhm), wfatdbeta(nwfcU,nhm) )
+   ALLOCATE (aux0(npwx,nkb), aux1(npwx,nkb) )
 
    ! here the derivative of the Bessel function
-   CALL gen_us_dj (ik,dbeta)
+   CALL gen_us_dj (ik, aux0)
    ! and here the derivative of the spherical harmonic
-   CALL gen_us_dy (ik,xyz(1,ipol),aux1)
+   CALL gen_us_dy (ik, xyz(1,ipol), aux1)
 
    ijkb0 = 0
    DO nt=1,ntyp
-      dbetapsi (:,:) = 0.0_dp
-      wfatbeta (:,:) = 0.0_dp
-      wfatdbeta(:,:) = 0.0_dp
+      ALLOCATE (dbeta(npwx,nh(nt)), dbetapsi(nh(nt),nbnd), betapsi(nh(nt),nbnd), &
+                wfatbeta(nwfcU,nh(nt)), wfatdbeta(nwfcU,nh(nt)) )
       DO na=1,nat
          IF ( ityp(na) .EQ. nt ) THEN
             DO ih=1,nh(nt)
                ! now we compute the true dbeta function
                DO ig = 1,npw
-                  dbeta(ig,ijkb0+ih) = - aux1(ig,ijkb0+ih)*gk(jpol,ig) - &
-                       dbeta(ig,ijkb0+ih) * gk(ipol,ig) * gk(jpol,ig) * qm1(ig)
+                  dbeta(ig,ih) = - aux1(ig,ijkb0+ih)*gk(jpol,ig) - &
+                       aux0(ig,ijkb0+ih) * gk(ipol,ig) * gk(jpol,ig) * qm1(ig)
                   IF (ipol.EQ.jpol) &
-                       dbeta(ig,ijkb0+ih) = dbeta(ig,ijkb0+ih) - vkb(ig,ijkb0+ih)*0.5d0
-               END DO
-               DO ibnd = 1,nbnd
-                  betapsi(ih,ibnd)= becp%r(ijkb0+ih,ibnd)
+                       dbeta(ig,ih) = dbeta(ig,ih) - vkb(ig,ijkb0+ih)*0.5d0
                END DO
             END DO
             !
-            CALL calbec(npw, dbeta(:,ijkb0+1:ijkb0+nh(nt)), evc, dbetapsi(1:nh(nt),:) )
-            CALL calbec(npw, wfcU, vkb(:,ijkb0+1:ijkb0+nh(nt)), wfatbeta(:,1:nh(nt)) )
-            CALL calbec(npw, wfcU, dbeta(:,ijkb0+1:ijkb0+nh(nt)),wfatdbeta(:,1:nh(nt)) )
+            CALL calbec(npw, dbeta, evc, dbetapsi )
+            CALL calbec(npw, wfcU, dbeta, wfatdbeta )
             !
-            ijkb0 = ijkb0 + nh(nt)
+            ! dbeta is now used as work space to store vkb
+            DO ih=1,nh(nt)
+               DO ig = 1,npw
+                  dbeta(ig,ih) = vkb(ig,ijkb0+ih)
+               END DO
+            END DO
+            CALL calbec(npw, wfcU, dbeta, wfatbeta )
             !
-            DO ibnd = 1,nbnd
-               DO ih=1,nh(nt)
+            ! here starts band parallelization
+            ! beta is here used as work space to calculate dbetapsi
+
+            betapsi(:,:) = 0.0_dp
+            DO ih=1,nh(nt)
+               DO ibnd = nb_s,nb_e
                   DO jh = 1,nh(nt)
-                     DO iwf=1,nwfcU
-                        dproj(iwf,ibnd) = dproj(iwf,ibnd) +               &
-                                          qq(ih,jh,nt) *                  &
-                               ( wfatdbeta(iwf,ih)*betapsi(jh,ibnd) +     &
-                                 wfatbeta(iwf,ih)*dbetapsi(jh,ibnd) )
-                     END DO
+                     betapsi(ih,ibnd) = betapsi(ih,ibnd) + &
+                          qq(ih,jh,nt)  * dbetapsi(jh,ibnd)
                   END DO
                END DO
             END DO
+            dbetapsi (:,:) =  betapsi(:,:)
+            !
+            DO ih=1,nh(nt)
+               DO ibnd = nb_s,nb_e
+                  betapsi(ih,ibnd)= 0.0_dp
+                  DO jh = 1,nh(nt)
+                     betapsi(ih,ibnd) = betapsi(ih,ibnd) + &
+                          qq(ih,jh,nt) * becp%r(ijkb0+jh,ibnd)
+                  END DO
+               END DO
+            END DO
+            !
+            ijkb0 = ijkb0 + nh(nt)
+            !
+            ! dproj(iwf,ibnd) = \sum_ih wfatdbeta(iwf,ih)*betapsi(ih,ibnd) +
+            !                           wfatbeta(iwf,ih)*dbetapsi(ih,ibnd) 
+            !
+            IF ( mykey == 0 ) THEN
+               CALL DGEMM('N','N',nwfcU, nb_e-nb_s+1, nh(nt), 1.0_dp,  &
+                    wfatdbeta, nwfcU, betapsi(1,nb_s), nh(nt), 1.0_dp,&
+                    dproj(1,nb_s), nwfcU)
+               CALL DGEMM('N','N',nwfcU, nb_e-nb_s+1, nh(nt), 1.0_dp,  &
+                    wfatbeta, nwfcU, dbetapsi(1,nb_s), nh(nt), 1.0_dp,&
+                    dproj(1,nb_s), nwfcU)
+            END IF
+            ! end band parallelization - only dproj(1,nb_s:nb_e) are calculated
          END IF
       END DO
+      DEALLOCATE (dbeta, dbetapsi, betapsi, wfatbeta, wfatdbeta )
    END DO
 
-   DEALLOCATE (dbeta, aux1, dbetapsi, betapsi, wfatbeta, wfatdbeta )
+   DEALLOCATE ( aux0, aux1 )
    DEALLOCATE ( qm1, gk )
 
    RETURN
