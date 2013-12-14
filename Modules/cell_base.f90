b792fd708b6178ef23d961df64a938e3a59232d5
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2013-12-14 21:41:35 +0000

    Added check on values of cosab, cosac, cosbc
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10641 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/cell_base.f90
+++ Modules/cell_base.f90
@@ -198,6 +198,9 @@
         celldm(1) = a / bohr_radius_angs
         celldm(2) = b / a
         celldm(3) = c / a
+        IF ( (ABS(cosab) > 1.0_dp) .OR. (ABS(cosac) > 1.0_dp) .OR. &
+             (ABS(cosbc) > 1.0_dp) ) CALL errore ('cell_base_init',&
+                         'incorrect values for cosab, cosac, cosbc',1)
         !
         IF ( ibrav == 14 ) THEN
            !
