362a8cbaedc1e2e24d413fdfcc1a66daa4458626
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2013-12-19 20:29:57 +0000

    More explicit message, for those who do not know about solid state physics
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10644 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/lchk_tauxk.f90
+++ PW/src/lchk_tauxk.f90
@@ -30,7 +30,7 @@ subroutine check_atoms (nvec, vec, trmat)
   integer :: nv1, nv2
   real(DP), allocatable :: vaux(:,:)
   real(DP) :: zero (3) = 0.0_dp
-  character(len=30) :: message
+  character(len=80) :: message
   logical, external :: eqvect
   !
   !   Copy input positions and transform them to crystal units
@@ -44,8 +44,15 @@ subroutine check_atoms (nvec, vec, trmat)
   do nv1 = 1, nvec-1
      do nv2 = nv1+1, nvec
         if ( eqvect ( vaux (1,nv1), vaux (1,nv2), zero, accep ) ) then
-           write (message,'("atoms #",i4," and #",i4," overlap!")') nv1, nv2
-           call errore ( 'check_atoms', message, 1)
+           zero(:) = vaux (:,nv1)-vaux(:,nv2)
+           IF ( ALL ( abs(zero) < accep ) ) THEN
+              WRITE (message,'("atoms #",i4," and #",i4," overlap!")') nv1, nv2
+           ELSE
+              WRITE (message,'("atoms #",i4," and #",i4," differ by lattice &
+                    &vector (",i2,",",i2,",",i2,") in crystal axis")') &
+                    nv1, nv2, NINT(zero)
+           ENDIF
+           CALL errore ( 'check_atoms', TRIM(message), 1)
         end if
      enddo
   enddo
