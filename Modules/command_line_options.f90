0fd8cef8451e8dbdde3ed80209765bdd6a44c444
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2013-12-22 14:39:29 +0000

    Added possibility to read command line options from an input string
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10645 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/command_line_options.f90
+++ Modules/command_line_options.f90
@@ -9,11 +9,22 @@
 MODULE command_line_options
   !----------------------------------------------------------------------------
   !
-  ! ... Read variables from command line and broadcast it to all processors
-  ! ... ( this is done because there is no guarantee that all processors
-  ! ...   have access to command-line options in parallel execution )
-  ! ... Interpret QE-specific variables, store the corresponding values
-  ! ... Leave the rest (including the code name) in "command_line"
+  ! ... Utilities to read command-line variables and to set related variables:
+  ! ... "get_command_line()" with no arguments: 
+  ! ...                      reads the command line,
+  ! ...                      interprets QE-specific variables,
+  ! ...                      stores the corresponding values
+  ! ...                      (nimage, npot, npool, ntg, nband, ndiag),
+  ! ...                      broadcasts them to all processors,
+  ! ...                      leaves the rest of the command line 
+  ! ...                      (including the code name) in "command_line"
+  ! ... "get_command_line(input_command_line)" with a string argument:
+  ! ...                      as above, but reading from the input string
+  ! ... Variables are read on one processor and broadcast to all others
+  ! ... because there is no guarantee that all processors have access to
+  ! ... command-line options in parallel execution.
+  ! ... "set_command_line" directly sets nimage, npot, npool, ntg, nband, ndiag.
+  ! ... Useful to initialize parallelism when QE is used as a library
   !
   USE mp,        ONLY : mp_bcast
   USE mp_world,  ONLY : root, world_comm
@@ -28,27 +39,35 @@ MODULE command_line_options
   INTEGER :: nimage_= 1, npool_= 1, npot_= 1, ndiag_ = 0, nband_= 1, ntg_= 1
   ! ... Indicate if using library init
   LOGICAL :: library_init = .FALSE.
+  ! ... input file name read from command line
   CHARACTER(LEN=256) :: input_file_ = ' '
-  ! ... Command line arguments not identified
+  ! ... Command line arguments that were not identified and processed
   CHARACTER(LEN=512) :: command_line = ' '
   !
 CONTAINS
   !
-  SUBROUTINE get_command_line ( )
+  SUBROUTINE get_command_line ( input_command_line )
 
      IMPLICIT NONE
+     CHARACTER(LEN=*), OPTIONAL :: input_command_line 
      INTEGER :: narg
      ! Do not define iargc as external: gfortran doesn't like it
-     INTEGER :: iargc
+     INTEGER :: iargc 
+     LOGICAL :: read_string
      CHARACTER(LEN=256) :: arg 
      CHARACTER(LEN=6), EXTERNAL :: int_to_char
      !
      command_line = ' '
+     read_string = PRESENT ( input_command_line )
      !
      ! command line parameters have already been set via set_command_line()
      IF (library_init) GO TO 20
      !
-     nargs = iargc()
+     IF (read_string) THEN
+        nargs = my_iargc ( input_command_line )
+     ELSE
+        nargs = iargc()
+     ENDIF
      CALL mp_bcast ( nargs, root, world_comm )
      !
      ! ... Only the first node reads and broadcasts
@@ -58,35 +77,67 @@ CONTAINS
      arg = ' '
      narg=0
 10   CONTINUE
-        CALL getarg ( narg, arg )
+        IF (read_string) THEN
+           CALL my_getarg ( input_command_line, narg, arg )
+        ELSE
+           CALL getarg ( narg, arg )
+        ENDIF
         narg = narg + 1
         SELECT CASE ( TRIM(arg) )
            CASE ( '-i', '-in', '-inp', '-input' ) 
+           IF (read_string) THEN
+              CALL my_getarg ( input_command_line, narg, input_file_ )
+           ELSE
               CALL getarg ( narg, input_file_ )
+           ENDIF
               IF ( TRIM (input_file_) == ' ' ) GO TO 15
               narg = narg + 1
            CASE ( '-ni', '-nimage', '-nimages' ) 
-              CALL getarg ( narg, arg )
+              IF (read_string) THEN
+                 CALL my_getarg ( input_command_line, narg, arg )
+              ELSE
+                 CALL getarg ( narg, arg )
+              ENDIF
               READ ( arg, *, ERR = 15, END = 15) nimage_
               narg = narg + 1
            CASE ( '-npot', '-npots' ) 
-              CALL getarg ( narg, arg )
+              IF (read_string) THEN
+                 CALL my_getarg ( input_command_line, narg, arg )
+              ELSE
+                 CALL getarg ( narg, arg )
+              ENDIF
               READ ( arg, *, ERR = 15, END = 15) npot_
               narg = narg + 1
            CASE ( '-nk', '-npool', '-npools') 
-              CALL getarg ( narg, arg )
+              IF (read_string) THEN
+                 CALL my_getarg ( input_command_line, narg, arg )
+              ELSE
+                 CALL getarg ( narg, arg )
+              ENDIF
               READ ( arg, *, ERR = 15, END = 15) npool_
               narg = narg + 1
            CASE ( '-nt', '-ntg', '-ntask_groups') 
-              CALL getarg ( narg, arg )
+              IF (read_string) THEN
+                 CALL my_getarg ( input_command_line, narg, arg )
+              ELSE
+                 CALL getarg ( narg, arg )
+              ENDIF
               READ ( arg, *, ERR = 15, END = 15) ntg_
               narg = narg + 1
            CASE ( '-nb', '-nband', '-nbgrp', '-nband_group') 
-              CALL getarg ( narg, arg )
+              IF (read_string) THEN
+                 CALL my_getarg ( input_command_line, narg, arg )
+              ELSE
+                 CALL getarg ( narg, arg )
+              ENDIF
               READ ( arg, *, ERR = 15, END = 15) nband_
               narg = narg + 1
            CASE ( '-nd', '-ndiag', '-northo', '-nproc_diag', '-nproc_ortho') 
-              CALL getarg ( narg, arg )
+              IF (read_string) THEN
+                 CALL my_getarg ( input_command_line, narg, arg )
+              ELSE
+                 CALL getarg ( narg, arg )
+              ENDIF
               READ ( arg, *, ERR = 15, END = 15) ndiag_
               narg = narg + 1
            CASE DEFAULT
@@ -112,9 +163,51 @@ CONTAINS
      
   END SUBROUTINE get_command_line
   !
-  ! command line flags set from library
-  SUBROUTINE set_command_line ( nimage, npot, npool, ntg, nband, ndiag)
+  INTEGER FUNCTION my_iargc ( input_command_line )
+     IMPLICIT NONE
+     CHARACTER(LEN=*), INTENT(IN) :: input_command_line 
+     CHARACTER(LEN=1) :: previous, current
+     INTEGER :: i
 
+     my_iargc = 0
+     previous = ' '
+     DO i=1,LEN_TRIM(input_command_line)
+        current = input_command_line(i:i)
+        IF ( current /= ' ' .AND. previous == ' ' ) my_iargc = my_iargc+1
+        previous = current
+     END DO
+  
+  END FUNCTION my_iargc
+  !
+  SUBROUTINE my_getarg ( input_command_line, narg, arg )
+     IMPLICIT NONE
+     CHARACTER(LEN=*), INTENT(IN) :: input_command_line 
+     INTEGER, INTENT(IN) :: narg 
+     CHARACTER(LEN=*), INTENT(OUT) :: arg
+     CHARACTER(LEN=1) :: previous, current
+     INTEGER :: iarg, i, indx
+
+     iarg = 0
+     previous = ' '
+     arg = ' '
+     indx= 0
+     DO i=1,LEN_TRIM(input_command_line)
+        current = input_command_line(i:i)
+        IF ( current /= ' ' .AND. previous == ' ' ) iarg = iarg+1
+        IF ( iarg == narg ) THEN
+           indx = indx + 1
+           arg(indx:indx) = current           
+           IF ( indx == LEN(arg) ) RETURN
+        ELSE IF ( iarg > narg ) THEN
+           RETURN
+        END IF
+        previous = current
+     END DO
+
+  END SUBROUTINE my_getarg 
+
+  SUBROUTINE set_command_line ( nimage, npot, npool, ntg, nband, ndiag)
+     ! directly set command line options without going through the command line
      IMPLICIT NONE
 
      INTEGER, INTENT(IN), OPTIONAL :: nimage, npot, npool, ntg, nband, ndiag
