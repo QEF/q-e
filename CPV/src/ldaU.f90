41d80c532d628b239e2b9993119b045966e07808
spigafi <spigafi@c92efa57-630b-4861-b058-cf58834340f0>
2013-12-31 12:19:17 +0000

    Dependencies issue fully resolved.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10655 c92efa57-630b-4861-b058-cf58834340f0

--- CPV/src/ldaU.f90
+++ CPV/src/ldaU.f90
@@ -6,47 +6,6 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !-------------------------------------------------------------------------
-MODULE ldaU_cp
-!-------------------------------------------------------------------------
-  USE parameters, ONLY: nsx
-  USE kinds
-  implicit none
-  save
-  real(DP) :: Hubbard_U(nsx)
-  real(DP) :: e_hubbard = 0.d0
-  real(DP), allocatable :: ns(:,:,:,:)
-  integer :: Hubbard_l(nsx), Hubbard_lmax=0, ldmx=0, n_atomic_wfc
-  logical :: lda_plus_u
-  COMPLEX(DP), allocatable::  vupsi(:,:)
-  !
-contains
-  !
-  subroutine ldaU_init0 ( nsp, lda_plus_u_, Hubbard_U_ )
-!-----------------------------------------------------------------------
-!
-      USE constants,        ONLY: autoev
-      !
-      IMPLICIT NONE
-      INTEGER, INTENT(IN) :: nsp
-      LOGICAL, INTENT(IN) :: lda_plus_u_
-      REAL(DP),INTENT(IN) :: Hubbard_U_(nsp)
-
-      lda_plus_u = lda_plus_u_
-      Hubbard_U(1:nsp) = Hubbard_U_(1:nsp) / autoev
-      !
-  END SUBROUTINE ldaU_init0
-  !
-  subroutine deallocate_lda_plus_u()
-     !
-     IF( ALLOCATED( ns ) ) DEALLOCATE( ns )
-     IF( ALLOCATED( vupsi ) ) DEALLOCATE( vupsi )
-     !
-     !
-  end subroutine
-  !
-end module ldaU_cp
-!
-!-------------------------------------------------------------------------
       SUBROUTINE s_wfc(n_atomic_wfc1,becwfc,betae,wfc,swfc)
 !-----------------------------------------------------------------------
 !
