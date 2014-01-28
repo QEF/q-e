76f7402dd29ea04b0ae0bc59e8475dec39b5363c
dalcorso <dalcorso@c92efa57-630b-4861-b058-cf58834340f0>
2014-01-28 15:25:35 +0000

    Missing copyright.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10701 c92efa57-630b-4861-b058-cf58834340f0

--- PHonon/PH/write_qplot_data.f90
+++ PHonon/PH/write_qplot_data.f90
@@ -1,3 +1,10 @@
+!
+! Copyright (C) 2013 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
   SUBROUTINE write_qplot_data(auxdyn)
 !
 !  This routine writes on output the phonon frequencies in a format that
