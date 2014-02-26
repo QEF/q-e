f471967b09f2a361c7fb8200294b3fddbfa1e74e
marsamos <marsamos@c92efa57-630b-4861-b058-cf58834340f0>
2014-02-26 12:18:28 +0000

    added file plugin_variables to Modules dir
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@10770 c92efa57-630b-4861-b058-cf58834340f0

---
+++ Modules/plugin_variables.f90
@@ -0,0 +1,27 @@
+!
+! Copyright (C) 2002-2007 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!=----------------------------------------------------------------------------=!
+MODULE plugin_variables
+  !=--------------------------------------------------------------------------=!
+  !
+  ! ... this module contains all basic variables possibly
+  ! ... used by plug ins
+  !----------------------------------------------
+  !
+  USE kinds
+  USE parameters
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+  PRIVATE
+  !
+  REAL(DP) :: plugin_etot
+  !
+END MODULE plugin_variables
