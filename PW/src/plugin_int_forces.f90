59422790e58dc969574fdc72da554287b2b39bdd
oliviero <oliviero@c92efa57-630b-4861-b058-cf58834340f0>
2014-05-28 11:57:21 +0000

    adding a module to plugin_int_forces and updating the make.depend accordingly
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@11008 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/plugin_int_forces.f90
+++ PW/src/plugin_int_forces.f90
@@ -18,12 +18,13 @@ USE io_files,         ONLY : outdir
 USE cell_base,        ONLY : at, bg, alat, omega
 USE ions_base,        ONLY : nat, ntyp => nsp, ityp, tau, zv, amass
 USE fft_base,         ONLY : dfftp
+USE fft_interfaces,   ONLY : fwfft
 USE gvect,            ONLY : ngm, gstart, ngl, nl, igtongl, g, gg, gcutm
 USE lsda_mod,         ONLY : nspin
 USE force_mod,        ONLY : force
 USE scf,              ONLY : rho
 USE vlocal,           ONLY : strf, vloc
-USE control_flags,    ONLY : gamma_only
+USE control_flags,    ONLY : iverbosity, gamma_only
 USE martyna_tuckerman, ONLY: do_comp_mt, wg_corr_force
 !
 USE plugin_flags
