8eb47b3689322a7b81708d6ba65f39d5cc7ab283
ccavazzoni <ccavazzoni@c92efa57-630b-4861-b058-cf58834340f0>
2014-08-06 06:45:45 +0000

    - making cft_b_omp thread safe, all threads now initialize their own private FFTW PLAN.   This parallelization is supported only for internal FFTW.   It can be extended to other libraries supporting thread nesting, not sure if MKL/FFTW3 can do it.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@11133 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/fft_scalar.f90
+++ Modules/fft_scalar.f90
@@ -67,9 +67,13 @@
 #if defined __FFTW 
 
         INTEGER   :: cft_b_dims( 4 )
+!$omp threadprivate (cft_b_dims)
         C_POINTER :: cft_b_bw_planz = 0
+!$omp threadprivate (cft_b_bw_planz)
         C_POINTER :: cft_b_bw_planx = 0
+!$omp threadprivate (cft_b_bw_planx)
         C_POINTER :: cft_b_bw_plany = 0
+!$omp threadprivate (cft_b_bw_plany)
 
 #endif
 
@@ -1859,7 +1863,7 @@ SUBROUTINE cfft3ds (f, nx, ny, nz, ldx, ldy, ldz, isign, &
       !
 #if defined __FFTW
 
-!$omp single
+!$omp parallel
 
       IF( cft_b_bw_planz  == 0 ) THEN
          CALL CREATE_PLAN_1D( cft_b_bw_planz, nz, 1 )
@@ -1874,7 +1878,7 @@ SUBROUTINE cfft3ds (f, nx, ny, nz, ldx, ldy, ldz, isign, &
          cft_b_dims(2) = ny
       END IF
 
-!$omp end single
+!$omp end parallel
 
 #else
 
