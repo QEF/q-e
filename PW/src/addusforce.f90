e0be6b82d008e4c70d287dd11dc1d371b34125f6
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2015-03-22 21:01:54 +0000

    Cleanup and speedup of USPP term in forces
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@11452 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/addusforce.f90
+++ PW/src/addusforce.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2001-2007 Quantum ESPRESSO group
+! Copyright (C) 2001-2015 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -7,11 +7,13 @@
 !
 !
 !----------------------------------------------------------------------
-subroutine addusforce (forcenl)
+SUBROUTINE addusforce (forcenl)
   !----------------------------------------------------------------------
   !
   !   This routine computes the contribution to atomic forces due
   !   to the dependence of the Q function on the atomic position.
+  !   F_j,at= sum_G sum_lm iG_j exp(-iG*R_at) V^*(G) Q_lm(G) becsum(lm,at)
+  !   where becsum(lm,at) = sum_i <psi_i|beta_l>w_i<beta_m|psi_i>
   !   On output: the contribution is added to forcenl
   !
   USE kinds,      ONLY : DP
@@ -28,128 +30,140 @@ subroutine addusforce (forcenl)
   USE control_flags, ONLY : gamma_only
   USE fft_interfaces,ONLY : fwfft
   !
-  implicit none
+  IMPLICIT NONE
   !
-  real(DP) :: forcenl (3, nat)
-  ! local variables
-  integer :: ig, ir, dim, nt, ih, jh, ijh, ipol, is, na
-  complex(DP):: cfac
-  real(DP) :: fact, ddot
+  REAL(DP), INTENT(INOUT) :: forcenl (3, nat)
+  !
+  INTEGER :: ig, nt, ih, jh, ijh, nij, ipol, is, na, nb, nab
+  REAL(DP) :: fact
+  COMPLEX(DP) :: cfac
   ! work space
-  complex(DP), allocatable :: aux(:,:), aux1(:,:), vg(:), qgm(:)
-  real(DP) , allocatable :: ddeeq(:,:,:,:), qmod(:), ylmk0(:,:)
-
+  COMPLEX(DP), ALLOCATABLE :: aux(:), aux1(:,:,:), vg(:,:), qgm(:,:)
+  REAL(DP) , ALLOCATABLE :: ddeeq(:,:,:,:), qmod(:), ylmk0(:,:), forceq(:,:)
   !
-  if (.not.okvan) return
+  IF (.not.okvan) RETURN
   !
-  if (gamma_only) then
-     fact = 2.d0
-  else
-     fact = 1.d0
-  end if
-  allocate (aux(ngm,nspin_mag))    
+  ALLOCATE ( forceq(3,nat) )
+  forceq(:,:) = 0.0_dp
+  IF (gamma_only) THEN
+     fact = 2.d0*omega
+  ELSE
+     fact = 1.d0*omega
+  ENDIF
   !
   ! fourier transform of the total effective potential
   !
-  allocate (vg(dfftp%nnr))    
-  do is = 1, nspin_mag
-     if (nspin_mag.eq.4.and.is.ne.1) then
-        vg (:) = v%of_r(:,is)
-     else
-        vg (:) = vltot (:) + v%of_r (:, is)
-     endif
-     CALL fwfft ('Dense', vg, dfftp)
-     aux (:, is) = vg (nl (:) ) * tpiba * (0.d0, -1.d0)
-  enddo
-  deallocate (vg)
+  ALLOCATE ( vg(ngm,nspin_mag) )
+  ALLOCATE ( aux(dfftp%nnr) )
+  DO is = 1, nspin_mag
+     IF (nspin_mag==4.and.is/=1) THEN
+        aux(:) = v%of_r(:,is)
+     ELSE
+        aux(:) = vltot (:) + v%of_r (:, is)
+     ENDIF
+     CALL fwfft ('Dense', aux, dfftp)
+     ! Note the factors -i and 2pi/a *units of G) here in V(G) !
+     vg (:, is) = aux(nl (:) ) * tpiba * (0.d0, -1.d0)
+  ENDDO
+  DEALLOCATE (aux)
   !
-  allocate (aux1(ngm,3))    
-  allocate (ddeeq( 3, (nhm*(nhm+1))/2,nat,nspin_mag))    
-  allocate (qgm( ngm))
-  allocate (qmod( ngm))    
-  allocate (ylmk0(ngm,lmaxq*lmaxq))    
+  ALLOCATE (ylmk0(ngm,lmaxq*lmaxq))
+  CALL ylmr2 (lmaxq * lmaxq, ngm, g, gg, ylmk0)
   !
-  ddeeq(:,:,:,:) = 0.d0
-  !
-  call ylmr2 (lmaxq * lmaxq, ngm, g, gg, ylmk0)
-  !
-  !qmod (:) = sqrt (gg (:) )
-!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ig)
+  ALLOCATE (qmod( ngm))
+!$omp parallel do default(shared) private(ig)
   do ig = 1, ngm
      qmod (ig) = sqrt (gg (ig) )
-  enddo
-!$OMP END PARALLEL DO
+  ENDDO
+!$omp end parallel do
   !
-  ! here we compute the integral Q*V for each atom,
-  !       I = sum_G i G_a exp(-iR.G) Q_nm v^*
-  !
-  do nt = 1, ntyp
-     if ( upf(nt)%tvanp ) then
-        ijh = 1
-        do ih = 1, nh (nt)
-           do jh = ih, nh (nt)
-              call qvan2 (ngm, ih, jh, nt, qmod, qgm, ylmk0)
-              do na = 1, nat
-                 if (ityp (na) == nt) then
-                    !
-                    ! The product of potential, structure factor and iG
-                    !
-                    do is = 1, nspin_mag
-!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ig, cfac)
-                       do ig = 1, ngm
-                          cfac = aux (ig, is) * CONJG(eigts1 (mill(1,ig), na) *&
-                                                      eigts2 (mill(2,ig), na) *&
-                                                      eigts3 (mill(3,ig), na) )
-                          aux1 (ig, 1) = g (1, ig) * cfac
-                          aux1 (ig, 2) = g (2, ig) * cfac
-                          aux1 (ig, 3) = g (3, ig) * cfac
-                       enddo
-!$OMP END PARALLEL DO
-                       !
-                       !    and the product with the Q functions
-                       !    G=0 term gives no contribution
-                       !
-                       do ipol = 1, 3
-                          ddeeq (ipol, ijh, na, is) = omega * fact * &
-                               ddot (2 * ngm, aux1 (1, ipol), 1, qgm, 1)
-                       enddo
-                    enddo
-                 endif
-              enddo
+  DO nt = 1, ntyp
+     IF ( upf(nt)%tvanp ) THEN
+        !
+        ! nij = max number of (ih,jh) pairs per atom type nt
+        ! qgm contains the Q functions in G space
+        !
+        nij = nh(nt)*(nh(nt)+1)/2
+        ALLOCATE (qgm(ngm,nij))
+        ijh = 0
+        DO ih = 1, nh (nt)
+           DO jh = ih, nh (nt)
               ijh = ijh + 1
-           enddo
-        enddo
-     endif
+              CALL qvan2 (ngm, ih, jh, nt, qmod, qgm(1,ijh), ylmk0)
+           ENDDO
+        ENDDO
+        !
+        ! nab = number of atoms of type nt
+        !
+        nab = 0
+        DO na = 1, nat
+           IF ( ityp(na) == nt ) nab = nab + 1
+        ENDDO
+        ALLOCATE ( aux1( ngm, na, 3) )
+        ALLOCATE ( ddeeq(nij, nab, 3, nspin_mag) )
+        !
+        DO is = 1, nspin_mag
+           nb = 0
+           DO na = 1, nat
+              IF (ityp (na) == nt) THEN
+                 nb = nb + 1
+                 !
+                 ! aux1 = product of potential, structure factor and iG
+                 !
+!$omp parallel do default(shared) private(ig, cfac)
+                 do ig = 1, ngm
+                    cfac = vg (ig, is) * CONJG(eigts1 (mill(1,ig),na) * &
+                                               eigts2 (mill(2,ig),na) * &
+                                               eigts3 (mill(3,ig),na) )
+                    aux1 (ig, nb, 1) = g (1, ig) * cfac
+                    aux1 (ig, nb, 2) = g (2, ig) * cfac
+                    aux1 (ig, nb, 3) = g (3, ig) * cfac
+                 ENDDO
+!$omp end parallel do
+                 !
+              ENDIF
+           ENDDO
+           !
+           !    ddeeq = dot product of aux1 with the Q functions
+           !    No need for special treatment of the G=0 term (is zero)
+           !
+           DO ipol = 1, 3
+              CALL DGEMM( 'C', 'N', nij, nab, 2*ngm, fact, qgm, 2*ngm, &
+                   aux1(1,1,ipol), 2*ngm, 0.0_dp, ddeeq(1,1,ipol,is), nij )
+           ENDDO
+           !
+        ENDDO
+        !
+        DEALLOCATE (aux1)
+        DEALLOCATE (qgm)
+        !
+        DO is = 1, nspin_mag
+           nb = 0
+           DO na = 1, nat
+              IF (ityp (na) == nt) THEN
+                 nb = nb + 1
+                 DO ipol = 1, 3
+                    DO ijh = 1, nij
+                       forceq (ipol, na) = forceq (ipol, na) + &
+                            ddeeq (ijh, nb, ipol, is) * becsum (ijh, na, is)
+                    ENDDO
+                 ENDDO
+              ENDIF
+           ENDDO
+        ENDDO
+        DEALLOCATE ( ddeeq )
+     ENDIF
+  ENDDO
+  !
+  CALL mp_sum ( forceq, intra_bgrp_comm )
+  !
+  forcenl(:,:) = forcenl(:,:) + forceq(:,:)
+  !
+  DEALLOCATE (qmod)
+  DEALLOCATE (ylmk0)
+  DEALLOCATE (vg)
+  DEALLOCATE (forceq)
 
-  enddo
-  call mp_sum ( ddeeq, intra_bgrp_comm )
-  !            WRITE( stdout,'( "dmatrix atom ",i4)') na
-  !            do ih = 1, nh(nt)
-  !               WRITE( stdout,'(8f9.4)') (ddeeq(ipol,ih,jh,na),jh=1,nh(nt))
-  !            end do
-  !            WRITE( stdout,'( "dion pseudo ",i4)') nt
-  !            do ih = 1, nh(nt)
-  !               WRITE( stdout,'(8f9.4)') (dvan(ih,jh,nt),jh=1,nh(nt))
-  !            end do
-  do is = 1, nspin_mag
-     do na = 1, nat
-        nt = ityp (na)
-        dim = (nh (nt) * (nh (nt) + 1) ) / 2
-        do ipol = 1, 3
-           do ir = 1, dim
-              forcenl (ipol, na) = forcenl (ipol, na) + &
-                   ddeeq (ipol, ir, na, is) * becsum (ir, na, is)
-           enddo
-        enddo
-     enddo
-  enddo
-  deallocate (ylmk0)
-  deallocate (qgm)
-  deallocate (qmod)
-  deallocate (ddeeq)
-  deallocate (aux1)
-  deallocate (aux)
-
-  return
-end subroutine addusforce
+  RETURN
+END SUBROUTINE addusforce
 
