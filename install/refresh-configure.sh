fd26b4823b997e08f02b2eefeb98e4bebc708f41
spigafi <spigafi@c92efa57-630b-4861-b058-cf58834340f0>
2016-01-05 17:23:05 +0000

    Script to (re)generate configure
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@11937 c92efa57-630b-4861-b058-cf58834340f0

--- install/refresh-configure.sh
+++ install/refresh-configure.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+#
+# Copyright (C) 2001-2016 Quantum ESPRESSO group
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License. See the file `License' in the root directory
+# of the present distribution.
+#
+# Dependency: GNU AUTOCONF (minimum 2.60, 2.69 suggested)
+
 aclocal -I m4 --install
-autoconf -f -v --output=configure-new configure.ac.new
+
+autoconf -f -v --output=configure configure.ac
