bce6955944a63af526fa79766366feff15e26679
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2016-02-03 17:06:23 +0000

    Missing deallocation, some "harmonization" of latest patch
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12097 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/exx.f90
+++ PW/src/exx.f90
@@ -1443,8 +1443,7 @@ MODULE exx
     ! local variables
     COMPLEX(DP),ALLOCATABLE :: temppsic(:), result(:)
     COMPLEX(DP),ALLOCATABLE :: temppsic_nc(:,:),result_nc(:,:)
-    COMPLEX(DP),ALLOCATABLE :: result_g(:)
-    COMPLEX(DP),ALLOCATABLE :: result_nc_g(:,:)
+    COMPLEX(DP),ALLOCATABLE :: result_g(:), result_nc_g(:,:)
     !
     COMPLEX(DP),ALLOCATABLE :: rhoc(:), vc(:), deexx(:)
     REAL(DP),   ALLOCATABLE :: fac(:)
@@ -1463,7 +1462,7 @@ MODULE exx
     !
     IF (noncolin) THEN
        ALLOCATE( temppsic_nc(nrxxs,npol), result_nc(nrxxs,npol) )
-       ALLOCATE( result_nc_g(n,2) )
+       ALLOCATE( result_nc_g(n,npol) )
     ELSE
        ALLOCATE( temppsic(nrxxs), result(nrxxs) )
        ALLOCATE( result_g(n) )
@@ -1636,21 +1635,18 @@ MODULE exx
          CALL mp_sum(deexx,inter_bgrp_comm)
        ENDIF
        !
-       !brings back result in G-space
+       ! bring result back to G-space
        !
        IF (noncolin) THEN
-          !brings back result in G-space
+          !
           CALL fwfft ('CustomWave', result_nc(:,1), exx_fft%dfftt)
           CALL fwfft ('CustomWave', result_nc(:,2), exx_fft%dfftt)
           !
           !communicate result
           DO ig = 1, n
-             result_nc_g(ig,1) = result_nc(exx_fft%nlt(igk(ig)),1)
+             result_nc_g(ig,1:npol) = result_nc(exx_fft%nlt(igk(ig)),1:npol)
           ENDDO
-          DO ig = 1, n
-             result_nc_g(ig,2) = result_nc(exx_fft%nlt(igk(ig)),2)
-          ENDDO
-          CALL mp_sum( result_nc_g(1:n,1:2), inter_bgrp_comm)
+          CALL mp_sum( result_nc_g(1:n,1:npol), inter_bgrp_comm)
           !
           !adds it to hpsi
 !$omp parallel do default(shared), private(ig)
@@ -1690,9 +1686,9 @@ MODULE exx
     LOOP_ON_PSI_BANDS
     !
     IF (noncolin) THEN
-       DEALLOCATE(temppsic_nc, result_nc) 
+       DEALLOCATE(temppsic_nc, result_nc, result_nc_g )
     ELSE
-       DEALLOCATE(temppsic, result) 
+       DEALLOCATE(temppsic, result, result_g )
     END IF
     !
     DEALLOCATE(rhoc, vc, fac )
