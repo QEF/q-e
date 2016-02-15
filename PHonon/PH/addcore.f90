ff2a4ddc98de8e686015a4199643037316e13138
paulatto <paulatto@c92efa57-630b-4861-b058-cf58834340f0>
2016-02-15 14:09:39 +0000

    Subroutine generalized to take a q-vector as input, renamed to addcore_ofq The old subroutine name is used for a wrapper that takes the variables from modules and calls the new one
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12136 c92efa57-630b-4861-b058-cf58834340f0

--- PHonon/PH/addcore.f90
+++ PHonon/PH/addcore.f90
@@ -5,30 +5,48 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-subroutine addcore (mode, drhoc)
+subroutine addcore(mode, drhoc)
+  use kinds,     only: DP
+  use modes,     only: u
+  use qpoint,    only: xq
+  use nlcc_ph,   only: drc
+  use fft_base,  only: dfftp
+  use gvect,     only: ngm
+  use ions_base, only: ntyp => nsp
+  implicit none
+  integer, intent (IN) :: mode
+  !  input: the mode
+  complex(DP), intent(OUT) :: drhoc (dfftp%nnr)
+  ! output: the change of the core charge
+  CALL addcore_ofq (xq, mode, u, drc, drhoc, +1._dp)
+end subroutine
+!
+subroutine addcore_ofq (xq, mode, u, drc, drhoc, csign)
   !
   !    This routine computes the change of the core charge
   !    when the atoms moves along the given mode
   !
   !
-
   USE kinds, only : DP
   use uspp_param, only: upf
-  use ions_base, only: nat, ityp
+  use ions_base, only: nat, ityp, ntyp => nsp
   use cell_base, only: tpiba
   use fft_base,  only: dfftp
   use fft_interfaces, only: invfft
   use gvect, only: ngm, nl, mill, eigts1, eigts2, eigts3, g
-  use modes, only: u
-  use qpoint, only: eigqts, xq
-  use nlcc_ph, only: nlcc_any, drc
+  use qpoint, only: eigqts
+  use nlcc_ph, only: nlcc_any
   implicit none
 
   !
   !   The dummy variables
   !
-  integer, intent (IN) :: mode
-  !  input: the mode
+  integer, intent (IN) :: mode              ! the mode
+  REAL(DP),INTENT(in)    :: xq(3)           ! the q vector
+  COMPLEX(DP),INTENT(in) :: u(3*nat, 3*nat) ! dispalcement patterns
+  COMPLEX(DP),INTENT(in) :: drc(ngm, ntyp)  ! core rho without atomic factor
+  REAL(DP),INTENT(in) :: csign ! add core with +1, subtract with -1, or anything in between
+  !
   complex(DP), intent(OUT) :: drhoc (dfftp%nnr)
   ! output: the change of the core charge
   !
@@ -46,7 +64,7 @@ subroutine addcore (mode, drhoc)
   do na = 1, nat
      nt = ityp (na)
      if (upf(nt)%nlcc) then
-        fact = tpiba * (0.d0, -1.d0) * eigqts (na)
+        fact = csign*tpiba * (0.d0, -1.d0) * eigqts (na)
         mu = 3 * (na - 1)
         if ( abs (u (mu + 1, mode) ) + &
              abs (u (mu + 2, mode) ) + &
@@ -73,4 +91,4 @@ subroutine addcore (mode, drhoc)
   !
   return
 
-end subroutine addcore
+end subroutine addcore_ofq
