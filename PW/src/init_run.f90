49819b7b70413fbcd89e29e6e4ffa1cd9a5f7d2b
ccavazzoni <ccavazzoni@c92efa57-630b-4861-b058-cf58834340f0>
2016-05-05 12:37:08 +0000

    - synchronize wave functions in different band groups after init, to avoid wave functions divergence. In band group parallelization the G vectors and wave functions are replicated in all band groups, and they should remain synchronized in order the diagonalization to work properly
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12389 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/init_run.f90
+++ PW/src/init_run.f90
@@ -28,8 +28,10 @@ SUBROUTINE init_run()
   USE wannier_new,        ONLY : use_wannier    
   USE dfunct,             ONLY : newd
   USE esm,                ONLY : do_comp_esm, esm_init
-  USE mp_bands,           ONLY : intra_bgrp_comm
+  USE mp_bands,           ONLY : intra_bgrp_comm, inter_bgrp_comm, nproc_bgrp, root_bgrp
+  USE mp,                 ONLY : mp_bcast
   USE tsvdw_module,       ONLY : tsvdw_initialize
+  USE wavefunctions_module, ONLY : evc
   !
   IMPLICIT NONE
   !
@@ -110,6 +112,10 @@ SUBROUTINE init_run()
 #endif
   !
   IF ( lmd ) CALL allocate_dyn_vars()
+  !
+  IF( nproc_bgrp > 1 ) THEN
+     CALL mp_bcast( evc, root_bgrp, inter_bgrp_comm )
+  ENDIF
   !
   CALL stop_clock( 'init_run' )
   !
