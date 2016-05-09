dd26339de71ccc299c9c40588ad9bfa792c3e075
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2016-05-09 06:02:50 +0000

    Yet another bug in dynamical matrix writing in Gamma-only phonon code
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12396 c92efa57-630b-4861-b058-cf58834340f0

--- PHonon/Gamma/writedyn.f90
+++ PHonon/Gamma/writedyn.f90
@@ -39,7 +39,7 @@ SUBROUTINE writedyn ( )
        &         //,5x,"q = ( ",3f14.9," ) ",/)') 0.0d0,0.0d0,0.0d0
   DO na = 1, nat
      DO nb = 1, nat
-        WRITE(iudyn, '(2i3)') na, nb
+        WRITE(iudyn, '(2i5)') na, nb
         WRITE(iudyn,'(3e24.12)') &
              ( (dyn(3*(na-1)+i,3*(nb-1)+j),0.d0,j=1,3),i=1,3)
      ENDDO
