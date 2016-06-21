8b45e3e97b51891038920ed71277943a124cc454
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2016-06-21 15:35:50 +0000

    Useless reference to input_parameters module
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12518 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/scale_h.f90
+++ PW/src/scale_h.f90
@@ -22,8 +22,7 @@ subroutine scale_h
   USE klist,      ONLY : xk, wk, nkstot
   USE us,         ONLY : nqxq, qrad, tab, tab_at, dq
   USE control_flags, ONLY : iverbosity
-  USE start_k,          ONLY : nks_start, xk_start, nk1,nk2,nk3
-  USE input_parameters, ONLY : k_points
+  USE start_k,    ONLY : nks_start, xk_start, nk1,nk2,nk3
   USE exx,        ONLY : exx_grid_reinit
   USE funct,      ONLY : dft_is_hybrid
   USE mp,         ONLY : mp_max
