a04953a5eb05355b8d230e8334c5b677bd48dcf7
sponce <sponce@c92efa57-630b-4861-b058-cf58834340f0>
2016-06-22 15:24:09 +0000

    - Addition of a new test (t06 for lpolar) - Cleaning of depreciated sgam_at2.f90 and sgam_at_mag2.f90 routines - Update of all tests to remove tphase variable (depreciated) - Tweaking of the EPW test-suite so that it passes on buildbot
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12531 c92efa57-630b-4861-b058-cf58834340f0

--- EPW/src/sgama2.f90
+++ EPW/src/sgama2.f90
@@ -1184,3 +1184,69 @@ subroutine irrek (npk, nks, xk, wk, at, bg, nrot, invs, nsym, irg, &
   return
 end subroutine irrek
 
+!-----------------------------------------------------------------------
+subroutine checksym (ir, nat, ityp, xau, rau, ft, sym, irt)
+  !-----------------------------------------------------------------------
+  !
+  !   This routine receives as input all the atomic positions xau,
+  !   and the rotated rau by the symmetry operation ir. It sets to true
+  !   sym(ir) if for each atom na, it is possible to find an atom nb
+  !   which is of the same type of na, and coincide with it after the
+  !   symmetry operation. Fractional translations are allowed.
+  !
+  !   Revised layout 1 may 1995 by A. Dal Corso
+  !
+  USE kinds
+!  USE symm_base,        ONLY : accep
+  implicit none
+  !
+  !     first the dummy variables
+  !
+  integer :: nat, ityp (nat), irt (48, nat), ir
+  ! input: the total number of atoms
+  ! input: the type of each atom
+  ! output: the rotated of each atom
+  ! input: the rotation to be tested
+  real(DP) :: xau (3, nat), rau (3, nat), ft (3)
+  ! input: the initial vectors
+  ! input: the rotated vectors
+  ! input: the possible fractionary translat
+  logical :: sym (48)
+  ! output: if true this is a symmetry opera
+  !
+  !  few local variables
+  !
+  integer :: na, nb
+  ! counter on atoms
+  ! counter on atoms
+  logical :: eqvect
+  ! the testing function
+
+  external eqvect
+  do na = 1, nat
+     do nb = 1, nat
+      !  sym (ir) = ityp (na) .eq.ityp (nb) .and.eqvect (rau (1, na), &
+      !       xau (1, nb), ft,accep)
+        sym (ir) = ityp (na) .eq.ityp (nb) .and.eqvect (rau (1, na), &
+             xau (1, nb), ft,1.0d-5)
+        if (sym (ir) ) then
+           !
+           ! the rotated atom does coincide with one of the like atoms
+           ! keep track of which atom the rotated atom coincides with
+           !
+           irt (ir, na) = nb
+           goto 10
+        endif
+     enddo
+     !
+     ! the rotated atom does not coincide with any of the like atoms
+     ! s(ir) + ft is not a symmetry operation
+     !
+     return
+10   continue
+  enddo
+  !
+  ! s(ir) + ft is a symmetry operation
+  !
+  return
+end subroutine checksym
--- EPW/tests/README.txt
+++ EPW/tests/README.txt
@@ -7,12 +7,17 @@ The following feature of the code are tested
 - Phonon & electron self-energy
 - Eliashberg a2F
 - Homogeneous fine k and q-grid integration
+- Test parallel_k and parallel_q
+- Test restart feature epwread = .true.
+- Test nesting function
+- Test spectral function
 
 #################
 # Test 02 : SiC #
 #################
 The following feature of the code is tested
 - Time-reversal symmetry when inversion sym. is not part of the small group of q.
+- Test memory optimization etf_mem = .false. 
 
 ################
 # Test 03 : Pb #
@@ -35,3 +40,15 @@ The following feature of the code is tested
 - Superconducting properties within the Migdal-Eliashberg framework
 - Isotropic superconducting properties with contour integration
 - Anisotropic superconducting properties with Pade continuation
+
+#################
+# Test 06 : GaN #
+#################
+The following feature of the code is tested
+- Polar correction
+- Test reading fine q-grid from file
+
+
+
+
+
