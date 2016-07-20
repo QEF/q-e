18dc5b3bd1201440123b082b2cb570f69c4c6691
degironc <degironc@c92efa57-630b-4861-b058-cf58834340f0>
2016-07-20 11:42:37 +0000

    routine upf_to_internal modified so that for USPP we set the augmentation charge as an  l-dependent array in all cases.  This is already the case when upf%tpawp or upf%q_with_l are .true. .  For vanderbilt US pseudos, where nqf and rinner are non zero, we do here what otherwise  would be done multiple times in many parts of the code (such as in init_us_1, addusforce_r,  bp_calc_btq, compute_qdipol) whenever the q_l(r) were to be constructed.  For simple rrkj3 pseudos we duplicate the infomation contained in q(r) for all q_l(r).
     This requires a little extra memory but unifies the treatment of q_l(r) and allows further 
     tweaking with the augmentation charge.
    
     Variable upf%q_with_l set .true. at the end of the operation. It would be better to leave the 
     variable untouched at its input value  and modify the routines that compute q_l(r) to just use
     the now always present upf%qfuncl array  but this is the first step before some cleanup.
    
     setqf.f90 moved from PW/src to Modules, Makefiles and dependencies updated
    
    
    
    
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12591 c92efa57-630b-4861-b058-cf58834340f0

--- Modules/upf_to_internal.f90
+++ Modules/upf_to_internal.f90
@@ -44,6 +44,7 @@ subroutine set_pseudo_upf (is, upf, grid)
   !     Local variables
   !
   integer :: iexch,icorr,igcx,igcc
+  INTEGER :: nb, mb, ijv, ir, ilast, l, l1, l2
   !
   ! old formats never contain "1/r" pseudopotentials
   !
@@ -70,6 +71,45 @@ subroutine set_pseudo_upf (is, upf, grid)
     upf%grid => grid
   endif
   !
+  ! For USPP we set the augmentation charge as an l-dependent array in all cases.
+  ! This is already the case when upf%tpawp or upf%q_with_l are .true. .
+  ! For vanderbilt US pseudos, where nqf and rinner are non zero, we do here what otherwise
+  ! would be done multiple times in many parts of the code (such as in init_us_1, addusforce_r, 
+  ! bp_calc_btq, compute_qdipol) whenever the q_l(r) were to be constructed. 
+  ! For simple rrkj3 pseudos we duplicate the infomation contained in q(r) for all q_l(r).
+  !
+  ! This requires a little extra memory but unifies the treatment of q_l(r) and allows further 
+  ! tweaking with the augmentation charge.
+  !
+  if ( upf%tvanp .and. .not.upf%q_with_l ) then
+     ALLOCATE( upf%qfuncl ( upf%mesh, upf%nbeta*(upf%nbeta+1)/2, 0:2*upf%lmax ) )
+     upf%qfuncl  = 0.0_DP
+
+     do nb = 1, upf%nbeta
+        do mb = nb, upf%nbeta
+           ! ijv is the combined (nb,mb) index
+           ijv = mb * (mb-1) / 2 + nb
+           l1=upf%lll(nb) ; l2=upf%lll(mb)
+! copy q(r) to the l-dependent grid 
+           DO l=abs(l1-l2),l1+l2
+              upf%qfuncl(1:upf%mesh,ijv,l) = upf%qfunc(1:upf%mesh,ijv)
+           END DO
+! adjust the inner values on the l-dependent grid if nqf and rinner are defined
+           if ( upf%nqf > 0 ) then
+              do l = abs(l1-l2),l1+l2
+                 if ( upf%rinner (l+1) > 0.0_dp) then
+                    do ir = 1, upf%kkbeta
+                       if (upf%r(ir) <upf%rinner (l+1) ) ilast = ir
+                    enddo
+                    call setqfnew( upf%nqf,upf%qfcoef(1,l+1,nb,mb), ilast, upf%r, l, 2, upf%qfuncl(1,ijv,l) )
+                 end if
+              end do
+           end if
+        enddo
+     enddo
+     upf%q_with_l = .true.
+  end if
+
 end subroutine set_pseudo_upf
 
 !=----------------------------------------------------------------------------=!
