3fd0e986b89b97ebfc1cadcb1ac8a8dacf3f52fb
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2016-08-10 07:05:34 +0000

    First draft of a memory estimator
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12705 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/memory_report.f90
+++ PW/src/memory_report.f90
@@ -10,9 +10,9 @@ SUBROUTINE memory_report()
   !----------------------------------------------------------------------------
   !
   ! Very rough estimate of the dynamical memory allocated by the pw.x code
-  ! Some dimensions are estimated since they are not known when this routine 
-  ! is called (must be called after the first steps of initialization are done
-  ! but before large arrays are actually allocated). 
+  ! Should be called after the first steps of initialization are done, but
+  ! before large arrays are actually allocated. Does not cover cases like:
+  ! real-space q/beta, hybrid and vdW functionals, finite electric fields
   !
   USE io_global, ONLY : stdout
   USE kinds,     ONLY : dp 
@@ -20,93 +20,126 @@ SUBROUTINE memory_report()
   USE wvfct,     ONLY : nbnd, nbndx
   USE basis,     ONLY : natomwfc
   USE cell_base, ONLY : omega
-  USE fft_base,  ONLY : dfftp
-  USE gvect,     ONLY : ngm
+  USE fft_base,  ONLY : dffts, dfftp
+  USE gvect,     ONLY : ngm, ngm_g
+  USE gvecs,     ONLY : ngms, doublegrid
   USE gvecw,     ONLY : ecutwfc
-  USE uspp,      ONLY : nkb
+  USE klist,     ONLY : nks, nkstot
+  USE uspp,      ONLY : nkb, okvan
+  USE funct,     ONLY : dft_is_meta, dft_is_hybrid
   USE ldaU,      ONLY : lda_plus_u, U_projection, nwfcU
   USE fixed_occ, ONLY : one_atom_occupations
   USE wannier_new,ONLY: use_wannier
   USE lsda_mod,  ONLY : nspin
   USE noncollin_module, ONLY : npol
-  USE control_flags, ONLY: isolve, nmix, gamma_only, lscf
+  USE control_flags, ONLY: isolve, nmix, imix, gamma_only, lscf, io_level, &
+       lxdm, smallmem
   USE mp_diag,   ONLY : np_ortho
   USE mp_bands,  ONLY : nproc_bgrp
+  USE mp_images, ONLY : nproc_image  
   !
   IMPLICIT NONE
   !
-  REAL(dp) :: omegabz
-  INTEGER, PARAMETER :: Mb=1024*1024, complex_size=16, real_size=8
-  INTEGER :: g_fact, nbnd_l, npwx_g, npwx_l
+  INTEGER, PARAMETER :: Mb=1024*1024
+  INTEGER :: g_fact, nk, nbnd_l, npwx_g, npwx_l, mix_type_size, scf_type_size
+  !
+  ! these quantities are real in order to prevent integer overflow
+  !
+  REAL(dp), PARAMETER :: complex_size=16_dp, real_size=8_dp, int_size=4_dp
+  REAL(dp) :: ram, ram1, maxram, totram
   !
   IF ( gamma_only) THEN
-     g_fact = 2
+     g_fact = 2  ! use half plane waves or G-vectors
   ELSE
-     g_fact = 1
+     g_fact = 1  ! use  all plane waves or G-vectors
   END IF
-  ! estimate npwx_l (max number of plane waves) on this processor 
-  ! npwx_g is npwx_l summed over all processors
   !
-  omegabz=tpi**3/omega
-  npwx_g = fpi/3.0_dp * SQRT(ecutwfc)**3 / omegabz / g_fact
+  ! npwx (max number of plane waves) is roughly estimated from the volume
+  ! of the G-vector sphere, divided by the volume of the Brillouin Zone
+  ! (the exact value can be computed only after G-vectors are computed)
+  ! npwx_l is npwx on this processor (local)
+  ! npwx_g is npwx summed over all processors (global)
+  !
+  npwx_g = NINT ( fpi/3.0_dp * SQRT(ecutwfc)**3 / (tpi**3/omega) / g_fact )
   npwx_l = npwx_g/nproc_bgrp
-  ! 
-  ! the conversions to double prevent integer overflow in very large runs
   !
-  WRITE( stdout, '(/5x,"Largest allocated arrays",5x,"est. size (Mb)", &
-                   &5x,"approx. dimensions")')
-  WRITE( stdout, '(8x,"Kohn-Sham Wavefunctions   ",f10.2," Mb", &
-                 & 5x,"(",i8,",",i5,")")') &
-     complex_size*nbnd*npol*DBLE(npwx_l)/Mb, npwx_l*npol,nbnd
+  ! ram   = dynamically (permanently) allocated RAM, per process
+  ! maxram= "high watermark": max ram needed during a run
+  ! totram= max ram needed summed over all processors
+  !
+  ! Wavefunctions (including buffer space)
+  IF ( io_level > 0 .OR. nks == 1) THEN
+     nk = 1      ! store just one wavefunction array at the time
+  ELSE
+     nk = nks+1  ! store nks wavefunctions in memory (buffers)
+  END IF
+  ram =  complex_size * nbnd * npol * npwx_l * nk
+  ! atomic wavefunctions 
   IF ( one_atom_occupations .OR. use_wannier ) &
-     WRITE( stdout, '(8x,"Atomic wavefunctions      ",f10.2," Mb", &
-                    & 5x,"(",i8,",",i5,")")') &
-   & complex_size*natomwfc*npol*DBLE(npwx_l)/Mb, npwx_l*npol,natomwfc
+     ram = ram + complex_size * natomwfc * npol * npwx_l 
+  ! Hubbard wavefunctions
   IF ( lda_plus_u .AND. U_projection .NE. 'pseudo' ) &
-     WRITE( stdout, '(8x,"Atomic Hubbard wavefuncts ",f10.2," Mb", &
-                    & 5x,"(",i8,",",i5,")")') &
-   & complex_size*nwfcU*npol*DBLE(npwx_l)/Mb, npwx_l*npol,nwfcU
-  WRITE( stdout, '(8x,"NL pseudopotentials       ",f10.2," Mb", &
-                 & 5x,"(",i8,",",i5,")")') &
-     complex_size*nkb*DBLE(npwx_l)/Mb, npwx_l, nkb
-  IF ( nspin == 2 ) THEN
-     WRITE( stdout, '(8x,"Each V/rho on FFT grid    ",f10.2," Mb", &
-                    & 5x,"(",i8,",",i4,")")') &
-                    complex_size*nspin*DBLE(dfftp%nnr)/Mb, dfftp%nnr, nspin
-  ELSE
-     WRITE( stdout, '(8x,"Each V/rho on FFT grid    ",f10.2," Mb", &
-                    & 5x,"(",i8,")")') DBLE(complex_size*dfftp%nnr)/Mb, dfftp%nnr
+     ram = ram + complex_size * nwfcU * npol * npwx_l 
+  ! Nonlocal pseudopotentials V_NL (beta functions), reciprocal space
+  ram =  ram + complex_size * nkb * npwx_l
+  ! Charge density and potentials - see scf_type in scf_mod
+  scf_type_size =  (complex_size * ngm + real_size * dfftp%nnr ) * nspin
+  IF ( dft_is_meta() .or. lxdm ) scf_type_size =  2 * scf_type_size
+  ! rho, v, vnew
+  ram =  ram + 3 * scf_type_size
+  ! vltot, vrs, rho_core, rhog_core, psic, kedtau if needed
+  ram =  ram + complex_size * ( dfftp%nnr + ngm) + &
+       real_size * dfftp%nnr*(2+nspin)
+  IF ( dft_is_meta() ) ram = ram + real_size * dfftp%nnr*nspin
+  
+  ! arrays for rho mixing
+  IF ( lscf ) THEN
+     ! rhoin
+     ram =  ram + scf_type_size
+     ! see mix_type in scf_mod
+     mix_type_size =  complex_size * ngm * nspin
+     IF ( dft_is_meta() .or. lxdm ) mix_type_size =  2 * mix_type_size
+     ! df, dv (if kept in memory)
+     IF ( io_level < 2 ) &
+          ram =  ram + complex_size * mix_type_size * 2 * nmix
   END IF
-  WRITE( stdout, '(8x,"Each G-vector array       ",f10.2," Mb", &
-                 & 5x,"(",i8,")")') DBLE(real_size*ngm)/Mb, ngm
+  ! G-vectors: g, gg, mill, nl, nlm, ig_l2g, igtongl
+  ram =  ram + real_size * ngm * 4 + int_size * ngm * 7
+  ! double grid: nls, nlsm
+  ram =  ram + int_size * ngms * 2
   !
-  WRITE( stdout, '(5x,"Largest temporary arrays",5x,"est. size (Mb)", &
-                   &5x,"dimensions")')
-  IF ( isolve == 0 ) THEN
-     WRITE( stdout, '(8x,"Auxiliary wavefunctions   ",f10.2," Mb", &
-                 & 5x,"(",i8,",",i5,")")') &
-     complex_size*nbndx*npol*DBLE(npwx_l)/Mb, npwx_l*npol, nbndx
-  ENDIF
+  ! now scratch space that raises the "high watermark"
+  !
+  ! hpsi, spsi, matrices allocated in iterative diagonalization 
   ! nbnd_l : estimated dimension of distributed matrices
-  nbnd_l = nbndx/np_ortho(1)
-  WRITE( stdout, '(8x,"Each subspace H/S matrix  ",f10.2," Mb", &
-                 & 5x,"(",i8,",",i5,")")') &
-     complex_size*nbnd_l*DBLE(nbnd_l)/g_fact/Mb, nbnd_l, nbnd_l
   !
-  IF ( npol > 1 ) THEN
-     WRITE( stdout, '(8x,"Each <psi_i|beta_j> matrix",f10.2," Mb", &
-                 & 5x,"(",i8,",",i4,",",i5,")")') &
-     complex_size*nkb*DBLE(npol*nbnd)/g_fact/Mb, nkb, npol, nbnd
-  ELSE
-     WRITE( stdout, '(8x,"Each <psi_i|beta_j> matrix",f10.2," Mb", &
-                 & 5x,"(",i8,",",i5,")")') &
-     complex_size*nkb*DBLE(nbnd)/g_fact/Mb, nkb, nbnd
+  nbnd_l = nbndx/np_ortho(1)
+  ram1 = complex_size/g_fact * ( 2*nbnd_l**2 + & ! hr, sr
+                                 nkb*npol*nbnd ) ! <psi|beta>
+  IF ( isolve == 0 ) THEN
+     ram1 = ram1 + complex_size * nbndx * npol * npwx_l              ! hpsi
+     IF ( okvan ) ram1 = ram1 + complex_size * nbndx * npol * npwx_l ! spsi
   END IF
   !
-  IF ( lscf) WRITE( stdout, &
-     '(8x,"Arrays for rho mixing     ",f10.2," Mb", 5x,"(",i8,",",i5,")")') &
-     complex_size*dfftp%nnr*DBLE(nmix)/Mb, dfftp%nnr, nmix
+  ! the following is needed during self-consistency in approx_screening2
+  ! but shouldn't actually raise the watermark
   !
-  RETURN
+  IF ( lscf .AND. imix > 1 ) &
+     ram1 = MAX( ram1, complex_size * ngm * 27 + real_size * dffts%nnr )
+
+  maxram = ram + ram1
+
+  ! arrays used for global sorting in ggen:
+  !    mill_g, mill_unsorted, igsrt, g2sort_g, total dimensions:
+  !
+  IF ( .NOT. smallmem ) maxram = MAX ( maxram, &
+       int_size * 7 * ngm_g + real_size * ngm_g )
+
+  totram = maxram * nproc_image
+  
+  WRITE( stdout, '(/5x,"Dynamical RAM per process     > ",F10.2,"Mb")' ) &
+       maxram/Mb
+  IF ( nproc_image > 1) WRITE( stdout, &
+       '(/5x,"Total allocated dynamical RAM > ",F10.2,"Mb")' ) totram/Mb
   !
 END subroutine memory_report
