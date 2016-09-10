1a4be92da9f90fdcf463c25d1d740377680a6213
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2016-09-10 07:13:27 +0000

    Prevent bogus out-of-bound errors in some unlikely cases
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@12941 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/symme.f90
+++ PW/src/symme.f90
@@ -501,7 +501,7 @@ CONTAINS
           ngloc = ngloc+1
        END DO
        IF ( ngloc < 1 ) CALL infomsg('sym_rho_init', &
-            'likely internal error: no G-vectors found')
+            'some processors have no G-vectors for symmetrization')
        sendcnt(np) = ngloc
        ngpos = ngpos + ngloc
        IF ( ngpos > ngm ) &
@@ -695,7 +695,7 @@ gloop:    DO jg=iig,ngm_
     ! we transpose the matrix of G-vectors and their coefficients
     !
     ngm_ = SUM(recvcnt)
-    ALLOCATE (rhog_(ngm_,nspin),g_(3,ngm_))
+    ALLOCATE (rhog_(MAX(ngm_,1),nspin),g_(3,ngm_))
     DO is=1,nspin
        CALL mpi_alltoallv (rhog (1,is) , sendcnt, sdispls, MPI_DOUBLE_COMPLEX,&
             rhog_(1,is), recvcnt, rdispls, MPI_DOUBLE_COMPLEX, &
