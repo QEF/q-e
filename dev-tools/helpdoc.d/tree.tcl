187c224a086d88b2ea6a3ee8aa125958b63913e5
kokalj <kokalj@c92efa57-630b-4861-b058-cf58834340f0>
2016-10-02 16:58:02 +0000

    Dirty fix: reintroducing the getDescendantText and getDescendantAttribute routines that are needed to build the QE-modes.
    In future these two should be merged with getTextFromDescendant and getAttributeFromDescendantPath.
    
    
    
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13066 c92efa57-630b-4861-b058-cf58834340f0

--- dev-tools/helpdoc.d/tree.tcl
+++ dev-tools/helpdoc.d/tree.tcl
@@ -96,3 +96,68 @@ proc ::helpdoc::getAttributeFromDescendantPath {tree node args} {
     
     return $result
 }
+
+
+proc ::helpdoc::getDescendantText {tree node args} {
+    # Usage: getDescendantText $tree $node tag1 tag2 last_tag
+    # Beware: it will get the text from all tags that matches
+
+    set result ""
+    set tag [lindex $args 0]
+
+    foreach child [$tree children $node] {
+
+	set _tag [getFromTree $tree $child tag]
+	
+	if { $tag == $_tag } {
+	    if { $tag == $args } {
+		append result "[getFromTree $tree $child text] "
+	    } else {
+		set args1 [lrange $args 1 end]
+		return [getDescendantText $tree $child $args1]
+	    }
+	}
+    }
+    
+    return $result
+}
+
+
+proc ::helpdoc::getDescendantAttribute {tree node args} {
+    # Usage: getDescendantText $tree $node tag1 tag2 last_tag attribute_of_last_tag
+    # Beware: it will get the requested attribute from all tags that matches
+
+    set result ""
+    set tag [lindex $args 0]
+    set att [lindex $args end]
+    
+    foreach child [$tree children $node] {
+	
+	set _tag [getFromTree $tree $child tag]
+	
+	if { $tag == $_tag } {
+
+
+	    
+	    if { [llength $args] == 2 } {
+		
+		# ok _tag is the attribute
+
+		set attr [getFromTree $tree $child attributes]
+		attr2array_ arr $attr
+		
+		if { [info exists arr($att)] } {
+		    append result $arr($att)
+		}
+		
+	    } else {
+
+
+		set args1 [lrange $args 1 end]
+		return [getDescendantAttribute $tree $child $args1]
+	    }
+	}
+    }
+    
+    return $result
+}
