b491b3a47b6a12a1e06320c5931a3ec6e6ebf7c8
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2016-11-14 17:27:47 +0000

    Reference to mp_world removed - there are a few more that shouldn't be there
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13153 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/sum_band.f90
+++ PW/src/sum_band.f90
@@ -909,10 +909,9 @@ SUBROUTINE sum_bec ( ik, current_spin, ibnd_start, ibnd_end, this_bgrp_nbnd )
   USE realus,        ONLY : real_space, &
                             invfft_orbital_gamma, calbec_rs_gamma, &
                             invfft_orbital_k, calbec_rs_k
+  USE us_exx,        ONLY : store_becxx0
   USE mp_bands,      ONLY : nbgrp,inter_bgrp_comm
   USE mp,            ONLY : mp_sum
-  USE us_exx,        ONLY : store_becxx0
-  USE mp_world,      ONLY : mpime
   USE mp_pools,      ONLY : me_pool, my_pool_id
   !
   IMPLICIT NONE
