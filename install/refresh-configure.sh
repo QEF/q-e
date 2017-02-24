c939f2f34734efd7baf06087288057a22ae03c60
spigafi <spigafi@c92efa57-630b-4861-b058-cf58834340f0>
2017-02-24 09:34:51 +0000

    Refresh configure
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13341 c92efa57-630b-4861-b058-cf58834340f0

--- install/refresh-configure.sh
+++ install/refresh-configure.sh
@@ -9,6 +9,11 @@
 # of the present distribution.
 #
 # Dependency: GNU AUTOCONF (minimum 2.60, 2.69 suggested)
+#
+# Tested using:
+# - autoconf (GNU Autoconf) 2.63
+# - aclocal (GNU automake) 1.11.1
+# - m4 (GNU M4) 1.4.13
 
 aclocal -I m4 --install
 
