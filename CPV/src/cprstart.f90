fb981eb8f1bd86f879c965d94742f8e78c2f0881
ccavazzoni <ccavazzoni@c92efa57-630b-4861-b058-cf58834340f0>
2017-03-03 09:10:32 +0000

    - adding the possibility to restore "old" band parallelization in CP
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13373 c92efa57-630b-4861-b058-cf58834340f0

--- CPV/src/cprstart.f90
+++ CPV/src/cprstart.f90
@@ -33,7 +33,7 @@ PROGRAM main
   !
   ! ... initialize MPI (parallel processing handling)
   !
-  CALL mp_startup ( diag_in_band_group = .true. )
+  CALL mp_startup ( diag_in_band_group = .true. , what_band_group = 0 )
   !
   ! ... start the environment
   !
