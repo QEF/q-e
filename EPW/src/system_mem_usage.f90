e08a93e1528435bfc34fd3974979dcb7e41dd8f2
sponce <sponce@c92efa57-630b-4861-b058-cf58834340f0>
2017-06-30 11:15:39 +0000

    Addition of the memory report file.
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13585 c92efa57-630b-4861-b058-cf58834340f0

---
+++ EPW/src/system_mem_usage.f90
@@ -0,0 +1,63 @@
+  !                                                                            
+  ! Copyright (C) 2010-2016 Samuel Ponce', Roxana Margine, Carla Verdi, Feliciano Giustino
+  !                                                                            
+  ! This file is distributed under the terms of the GNU General Public         
+  ! License. See the file `LICENSE' in the root directory of the               
+  ! present distribution, or http://www.gnu.org/copyleft.gpl.txt .             
+  !                                                                            
+  !----------------------------------------------------------------------
+  SUBROUTINE system_mem_usage (valueRSS)
+  !----------------------------------------------------------------------
+  !! Report the memory usage ( VIRT and REAL ) from the current PID 
+  !! process ( so it will be master only in case of MPI ).
+  !! Memory is reported from the /proc/PID_NUMBER/status file
+  ! ---------------------------------------------------------------------
+  !USE ifport !if on intel compiler
+  USE io_global,   ONLY : stdout
+  USE io_epw,      ONLY : iunimem
+  ! 
+  implicit none
+  ! 
+  INTEGER, INTENT (inout) :: valueRSS(2)
+  !! Contains the value of the memory in kB
+  ! 
+  CHARACTER(len=200):: filename=' '
+  CHARACTER(len=80) :: line
+  CHARACTER(len=8)  :: pid_char=' '
+  INTEGER :: pid
+  LOGICAL :: ifxst
+
+  valueRSS = -1    ! return negative number if not found
+
+  !--- get process ID
+
+  pid=getpid()
+  WRITE(pid_char,'(I8)') pid
+  filename='/proc/'//trim(adjustl(pid_char))//'/status'
+
+  !--- read system file
+
+  inquire (file=filename,exist=ifxst)
+  if (.not.ifxst) then
+    write (stdout,'(a)') 'System file does not exist'
+    return
+  endif
+
+  open(unit=iunimem, file=filename, action='read')
+  do
+    read (iunimem,'(a)',end=120) line
+    ! Peak virtual memory usage
+    if (line(1:7).eq.'VmPeak:') then
+       read (line(8:),*) valueRSS(1)
+    endif
+    ! Peak resident set size
+    if (line(1:6).eq.'VmHWM:') then
+       read (line(7:),*) valueRSS(2)
+       CLOSE (unit = iunimem, status = 'keep')
+       exit
+    endif        
+  enddo
+  120 continue
+
+  END SUBROUTINE system_mem_usage
+  !--------------------------------------------------------------------------
