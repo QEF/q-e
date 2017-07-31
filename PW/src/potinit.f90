5c32736c3c2bfdeb43ac8c71081e3a16d30a7e4f
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2017-07-31 19:35:11 +0000

    Magnetic anisotropy with Force Theorem wasn't working any longer with new I/O
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13669 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/potinit.f90
+++ PW/src/potinit.f90
@@ -294,11 +294,13 @@ SUBROUTINE nc_magnetization_from_lsda ( nnr, nspin, rho )
        angle1(1)/PI*180.d0, angle2(1)/PI*180.d0 
   WRITE(stdout,*) '-----------'
   !
+#ifdef __OLDXML
   ! On input, rho(1)=rho_up, rho(2)=rho_down
   ! Set rho(1)=rho_tot, rho(3)=rho_up-rho_down=magnetization
   ! 
   rho(:,3) = rho(:,1)-rho(:,2)
   rho(:,1) = rho(:,1)+rho(:,2)
+#endif
   !
   ! now set rho(2)=magn*sin(theta)*cos(phi)   x
   !         rho(3)=magn*sin(theta)*sin(phi)   y
