8f20e8c76292fa05761840897963283d5d23e39b
degironc <degironc@c92efa57-630b-4861-b058-cf58834340f0>
2017-08-08 21:33:33 +0000

    add a PARA_POSTFIX to run-cp.sh so that different parallelization levels can be tested
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13711 c92efa57-630b-4861-b058-cf58834340f0

--- test-suite/run-cp.sh
+++ test-suite/run-cp.sh
@@ -12,10 +12,23 @@ include ${ESPRESSO_ROOT}/test-suite/ENVIRONMENT
 
 if [ $QE_USE_MPI == 1 ]; then
   export PARA_PREFIX="mpirun -np ${TESTCODE_NPROCS}"
+#
+# available flags: 
+#                  -ni n        number of images        (or -nimage)
+#                               (only for NEB; for PHonon, see below)
+#                  -nk n        number of pools         (or -npool, -npools)
+#                  -nb n        number of band groups   (or -nbgrp,-nband_group)
+#                  -nt n        number of task groups   (or -ntg, -ntask_groups)
+#                  -nd n        number of processors for linear algebra 
+#                                            (or -ndiag, -northo) 
+#
+  export PARA_POSTFIX=" -nk 1 -nd 1 -nb 1 -nt 1 "
 else
   unset PARA_PREFIX
+  unset PARA_POSTFIX
 fi
 
-${PARA_PREFIX} ${ESPRESSO_ROOT}/bin/cp.x "$@"
+echo ' RUNNING ',${PARA_PREFIX} ${ESPRESSO_ROOT}/bin/cp.x ${PARA_POSTFIX} "$@"
+${PARA_PREFIX} ${ESPRESSO_ROOT}/bin/cp.x ${PARA_POSTFIX} "$@"
 
 rm -f input_tmp.in
