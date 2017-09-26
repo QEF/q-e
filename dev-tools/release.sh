bb6e1c12a130471a54ffa3d010003bac3b3e857b
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2017-09-26 14:12:47 +0000

    Yet another version of the release script
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13872 c92efa57-630b-4861-b058-cf58834340f0

--- dev-tools/release.sh
+++ dev-tools/release.sh
@@ -1,33 +1,33 @@
 #!/bin/sh -x
 
 version=6.2
-revision=13866
+revision=13871
 user=giannozz
+tempdir=$HOME/tempdir
 
-# make sure there is no locale setting creating unneeded differences.
+# Make sure there is no locale setting creating unneeded differences.
 LC_ALL=C
 export LC_ALL
 
-# get the svn copy via tag
-# svn checkout http://qeforge.qe-forge.org/svn/q-e/tags/QE-$version/espresso qe-$version
+# work in $tempdir/qe-$version
+cd $tempdir
 
+# Get the svn copy via tag
+# svn checkout http://qeforge.qe-forge.org/svn/q-e/tags/QE-$version/espresso qe-$version
 # -OR- get the svn copy via revision checkout
 svn checkout -r$revision svn+ssh://$user@qeforge.qe-forge.org/svnroot/q-e/trunk/espresso qe-$version
 cd qe-$version
 
-# generate version.f90 (requires make.inc and svn files)
+# Following operations require make.inc and svn files
 touch make.inc
-cd Modules
+
+# Generate version.f90
+cd Modules/
 make version
-rm version.f90.in
+/bin/rm version.f90.in
 cd ..
 
-# remove .svn directories, unneeded files, packages not to be packaged
-find . -type d -name .svn -exec /bin/rm -rf {} \;
-rm .??* TODO archive/plumed-1.3-qe.tar.gz archive/wannier90-2.1.0.tar.gz
-rm -rf QHA/ PlotPhon/ West/ GIPAW/
-
-# generate documentation - NOTA BENE:
+# Generate documentation - NOTA BENE:
 # in order to build the .html and .txt documentation in Doc,
 # "tcl", "tcllib", "xsltproc" are needed
 # in order to build the .pdf files in Doc, "pdflatex" is needed
@@ -36,40 +36,43 @@ rm -rf QHA/ PlotPhon/ West/ GIPAW/
 
 make doc VERSION=$version
 
-# generate PWGUI
+# Generate PWGUI
 make tar-gui PWGUI_VERSION=$version
-mv PWgui-$version.tar.gz ../qe-PWgui-$version.tar.gz
-/bin/rm -r GUI
+mv PWgui-$version.tgz ../qe-$version-PWgui.tar.gz
 
-# generate QE-modes (requires tcllib, emacs, texlive-upquote)
+# Generate QE-modes (requires tcllib, emacs, texlive-upquote)
 make tar-qe-modes VERSION=$version
 mv QE-modes-$version.tar.gz ../qe-$version-emacs_modes.tar.gz
 
-# Updating reference outputs on test-suite
-cd test-suite
+# Move out svn directories, unneeded files, packages not to be packaged
+/bin/rm .??* TODO archive/plumed-1.3-qe.tar.gz
+/bin/rm -rf .svn/ QHA/ PlotPhon/ West/ GIPAW/ GUI/
+
+# Update reference outputs on test-suite
+cd test-suite/
 for file in */benchmark.out*; do
     file2=`echo $file | sed 's/SVN/$version'`
     mv $file $file2
 done
 cd ..
 
-# packacking test-suite
+# Package test-suite
 cp License test-suite/
 tar -czvf ../qe-$version-test-suite.tar.gz test-suite
 /bin/rm -rf test-suite
 
-# Grouping Examples in the same directory and packacking them
+# Package example
 tar -czvf ../qe-$version-examples.tar.gz License */Examples */examples */*/examples
 /bin/rm -rf */Examples */examples */*/examples
 
-# core espresso
+# Package sources (in directory qe-$version)
 cd ..
 tar -czvf qe-$version.tar.gz qe-$version
 cd qe-$version
 
-# Preparing documentation for upload
-cd Doc
-rm *.xml *.txt *.html
+# Prepare documentation for upload
+cd Doc/
+/bin/rm *.xml *.txt *.html
 cp ../*/Doc/*.html ../*/Doc/*.txt .
 
 cp ../PW/Doc/user_guide.pdf ./pw_user_guide.pdf
@@ -87,5 +90,5 @@ cp -R ../NEB/Doc/user_guide ./neb_user_guide
 cp -R ../atomic/Doc/pseudo-gen ./pseudo-gen
 
 echo Now copy "Docs" to QE website
-echo scp -R Doc <...>@<...>/wp-content/uploads/Doc-$version
+echo scp -R Doc user@site/wp-content/uploads/Doc-$version
 echo connect to the website and create/update symbolic link to "Doc-$version"
