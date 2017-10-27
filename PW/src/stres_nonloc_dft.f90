01468885a63150174553bcfffb38cc01797ea11b
giannozz <giannozz@c92efa57-630b-4861-b058-cf58834340f0>
2017-10-27 18:33:10 +0000

    Checking for correctness is a good habit, but there is no good reason to check twice the same variables
    git-svn-id: http://qeforge.qe-forge.org/svn/q-e/trunk/espresso@13964 c92efa57-630b-4861-b058-cf58834340f0

--- PW/src/stres_nonloc_dft.f90
+++ PW/src/stres_nonloc_dft.f90
@@ -32,17 +32,6 @@ subroutine stres_nonloc_dft( rho, rho_core, nspin, sigma_nonloc_dft )
   inlc = get_inlc()
 
   if ( inlc==1 .or. inlc==2 .or. inlc==4 .or. inlc==5 .or. inlc==6 ) then
-#if defined (__SPIN_BALANCED)
-     if ( nspin==2 ) then
-         write(stdout,'(/,/ "     Performing spin-balanced Ecnl stress calculation!")')
-     else if ( nspin > 2 ) then
-         call errore ('stres_vdW_DF','noncollinear vdW stress not implemented',1)
-     end if
-#else
-     if ( nspin>=2 ) then
-        call errore ('stres_vdW_DF',   'vdW stress not implemented for nspin > 1',1)
-     end if
-#endif
      CALL stress_vdW_DF(rho, rho_core, nspin, sigma_nonloc_dft)
   elseif ( inlc == 3 ) then
      if ( nspin>2 ) call errore ('stress_rVV10', 'rVV10 stress not implemented for nspin > 2',1)
