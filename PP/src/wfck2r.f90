7d1e4f3c0d77194ad9d645a1b6f627e40ae4bcce
Davide Ceresoli <dceresoli@gmail.com>
2018-01-16 15:59:31 +0100

    Improvement of wfck2r: - output a subset of wfcs - output in matlab format

--- PP/src/wfck2r.f90
+++ PP/src/wfck2r.f90
@@ -12,6 +12,7 @@
 ! grid, as such it occupies much more disk space then that in G-space.
 !
 ! Program written by Matteo Calandra.
+! Modified by D. Ceresoli (2017)
 ! 
 !-----------------------------------------------------------------------
 PROGRAM wfck2r
@@ -42,8 +43,10 @@ PROGRAM wfck2r
   INTEGER            :: npw, iunitout,ios,ik,i,iuwfcr,lrwfcr,ibnd, ig, is
   LOGICAL            :: exst
   COMPLEX(DP), ALLOCATABLE :: evc_r(:,:), dist_evc_r(:,:)
+  INTEGER :: first_k, last_k, first_band, last_band
+  LOGICAL :: lmatlab
 
-  NAMELIST / inputpp / outdir, prefix
+  NAMELIST / inputpp / outdir, prefix, first_k, last_k, first_band, last_band, lmatlab
 
 
   !
@@ -61,6 +64,12 @@ PROGRAM wfck2r
   !
   IF ( ionode )  THEN
      !
+     ! set defaults:
+     first_k = 0
+     last_k = 0
+     first_band = 0
+     last_band = 0
+     !
      CALL input_from_file ( )
      ! 
      READ (5, inputpp, err = 200, iostat = ios)
@@ -76,6 +85,11 @@ PROGRAM wfck2r
 
   CALL mp_bcast( tmp_dir, ionode_id, world_comm )
   CALL mp_bcast( prefix, ionode_id, world_comm )
+  CALL mp_bcast( first_k, ionode_id, world_comm )
+  CALL mp_bcast( last_k, ionode_id, world_comm )
+  CALL mp_bcast( first_band, ionode_id, world_comm )
+  CALL mp_bcast( last_band, ionode_id, world_comm )
+  CALL mp_bcast( lmatlab, ionode_id, world_comm )
 
   !
   !   Now allocate space for pwscf variables, read and check them.
@@ -86,33 +100,51 @@ PROGRAM wfck2r
   exst=.false.
 
   filename='wfc_r'
-  write(6,*) 'filename=',filename
+  write(6,*) 'filename              = ', trim(filename)
   iuwfcr=877
   lrwfcr = 2 * dffts%nr1x*dffts%nr2x*dffts%nr3x * npol
   ! lrwfc = 2 * nbnd * npwx * npol
-  write(6,*) dffts%nnr, npwx
-  
+  write(6,*) 'dffts%nnr, npwx       =', dffts%nnr, npwx
  
+  if (first_k <= 0) first_k = 1 
+  if (last_k <= 0) last_k = nks
+  if (first_band <= 0) first_band = 1 
+  if (last_band <= 0) last_band = nbnd
+  write(6,*) 'first_k, last_k       =', first_k, last_k
+  write(6,*) 'first_band, last_band =', first_band, last_band
+  write(6,*)
 
-  write(6,*) 'length of wfc in real space/per band', nks*lrwfcr*8
-  write(6,*) 'length of wfc in k space', 2*nbnd*npwx*nks*8
+  write(6,*) 'length of wfc in real space/per band', (last_k-first_k+1)*lrwfcr*8
+  write(6,*) 'length of wfc in k space', 2*(last_band-first_band+1)*npwx*nks*8
   CALL init_us_1
 
 !
 !define lrwfcr
 !
   IF (ionode) CALL diropn (iuwfcr, filename, lrwfcr, exst)
+  IF (lmatlab .and. ionode) then
+     open(unit=iuwfcr+1, file='wfck2r.mat', status='unknown', form='formatted')
+     write(iuwfcr+1,'(A)') '# created by wfck2r.x of Quantum-Espresso'
+     write(iuwfcr+1,'(''# name: '',A,/,''# type: scalar'',/,I,//)') 'nkpoints', (last_k-first_k+1)
+     write(iuwfcr+1,'(''# name: '',A,/,''# type: scalar'',/,I,//)') 'nbands', (last_band-first_band+1)
+     write(iuwfcr+1,'(''# name: '',A,/,''# type: scalar'',/,I,//)') 'nr1x', dffts%nr1x
+     write(iuwfcr+1,'(''# name: '',A,/,''# type: scalar'',/,I,//)') 'nr2x', dffts%nr2x
+     write(iuwfcr+1,'(''# name: '',A,/,''# type: scalar'',/,I,//)') 'nr3x', dffts%nr3x
+     write(iuwfcr+1,'(''# name: '',A,/,''# type: complex matrix'')') 'unkr'
+     write(iuwfcr+1,'(''# ndims: 5'')')
+     write(iuwfcr+1,'(5I10)') dffts%nr1x, dffts%nr2x, dffts%nr3x, last_band-first_band+1, last_k-first_k+1
+  endif
 
   ALLOCATE ( evc_r(dffts%nnr,npol) )
   ALLOCATE ( dist_evc_r(dffts%nr1x*dffts%nr2x*dffts%nr3x,npol) )
   
 
-  DO ik = 1,nks
+  DO ik = first_k, last_k
      
      npw = ngk(ik)
      CALL davcio (evc, 2*nwordwfc, iunwfc, ik, - 1)
 
-     do ibnd=1,nbnd 
+     do ibnd = first_band, last_band
         !
         ! perform the fourier transform
         !
@@ -140,7 +172,8 @@ PROGRAM wfck2r
         dist_evc_r(1:dffts%nnr,:)=evc_r(1:dffts%nnr,:)
 #endif
 
-        if(ionode) call davcio (dist_evc_r, lrwfcr, iuwfcr, (ik-1)*nbnd+ibnd, +1)
+        if (ionode) call davcio (dist_evc_r, lrwfcr, iuwfcr, (ik-1)*nbnd+ibnd, +1)
+        if (ionode .and. lmatlab) write(iuwfcr+1,'(E20.14,4X,E20.14)') (dist_evc_r(i,1), i=1,dffts%nr1x*dffts%nr2x*dffts%nr3x)
      enddo
         
      !
@@ -149,7 +182,8 @@ PROGRAM wfck2r
 
   enddo
 
-  if(ionode) close(iuwfcr)
+  if (ionode) close(iuwfcr)
+  if (lmatlab .and. ionode) close(iuwfcr+1)
   DEALLOCATE (evc_r)
 
   CALL environment_end ( 'WFCK2R' )
