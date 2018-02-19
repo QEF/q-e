69a20a3a4d83839d8846c5519fc0fff10d95a46b
Pietro Bonfa <p.bonfa@nospam.com>
2018-02-19 18:07:14 +0100

    Genrate only relevant tests + more variables for gather

--- UtilXlib/tests/gen_tests.py
+++ UtilXlib/tests/gen_tests.py
@@ -4,26 +4,53 @@ typeconv={'INTEGER': 'INT', 'REAL': 'REAL', 'REAL(8)': 'DBLE', 'COMPLEX': 'CMPLX
 
 datasize='10'
 ranks  ={'1': '', 'v': '(datasize)', 'm': '(datasize,datasize)', 't': '(datasize,datasize,datasize)'}
-alldata={'1': '', 'v': '(:)', 'm': '(:,:)', 't': '(:,:,:)'}
-import os
+nextr={'1': 'v', 'v': 'm', 'm': 't', 't': ''}
+
+import os, re
+
+mkfile = open('./autotest.inc', 'w')
+mkfile.write('override SRCS += ')
+
 for file in os.listdir("."):
   if file.endswith(".tmpl"):
     
     with open(file,'r') as f:
       data = f.read()
+      
+      # ! Implemented: i1, iv, rv, iv, rm, im, cv
+      mtch = re.findall(r'Implemented\s*:\s*((\w\w|\*)(,\s*\w\w)*)',data)
+      if mtch:
+          impl_interfaces = mtch[0][0].replace(' ','').split(',')
+      else:
+          impl_interfaces = ['*']
+          
       for t in types:
         for k,s in ranks.items():
           tmp = os.path.splitext(os.path.basename(file))[0]
-          ofile = 'auto' + tmp +'_'+t[0].lower()+k+'.f90'
+          tname = t[0].lower()+k
+          ofile = 'auto' + tmp + '_' + tname + '.f90'
+          
+          if not ( ('*' in impl_interfaces) or (tname in impl_interfaces) ):
+              continue
+          
           with open(ofile,'w') as fo:
             if k == '1':
               allf = ''; sumf = ''
             else:
               allf = 'ALL'; sumf = 'SUM'
-            
+
             fo.write(data.format(  vname=(t[0].lower()+k), \
                                     datasize=datasize, \
                                     type=t, size=s, \
-                                    all=alldata[k], \
+                                    all=s.replace('datasize',':'), \
+                                    sizep1=ranks.get(nextr[k],'invalid'), \
                                     allf=allf, sumf=sumf, typeconv=typeconv[t]))
-          print (ofile + '\\')
+          mkfile.write(ofile + '\\\n')
+
+
+def implemented(v, lst):
+    if '*' in lst:
+        return True
+    else:
+        return (v in lst)
+    
