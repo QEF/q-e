92ecd0bbad81720b5e4b19cfd95aa23ca89fd893
Pietro Bonfa <bonfus@gmail.com>
2018-02-27 22:01:28 +0100

    Use DP for complex

--- UtilXlib/tests/test_mp_bcast_c5d_gpu.f90
+++ UtilXlib/tests/test_mp_bcast_c5d_gpu.f90
@@ -8,6 +8,7 @@ PROGRAM test_mp_bcast_c5d_gpu
     USE MPI
 #endif
     USE mp, ONLY : mp_bcast
+    USE util_param, ONLY : DP
     USE mp_world, ONLY : mp_world_start, mp_world_end, mpime, &
                           root, nproc, world_comm
     USE tester
@@ -16,8 +17,8 @@ PROGRAM test_mp_bcast_c5d_gpu
     TYPE(tester_t) :: test
     INTEGER :: world_group = 0
     ! test variable
-    COMPLEX(8), DEVICE :: im_d(10, 10, 10, 10, 10)
-    COMPLEX(8)         :: im_h(10, 10, 10, 10, 10)
+    COMPLEX(DP), DEVICE :: im_d(10, 10, 10, 10, 10)
+    COMPLEX(DP)         :: im_h(10, 10, 10, 10, 10)
     
     !    
     CALL test%init()
