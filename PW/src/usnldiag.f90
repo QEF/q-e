5325e92cb3f307e0922392092f941177955399d3
Ye Luo <yeluo@anl.gov>
2018-03-18 18:44:27 -0500

    Threading and cache blocking usnldiag.

--- PW/src/usnldiag.f90
+++ PW/src/usnldiag.f90
@@ -33,73 +33,88 @@ SUBROUTINE usnldiag (npw, h_diag, s_diag)
   !
   INTEGER :: ikb, jkb, ih, jh, na, nt, ig, ipol
   COMPLEX(DP) :: ps1(2), ps2(2), ar
+  ! cache blocking parameters
+  INTEGER :: iblock, numblock, blocksize
   !
-  ! initialise s_diag
+  ! setting cache blocking size
+  blocksize = 256
+  numblock  = (npw+blocksize-1)/blocksize
   !
-  s_diag = 1.d0
-  !
-  !    multiply on projectors
-  !
-  DO nt = 1, ntyp
-     DO na = 1, nat
-        IF (ityp (na) == nt) THEN
-           DO ih = 1, nh(nt)
-              ikb = indv_ijkb0(na) + ih
-              IF (lspinorb) THEN
-                 ps1(1) = deeq_nc (ih, ih, na, 1)
-                 ps1(2) = deeq_nc (ih, ih, na, 4)
-                 ps2(1) = qq_so(ih, ih, 1, nt)
-                 ps2(2) = qq_so(ih, ih, 4, nt)
-              ELSEIF (noncolin) THEN
-                 ps1(1) = deeq_nc (ih, ih, na, 1)
-                 ps1(2) = deeq_nc (ih, ih, na, 4)
-                 ps2(1) = qq_at (ih, ih, na)
-                 ps2(2) = qq_at (ih, ih, na)
-              ELSE
-                 ps1(1) = deeq (ih, ih, na, current_spin)
-                 ps2(1) = qq_at (ih, ih, na)
-              ENDIF
-              DO ipol =1, npol
-                 DO ig = 1, npw
-                    ar = vkb (ig, ikb)*conjg(vkb (ig, ikb))
-                    h_diag (ig,ipol) = h_diag (ig,ipol) + ps1(ipol) * ar
-                    s_diag (ig,ipol) = s_diag (ig,ipol) + ps2(ipol) * ar
+!$omp parallel do private(ikb, jkb, ps1, ps2, ar)
+  DO iblock = 1, numblock
+     !
+     ! initialise s_diag
+     !
+     DO ipol =1, npol
+        DO ig = (iblock-1)*blocksize+1, MIN(iblock*blocksize, npw)
+           s_diag(ig, ipol) = 1.d0
+        ENDDO
+     ENDDO
+     !
+     !    multiply on projectors
+     !
+     DO nt = 1, ntyp
+        DO na = 1, nat
+           IF (ityp (na) == nt) THEN
+              DO ih = 1, nh(nt)
+                 ikb = indv_ijkb0(na) + ih
+                 IF (lspinorb) THEN
+                    ps1(1) = deeq_nc (ih, ih, na, 1)
+                    ps1(2) = deeq_nc (ih, ih, na, 4)
+                    ps2(1) = qq_so(ih, ih, 1, nt)
+                    ps2(2) = qq_so(ih, ih, 4, nt)
+                 ELSEIF (noncolin) THEN
+                    ps1(1) = deeq_nc (ih, ih, na, 1)
+                    ps1(2) = deeq_nc (ih, ih, na, 4)
+                    ps2(1) = qq_at (ih, ih, na)
+                    ps2(2) = qq_at (ih, ih, na)
+                 ELSE
+                    ps1(1) = deeq (ih, ih, na, current_spin)
+                    ps2(1) = qq_at (ih, ih, na)
+                 ENDIF
+                 DO ipol =1, npol
+                    DO ig = (iblock-1)*blocksize+1, MIN(iblock*blocksize, npw)
+                       ar = vkb (ig, ikb)*conjg(vkb (ig, ikb))
+                       h_diag (ig,ipol) = h_diag (ig,ipol) + ps1(ipol) * ar
+                       s_diag (ig,ipol) = s_diag (ig,ipol) + ps2(ipol) * ar
+                    ENDDO
                  ENDDO
-              ENDDO
-              IF ( upf(nt)%tvanp .or.newpseudo (nt) ) THEN
-                 DO jh = 1, nh (nt)
-                    IF (jh/=ih) THEN
-                       jkb = indv_ijkb0(na) + jh
-                       IF (lspinorb) THEN
-                          ps1(1) = deeq_nc (ih, jh, na, 1)
-                          ps1(2) = deeq_nc (ih, jh, na, 4)
-                          ps2(1) = qq_so(ih, jh, 1, nt)
-                          ps2(2) = qq_so(ih, jh, 4, nt)
-                       ELSEIF (noncolin) THEN
-                          ps1(1) = deeq_nc (ih, jh, na, 1)
-                          ps1(2) = deeq_nc (ih, jh, na, 4)
-                          ps2(1) = qq_at (ih, jh, na)
-                          ps2(2) = qq_at (ih, jh, na)
-                       ELSE
-                          ps1(1) = deeq (ih, jh, na, current_spin)
-                          ps2(1) = qq_at (ih, jh, na)
-                       ENDIF
-                       DO ipol = 1, npol
-                          DO ig = 1, npw
-                             ar = vkb (ig, ikb) *conjg( vkb (ig, jkb))
-                             h_diag (ig,ipol) = h_diag (ig,ipol) + &
-                                  ps1(ipol) * ar
-                             s_diag (ig,ipol) = s_diag (ig,ipol) + &
-                                  ps2(ipol) * ar
+                 IF ( upf(nt)%tvanp .or.newpseudo (nt) ) THEN
+                    DO jh = 1, nh (nt)
+                       IF (jh/=ih) THEN
+                          jkb = indv_ijkb0(na) + jh
+                          IF (lspinorb) THEN
+                             ps1(1) = deeq_nc (ih, jh, na, 1)
+                             ps1(2) = deeq_nc (ih, jh, na, 4)
+                             ps2(1) = qq_so(ih, jh, 1, nt)
+                             ps2(2) = qq_so(ih, jh, 4, nt)
+                          ELSEIF (noncolin) THEN
+                             ps1(1) = deeq_nc (ih, jh, na, 1)
+                             ps1(2) = deeq_nc (ih, jh, na, 4)
+                             ps2(1) = qq_at (ih, jh, na)
+                             ps2(2) = qq_at (ih, jh, na)
+                          ELSE
+                             ps1(1) = deeq (ih, jh, na, current_spin)
+                             ps2(1) = qq_at (ih, jh, na)
+                          ENDIF
+                          DO ipol = 1, npol
+                             DO ig = (iblock-1)*blocksize+1, MIN(iblock*blocksize, npw)
+                                ar = vkb (ig, ikb) *conjg( vkb (ig, jkb))
+                                h_diag (ig,ipol) = h_diag (ig,ipol) + &
+                                     ps1(ipol) * ar
+                                s_diag (ig,ipol) = s_diag (ig,ipol) + &
+                                     ps2(ipol) * ar
+                             ENDDO
                           ENDDO
-                       ENDDO
-                    ENDIF
-                 ENDDO
-              ENDIF
-           ENDDO
-        ENDIF
+                       ENDIF
+                    ENDDO
+                 ENDIF
+              ENDDO
+           ENDIF
+        ENDDO
      ENDDO
   ENDDO
+!$omp end parallel do
 
   RETURN
 END SUBROUTINE usnldiag
