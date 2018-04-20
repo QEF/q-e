1b50a031097d8a259fe0e7972029d4a607be9681
Paolo Giannozzi <p.giannozzi@gmail.com>
2018-04-20 18:19:55 +0200

    Preprocessed variable DIRECT_IO_FACTOR replaced by a standard fortran call to "inquire". Obsolete for PWscf but still used by postprocessing and phonons. To be tested. To be done: EPW, containing several such preprocessed variables.

--- Modules/io_files.f90
+++ Modules/io_files.f90
@@ -10,6 +10,7 @@ MODULE io_files
 !=----------------------------------------------------------------------------=!
   !
   USE parameters, ONLY: ntypx
+  USE kinds,      ONLY: dp
   USE io_global,  ONLY: ionode, ionode_id, stdout
   !
   ! ... I/O related variables: file names, units, utilities
@@ -310,15 +311,6 @@ subroutine diropn (unit, extension, recl, exst, tmp_dir_)
   !     On output, "exst" is .T. if opened file already exists
   !     If recl=-1, the file existence is checked, nothing else is done
   !
-#if defined(__SX6)
-#  define DIRECT_IO_FACTOR 1
-#else
-#  define DIRECT_IO_FACTOR 8 
-#endif
-  !
-  ! the  record length in direct-access I/O is given by the number of
-  ! real*8 words times DIRECT_IO_FACTOR (may depend on the compiler)
-  !
   implicit none
   !
   !    first the input variables
@@ -337,26 +329,23 @@ subroutine diropn (unit, extension, recl, exst, tmp_dir_)
   !
   character(len=256) :: tempfile, filename
   ! complete file name
-  integer :: ios
+  real(dp):: dummy
   integer*8 :: unf_recl
-  ! used to check I/O operations
-  ! length of the record
+  ! double precision to prevent integer overflow
+  integer :: ios, direct_io_factor
   logical :: opnd
-
-  ! Check if the optional variable tmp_dir is included
   !
-
-  ! if true the file is already opened
+  !    initial checks
   !
   if (unit < 0) call errore ('diropn', 'wrong unit', 1)
   !
-  !    we first check that the file is not already openend
+  !    ifirst we check that the file is not already openend
   !
   ios = 0
   inquire (unit = unit, opened = opnd)
   if (opnd) call errore ('diropn', "can't open a connected unit", abs(unit))
   !
-  !      then we check the filename extension
+  !    then we check the filename extension
   !
   if (extension == ' ') call errore ('diropn','filename extension not given',2)
   filename = trim(prefix) // "." // trim(extension)
@@ -369,12 +358,14 @@ subroutine diropn (unit, extension, recl, exst, tmp_dir_)
   inquire (file = tempfile, exist = exst)
   if (recl == -1) RETURN
   !
-  !      the unit for record length is unfortunately machine-dependent
+  ! the  record length in direct-access I/O is given by the number of
+  ! real*8 words times direct_io_factor (may depend on the compiler)
   !
-  unf_recl = DIRECT_IO_FACTOR * int(recl, kind=kind(unf_recl))
+  INQUIRE (IOLENGTH=direct_io_factor) dummy
+  unf_recl = direct_io_factor * int(recl, kind=kind(unf_recl))
   if (unf_recl <= 0) call errore ('diropn', 'wrong record length', 3)
   !
-  open (unit, file = trim(adjustl(tempfile)), iostat = ios, form = 'unformatted', &
+  open (unit, file=trim(adjustl(tempfile)), iostat=ios, form='unformatted', &
        status = 'unknown', access = 'direct', recl = unf_recl)
 
   if (ios /= 0) call errore ('diropn', 'error opening '//trim(tempfile), unit)
