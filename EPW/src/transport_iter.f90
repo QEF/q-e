2cc17ae28fbd39e58a7774b3ada7fad22d264590
Samuel Poncé <samuel.pon@gmail.com>
2018-07-08 16:06:15 +0100

    Add the possibility of linear mixing for IBTE.

--- EPW/src/transport_iter.f90
+++ EPW/src/transport_iter.f90
@@ -33,7 +33,7 @@
     USE epwcom,        ONLY : fsthick, & 
                               eps_acustic, degaussw, nstemp, & 
                               system_2d, int_mob, ncarrier, restart, restart_freq,&
-                              mp_mesh_k, nkf1, nkf2, nkf3, vme
+                              mp_mesh_k, nkf1, nkf2, nkf3, vme, broyden_beta
     USE pwcom,         ONLY : ef 
     USE elph2,         ONLY : ibndmax, ibndmin, etf, nkqf, nkf, wkf, dmef, vmef, & 
                               wf, wqf, xkf, epf17, nqtotf, nkqtotf, inv_tau_all, xqf, & 
@@ -602,7 +602,9 @@
       IF ( ABS(efcb(1)) > eps6 ) CALL mp_sum( F_currentcb, inter_pool_comm )
       !
       ! The next Fi is equal to the current Fi+1 F_current. 
-      Fi_all = F_current
+      ! Addition of possible linear mixing
+      Fi_all = (1.0 - broyden_beta ) * Fi_all + broyden_beta * F_current
+      ! 
       F_current = zero
       ! 
       IF ( ABS(efcb(1)) > eps6 ) THEN
