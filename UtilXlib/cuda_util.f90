f6ea23b0a3443fe9bd729a91717d9d8dcc19eecf
Pietro Bonfa <p.bonfa@nospam.com>
2018-11-09 19:44:28 +0100

    New cuda_util

--- UtilXlib/cuda_util.f90
+++ UtilXlib/cuda_util.f90
@@ -8,295 +8,487 @@
 ! Utility functions to perform memcpy and memset on the device with CUDA Fortran
 ! cuf_memXXX contains a CUF KERNEL to perform the selected operation
 !
-SUBROUTINE cuf_memcpy_r1d(array, array_in, l)
+MODULE cuda_util
   !
   USE util_param,   ONLY : DP
   !
   IMPLICIT NONE
   !
-  REAL(DP), INTENT(OUT) :: array(l)
-  REAL(DP), INTENT(IN) :: array_in(l)
-  INTEGER, INTENT(IN) :: l
-#if defined(__CUDA)
-  attributes(DEVICE) :: array, array_in
-#endif
+  PUBLIC :: cuf_memcpy, cuf_memset
   !
-  INTEGER :: i
+  INTERFACE cuf_memcpy
+    MODULE PROCEDURE &
+      cuf_memcpy_r1d, &
+      cuf_memcpy_r2d, &
+      cuf_memcpy_r3d, &
+      cuf_memcpy_c1d, &
+      cuf_memcpy_c2d, &
+      cuf_memcpy_c3d
+  END INTERFACE
   !
-  !$cuf kernel do
-  DO i=1, l
-     array(i) = array_in(i)
-  ENDDO
+  INTERFACE cuf_memset
+    MODULE PROCEDURE &
+      cuf_memset_r1d, &
+      cuf_memset_r2d, &
+      cuf_memset_r3d, &
+      cuf_memset_c1d, &
+      cuf_memset_c2d, &
+      cuf_memset_c3d
+  END INTERFACE
   !
-END SUBROUTINE cuf_memcpy_r1d
+  CONTAINS
+  !
+  SUBROUTINE cuf_memcpy_r1d(array_out, array_in, range1 )
+    !
+    IMPLICIT NONE
+    !
+    REAL(DP), INTENT(OUT) :: array_out(:)
+    REAL(DP), INTENT(IN) :: array_in(:)
+    INTEGER, INTENT(IN) ::  range1(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out, array_in
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    !
+    !$cuf kernel do(1)
+    DO i1 = d1s, d1e
+       array_out(i1 ) = array_in(i1 )
+    ENDDO
+    !
+  END SUBROUTINE cuf_memcpy_r1d
+  SUBROUTINE cuf_memcpy_r2d(array_out, array_in, range1, range2 )
+    !
+    IMPLICIT NONE
+    !
+    REAL(DP), INTENT(OUT) :: array_out(:,:)
+    REAL(DP), INTENT(IN) :: array_in(:,:)
+    INTEGER, INTENT(IN) ::  range1(2), range2(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out, array_in
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    !
+    !$cuf kernel do(2)
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2 ) = array_in(i1,i2 )
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memcpy_r2d
+  SUBROUTINE cuf_memcpy_r3d(array_out, array_in, range1, range2, range3 )
+    !
+    IMPLICIT NONE
+    !
+    REAL(DP), INTENT(OUT) :: array_out(:,:,:)
+    REAL(DP), INTENT(IN) :: array_in(:,:,:)
+    INTEGER, INTENT(IN) ::  range1(2), range2(2), range3(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out, array_in
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    INTEGER :: i3, d3s, d3e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    d3s = range3(1)
+    d3e = range3(2)
+    !
+    !$cuf kernel do(3)
+    DO i3 = d3s, d3e
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2,i3 ) = array_in(i1,i2,i3 )
+    ENDDO
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memcpy_r3d
+  SUBROUTINE cuf_memcpy_c1d(array_out, array_in, range1 )
+    !
+    IMPLICIT NONE
+    !
+    COMPLEX(DP), INTENT(OUT) :: array_out(:)
+    COMPLEX(DP), INTENT(IN) :: array_in(:)
+    INTEGER, INTENT(IN) ::  range1(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out, array_in
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    !
+    !$cuf kernel do(1)
+    DO i1 = d1s, d1e
+       array_out(i1 ) = array_in(i1 )
+    ENDDO
+    !
+  END SUBROUTINE cuf_memcpy_c1d
+  SUBROUTINE cuf_memcpy_c2d(array_out, array_in, range1, range2 )
+    !
+    IMPLICIT NONE
+    !
+    COMPLEX(DP), INTENT(OUT) :: array_out(:,:)
+    COMPLEX(DP), INTENT(IN) :: array_in(:,:)
+    INTEGER, INTENT(IN) ::  range1(2), range2(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out, array_in
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    !
+    !$cuf kernel do(2)
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2 ) = array_in(i1,i2 )
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memcpy_c2d
+  SUBROUTINE cuf_memcpy_c3d(array_out, array_in, range1, range2, range3 )
+    !
+    IMPLICIT NONE
+    !
+    COMPLEX(DP), INTENT(OUT) :: array_out(:,:,:)
+    COMPLEX(DP), INTENT(IN) :: array_in(:,:,:)
+    INTEGER, INTENT(IN) ::  range1(2), range2(2), range3(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out, array_in
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    INTEGER :: i3, d3s, d3e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    d3s = range3(1)
+    d3e = range3(2)
+    !
+    !$cuf kernel do(3)
+    DO i3 = d3s, d3e
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2,i3 ) = array_in(i1,i2,i3 )
+    ENDDO
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memcpy_c3d
+  !
+  SUBROUTINE cuf_memset_r1d(array_out, val, range1 )
+    !
+    IMPLICIT NONE
+    !
+    REAL(DP), INTENT(OUT) :: array_out(:)
+    REAL(DP), INTENT(IN) :: val
+    INTEGER, INTENT(IN) ::  range1(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    !
+    !$cuf kernel do(1)
+    DO i1 = d1s, d1e
+       array_out(i1 ) = val
+    ENDDO
+    !
+  END SUBROUTINE cuf_memset_r1d
+  SUBROUTINE cuf_memset_r2d(array_out, val, range1, range2 )
+    !
+    IMPLICIT NONE
+    !
+    REAL(DP), INTENT(OUT) :: array_out(:,:)
+    REAL(DP), INTENT(IN) :: val
+    INTEGER, INTENT(IN) ::  range1(2), range2(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    !
+    !$cuf kernel do(2)
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2 ) = val
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memset_r2d
+  SUBROUTINE cuf_memset_r3d(array_out, val, range1, range2, range3 )
+    !
+    IMPLICIT NONE
+    !
+    REAL(DP), INTENT(OUT) :: array_out(:,:,:)
+    REAL(DP), INTENT(IN) :: val
+    INTEGER, INTENT(IN) ::  range1(2), range2(2), range3(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    INTEGER :: i3, d3s, d3e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    d3s = range3(1)
+    d3e = range3(2)
+    !
+    !$cuf kernel do(3)
+    DO i3 = d3s, d3e
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2,i3 ) = val
+    ENDDO
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memset_r3d
+  SUBROUTINE cuf_memset_c1d(array_out, val, range1 )
+    !
+    IMPLICIT NONE
+    !
+    COMPLEX(DP), INTENT(OUT) :: array_out(:)
+    COMPLEX(DP), INTENT(IN) :: val
+    INTEGER, INTENT(IN) ::  range1(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    !
+    !$cuf kernel do(1)
+    DO i1 = d1s, d1e
+       array_out(i1 ) = val
+    ENDDO
+    !
+  END SUBROUTINE cuf_memset_c1d
+  SUBROUTINE cuf_memset_c2d(array_out, val, range1, range2 )
+    !
+    IMPLICIT NONE
+    !
+    COMPLEX(DP), INTENT(OUT) :: array_out(:,:)
+    COMPLEX(DP), INTENT(IN) :: val
+    INTEGER, INTENT(IN) ::  range1(2), range2(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    !
+    !$cuf kernel do(2)
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2 ) = val
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memset_c2d
+  SUBROUTINE cuf_memset_c3d(array_out, val, range1, range2, range3 )
+    !
+    IMPLICIT NONE
+    !
+    COMPLEX(DP), INTENT(OUT) :: array_out(:,:,:)
+    COMPLEX(DP), INTENT(IN) :: val
+    INTEGER, INTENT(IN) ::  range1(2), range2(2), range3(2)
+    !
+  #if defined(__CUDA)
+    attributes(DEVICE) :: array_out
+  #endif
+    !
+    INTEGER :: i1, d1s, d1e
+    INTEGER :: i2, d2s, d2e
+    INTEGER :: i3, d3s, d3e
+    !
+    d1s = range1(1)
+    d1e = range1(2)
+    d2s = range2(1)
+    d2e = range2(2)
+    d3s = range3(1)
+    d3e = range3(2)
+    !
+    !$cuf kernel do(3)
+    DO i3 = d3s, d3e
+    DO i2 = d2s, d2e
+    DO i1 = d1s, d1e
+       array_out(i1,i2,i3 ) = val
+    ENDDO
+    ENDDO
+    ENDDO
+    !
+  END SUBROUTINE cuf_memset_c3d
+END MODULE cuda_util
 !
-SUBROUTINE cuf_memcpy_r2d(array, array_in, l, m)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  REAL(DP), INTENT(OUT) :: array( l, m )
-  REAL(DP), INTENT(IN) :: array_in( l, m )
-  INTEGER, INTENT(IN) :: l, m
-#if defined(__CUDA)
-  attributes(DEVICE) :: array, array_in
-#endif
-
-  !
-  INTEGER :: i, j
-  !
-  !$cuf kernel do (2)
-  DO j=1, m
-     DO i=1, l
-        array(i, j) = array_in(i, j)
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memcpy_r2d
 !
-SUBROUTINE cuf_memcpy_r3d(array, array_in, l, m, n)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  REAL(DP), INTENT(OUT) :: array( l, m , n )
-  REAL(DP), INTENT(IN) :: array_in( l, m , n )
-  INTEGER, INTENT(IN) :: l, m, n
-#if defined(__CUDA)
-  attributes(DEVICE) :: array, array_in
-#endif
-  !
-  INTEGER :: i, j, k
-  !
-  !$cuf kernel do(3)
-  DO k=1, n
-     DO j=1, m
-        DO i=1, l
-           array(i, j, k) = array_in(i, j, k)
-        ENDDO
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memcpy_r3d
+! === TEMPLATE USED TO GENERATE THIS FILE ===
 !
-SUBROUTINE cuf_memset_r1d(array, val, l)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  REAL(DP), INTENT(OUT) :: array(l)
-  REAL(DP), INTENT(IN) :: val
-  INTEGER, INTENT(IN) :: l
-#if defined(__CUDA)
-  attributes(DEVICE) :: array
-#endif
-  !
-  INTEGER :: i
-  !
-  !$cuf kernel do
-  DO i=1, l
-     array(i) = val
-  ENDDO
-  !
-END SUBROUTINE cuf_memset_r1d
+!    MODULE cuda_util
+!      !
+!      USE util_param,   ONLY : DP
+!      !
+!      IMPLICIT NONE
+!      !
+!      PUBLIC :: cuf_memcpy, cuf_memset
+!      !
+!      INTERFACE cuf_memcpy
+!        MODULE PROCEDURE &
+!          {%- for t in types %}
+!          {%- for d in range(dimensions) %}
+!          cuf_memcpy_{{t[0]|lower}}{{d+1}}d{% if not loop.last %}, &{%- endif %}
+!          {%- endfor %}{% if not loop.last %}, &{%- endif %}
+!          {%- endfor %}
+!      END INTERFACE
+!      !
+!      INTERFACE cuf_memset
+!        MODULE PROCEDURE &
+!          {%- for t in types %}
+!          {%- for d in range(dimensions) %}
+!          cuf_memset_{{t[0]|lower}}{{d+1}}d{% if not loop.last %}, &{%- endif %}
+!          {%- endfor %}{% if not loop.last %}, &{%- endif %}
+!          {%- endfor %}
+!      END INTERFACE
+!      !
+!      CONTAINS
+!      !
+!    {%- for t in types %}
+!    {%- for d in range(1,dimensions+1) %}
+!      SUBROUTINE cuf_memcpy_{{t[0]|lower}}{{d}}d(array_out, array_in,{% for dd in range(d) %} {{ "range%s"|format(dd+1) }}{% if not loop.last %}, {%- endif %}{% endfor %} )
+!        !
+!        IMPLICIT NONE
+!        !
+!        {{t}}(DP), INTENT(OUT) :: array_out({% for dd in range(d) %}:{% if not loop.last %}, {%- endif %}{% endfor %})
+!        {{t}}(DP), INTENT(IN) :: array_in({% for dd in range(d) %}:{% if not loop.last %}, {%- endif %}{% endfor %})
+!        INTEGER, INTENT(IN) :: {% for dd in range(d) %} {{ "range%s(2)"|format(dd+1) }}{% if not loop.last %}, {%- endif %}{% endfor %}
+!        !
+!      #if defined(__CUDA)
+!        attributes(DEVICE) :: array_out, array_in
+!      #endif
+!        !
+!    {%- for dd in range(d) %}
+!        INTEGER :: i{{dd+1}}, d{{dd+1}}s, d{{dd+1}}e
+!    {%- endfor %}
+!        !
+!    {%- for dd in range(d) %}
+!        d{{dd+1}}s = range{{dd+1}}(1)
+!        d{{dd+1}}e = range{{dd+1}}(2)
+!    {%- endfor %}
+!        !
+!        !$cuf kernel do({{d}})
+!    {%- for dd in range(d,0,-1) %}
+!        DO i{{dd}} = d{{dd}}s, d{{dd}}e
+!    {%- endfor %}
+!           array_out( {%- for dd in range(d) %}i{{dd+1}}{% if not loop.last %}, {%- endif %} {%- endfor %} ) = array_in( {%- for dd in range(d) %}i{{dd+1}}{% if not loop.last %}, {%- endif %} {%- endfor %} )
+!    {%- for dd in range(d) %}
+!        ENDDO
+!    {%- endfor %}
+!        !
+!      END SUBROUTINE cuf_memcpy_{{t[0]|lower}}{{d}}d
+!    {%- endfor %}
+!    {%- endfor %}
+!      !
+!    {%- for t in types %}
+!    {%- for d in range(1,dimensions+1) %}
+!      SUBROUTINE cuf_memset_{{t[0]|lower}}{{d}}d(array_out, val,{% for dd in range(d) %} {{ "range%s"|format(dd+1) }}{% if not loop.last %}, {%- endif %}{% endfor %} )
+!        !
+!        IMPLICIT NONE
+!        !
+!        {{t}}(DP), INTENT(OUT) :: array_out({% for dd in range(d) %}:{% if not loop.last %}, {%- endif %}{% endfor %})
+!        {{t}}(DP), INTENT(IN) :: val
+!        INTEGER, INTENT(IN) :: {% for dd in range(d) %} {{ "range%s(2)"|format(dd+1) }}{% if not loop.last %}, {%- endif %}{% endfor %}
+!        !
+!      #if defined(__CUDA)
+!        attributes(DEVICE) :: array_out
+!      #endif
+!        !
+!    {%- for dd in range(d) %}
+!        INTEGER :: i{{dd+1}}, d{{dd+1}}s, d{{dd+1}}e
+!    {%- endfor %}
+!        !
+!    {%- for dd in range(d) %}
+!        d{{dd+1}}s = range{{dd+1}}(1)
+!        d{{dd+1}}e = range{{dd+1}}(2)
+!    {%- endfor %}
+!        !
+!        !$cuf kernel do({{d}})
+!    {%- for dd in range(d,0,-1) %}
+!        DO i{{dd}} = d{{dd}}s, d{{dd}}e
+!    {%- endfor %}
+!           array_out( {%- for dd in range(d) %}i{{dd+1}}{% if not loop.last %}, {%- endif %} {%- endfor %} ) = val
+!    {%- for dd in range(d) %}
+!        ENDDO
+!    {%- endfor %}
+!        !
+!      END SUBROUTINE cuf_memset_{{t[0]|lower}}{{d}}d
+!    {%- endfor %}
+!    {%- endfor %}
+!    END MODULE cuda_util
 !
-SUBROUTINE cuf_memset_r2d(array, val, l, m)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  REAL(DP), INTENT(OUT) :: array( l, m )
-  REAL(DP), INTENT(IN) :: val
-  INTEGER, INTENT(IN) :: l, m
-#if defined(__CUDA)
-  attributes(DEVICE) :: array
-#endif
-
-  !
-  INTEGER :: i, j
-  !
-  !$cuf kernel do (2)
-  DO j=1, m
-     DO i=1, l
-        array(i, j) = val
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memset_r2d
+! === CODE TO GENERATE THE f90 FILE ===
 !
-SUBROUTINE cuf_memset_r3d(array, val, l, m , n)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  REAL(DP), INTENT(OUT) :: array( l, m , n )
-  REAL(DP), INTENT(IN) :: val
-  INTEGER, INTENT(IN) :: l, m , n
-#if defined(__CUDA)
-  attributes(DEVICE) :: array
-#endif
-  !
-  INTEGER :: i, j, k
-  !
-  !$cuf kernel do(3)
-  DO k=1, n
-     DO j=1, m
-        DO i=1, l
-           array(i, j, k) = val
-        ENDDO
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memset_r3d
+!import sys, os, jinja2
 !
-SUBROUTINE cuf_memcpy_c1d(array, array_in, l)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(OUT) :: array(l)
-  COMPLEX(DP), INTENT(IN) :: array_in(l)
-  INTEGER, INTENT(IN) :: l
-#if defined(__CUDA)
-  attributes(DEVICE) :: array, array_in
-#endif
-  !
-  INTEGER :: i
-  !
-  !$cuf kernel do
-  DO i=1, l
-     array(i) = array_in(i)
-  ENDDO
-  !
-END SUBROUTINE cuf_memcpy_c1d
-!
-SUBROUTINE cuf_memcpy_c2d(array, array_in, l, m)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(OUT) :: array( l, m )
-  COMPLEX(DP), INTENT(IN) :: array_in( l, m )
-  INTEGER, INTENT(IN) :: l, m
-#if defined(__CUDA)
-  attributes(DEVICE) :: array, array_in
-#endif
-
-  !
-  INTEGER :: i, j
-  !
-  !$cuf kernel do (2)
-  DO j=1, m
-     DO i=1, l
-        array(i, j) = array_in(i, j)
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memcpy_c2d
-!
-SUBROUTINE cuf_memcpy_c3d(array, array_in, l, m , n)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(OUT) :: array( l, m , n )
-  COMPLEX(DP), INTENT(IN) :: array_in( l, m , n )
-  INTEGER, INTENT(IN) :: l, m , n
-#if defined(__CUDA)
-  attributes(DEVICE) :: array, array_in
-#endif
-  !
-  INTEGER :: i, j, k
-  !
-  !$cuf kernel do(3)
-  DO k=1, n
-     DO j=1, m
-        DO i=1, l
-           array(i, j, k) = array_in(i, j, k)
-        ENDDO
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memcpy_c3d
-!
-SUBROUTINE cuf_memset_c1d(array, val, l)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(OUT) :: array(l)
-  COMPLEX(DP), INTENT(IN) :: val
-  INTEGER, INTENT(IN) :: l
-#if defined(__CUDA)
-  attributes(DEVICE) :: array
-#endif
-  !
-  INTEGER :: i
-  !
-  !$cuf kernel do
-  DO i=1, l
-     array(i) = val
-  ENDDO
-  !
-END SUBROUTINE cuf_memset_c1d
-!
-SUBROUTINE cuf_memset_c2d(array, val, l, m)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(OUT) :: array( l, m )
-  COMPLEX(DP), INTENT(IN) :: val
-  INTEGER, INTENT(IN) :: l, m
-#if defined(__CUDA)
-  attributes(DEVICE) :: array
-#endif
-
-  !
-  INTEGER :: i, j
-  !
-  !$cuf kernel do (2)
-  DO j=1, m
-     DO i=1, l
-        array(i, j) = val
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memset_c2d
-!
-SUBROUTINE cuf_memset_c3d(array, val, l, m , n)
-  !
-  USE util_param,   ONLY : DP
-  !
-  IMPLICIT NONE
-  !
-  COMPLEX(DP), INTENT(OUT) :: array( l, m , n )
-  COMPLEX(DP), INTENT(IN) :: val
-  INTEGER, INTENT(IN) :: l, m , n
-#if defined(__CUDA)
-  attributes(DEVICE) :: array
-#endif
-  !
-  INTEGER :: i, j, k
-  !
-  !$cuf kernel do(3)
-  DO k=1, n
-     DO j=1, m
-        DO i=1, l
-           array(i, j, k) = val
-        ENDDO
-     ENDDO
-  ENDDO
-  !
-END SUBROUTINE cuf_memset_c3d
+!def render(tpl_path, context):
+!    path, filename = os.path.split(tpl_path)
+!    return jinja2.Environment(undefined=jinja2.StrictUndefined,
+!        loader=jinja2.FileSystemLoader(path or './')
+!    ).get_template(filename).render(context)
+!with open('cuda_util.f90', 'w') as f: f.write(render('cuda_util.jf90', {'types': ['REAL', 'COMPLEX'], 'dimensions': 3}))
 !
