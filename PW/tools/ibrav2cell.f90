6811c728d4086992130ea04f69189ecc95e4fc83
Lorenzo Paulatto (naquite) <paulatz@gmail.com>
2018-11-20 16:48:00 +0100

    Added self doc

--- PW/tools/ibrav2cell.f90
+++ PW/tools/ibrav2cell.f90
@@ -18,14 +18,21 @@ PROGRAM ibrav2cell
   REAL(DP) :: at(3,3), omega, alat
   !
   NAMELIST /system/ ibrav, celldm
+  WRITE(*,*) "This code reads a &system/ namelist from standard input"
+  WRITE(*,*) "containing ibrav and celldm(6)"
   READ(*,system)
   CALL latgen( ibrav, celldm, at(:,1), at(:,2), at(:,3), omega )
   !   at=at/celldm(1)
   !   CALL recips(at(:,1), at(:,2), at(:,3), bg(:,1), bg(:,2), bg(:,3))
-  !WRITE(*,'(a)') "Unit cell (bohr):"
-  WRITE(*,'(3es24.15)') at(:,1)
-  WRITE(*,'(3es24.15)') at(:,2)
-  WRITE(*,'(3es24.15)') at(:,3)
+  WRITE(*,'(a)') "Unit cell (bohr):"
+  WRITE(*,'(3f15.8)') at(:,1)
+  WRITE(*,'(3f15.8)') at(:,3)
+  WRITE(*,'(3f15.8)') at(:,2)
+  at=at/celldm(1)
+  WRITE(*,'(a,f12.6,a)') "Unit cell in units of alat (=", celldm(1)," bohr):"
+  WRITE(*,'(3f15.8)') at(:,1)
+  WRITE(*,'(3f15.8)') at(:,2)
+  WRITE(*,'(3f15.8)') at(:,3)
   !WRITE(*,'(a,es24.15)') "Volume (bohr^3):", omega
   !
 END PROGRAM ibrav2cell
