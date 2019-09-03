a4888d32a6516841030baf8a8c34a36e74b75b25
Samuel Ponce <samuel.pon@gmail.com>
2019-09-03 17:07:05 +0100

    Place set_nb in low_lvl module

--- EPW/src/set_ndnmbr.f90
+++ EPW/src/set_ndnmbr.f90
@@ -7,62 +7,68 @@
   ! present distribution, or http://www.gnu.org/copyleft.gpl.txt .             
   !                                                                            
   !----------------------------------------------------------------
-  SUBROUTINE set_ndnmbr ( pool, proc, procp, npool, ndlab)
+  SUBROUTINE set_ndnmbr(pool, proc, procp, npool, ndlab)
   !----------------------------------------------------------------
-  !
-  !  create ndlab label from pool and proc numbers
-  !
-  !  The rule for deciding the node number is based on 
-  !  the restriction set in startup.f90 that every pool 
-  !  has the same number of procs.
-  !
+  !!
+  !!  create ndlab label from pool and proc numbers
+  !!
+  !!  The rule for deciding the node number is based on 
+  !!  the restriction set in startup.f90 that every pool 
+  !!  has the same number of procs.
+  !!
   !----------------------------------------------------------------
   IMPLICIT NONE
-  character(len=3) :: ndlab
-  INTEGER :: pool, proc, procp, node, nprocs, npool 
-  ! pool = 1,...,npool
-  ! proc = 0,...,nproc_pool-1
+  ! 
+  CHARACTER(LEN = 3), INTENT(out) :: ndlab 
+  !! Label
+  INTEGER, INTENT(in) :: pool
+  !! Pool = 1,..., npool
+  INTEGER, INTENT(in) :: proc
+  !! Processor = 0,..., nproc_pool-1
+  INTEGER, INTENT(in) :: procp
+  !! 
+  INTEGER, INTENT(in) :: npool
+  !! 
+  ! Local variables
+  INTEGER :: node
+  !! Number of nodes 
+  INTEGER :: nprocs
+  !! 
   !
   nprocs = npool * procp
   !
-  node = (pool-1)*procp + proc + 1
+  node = (pool - 1) * procp + proc + 1
   !
   ndlab = '   '
   IF (nprocs < 10) THEN
-    WRITE (ndlab(1:1),'(i1)') node
+    WRITE(ndlab(1:1), '(i1)') node
   ELSEIF (nprocs < 100 ) then
-! SP: Seems like QE change its convention to
     IF (node < 10) THEN
-!       ndlab = '0'
-!       WRITE (ndlab(2:2),'(i1)') node
-       WRITE (ndlab(1:1),'(i1)') node
+      WRITE(ndlab(1:1), '(i1)') node
     ELSE
-       WRITE (ndlab(1:2),'(i2)') node
+      WRITE(ndlab(1:2), '(i2)') node
     ENDIF
   ELSEIF (nprocs < 100) THEN
     IF (node < 10) THEN
-!       ndlab = '00'
-       WRITE (ndlab(1:1),'(i1)') node
+      WRITE(ndlab(1:1), '(i1)') node
     ELSEIF (node < 100) THEN
-!       ndlab = '0'
-       WRITE (ndlab(1:2),'(i2)') node
+      WRITE(ndlab(1:2), '(i2)') node
     ELSE
-       WRITE (ndlab(1:3),'(i3)') node
+      WRITE(ndlab(1:3), '(i3)') node
     ENDIF
   ELSE
     IF (node < 10) THEN
-!       ndlab = '000'
-       WRITE (ndlab(1:1),'(i1)') node
+      WRITE(ndlab(1:1), '(i1)') node
     ELSEIF (node < 100) THEN
-!       ndlab = '00'
-       WRITE (ndlab(1:2),'(i2)') node
+      WRITE(ndlab(1:2), '(i2)') node
     ELSEIF (node < 1000) THEN
-!       ndlab = '0'
-       WRITE (ndlab(1:3),'(i3)') node
+      WRITE(ndlab(1:3), '(i3)') node
     ELSE
-       WRITE (ndlab,'(i4)') node
+      WRITE(ndlab, '(i4)') node
     ENDIF
   ENDIF
   !
+  !----------------------------------------------------------------
   END SUBROUTINE set_ndnmbr
+  !----------------------------------------------------------------
 
