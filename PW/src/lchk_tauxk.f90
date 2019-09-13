d932bdb68839cc301c4ec2108fe17237547b688e
fabrizio22 <ferrariruffino.fz@gmail.com>
2019-09-13 10:11:21 +0200

    Ford-PW part 10

--- PW/src/lchk_tauxk.f90
+++ PW/src/lchk_tauxk.f90
@@ -7,57 +7,62 @@
 !
 !
 !-----------------------------------------------------------------------
-subroutine check_atoms (nvec, vec, trmat)
+SUBROUTINE check_atoms( nvec, vec, trmat )
   !-----------------------------------------------------------------------
-  !
-  !     This routine tests that the atomic coordinates (or k-points)
-  !     are different and not related by a lattice translation
-  !
+  !! This routine tests that the atomic coordinates (or k-points)
+  !! are different and not related by a lattice translation.
   !
   USE kinds
-  implicit none
   !
-  real(DP), parameter :: accep=1.d-5
+  IMPLICIT NONE
   !
-  integer, intent(in) :: nvec
-  ! nvec : number of atomic positions (or k-points)
-  real(DP), intent(in) :: vec (3, nvec), trmat (3, 3)
-  ! vec  : cartesian coordinates of atomic positions (or k-points)
-  ! trmat: transformation matrix to crystal axis
-  !        ( = bg , basis of the real-space lattice, for atoms
-  !          = at , basis of the rec.-space lattice, for k-points )
+  INTEGER, INTENT(IN) :: nvec
+  !! number of atomic positions (or k-points)
+  REAL(DP), INTENT(IN) :: vec(3,nvec)
+  !! cartesian coordinates of atomic positions (or k-points)
+  REAL(DP), INTENT(IN) :: trmat(3,3)
+  !! transformation matrix to crystal axis
+  ! ( = bg , basis of the real-space lattice, for atoms
+  !   = at , basis of the rec.-space lattice, for k-points )
   !
-  integer :: nv1, nv2
-  real(DP), allocatable :: vaux(:,:)
-  real(DP) :: zero (3) = 0.0_dp
-  character(len=80) :: message
-  logical, external :: eqvect
+  ! ... local variables
   !
-  !   Copy input positions and transform them to crystal units
+  INTEGER :: nv1, nv2
+  REAL(DP), ALLOCATABLE :: vaux(:,:)
+  REAL(DP) :: zero(3) = 0.0_DP
+  CHARACTER(LEN=80) :: message
+  REAL(DP), PARAMETER :: accep=1.d-5
+  LOGICAL, EXTERNAL :: eqvect
   !
-  allocate ( vaux(3,nvec) )
+  ! ... Copy input positions and transform them to crystal units
+  !
+  ALLOCATE( vaux(3,nvec) )
   vaux = vec
-  call cryst_to_cart ( nvec, vaux, trmat, -1)
   !
-  !   Test that all the atomic positions (or k-points) are different
+  CALL cryst_to_cart( nvec, vaux, trmat, -1 )
   !
-  do nv1 = 1, nvec-1
-     do nv2 = nv1+1, nvec
-        if ( eqvect ( vaux (1,nv1), vaux (1,nv2), zero, accep ) ) then
-           zero(:) = vaux (:,nv1)-vaux(:,nv2)
-           IF ( ALL ( abs(zero) < accep ) ) THEN
+  ! ... Test that all the atomic positions (or k-points) are different
+  !
+  DO nv1 = 1, nvec-1
+     DO nv2 = nv1+1, nvec
+        !
+        IF ( eqvect( vaux(1,nv1), vaux(1,nv2), zero, accep ) ) THEN
+           zero(:) = vaux(:,nv1) - vaux(:,nv2)
+           IF ( ALL ( ABS(zero) < accep ) ) THEN
               WRITE (message,'("atoms #",i4," and #",i4," overlap!")') nv1, nv2
            ELSE
               WRITE (message,'("atoms #",i4," and #",i4," differ by lattice &
                     &vector (",i2,",",i2,",",i2,") in crystal axis")') &
                     nv1, nv2, NINT(zero)
            ENDIF
-           CALL errore ( 'check_atoms', TRIM(message), 1)
-        end if
-     enddo
-  enddo
+           CALL errore( 'check_atoms', TRIM(message), 1 )
+        ENDIF
+        !
+     ENDDO
+  ENDDO
   !
-  deallocate(vaux)
-  return
-end subroutine check_atoms
-
+  DEALLOCATE( vaux )
+  !
+  RETURN
+  !
+END SUBROUTINE check_atoms
