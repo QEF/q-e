99ae7e9fcccf144f1d9dc4cdb6fca574b388fcac
Samuel Ponce <samuel.pon@gmail.com>
2019-09-25 14:40:17 +0100

    Debug EPW in the case of use_ws and restart.
    Update the polar transport test.

--- EPW/src/elphon_shuffle_wrap.f90
+++ EPW/src/elphon_shuffle_wrap.f90
@@ -917,15 +917,15 @@
   !
   ! the electron-phonon wannier interpolation
   !
-  IF(etf_mem == 0 .OR. etf_mem == 1 ) CALL ephwann_shuffle(nqc, xqc)
+  IF(etf_mem == 0 .OR. etf_mem == 1 ) CALL ephwann_shuffle(nqc, xqc, w_centers)
   IF(etf_mem == 2) THEN
 #if defined(__MPI)         
-    CALL ephwann_shuffle_mem(nqc, xqc)
+    CALL ephwann_shuffle_mem(nqc, xqc, w_centers)
 #else
     WRITE(stdout,'(/5x,a)') 'WARNING: etf_mem==2 only works with MPI'
     WRITE(stdout,'(5x,a)')  '         Changing to etf_mem == 1 and continue ...'
     etf_mem = 1
-    CALL ephwann_shuffle(nqc, xqc)
+    CALL ephwann_shuffle(nqc, xqc, w_centers)
 #endif
   ENDIF        
   DEALLOCATE(xqc, STAT = ierr)
