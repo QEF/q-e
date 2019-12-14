6f6e7b47612bc11198e2ae28dc9cdc74e5f7472c
Pietro Bonfa <bonfus@gmail.com>
2019-12-14 15:01:20 +0100

    Reorganized buffers allocation in force_corr_gpu

--- PW/src/force_corr_gpu.f90
+++ PW/src/force_corr_gpu.f90
@@ -31,38 +31,38 @@ subroutine force_corr_gpu (forcescc)
   USE control_flags,        ONLY : gamma_only
   USE mp_bands,             ONLY : intra_bgrp_comm
   USE mp,                   ONLY : mp_sum
-  USE gbuffers,             ONLY : dev_buf 
+  USE gbuffers,             ONLY : dev_buf
   USE gvect_gpum,           ONLY : g_d
   !
   USE simpsn_gpum,          ONLY : simpsn_gpu_dev
-#if defined(__CUDA) 
-  USE cudafor 
+#if defined(__CUDA)
+  USE cudafor
   USE cublas
-#endif 
+#endif
   !
   implicit none
   !
-  real(DP) :: forcescc (3, nat)  
+  real(DP) :: forcescc (3, nat)
   !
-  real(DP), pointer, contiguous ::   rhocgnt_d(:),  aux_d (:,:), r_d(:), rab_d(:), rhoat_d(:), tau_d(:,:) 
-  complex(DP),pointer      ::   psic_d(:) 
-  integer, pointer      :: nl_d (:) 
+  real(DP), pointer, contiguous ::   rhocgnt_d(:),  aux_d (:,:), r_d(:), rab_d(:), rhoat_d(:), tau_d(:,:)
+  complex(DP),pointer      ::   psic_d(:)
+  integer, pointer      :: nl_d (:)
   real(DP), allocatable :: tmp(:)
-   
+
   ! work space
-  real(DP) ::  gx, arg, fact, forcesccx, forcesccy, forcesccz 
+  real(DP) ::  gx, arg, fact, forcesccx, forcesccy, forcesccz
   ! temp factors
-  integer :: ir, isup, isdw, ig, nt, na, ndm, ierr, msh_nt, igg, glblock,igl  
+  integer :: ir, isup, isdw, ig, nt, na, ndm, ierr, msh_nt, igg, glblock,igl
 #if defined(__CUDA)
-  ATTRIBUTES(DEVICE)  :: rhocgnt_d, aux_d, psic_d,r_d, rab_d, rhoat_d, nl_d, tau_d 
+  ATTRIBUTES(DEVICE)  :: rhocgnt_d, aux_d, psic_d,r_d, rab_d, rhoat_d, nl_d, tau_d
 #endif
 
   ! counters
   !
   ! vnew is V_out - V_in, psic is the temp space
   !
-  nl_d => dfftp%nl_d 
-  CALL dev_buf%lock_buffer(psic_d, size(vnew%of_r,1), ierr ) 
+  nl_d => dfftp%nl_d
+  CALL dev_buf%lock_buffer(psic_d, size(vnew%of_r,1), ierr )
   if (nspin == 1 .or. nspin == 4) then
      psic_d(:) = vnew%of_r (:, 1)
   else
@@ -72,52 +72,53 @@ subroutine force_corr_gpu (forcescc)
   end if
   !
   ndm = MAXVAL ( msh(1:ntyp) )
-  call dev_buf%lock_buffer ( rhocgnt_d, ngl, ierr )
-  call dev_buf%lock_buffer (r_d, ndm, ierr) 
-  call dev_buf%lock_buffer (rab_d, ndm, ierr ) 
-  call dev_buf%lock_buffer (rhoat_d, ndm, ierr) 
+  CALL dev_buf%lock_buffer ( rhocgnt_d, ngl, ierr )
+  CALL dev_buf%lock_buffer ( r_d, ndm, ierr)
+  CALL dev_buf%lock_buffer ( rab_d, ndm, ierr )
+  CALL dev_buf%lock_buffer ( rhoat_d, ndm, ierr)
+  CALL dev_buf%lock_buffer( aux_d, [ndm, ngl], ierr )
+  !
+  CALL dev_buf%lock_buffer ( tau_d, [3,nat], ierr)
+  tau_d(1:3,1:nat)=tau(1:3,1:nat)
+
   CALL fwfft ('Rho', psic_d, dfftp)
   if (gamma_only) then
      fact = 2.d0
   else
      fact = 1.d0
   end if
-  CALL dev_buf%lock_buffer(aux_d, [ndm, ngl], ierr )
   !
   do nt = 1, ntyp
      !
      ! Here we compute the G.ne.0 term
      !
-     msh_nt = msh(nt) 
+     msh_nt = msh(nt)
      r_d (1:msh_nt) =  rgrid(nt)%r(1:msh_nt)
-     rab_d(1:msh_nt) = rgrid(nt)%rab(1:msh_nt) 
+     rab_d(1:msh_nt) = rgrid(nt)%rab(1:msh_nt)
      rhoat_d (1:msh_nt) =  upf(nt)%rho_at (1:msh_nt)
      !
-     !$cuf kernel do(1) 
-       do ig = 1, ngl  
-          gx = sqrt (gl_d (ig) ) * tpiba 
-          do ir = 1, msh_nt    
-             if (r_d(ir) .lt.1.0d-8) then
-                aux_d (ir,ig) = rhoat_d (ir)
-             else
-                aux_d (ir,ig) = rhoat_d (ir) * sin(gx*r_d(ir)) / (r_d(ir)*gx)
-             endif
-          enddo
-           
-          call simpsn_gpu_dev(msh_nt, aux_d(:,ig), rab_d, rhocgnt_d(ig) )
-               
-       enddo
-      
+     !$cuf kernel do(1)
+     do ig = 1, ngl
+        gx = sqrt (gl_d (ig) ) * tpiba
+        do ir = 1, msh_nt
+           if (r_d(ir) .lt.1.0d-8) then
+              aux_d (ir,ig) = rhoat_d (ir)
+           else
+              aux_d (ir,ig) = rhoat_d (ir) * sin(gx*r_d(ir)) / (r_d(ir)*gx)
+           endif
+        enddo
+
+        call simpsn_gpu_dev(msh_nt, aux_d(:,ig), rab_d, rhocgnt_d(ig) )
+
+     enddo
      !
-     call dev_buf%lock_buffer(tau_d, [3,nat], ierr) 
-     tau_d(1:3,1:nat)=tau(1:3,1:nat)  
      do na = 1, nat
         if (nt.eq.ityp (na) ) then
            forcescc (1:3, na) = 0.0_DP
            forcesccx = 0.0d0
-           forcesccy = 0.0d0 
-           forcesccz = 0.0d0 
-           !$cuf kernel do(1)  
+           forcesccy = 0.0d0
+           forcesccz = 0.0d0
+           !$cuf kernel do(1)
            do ig = gstart, ngm
               arg = (g_d(1, ig) * tau_d (1, na) + g_d (2, ig) * tau_d (2, na) &
                    + g_d (3, ig) * tau_d (3, na) ) * tpi
@@ -132,17 +133,19 @@ subroutine force_corr_gpu (forcescc)
                       g_d(3,ig) * tpiba * CONJG(psic_d(nl_d(ig))))
            enddo
            forcescc(1, na) = forcesccx
-           forcescc(2, na) = forcesccy 
-           forcescc(3, na) = forcesccz  
+           forcescc(2, na) = forcesccy
+           forcescc(3, na) = forcesccz
         endif
      enddo
-     call dev_buf%release_buffer ( tau_d, ierr )
   enddo
-  call dev_buf%release_buffer ( aux_d, ierr )
-  call dev_buf%release_buffer ( psic_d, ierr )
+  !
   call dev_buf%release_buffer ( r_d, ierr )
   call dev_buf%release_buffer ( rab_d, ierr )
   call dev_buf%release_buffer ( rhoat_d, ierr )
+  call dev_buf%release_buffer ( aux_d, ierr )
+  !
+  call dev_buf%release_buffer ( tau_d, ierr )
+  call dev_buf%release_buffer ( psic_d, ierr )
   call dev_buf%release_buffer ( rhocgnt_d, ierr )
   !
   call mp_sum(  forcescc, intra_bgrp_comm )
