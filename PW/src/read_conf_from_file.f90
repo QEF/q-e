3460c35ff65f6c7c5ef9af0e20ed82e5fd522cc5
Paolo Giannozzi <p.giannozzi@gmail.com>
2020-01-02 22:10:48 +0100

    More cleanup

--- PW/src/read_conf_from_file.f90
+++ PW/src/read_conf_from_file.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2001-2008 Quantum ESPRESSO group
+! Copyright (C) 2001-2020 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -13,13 +13,11 @@ SUBROUTINE read_conf_from_file( stop_on_error, nat, nsp, tau, at )
   USE kinds,           ONLY : DP
   USE constants,       ONLY : eps8
   USE io_global,       ONLY : stdout, ionode, ionode_id
-  USE io_files,        ONLY : restart_dir, xmlfile, &
-                              psfile, pseudo_dir, pseudo_dir_cur
+  USE io_files,        ONLY : restart_dir, xmlfile
   USE mp,              ONLY : mp_bcast
   USE mp_images,       ONLY : intra_image_comm
   USE qexsd_module,    ONLY : qexsd_readschema
-  USE qexsd_copy,      ONLY : qexsd_copy_atomic_species, &
-                              qexsd_copy_atomic_structure
+  USE qexsd_copy,      ONLY : qexsd_copy_atomic_structure
   USE qes_types_module,ONLY : output_type
   USE qes_libs_module, ONLY : qes_reset
   USE qes_bcast_module,ONLY : qes_bcast
@@ -32,20 +30,18 @@ SUBROUTINE read_conf_from_file( stop_on_error, nat, nsp, tau, at )
   REAL(DP),INTENT(out)   :: at(3,3)
   REAL(DP),INTENT(inout) :: tau(3,nat)
   !
+  ! ... local variables
+  !
   TYPE ( output_type) :: output_obj
   !
-  INTEGER :: ierr  
-  REAL(dp) :: alat_
-  !
-  CHARACTER (LEN=3) :: atm_(nsp)
+  INTEGER :: ierr, nat_, ibrav_
   INTEGER, ALLOCATABLE :: ityp_(:)
+  REAL(dp) :: alat_
   REAL(dp), ALLOCATABLE :: tau_(:,:)
-  REAL(dp):: amass_(nsp)
-  INTEGER :: nat_, ibrav_
+  CHARACTER (LEN=3) :: atm_(nsp)
   !
-  pseudo_dir_cur = restart_dir () 
   WRITE( stdout, '(/5X,"Atomic positions and unit cell read from directory:", &
-                &  /,5X,A)') pseudo_dir_cur
+                &  /,5X,A)') restart_dir()
   !
   ! ... check if restart file is present, if so read config parameters
   !
@@ -54,14 +50,14 @@ SUBROUTINE read_conf_from_file( stop_on_error, nat, nsp, tau, at )
   IF ( ierr > 0 .OR. (ierr < 0 .AND. stop_on_error) ) &
        CALL errore ( 'read_conf_from_file', &
        'fatal error reading xml file', ABS(ierr) ) 
-  CALL qes_bcast(output_obj, ionode_id, intra_image_comm)
-  !
-  IF (ierr == 0 ) THEN
-     ! FIXME: what to do with the following data?
-     ! CALL qexsd_copy_atomic_species ( output_obj%atomic_species, &
-     !      nsp, atm, amass, PSFILE=psfile, PSEUDO_DIR=pseudo_dir )
-     !IF ( pseudo_dir == ' ' ) pseudo_dir=pseudo_dir_cur
+  IF (ierr < 0 ) THEN
      !
+     WRITE( stdout, '(5X,"Nothing found: ", &
+                       & "using input atomic positions and unit cell",/)' )
+     !
+  ELSE
+     !
+     CALL qes_bcast(output_obj, ionode_id, intra_image_comm)
      CALL qexsd_copy_atomic_structure (output_obj%atomic_structure, nsp, &
           atm_, nat_, tau_, ityp_, alat_, at(:,1), at(:,2), at(:,3), ibrav_ )
      CALL qes_reset (output_obj)
@@ -73,16 +69,9 @@ SUBROUTINE read_conf_from_file( stop_on_error, nat, nsp, tau, at )
         tau(:,1:nat) = tau_(:,1:nat)
      END IF
      DEALLOCATE ( tau_, ityp_ )
-     !
-  ELSE
-     !
-     WRITE( stdout, '(5X,"Nothing found: ", &
-                       & "using input atomic positions and unit cell",/)' )
-     RETURN
+     WRITE( stdout, * )
      !
   END IF
-  !
-  WRITE( stdout, * )
   !
   RETURN
   !
