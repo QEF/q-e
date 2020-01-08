7d9a120026aa7e557a94fb2799c4cb590bccf972
Federico Ficarelli <federico.ficarelli@gmail.com>
2020-01-08 18:35:02 +0100

    Import vendored fox as qe::fox target

--- external/external.cmake
+++ external/external.cmake
@@ -26,3 +26,45 @@ else(LAPACK_FOUND)
         endif(QE_ENABLE_VENDOR_DEPS)
     endif(TARGET QE::LAPACK)
 endif(LAPACK_FOUND)
+
+###########################################################
+# QE::FOX
+###########################################################
+find_package(FoX QUIET)
+if(FoX_FOUND)
+    add_library(qe_fox INTERFACE)
+    add_library(QE::FOX ALIAS qe_fox)
+    target_link_libraries(qe_fox INTERFACE ${FoX_LIBRARIES})
+else(FoX_FOUND)
+    if(TARGET QE::FOX)
+        message(STATUS "Using inherited QE::FOX target")
+    else(TARGET QE::FOX)
+        if(QE_ENABLE_VENDOR_DEPS)
+            message(STATUS "Installing QE::FOX via submodule")
+            execute_process(COMMAND git submodule update --init -- external/fox
+                            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+            add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/fox EXCLUDE_FROM_ALL)
+            add_library(qe_fox INTERFACE)
+            add_library(QE::FOX ALIAS qe_fox)
+            target_link_libraries(qe_fox
+                INTERFACE
+                    FoX_fsys
+                    FoX_utils
+                    FoX_common
+                    FoX_dom
+                    FoX_sax
+                    FoX_wxml)
+            target_include_directories(qe_fox
+                INTERFACE
+                    $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/external/fox/modules>)
+                    # TODO fix FoX module dir
+                    # INTERFACE
+                    #     $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/qe>)
+        else(QE_ENABLE_VENDOR_DEPS)
+            # No dep has been found via find_package,
+            # call it again with REQUIRED to make it fail
+            # explicitly (hoping in some helpful message)
+            find_package(FoX REQUIRED)
+        endif(QE_ENABLE_VENDOR_DEPS)
+    endif(TARGET QE::FOX)
+endif(FoX_FOUND)
