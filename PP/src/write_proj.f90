17f691924e1bfd4223859142b8254c71d5e2144f
Paolo Giannozzi <p.giannozzi@gmail.com>
2020-01-19 21:48:59 +0100

    WIP: removal of iotk from PP (xml file containing the projections) Still missing: reading

--- PP/src/write_proj.f90
+++ PP/src/write_proj.f90
@@ -6,171 +6,109 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !-----------------------------------------------------------------------
-SUBROUTINE  write_proj_iotk (filename, lbinary, projs, lwrite_ovp, ovps )
+SUBROUTINE  write_xml_proj (filename, projs, lwrite_ovp, ovps )
   !-----------------------------------------------------------------------
   !
   USE kinds
-  USE io_files,         ONLY : iun => iunsat, prefix, tmp_dir, postfix
+  USE io_files,         ONLY : iunpun, restart_dir
   USE basis,            ONLY : natomwfc
   USE klist,            ONLY : wk, xk, nkstot, nelec
-  USE noncollin_module, ONLY : noncolin
+  USE noncollin_module, ONLY : noncolin, nspin_lsda
   USE lsda_mod,         ONLY : nspin, isk
   USE ener,             ONLY : ef
   USE wvfct,            ONLY : et, nbnd
-  USE iotk_module
+  USE FoX_wxml,         ONLY : xmlf_t, xml_openfile, xml_close, &
+       xml_newElement, xml_addAttribute, xml_addCharacters, xml_endElement
   IMPLICIT NONE
 
   CHARACTER(*),  INTENT(IN) :: filename
-  LOGICAL,       INTENT(IN) :: lbinary
   COMPLEX(DP),   INTENT(IN) :: projs(natomwfc,nbnd,nkstot)
   LOGICAL,       INTENT(IN) :: lwrite_ovp
   COMPLEX(DP),   INTENT(IN) :: ovps(natomwfc,natomwfc,nkstot)
   !
-  CHARACTER(256)          :: tmp
-  CHARACTER(iotk_attlenx) :: attr
-  INTEGER :: ik, ik_eff, isp, ia, ierr, num_k_points
-
-!
-! subroutine body
-!
-
-  tmp = trim( tmp_dir ) // trim( prefix ) // postfix //trim(filename)
+  INTEGER :: ik, ik_eff, is, ia, ierr, num_k_points
+  TYPE(xmlf_t) :: xf
   !
-  IF ( lbinary ) THEN
-      tmp = TRIM(tmp) // ".dat"
-  ELSE
-      tmp = TRIM(tmp) // ".xml"
-  ENDIF
   !
-  CALL iotk_open_write(iun, FILE=trim(tmp), ROOT="ATOMIC_PROJECTIONS", &
-                       BINARY=lbinary, IERR=ierr )
+  CALL xml_openfile(FILENAME = TRIM( restart_dir() )//TRIM(filename), XF = xf, &
+       UNIT = iunpun, PRETTY_PRINT = .TRUE., REPLACE  = .TRUE., &
+       NAMESPACE = .FALSE., IOSTAT = ierr ) 
+  !
   IF ( ierr /= 0 ) RETURN
   !
+  num_k_points = nkstot / nspin_lsda
   !
-  num_k_points = nkstot
-  IF ( nspin == 2 ) num_k_points = nkstot / 2
+  ! <PROJECTIONS>
   !
-  CALL iotk_write_begin(iun, "HEADER")
+  CALL xml_newElement (xf, "PROJECTIONS")
   !
-  CALL iotk_write_dat(iun, "NUMBER_OF_BANDS", nbnd)
-  CALL iotk_write_dat(iun, "NUMBER_OF_K-POINTS", num_k_points )
-  CALL iotk_write_dat(iun, "NUMBER_OF_SPIN_COMPONENTS", nspin)
-  CALL iotk_write_dat(iun, "NON-COLINEAR_CALCULATION",noncolin)
-  CALL iotk_write_dat(iun, "NUMBER_OF_ATOMIC_WFC", natomwfc)
-  CALL iotk_write_dat(iun, "NUMBER_OF_ELECTRONS", nelec )
-  CALL iotk_write_attr(attr, "UNITS", " 2 pi / a", FIRST=.true.  )
-  CALL iotk_write_empty (iun,  "UNITS_FOR_K-POINTS", ATTR=attr)
-  CALL iotk_write_attr(attr, "UNITS", "Rydberg", FIRST=.true.  )
-  CALL iotk_write_empty (iun,  "UNITS_FOR_ENERGY", ATTR=attr)
-  CALL iotk_write_dat(iun, "FERMI_ENERGY", ef )
+  ! <HEADER>
   !
-  CALL iotk_write_end(iun, "HEADER")
+  CALL xml_newElement (xf, "HEADER")
+  CALL xml_addAttribute (xf, "NUMBER_OF_BANDS", nbnd)
+  CALL xml_addAttribute (xf, "NUMBER_OF_K-POINTS", num_k_points )
+  CALL xml_addAttribute (xf, "NUMBER_OF_SPIN_COMPONENTS", nspin)
+  CALL xml_addAttribute (xf, "NON-COLINEAR_CALCULATION",noncolin)
+  CALL xml_addAttribute (xf, "NUMBER_OF_ATOMIC_WFC", natomwfc)
+  CALL xml_addAttribute (xf, "NUMBER_OF_ELECTRONS", nelec )
+  CALL xml_addAttribute (xf, "FERMI_ENERGY", ef )
+  CALL xml_endElement (xf, "HEADER")
   !
+  ! </HEADER>
+  ! <EIGENSTATES>
   !
-  CALL iotk_write_dat(iun, "K-POINTS", xk(:,1:num_k_points) , COLUMNS=3 )
-  CALL iotk_write_dat(iun, "WEIGHT_OF_K-POINTS", wk(1:num_k_points), COLUMNS=8 )
-  !
-  CALL iotk_write_begin(iun, "EIGENVALUES")
-  !
-  DO ik=1,num_k_points
-     !
-     CALL iotk_write_begin( iun, "K-POINT"//trim(iotk_index(ik)) )
-     !
-     IF ( nspin == 2 ) THEN
+  CALL xml_newElement (xf, "EIGENSTATES")
+  DO ik = 1, num_k_points
+     DO is = 1, nspin_lsda
+        ik_eff = ik + (is-1)*num_k_points
+        CALL xml_newElement(xf, "K-POINT")
+        CALL xml_addAttribute (xf, "Weight", wk(ik_eff) )
+        CALL xml_addCharacters (xf, xk(:,ik_eff) )
+        CALL xml_endElement(xf, "K-POINT")
         !
-        ik_eff = ik + num_k_points
+        CALL xml_newElement (xf, "E")
+        CALL xml_addCharacters (xf, et(:,ik_eff) )
+        CALL xml_endElement (xf, "E" )
         !
-        CALL iotk_write_dat( iun, "EIG.1", et(:,ik) )
-        CALL iotk_write_dat( iun, "EIG.2", et(:,ik_eff) )
-        !
-     ELSE
-        !
-        CALL iotk_write_dat( iun, "EIG", et(:,ik) )
-        !
-     ENDIF
-     !
-     CALL iotk_write_end( iun, "K-POINT"//trim(iotk_index(ik)) )
-     !
-  ENDDO
-  !
-  CALL iotk_write_end(iun, "EIGENVALUES")
-
-  !
-  ! main loop atomic wfc
-  !
-  CALL iotk_write_begin(iun, "PROJECTIONS")
-  !
-  DO ik=1,num_k_points
-     !
-     CALL iotk_write_begin( iun, "K-POINT"//trim(iotk_index(ik)) )
-     !
-     IF ( nspin == 2 ) THEN
-        !
-        CALL iotk_write_begin ( iun, "SPIN.1" )
-           !
-           DO ia = 1, natomwfc
-               CALL iotk_write_dat(iun, "ATMWFC"//trim(iotk_index(ia)), projs(ia,:,ik)  )
-           ENDDO
-           !
-        CALL iotk_write_end ( iun, "SPIN.1" )
-        !
-        ik_eff = ik + num_k_points
-        !
-        CALL iotk_write_begin ( iun, "SPIN.2" )
-           !
-           DO ia = 1, natomwfc
-               CALL iotk_write_dat(iun, "ATMWFC"//trim(iotk_index(ia)), projs(ia,:,ik_eff)  )
-           ENDDO
-           !
-        CALL iotk_write_end ( iun, "SPIN.2" )
-        !
-     ELSE
-        !
-        DO ia = 1,natomwfc
-            CALL iotk_write_dat(iun, "ATMWFC"//trim(iotk_index(ia)), projs(ia,:,ik)  )
+        CALL xml_newElement (xf, "PROJS")
+        DO ia = 1, natomwfc
+           CALL xml_newElement (xf, "ATOMIC_WFC")
+           CALL xml_addAttribute (xf, "index", ia )
+           CALL xml_addAttribute (xf, "spin", is )
+           CALL xml_addCharacters (xf, projs(ia,:,ik_eff) )
+           CALL xml_endElement (xf, "ATOMIC_WFC" )
         ENDDO
+        CALL xml_endElement (xf, "PROJS" )
         !
-     ENDIF
-     !
-     CALL iotk_write_end( iun, "K-POINT"//trim(iotk_index(ik)) )
+     END DO
      !
   ENDDO
+  CALL xml_endElement (xf, "EIGENSTATES")
   !
-  CALL iotk_write_end(iun, "PROJECTIONS")
-
-  !
-  ! overlaps
-  !
+  ! </EIGENSTATE>
+  ! <OVERLAPS> if required
   IF ( lwrite_ovp ) THEN
-      !
-      CALL iotk_write_begin(iun, "OVERLAPS")
-      !
-      DO ik=1,num_k_points
-          !
-          CALL iotk_write_begin( iun, "K-POINT"//trim(iotk_index(ik)) )
-          !
-          DO isp = 1, nspin
-              !
-              ik_eff = ik + num_k_points * ( isp -1 )
-              !
-              CALL iotk_write_dat(iun, "OVERLAP"//trim(iotk_index(isp)), ovps(:,:,ik_eff)  )
-              !
-              !
-          ENDDO
-          !
-          CALL iotk_write_end( iun, "K-POINT"//trim(iotk_index(ik)) )
-          !
-      ENDDO
-      !
-      CALL iotk_write_end(iun, "OVERLAPS")
-      !
-  ENDIF
+     CALL xml_newElement (xf, "OVERLAPS" )
+     DO ik = 1, num_k_points
+        DO is = 1, nspin_lsda
+           ik_eff = ik + (is-1)*num_k_points
+           CALL xml_newElement (xf, "OVPS")
+           CALL xml_addAttribute (xf, "dim", natomwfc )
+           CALL xml_addAttribute (xf, "spin", is )
+           CALL xml_addCharacters (xf, ovps(:,:,ik_eff) )
+           CALL xml_endElement (xf, "OVPS" )
+        END DO
+     END DO
+     CALL xml_endElement (xf, "OVERLAPS" )
+  END IF
   !
-  ! closing the file
+  CALL xml_endElement (xf, "PROJECTIONS")
   !
-  CALL iotk_close_write(iun)
-
-END SUBROUTINE write_proj_iotk
+  ! </PROJECTIONS>
+  !
+  CALL xml_close ( xf)
+  !
+END SUBROUTINE write_xml_proj
 !
 !-----------------------------------------------------------------------
 SUBROUTINE write_proj_file ( filproj, proj )
