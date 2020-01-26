6f308f5658823feac41a06e22bdc28a88299c4b2
Paolo Giannozzi <p.giannozzi@gmail.com>
2020-01-26 17:38:54 +0000

    Unused

--- PP/src/compute_ppsi.f90
+++ PP/src/compute_ppsi.f90
@@ -51,8 +51,6 @@ SUBROUTINE compute_ppsi (ppsi, ppsi_us, ik, ipol, nbnd_occ, current_spin)
 
   REAL(DP), ALLOCATABLE :: dpqq(:,:,:,:)
 
-  COMPLEX(DP), EXTERNAL :: zdotc
-  !
   ALLOCATE (work ( npwx, max(nkb,1)))
   ALLOCATE (gk ( 3, npwx))
   IF (nkb > 0) THEN
