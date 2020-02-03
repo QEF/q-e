150b200af010138809c0c62f459fdb4b99f2e9cb
andreaurru247 <aurru@lucy.phys.sissa.it>
2020-02-03 20:17:37 +0100

    Minor changes on apply_trev

--- PHonon/PH/apply_trev.f90
+++ PHonon/PH/apply_trev.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2018 Andrea Dal Corso
+! Copyright (C) 2018 Andrea Dal Corso and Andrea Urru
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
