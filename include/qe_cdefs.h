11b710fc12210c49c2499522ad9d59af6a495dd6
Paolo Giannozzi <p.giannozzi@gmail.com>
2020-02-18 11:39:11 +0100

    Rename c_defs.h (used only in memstat.c) to qe_cdefs.h to prevent conflicts with other c_defs.h. Should fix issue #161 (and break cmake!)

---
+++ include/qe_cdefs.h
@@ -0,0 +1,15 @@
+/*
+Copyright (C) 2006 Quantum-ESPRESSO group
+This file is distributed under the terms of the
+GNU General Public License. See the file `License'
+in the root directory of the present distribution,
+or http://www.gnu.org/copyleft/gpl.txt .
+*/
+
+/* File c_defs.h.in is used by configure to generate c_defs.h
+   Variables that configure defines should be #undef-ined in
+   include/c_defs.h.in !!! */
+
+/* do we have the mallinfo structure (see clib/memstat.c) ? */
+
+#undef HAVE_MALLINFO
