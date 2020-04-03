c696bad359921527434947feb7456829006453e5
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2020-04-03 19:36:38 +0200

    init_us_1a splitted in pw part + new part

--- all_currents/init_us_1a.f90
+++ all_currents/init_us_1a.f90
@@ -1,4 +1,13 @@
 
+
+subroutine init_us_1all
+
+    implicit none
+    call init_us_1()
+    call init_us_1a()
+
+end subroutine
+
 ! Copyright (C) 2001-2007 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
@@ -24,7 +33,7 @@ subroutine init_us_1a
   USE mp,           ONLY : mp_sum
   use zero_mod,     ONLY : l_tab,l_zero,tabr,tabr_d2y,&
 tablocal_hg,tablocal_d2y_hg
-  
+
   USE ions_base,    ONLY : nsp
   !
   implicit none
@@ -52,59 +61,23 @@ tablocal_hg,tablocal_d2y_hg
   call start_clock ('init_us_1a')
   !    Initialization of the variables
   !
-  ndm = MAXVAL ( upf(:)%kkbeta )
-  allocate (aux ( ndm))    
-  allocate (besr( ndm))    
-  !     fill the interpolation table tab
-  !
-  pref = fpi / sqrt (omega)
-  call divide (intra_bgrp_comm, nqx, startq, lastq)
-  tab (:,:,:) = 0.d0
-  do nt = 1, ntyp
-     do nb = 1, upf(nt)%nbeta
-        l = upf(nt)%lll (nb)
-        do iq = startq, lastq
-           qi = (iq - 1) * dq
-           call sph_bes (upf(nt)%kkbeta, rgrid(nt)%r, qi, l, besr)
-           do ir = 1, upf(nt)%kkbeta
-              aux (ir) = upf(nt)%beta (ir, nb) * besr (ir) * rgrid(nt)%r(ir)
-           enddo
-           call simpson (upf(nt)%kkbeta, aux, rgrid(nt)%rab, vqint)
-           tab (iq, nb, nt) = vqint * pref
-        enddo
-     enddo
-  enddo
-
-#ifdef __MPI
-  call mp_sum(  tab, intra_bgrp_comm )
-#endif
-
-  ! initialize spline interpolation
-  if (spline_ps) then
-     allocate( xdata(nqx) )
-     do iq = 1, nqx
-        xdata(iq) = (iq - 1) * dq
-     enddo
-     do nt = 1, ntyp
-        do nb = 1, upf(nt)%nbeta 
-           d1 = (tab(2,nb,nt) - tab(1,nb,nt)) / dq
-           call spline(xdata, tab(:,nb,nt), 0.d0, d1, tab_d2y(:,nb,nt))
-        enddo
-     enddo
-     deallocate(xdata)
-  endif
 
 !!!!!!!aggiunta di codice per la zero_current
 !tabr(nqxq,nbetam,nsp,-1:1) e tablocal(nqxq,nsp,0:2)
 !contiene f_(la,lb)(q)=\int _0 ^\infty dr r^3 f_la(r) j_lb(q.r)
-!dove la è il momento angolare della beta function (ce ne possono 
+!dove la è il momento angolare della beta function (ce ne possono
 !essere più di una con il medesimo l) e
 !lb è il momento angolare della funzione di bessel
 !si può avere solo lb=la+1 lb=la-1 o lb=la
 !e questi tre casi sono rappresentati dall'ultimo indice
   if (l_zero) then
+     ndm = MAXVAL ( upf(:)%kkbeta )
+     allocate( aux (ndm) )
+     allocate (besr( ndm))
+
 !
 !inizializzazione di tabr
+! questo può essere estratto. Il resto della routine è init_us_1.f90 di PW
 !
      pref = fpi / sqrt (omega)
      call divide (intra_bgrp_comm, nqxq, startq, lastq)
@@ -136,7 +109,7 @@ tablocal_hg,tablocal_d2y_hg
                   enddo
                   call simpson (upf(nt)%kkbeta, aux, rgrid(nt)%rab, vqint)
                   tabr (iq, nb, nt,-1) = vqint * pref
-              end if   
+              end if
            enddo
         enddo
      enddo
@@ -167,7 +140,7 @@ tablocal_hg,tablocal_d2y_hg
            call sph_bes (rgrid(nt)%mesh, rgrid(nt)%r, qi, 0, besr)
            do ir = 1, rgrid(nt)%mesh
               aux (ir) = (rgrid(nt)%r(ir)*upf(nt)%vloc(ir)+2.d0*zv(nt))*&
-besr (ir) * rgrid(nt)%r(ir) 
+besr (ir) * rgrid(nt)%r(ir)
            end do
            call simpson (rgrid(nt)%mesh, aux, rgrid(nt)%rab, vqint)
            tablocal_hg(iq,nt,0) = vqint * pref
@@ -179,7 +152,7 @@ besr (ir) * rgrid(nt)%r(ir) * rgrid(nt)%r(ir)
            end do
            call simpson (rgrid(nt)%mesh, aux, rgrid(nt)%rab, vqint)
            tablocal_hg(iq,nt,1) = vqint * pref
-         end do             
+         end do
       end do
 #ifdef __MPI
       call mp_sum(  tablocal_hg, intra_bgrp_comm )
@@ -187,7 +160,7 @@ besr (ir) * rgrid(nt)%r(ir) * rgrid(nt)%r(ir)
   ! initialize spline interpolation
       if (spline_ps) then
           CALL errore( 'init_us_1a', 'splines for tablocal not implemented', 1 )
-      endif 
+      endif
   end if
   deallocate (besr)
   deallocate (aux)
