641b9b8b80d35bcfe5702b44f4c35054dd806383
fabrizio22 <ferrariruffino.fz@gmail.com>
2020-04-14 00:02:50 +0200

    stres US - gen_us_dy

--- PW/src/stres_us_gpu.f90
+++ PW/src/stres_us_gpu.f90
@@ -168,7 +168,7 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
   
   DEALLOCATE( is_multinp_d  )
   DEALLOCATE( na_d, ih_d    )
-  DEALLOCATE( ikb_d, nhnp_d ) 
+  DEALLOCATE( ikb_d, nhnp_d )
   DEALLOCATE( ishift_d, shift_d )
   DEALLOCATE( ityp_d )
   DEALLOCATE( nh_d )
@@ -212,7 +212,7 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
        
        integer :: nhmx
        complex(dp) :: becy, defy
-       !^^PROVV^^
+       !^^PROV^^
        !
        COMPLEX(DP), ALLOCATABLE :: dvkb(:,:,:)
        ! dvkb contains the derivatives of the kb potential
@@ -289,10 +289,10 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
        CALL gen_us_dj( ik, dvkb(:,:,4) )
        IF ( lmaxkb > 0 ) THEN 
          DO ipol = 1, 3
-           CALL gen_us_dy( ik, xyz(1,ipol), dvkb(:,:,ipol))
+           CALL gen_us_dy_gpu( ik, xyz(1,ipol), dvkb_d(:,:,ipol))
          ENDDO
        ENDIF
-       dvkb_d = dvkb
+       dvkb_d(:,:,4) = dvkb(:,:,4)
        !
        !
        DO icyc = 0, nproc -1
@@ -483,7 +483,7 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
        COMPLEX(DP), ALLOCATABLE, DEVICE :: becpnc_d(:,:)
        INTEGER :: nhmx !, npwt
        complex(dp) :: becy, deqy
-       !^^PROVV^^
+       !^^PROV^^
        
 #if defined(__CUDA) 
   ATTRIBUTES(DEVICE) :: ps_d, ps_nc_d
@@ -516,21 +516,15 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
        evps = 0.D0
        ! ... diagonal contribution
        !
-       !
        ALLOCATE( dvkb(npwx,nkb,4) )
-       !^^^^
-       !CALL gen_us_dj( ik, dvkb )
-       !
-       !^^^^^^^^^^^^^^^^^^^^^^
-       
        ALLOCATE( dvkb_d(npwx,nkb,4) )
        CALL gen_us_dj( ik, dvkb(:,:,4) )
        IF ( lmaxkb > 0 ) THEN 
          DO ipol = 1, 3
-           CALL gen_us_dy( ik, xyz(1,ipol), dvkb(:,:,ipol))
+           CALL gen_us_dy_gpu( ik, xyz(1,ipol), dvkb_d(:,:,ipol))
          ENDDO
        ENDIF
-       dvkb_d = dvkb
+       dvkb_d(:,:,4) = dvkb(:,:,4)
        !
        !
        IF (noncolin) THEN 
@@ -548,7 +542,7 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
           CALL dev_buf%lock_buffer ( ps_d, nkb, ierr)
           !becpk_d => becp_d%k_d
           ALLOCATE( becpk_d(nkb,nbnd) )
-          becpk_d = becp%k 
+          becpk_d = becp%k
           
           ALLOCATE( deff_d(nhm,nhm,nat) )
        END IF
@@ -559,7 +553,7 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
        CALL using_deeq(0)
        
        !
-       !^^^^^^^^^^^^^^^^^^^^^^^^ 
+       !^^^^^^^^^^^^^^^^^^^^^^^^
        nhmx = SIZE(deeq(:,1,1,1))
        ALLOCATE( deeq_d(nhmx,nhmx,nat,nspin) )
        deeq_d = deeq
@@ -568,7 +562,7 @@ SUBROUTINE stres_us_gpu( ik, gk_d, sigmanlc )
        CALL start_clock('knl_ciclo2')
        !
        CALL using_et(0) ! compute_deff : intent(in)
-       
+       !
        IF ( me_bgrp /= root_bgrp ) GO TO 100
        !
        ! ... the contribution is calculated only on one processor because
