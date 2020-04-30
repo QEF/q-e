38317c19a85cab56f05eb08e1f1c53019eb8c4c5
Samuel Ponce <samuel.pon@gmail.com>
2020-04-30 11:23:30 +0200

    Cleaning of the polaron shell implementation.

--- EPW/src/ephwann_shuffle_mem.f90
+++ EPW/src/ephwann_shuffle_mem.f90
@@ -22,7 +22,7 @@
   !-----------------------------------------------------------------------
   !
   USE kinds,         ONLY : DP, i4b, i8b
-  USE pwcom,         ONLY : nbnd, nks, nkstot, ef,  nelec
+  USE pwcom,         ONLY : nbnd, nks, nkstot, ef, nelec
   USE klist_epw,     ONLY : et_loc, xk_loc, isk_dummy
   USE cell_base,     ONLY : at, bg
   USE ions_base,     ONLY : nat, amass, ityp, tau
