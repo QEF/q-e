80e368bf304e9cc17afea29ba104987fba651539
Pietro Bonfa <bonfus@gmail.com>
2020-06-04 14:29:24 +0200

    Removed some code from cuf kernel (hopefully qe_erf is equivalent to Fortran's erf) and fixed #43.

--- PW/src/dvloc_of_g_gpu.f90
+++ PW/src/dvloc_of_g_gpu.f90
@@ -7,13 +7,13 @@
 !
 !
 #if defined(__CUDA)
-MODULE compute_dvloc_gpu_m
+MODULE compute_dvloc_gpum
   USE cudafor
   IMPLICIT NONE
   TYPE(dim3) :: dvloc_threads = dim3(32,8,1)
   PUBLIC     :: dvloc_threads
   CONTAINS
-   ATTRIBUTES( global ) SUBROUTINE compute_dvloc_gpu( mesh, n, tpiba2, omega, zp, gl, aux1, r, rab ,dvloc, do_cutoff_2D, do_comp_esm, esm_bc_l )
+  ATTRIBUTES( global ) SUBROUTINE compute_dvloc_gpu( mesh, n, tpiba2, omega, zp, gl, aux1, r, rab ,dvloc, do_cutoff_2D, do_comp_esm, esm_bc_l )
   !
   !
   USE cudafor
@@ -95,106 +95,7 @@ MODULE compute_dvloc_gpu_m
   !
 END SUBROUTINE compute_dvloc_gpu
 !
-!---------------------------------------------------------------------
-ATTRIBUTES(DEVICE) FUNCTION qe_erf_d( x )
-  !---------------------------------------------------------------------
-  !     Error function - computed from the rational approximations of
-  !     W. J. Cody, Math. Comp. 22 (1969), pages 631-637.
-  !
-  !     for abs(x) le 0.47 erf is calculated directly
-  !     for abs(x) gt 0.47 erf is calculated via erf(x)=1-erfc(x)
-  USE kinds,   ONLY: DP
-  implicit none
-  REAL(DP), INTENT(IN) :: x
-  REAL(DP) :: x2, p1 (4), q1 (4)
-  REAL(DP) :: qe_erf_d
-  DATA p1 / 2.426679552305318E2_DP, 2.197926161829415E1_DP, &
-            6.996383488619136_DP,  -3.560984370181538E-2_DP /
-  DATA q1 / 2.150588758698612E2_DP, 9.116490540451490E1_DP, &
-            1.508279763040779E1_DP, 1.000000000000000_DP /
-  !
-  IF (ABS(x) > 6.0_DP) THEN
-     !
-     !  erf(6)=1-10^(-17) cannot be distinguished from 1
-     !
-     qe_erf_d = SIGN(1.0_DP, x)
-  ELSE
-     IF (ABS(x)  <= 0.47_DP) THEN
-        x2 = x**2
-        qe_erf_d=x *(p1 (1) + x2 * (p1 (2) + x2 * (p1 (3) + x2 * p1 (4) ) ) ) &
-                / (q1 (1) + x2 * (q1 (2) + x2 * (q1 (3) + x2 * q1 (4) ) ) )
-     ELSE
-        qe_erf_d = 1.0_DP - qe_erfc_d(x)
-     ENDIF
-  ENDIF
-  !
-  RETURN
-  !
-END FUNCTION qe_erf_d
-!
-!-----------------------------------------------------------------------------
-ATTRIBUTES(DEVICE) FUNCTION qe_erfc_d( x )
-  !---------------------------------------------------------------------
-  ! erfc(x) = 1-erf(x)  - See comments in erf
-  !
-  USE kinds,   ONLY: DP
-  IMPLICIT NONE
-  !
-  REAL(DP), INTENT(IN) :: x
-  REAL(DP) :: qe_erfc_d
-  REAL(DP) :: ax, x2, xm2, p2 (8), q2 (8), p3 (5), q3 (5), pim1
-  !
-  DATA p2 / 3.004592610201616E2_DP,  4.519189537118719E2_DP, &
-            3.393208167343437E2_DP,  1.529892850469404E2_DP, &
-            4.316222722205674E1_DP,  7.211758250883094_DP,   &
-            5.641955174789740E-1_DP,-1.368648573827167E-7_DP /
-  DATA q2 / 3.004592609569833E2_DP,  7.909509253278980E2_DP, &
-            9.313540948506096E2_DP,  6.389802644656312E2_DP, &
-            2.775854447439876E2_DP,  7.700015293522947E1_DP, &
-            1.278272731962942E1_DP,  1.000000000000000_DP /
-  DATA p3 /-2.996107077035422E-3_DP,-4.947309106232507E-2_DP, &
-           -2.269565935396869E-1_DP,-2.786613086096478E-1_DP, &
-           -2.231924597341847E-2_DP /
-  DATA q3 / 1.062092305284679E-2_DP, 1.913089261078298E-1_DP, &
-            1.051675107067932_DP,    1.987332018171353_DP,    &
-            1.000000000000000_DP /
-
-  DATA pim1 / 0.56418958354775629_DP /
-  !        ( pim1= sqrt(1/pi) )
-  ax = ABS(x)
-  IF (ax > 26.0_DP) THEN
-     !
-     !  erfc(26.0)=10^(-296); erfc( 9.0)=10^(-37);
-     !
-     qe_erfc_d = 0.0_DP
-  ELSEIF (ax > 4.0_DP) THEN
-     x2 = x**2
-     xm2 = (1.0_DP / ax) **2
-     qe_erfc_d = (1.0_DP / ax) * EXP( - x2) * (pim1 + xm2 * (p3 (1) &
-          + xm2 * (p3 (2) + xm2 * (p3 (3) + xm2 * (p3 (4) + xm2 * p3 (5) &
-          ) ) ) ) / (q3 (1) + xm2 * (q3 (2) + xm2 * (q3 (3) + xm2 * &
-          (q3 (4) + xm2 * q3 (5) ) ) ) ) )
-  ELSEIF (ax > 0.47_DP) THEN
-     x2 = x**2
-     qe_erfc_d = EXP( - x2) * (p2 (1) + ax * (p2 (2) + ax * (p2 (3)   &
-          + ax * (p2 (4) + ax * (p2 (5) + ax * (p2 (6) + ax * (p2 (7) &
-          + ax * p2 (8) ) ) ) ) ) ) ) / (q2 (1) + ax * (q2 (2) + ax * &
-          (q2 (3) + ax * (q2 (4) + ax * (q2 (5) + ax * (q2 (6) + ax * &
-          (q2 (7) + ax * q2 (8) ) ) ) ) ) ) )
-  ELSE
-     qe_erfc_d = 1.0_DP - qe_erf_d(ax)
-  ENDIF
-  !
-  ! erf(-x)=-erf(x)  =>  erfc(-x) = 2-erfc(x)
-  !
-  IF (x < 0.0_DP) qe_erfc_d = 2.0_DP - qe_erfc_d
-  !
-  RETURN
-  !
-END FUNCTION qe_erfc_d
-!
-!
-END MODULE compute_dvloc_gpu_m
+END MODULE compute_dvloc_gpum
 #endif
 
 
@@ -213,7 +114,7 @@ SUBROUTINE dvloc_of_g_gpu( mesh, msh, rab_d, r_d, vloc_at_d, zp, tpiba2, &
   USE Coul_cut_2D,        ONLY : do_cutoff_2D
   USE esm,                ONLY : do_comp_esm, esm_bc
 #if defined(__CUDA)
-  USE compute_dvloc_gpu_m
+  USE compute_dvloc_gpum, ONLY : compute_dvloc_gpu
 #endif
   !
   IMPLICIT NONE
@@ -281,7 +182,7 @@ SUBROUTINE dvloc_of_g_gpu( mesh, msh, rab_d, r_d, vloc_at_d, zp, tpiba2, &
 #if defined(__CUDA)
   !$cuf kernel do(1)<<<*,*>>>
   DO i = 1, msh
-     aux1_d(i) = r_d(i) * vloc_at_d(i) + zp * e2 * qe_erf_d(r_d(i))
+     aux1_d(i) = r_d(i) * vloc_at_d(i) + zp * e2 * erf(r_d(i))
   ENDDO
   !
   esm_bc_lg = ( esm_bc == 'pbe' )
