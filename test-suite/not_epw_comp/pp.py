40cb01a6a40d4e4a1f23b511074022eb7dd82065
Eisuke Kawashima <2618741-e-kwsm@users.noreply.gitlab.com>
2020-07-09 13:47:25 +0900

    Miscellaneous updates
    - fix shebang
    - add `-f` for `rm`

--- test-suite/not_epw_comp/pp.py
+++ test-suite/not_epw_comp/pp.py
@@ -27,5 +27,5 @@ for iqpt in np.arange(1,nqpt+1):
     os.system('cp -r _ph0/'+prefix+'.phsave save/')
   else:
     os.system('cp _ph0/'+prefix+'.q_'+str(iqpt)+'/'+prefix+'.dvscf1 save/'+prefix+'.dvscf_q'+label)
-    os.system('rm _ph0/'+prefix+'.q_'+str(iqpt)+'/*wfc*' )
+    os.system('rm -f _ph0/'+prefix+'.q_'+str(iqpt)+'/*wfc*')
 
