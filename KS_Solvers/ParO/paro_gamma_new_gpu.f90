aa96512c877d8936ba43087aad596ad5391d8e80
Ivan Carnimeo <icarnime@r053c06s05.galileo.cineca.it>
2020-07-28 17:28:47 +0200

    unused host variables removed from rotate_HSpsi_gamma_gpu

--- KS_Solvers/ParO/paro_gamma_new_gpu.f90
+++ KS_Solvers/ParO/paro_gamma_new_gpu.f90
@@ -141,12 +141,17 @@ SUBROUTINE paro_gamma_new_gpu( h_psi, s_psi, hs_psi, g_1psi, overlap, &
 #if defined(__MPI)
   IF ( nproc_ortho == 1 ) THEN
 #endif
-     CALL rotate_HSpsi_gamma_gpu (  npwx, npw, nbnd, nbnd, psi, hpsi, overlap, spsi, eig )
+     CALL rotate_HSpsi_gamma_gpu (  npwx, npw, nbnd, nbnd, psi_d, hpsi_d, overlap, spsi_d, eig )
 #if defined(__MPI)
   ELSE
      CALL protate_HSpsi_gamma(  npwx, npw, nbnd, nbnd, psi, hpsi, overlap, spsi, eig )
   ENDIF
 #endif
+
+!civn 2fix
+  psi  = psi_d
+  hpsi = hpsi_d
+  spsi = spsi_d
 
   !write (6,'(10f10.4)') psi(1:5,1:3)
 
