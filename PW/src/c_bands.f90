00d02238432041a3ce97fa76c7dd59af2bffb257
Pietro Bonfa <bonfus@gmail.com>
2020-09-04 16:55:01 +0200

    evc host address was passed instead of evc_d device address in c_bands.f90. Weirdly the thing has not caused crashes or evidently wrong results ( noticed by E. DePaoli).

--- PW/src/c_bands.f90
+++ PW/src/c_bands.f90
@@ -354,7 +354,7 @@ SUBROUTINE diag_bands( iter, ik, avg_iter )
                    CALL rotate_wfc( npwx, npw, nbnd, gstart, nbnd, evc, npol, okvan, evc, et(1,ik) )
                 ELSE
                    CALL using_evc_d(1);  CALL using_et_d(1); ! et is used as intent(out), set intento=2?
-                   CALL rotate_wfc_gpu( npwx, npw, nbnd, gstart, nbnd, evc_d, npol, okvan, evc, et_d(1,ik) )
+                   CALL rotate_wfc_gpu( npwx, npw, nbnd, gstart, nbnd, evc_d, npol, okvan, evc_d, et_d(1,ik) )
                 END IF
                 !
                 avg_iter = avg_iter + 1.D0
