5aae2ef24baafff384e751ed21aa50c402bd200d
Lorenzo Paulatto (naquite) <paulatz@gmail.com>
2020-09-24 12:12:33 +0200

    bugfix

--- Modules/additional_kpoints.f90
+++ Modules/additional_kpoints.f90
@@ -61,7 +61,7 @@ MODULE additional_kpoints
      
      ! Simple case, EXX not used or used with self-exchange only: 
      IF( nqx1<=1 .and. nqx2<=1 .and. nqx3<=1 ) THEN
-       print*, "CASE ONE ============================================================", nkstot_old
+       !print*, "CASE ONE ============================================================", nkstot_old
        nkstot = nkstot_old + nkstot_add
        IF(nkstot>npk) CALL errore("add_kpoint", "Number of k-points exceeded: increase npk in pwcom", 1)
 !       ALLOCATE(xk(3,nkstot))
@@ -72,11 +72,11 @@ MODULE additional_kpoints
        wk(nkstot_old+1:nkstot_old+nkstot_add) = 0._dp
        nqtot=1
      ELSE
-       print*, "CASE TWO ============================================================"
+     !  print*, "CASE TWO ============================================================"
      ! Complex case, EXX with a finite grid of q-points. Ideally, we would want to use
      ! The grid from module EXX, but it may not have been computed at this points.
-     ! Furthermore, the q-point grid is obtained by opening the k-points one, so this would
-     ! be a dog wagging its own tails
+     ! Furthermore, the EXX q-point grid is obtained by rotating the existing k-points,
+     ! this would be a dog wagging its own tail
        nqtot = nqx1*nqx2*nqx3
        nkstot = nkstot_old + nkstot_add*nqtot
        IF(nkstot>npk) CALL errore("add_kpoint", "Number of k-points exceeded: increase npk in pwcom", 1)
@@ -87,11 +87,11 @@ MODULE additional_kpoints
        
        rq = (/nqx1,nqx2,nqx3/)
        rq = 1._dp / rq
-       iq = nqtot
+       iq = nkstot_old !nqtot
        ! We do these loops backward, in this way the path is found in the last k-points
-       DO  i = 0,nqx1-1
-       DO  j = 0,nqx2-1
-       DO  k = 0,nqx3-1
+       DO  i = nqx1-1,0,-1
+       DO  j = nqx2-1,0,-1
+       DO  k = nqx3-1,0,-1
          xq = rq*(/i,j,k/)
          CALL cryst_to_cart(1,xq,bg,+1)
          DO jq = 1, nkstot_add
@@ -105,10 +105,13 @@ MODULE additional_kpoints
    
      
      WRITE(stdout,"(5x,a)")    " --- Additional k-points: --- "
-     WRITE(stdout,"(5x,a,i6,a)")    "A request of ",nkstot_add," k-points with zero weight added to list"
-     IF(nqtot>1) WRITE(stdout,"(5x,a,i6,a)") "Furthermore,  ",nkstot_add*nqtot, " k-points where added to perform EXX calculation"
+     WRITE(stdout,"(5x,a,i6,a)")   "A list of ", nkstot_add," k-points with zero weight added to list"
+     IF(nqtot>1) WRITE(stdout,"(5x,a,i6,a)") "Furthermore,  ",nkstot_add*nqtot, &
+                                             " k-points were added to perform the EXX calculation"
 !     WRITE(stdout,"(5x,a)")         "They can be extracted with bands.x using:"
-     WRITE(stdout,"(5x,a,i6,a,i6)") "first=",nkstot_old+1,",  last=",nkstot_old+nkstot_add
+     !WRITE(stdout,"(5x,a,i6,a,i6)") "first=",nkstot_old+1,",  last=",nkstot_old+nkstot_add
+     WRITE(stdout,"(5x,a)")         "They can be extracted with bands.x using:"
+     WRITE(stdout,"(5x,a,i6,a,i6)") "first=",nkstot-nkstot_add+1,",  last=",nkstot
      WRITE(stdout,*)
 
   END SUBROUTINE
