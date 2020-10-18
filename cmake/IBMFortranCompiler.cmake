8ca8e83b6d784e1df2641b3aad44bbf07d95e1b1
Ye Luo <yeluo@anl.gov>
2020-10-18 01:57:07 -0400

    Remove -qextname. Handling Fortran underscore is a mess with XL.

--- cmake/IBMFortranCompiler.cmake
+++ cmake/IBMFortranCompiler.cmake
@@ -1,5 +1,2 @@
 # set preprocessor specific flag
 set(Fortran_PREPROCESSOR_FLAGS "-qsuffix=cpp=f90")
-
-# set the flag to ensure symbols with underscore added for linking Fortran libraries built by other compilers.
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qextname")
