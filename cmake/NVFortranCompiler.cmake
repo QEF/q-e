586f66aadf1b1f8dfcd49f3d1581b94a34f603f8
Daniele Cesarini <d.cesarini@cineca.it>
2021-01-18 14:50:50 +0000

    Introduce CUDA support in CMake with some refactoring.

--- cmake/NVFortranCompiler.cmake
+++ cmake/NVFortranCompiler.cmake
@@ -2,3 +2,8 @@ qe_add_global_compile_definitions(__PGI)
 
 # set optimization specific flags
 set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Mcache_align -Mlarge_arrays")
+
+if(QE_ENABLE_CUDA)
+    set(QE_CUDA_COMPILE_OPTIONS "-Mcuda")
+    set(QE_CUDA_LINK_OPTIONS "-Mcuda")
+endif()
