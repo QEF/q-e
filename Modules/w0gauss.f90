c30c8c8a59f9a2372ce04d823be422fe2a5d318b
Flaviano José dos Santos <flaviano.dossantos@epfl.ch>
2021-01-26 11:38:06 +0100

    Implement a new protocol for Fermi energy determination based on Newtons minimization method

--- Modules/w0gauss.f90
+++ Modules/w0gauss.f90
@@ -81,3 +81,88 @@ function w0gauss (x, n)
   enddo
   return
 end function w0gauss
+
+!
+! Copyright (C) 2001 PWSCF group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!
+!-----------------------------------------------------------------------
+function w2gauss (x, n)
+  !-----------------------------------------------------------------------
+  !
+  !     the second derivative of wgauss, that is, the first derivative of the delta function
+  !     
+  ! --> (n>=0) : second derivative of the corresponding Methfessel-Paxton wgauss
+  !              delta' =- sum_{n=0}^N A_n + H_{2n+1} exp(-x^2)
+  !      where      A_n = (-1)^n / (n! 4^n sqrt(pi) ) 
+  !      and    H_{n+1} = 2x H_n(x) - 2n H_{n-1}(x)
+  ! 
+  ! --> (n=-1 ): second derivative of cold smearing:
+  !              delta' = 1/sqrt(pi) * exp( -(x - 1/sqrt(2))^2 ) * (2 sqrt(2) x^2 - 6x + sqrt(2))
+  !
+  ! --> (n=-99): second derivative of Fermi-Dirac function:
+  !              delta' = -2 sinh(x) / (2 + 2 cosh(x))^2
+  !
+  USE kinds, ONLY : DP
+  USE constants, ONLY : sqrtpm1, sqrt2
+  implicit none
+  real(DP) :: w2gauss, x
+  ! output: the value of the function
+  ! input: the point where to compute the function
+
+  integer :: n
+  ! input: the order of the smearing function
+  !
+  !    here the local variables
+  !
+  real(DP) :: a, arg, hp, hd
+  ! the coefficients a_n
+  ! the argument of the exponential
+  ! the hermite function
+  ! the hermite function
+
+  integer :: i, ni
+  ! counter on n values
+  ! counter on 2n values
+
+  ! Fermi-Dirac smearing
+
+  if (n.eq. - 99) then
+     if (abs (x) .le.36.0) then
+        w2gauss = -( exp(x) - exp(-x) ) / ( 2.0d0 + exp(-x) + exp(x) )**2
+        ! in order to avoid problems for large values of x in the e
+     else
+        w2gauss = 0.0d0
+     endif
+     return
+
+  endif
+  ! cold smearing  (Marzari-Vanderbilt-DeVita-Payne)
+  if (n.eq. - 1) then
+     arg = min (200.d0, (x - 1.0d0 / sqrt2 ) **2)
+     w2gauss = sqrtpm1 * exp ( - arg) * (2.d0*sqrt2*x*x - 6.d0*x + sqrt2)
+     return
+
+  endif
+
+   if (n.gt.10 .or. n.lt.0) call errore('w0gauss','higher order smearing is untested and unstable',abs(n))
+
+  ! Methfessel-Paxton
+  arg = min (200.d0, x**2)
+  a = sqrtpm1 * exp ( - arg)
+  w2gauss = - a * 2.d0 * x 
+  if (n.eq.0) return
+  hd = 2.d0 * x 
+  hp = 2.d0 * x * hd - 2.d0
+  do i = 1, n
+     hd = 2.0d0 * x * hp - 2.0d0 * DBLE (2*i) * hd
+     a = - a / (DBLE (i) * 4.0d0)
+     w2gauss = w2gauss - a * hd
+     hp = 2.0d0 * x * hd - 2.0d0 * DBLE (2*i+1) * hp
+  enddo
+  return
+end function w2gauss
