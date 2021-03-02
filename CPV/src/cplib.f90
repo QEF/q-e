0bc84011cb841525953aaad234842b8c08b5bfdb
Paolo Giannozzi <p.giannozzi@gmail.com>
2021-03-02 15:07:56 +0000

    Missing allocation of a GPU variable in CP

--- CPV/src/cplib.f90
+++ CPV/src/cplib.f90
@@ -1090,7 +1090,7 @@ subroutine nlinit
       use constants,       ONLY : pi, fpi
       use ions_base,       ONLY : na, nsp
       use uspp,            ONLY : aainit, beta, qq_nt, dvan, nhtol, nhtolm, indv,&
-                                  dbeta
+                                  dbeta, qq_nt_d
       use uspp_param,      ONLY : upf, lmaxq, nbetam, lmaxkb, nhm, nh
       use atom,            ONLY : rgrid
       use qgb_mod,         ONLY : qgb, dqgb
@@ -1141,6 +1141,9 @@ subroutine nlinit
          allocate( dqgb( ngb, nhm*(nhm+1)/2, nsp, 3, 3 ) )
          allocate( dbeta( ngw, nhm, nsp, 3, 3 ) )
       END IF
+#ifdef __CUDA
+      ALLOCATE( qq_nt_d(nhm,nhm,nsp) )
+#endif
       !
       !     initialization for vanderbilt species
       !
