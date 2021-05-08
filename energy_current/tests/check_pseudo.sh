b4da56372d8c16afa79192cfb616a29a2a8f8494
DavideTisi <davide.tisi93@gmail.com>
2021-05-08 11:32:34 +0200

    modified test to used new pseudo added check_pseudo.sh and modified Makefile in order to automatically download the pseudo from QE directory !!!! problem the test do not run due to a bug

---
+++ energy_current/tests/check_pseudo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (C) 2001-2016 Quantum ESPRESSO group
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License. See the file `License' in the root directory
+# of the present distribution.
+
+if test "`which curl`" = "" ; then
+   if test "`which wget`" = "" ; then
+      echo "### wget or curl not found: will not be able to download missing PP ###"
+   else
+      DOWNLOADER="wget -O"
+      # echo "wget found"
+   fi
+else
+   DOWNLOADER="curl -o"
+   # echo "curl found"
+fi
+
+echo ${ESPRESSO_PSEUDO}
+echo ${NETWORK_PSEUDO}
+inputs=`find $1* -type f -name "*.in" -not -name "test.*" -not -name "benchmark.*"`
+pp_files=`for x in ${inputs}; do grep UPF ${x} | awk '{print $3}'; done`
+
+for pp_file in ${pp_files} ; do
+if ! test -f ${ESPRESSO_PSEUDO}/${pp_file} ; then 
+	echo -n "Downloading ${pp_file} to ${ESPRESSO_PSEUDO} ... "
+	#${DOWNLOADER} ${ESPRESSO_PSEUDO}/${pp_file} ${NETWORK_PSEUDO}/${pp_file} 2> /dev/null
+	${DOWNLOADER} ${ESPRESSO_PSEUDO}/${pp_file} ${NETWORK_PSEUDO}/${pp_file} 
+	if test $? != 0 ; then 
+		echo "Download of" ${pp_file} "FAILED, do it manually -- Testing aborted!"
+		exit -1
+	#else
+		#echo "done."
+	fi
+#else
+	#echo "No need to download ${pp_file}."
+fi
+done
