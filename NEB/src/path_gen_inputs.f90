d0d7afd278b78759e3c9ffc4166d7ed991856352
Paolo Giannozzi <p.giannozzi@gmail.com>
2021-06-04 08:50:02 +0200

    Much simpler parsing

--- NEB/src/path_gen_inputs.f90
+++ NEB/src/path_gen_inputs.f90
@@ -7,28 +7,7 @@
 !
 !----------------------------------------------------------------------------
 !
-LOGICAL FUNCTION skip_line(dummy)
-  !
-  ! True if line is empty or starts with # or !
-  !
-  IMPLICIT NONE
-  !
-  CHARACTER(len=*), intent(in) :: dummy 
-  CHARACTER(len=1) :: dum1
-  !
-  IF (len_trim(dummy) < 1) THEN
-    ! empty line
-    skip_line = .TRUE.
-    RETURN
-  END IF
-  !
-  dum1 = trim(adjustl(dummy))
-  ! Comment line (starts with ! or #)
-  skip_line = dum1 .EQ. "!" .OR. dum1 .EQ. "#"
-  !
-END FUNCTION skip_line
-!
-SUBROUTINE path_gen_inputs(parse_file_name,engine_prefix,nimage,root,comm)
+SUBROUTINE path_gen_inputs (parse_file_name, engine_prefix, nimage, root, comm)
   !
   USE mp, ONLY : mp_rank, mp_bcast
   !
@@ -40,114 +19,135 @@ SUBROUTINE path_gen_inputs(parse_file_name,engine_prefix,nimage,root,comm)
   INTEGER, INTENT(in) :: root
   INTEGER, INTENT(in) :: comm
   !
-  CHARACTER(len=512) :: dummy
-  CHARACTER(LEN=80)  :: engine_file, tmp_file
+  CHARACTER(LEN=80)  :: neb_file
   CHARACTER(LEN=6), EXTERNAL :: int_to_char
-  INTEGER :: iimage, status
-  INTEGER :: parse_unit, engine_unit, tmp_unit
-  LOGICAL :: skip_line
+  INTEGER :: iimage
+  INTEGER :: parse_unit, neb_unit
   !
   IF( mp_rank(comm) == root) THEN
      !
      OPEN( newunit=parse_unit, file=trim(parse_file_name), status='old')
-     nimage= 0
-     status=-1
+     iimage = 0
+     neb_file = 'neb.dat'
+     OPEN( newunit=neb_unit, file=neb_file, status='unknown')
+     CALL parse_and_split (parse_unit, neb_unit, iimage, nimage)
+     CLOSE( neb_unit )
      !
-     read_loop: DO
+     image_loop: DO iimage = 1, nimage
         !
-        READ(parse_unit,fmt='(A512)',END=10) dummy
-        IF ( skip_line(dummy) ) CYCLE
-        IF( dummy == "END" ) THEN
-           EXIT read_loop
-        ELSE IF( trim(adjustl(dummy)) == "BEGIN" ) THEN
-           status=0
-        ELSE IF( trim(adjustl(dummy)) == "BEGIN_PATH_INPUT"  ) THEN
-           status=1
-           OPEN( newunit=tmp_unit, file='neb.dat', status='unknown' )
-           ! ---------------------------------------------------
-           ! NEB input data
-           ! ---------------------------------------------------
-        ELSE IF( trim(adjustl(dummy)) == "END_PATH_INPUT"    ) THEN
-           status=0
-           CLOSE( tmp_unit )
-        ELSE IF( trim(adjustl(dummy)) == "BEGIN_ENGINE_INPUT") THEN
-           status=2
-           tmp_file = 'tmp.in' // trim(int_to_char(nimage))
-           OPEN( newunit=tmp_unit, file=tmp_file, status='unknown' )
-           ! ---------------------------------------------------
-           ! Engine generic input data
-           ! ---------------------------------------------------
-        ELSE IF( trim(adjustl(dummy)) == "END_ENGINE_INPUT"  ) THEN
-           status=0
-           CLOSE( tmp_unit )
-        ELSE IF( trim(adjustl(dummy)) == "BEGIN_POSITIONS"   ) THEN
-           status=3
-           ! ---------------------------------------------------
-           ! Engine image-specific data here
-           ! ---------------------------------------------------
-        ELSE IF( trim(adjustl(dummy)) == "END_POSITIONS"      ) THEN
-           status=2
-           CLOSE (tmp_unit)
-           tmp_file = 'tmp.in' // trim(int_to_char(nimage+1))
-           OPEN( newunit=tmp_unit, file=tmp_file, status='unknown' )
-        ELSE IF( trim(adjustl(dummy)) == "FIRST_IMAGE"       .OR. &
-                trim(adjustl(dummy)) == "INTERMEDIATE_IMAGE" .OR. &
-                trim(adjustl(dummy)) == "LAST_IMAGE"        ) THEN
-           nimage = nimage + 1
-           IF ( nimage > 1 ) CLOSE (tmp_unit)
-           tmp_file = 'tmp.in' // trim(int_to_char(nimage))
-           OPEN( newunit=tmp_unit, file=tmp_file, status='unknown' )
-        ELSE IF( status > 0 ) THEN
-           !
-           WRITE(tmp_unit,*) trim(dummy)
-           !
-        END IF
+        REWIND (parse_unit)
+        neb_file = trim(engine_prefix) // trim(int_to_char(iimage)) //".in"
+        OPEN( newunit=neb_unit, file=neb_file, status='unknown' )
+        CALL parse_and_split (parse_unit, neb_unit, iimage, nimage)
+        CLOSE (neb_unit)
         !
-     END DO read_loop
+     END DO image_loop
      !
-10   CONTINUE
      CLOSE (parse_unit)
-     CLOSE (tmp_unit)
      !
   END IF
   !
   CALL mp_bcast(nimage, root, comm)
   !
-  IF( mp_rank(comm) == root) THEN
+END SUBROUTINE path_gen_inputs
+!
+SUBROUTINE parse_and_split ( parse_unit, neb_unit, iimage, nimage )
+  !
+  ! Poor man parsing of NEB input file
+  ! Data is read from "parse_unit", written to "neb_unit", for image "iimage"
+  ! On output, "nimage" is the number of images read from "parse_unit"
+  ! Must be called first with iimage = 0, later with iimage = 1, nimage
+  ! parse_unit  must connected to the NEB input file
+  ! neb_unit    must be connected to the output file, either neb input
+  !             for iimage=0 or engine input for iimage > 0
+  ! iimage      neb data is written if iimage = 0
+  !             data for image iimage=1,..., nimage is written otherwise
+  !  
+  IMPLICIT NONE
+  !
+  INTEGER, INTENT(in)  :: parse_unit, neb_unit, iimage
+  INTEGER, INTENT(out) :: nimage
+  !
+  CHARACTER(len=512) :: dummy
+  INTEGER :: status
+  LOGICAL :: skip_line
+  !
+  nimage= 0
+  status=-1
+  !
+  read_loop: DO
      !
-     DO iimage = 1, nimage
+     READ(parse_unit,fmt='(A512)',END=10) dummy
+     IF ( skip_line(dummy) ) CYCLE
+     IF( dummy == "END" ) THEN
+        EXIT read_loop
+     ELSE IF( trim(adjustl(dummy)) == "BEGIN" ) THEN
+        status=0
+     ELSE IF( trim(adjustl(dummy)) == "BEGIN_PATH_INPUT"  ) THEN
+        status=1
+        ! ---------------------------------------------------
+        ! NEB input data
+        ! ---------------------------------------------------
+     ELSE IF( trim(adjustl(dummy)) == "END_PATH_INPUT"    ) THEN
+        status=0
+     ELSE IF( trim(adjustl(dummy)) == "BEGIN_ENGINE_INPUT") THEN
+        status=2
+        ! ---------------------------------------------------
+        ! Engine generic input data
+        ! ---------------------------------------------------
+     ELSE IF( trim(adjustl(dummy)) == "END_ENGINE_INPUT"  ) THEN
+        status=0
+     ELSE IF( trim(adjustl(dummy)) == "BEGIN_POSITIONS"   ) THEN
+        status=3
+        ! ---------------------------------------------------
+        ! Engine image-specific data
+        ! ---------------------------------------------------
+     ELSE IF( trim(adjustl(dummy)) == "END_POSITIONS"      ) THEN
+        status=2
+     ELSE IF( trim(adjustl(dummy)) == "FIRST_IMAGE"       .OR. &
+          trim(adjustl(dummy)) == "INTERMEDIATE_IMAGE" .OR. &
+          trim(adjustl(dummy)) == "LAST_IMAGE"        ) THEN
         !
-        engine_file = trim(engine_prefix) // trim(int_to_char(iimage)) //".in"
-        OPEN( newunit=engine_unit, file=engine_file, status='unknown' )
+        nimage = nimage + 1
         !
-        tmp_file = 'tmp.in0'
-        OPEN( newunit=tmp_unit, file=tmp_file, status='old' )
-        DO
-           READ ( tmp_unit, fmt='(A512)', END=20) dummy
-           WRITE( engine_unit, '(A)' ) trim(dummy)
-        END DO
-20      CLOSE (tmp_unit)
+     ELSE IF( status == 1 .AND. iimage == 0 ) THEN
         !
-        tmp_file = 'tmp.in' // trim(int_to_char(iimage))
-        OPEN( newunit=tmp_unit, file=tmp_file, status='old' )
-        DO
-           READ ( tmp_unit, fmt='(A512)', END=30) dummy
-           WRITE( engine_unit, '(A)' ) trim(dummy)
-        END DO
-30      CLOSE (tmp_unit) 
+        WRITE(neb_unit,*) trim(dummy)
         !
-        tmp_file = 'tmp.in' // trim(int_to_char(nimage+1))
-        OPEN( newunit=tmp_unit, file=tmp_file, status='old' )
-        DO
-           READ ( tmp_unit, fmt='(A512)', END=40) dummy
-           WRITE( engine_unit, '(A)' ) trim(dummy)
-        END DO
-40      CLOSE (tmp_unit)
+     ELSE IF( status == 2 .AND. iimage > 0 ) THEN
         !
-        CLOSE (engine_unit) 
+        WRITE(neb_unit,*) trim(dummy)
         !
-     END DO
+     ELSE IF( status == 3 .AND. iimage > 0 .AND. iimage == nimage ) THEN
+        !
+        WRITE(neb_unit,*) trim(dummy)
+        !
+     END IF
      !
-   END IF
-   !
-   END SUBROUTINE path_gen_inputs
+  END DO read_loop
+  !
+  10 IF ( status /= 0 ) CALL infomsg('parse_and_split','parsing error?')
+  !
+END SUBROUTINE parse_and_split
+!
+LOGICAL FUNCTION skip_line(dummy)
+  !
+  ! True if line is empty or starts with # or !
+  !
+  IMPLICIT NONE
+  !
+  CHARACTER(len=*), intent(in) :: dummy 
+  INTEGER :: i
+  !
+  IF (len_trim(dummy) < 1) THEN
+    ! empty line
+    skip_line = .TRUE.
+  ELSE
+    DO i=1,len_trim(dummy)
+       IF ( dummy(i:i) /= ' ' ) EXIT
+    END DO 
+    ! Comment line (starts with ! or #)
+    skip_line = dummy(i:i) .EQ. '!' .OR. dummy(i:i) .EQ. '#'
+  END IF
+  !
+END FUNCTION skip_line
