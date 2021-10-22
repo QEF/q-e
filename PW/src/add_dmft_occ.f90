041bc0cc0a4181cdf047391d993628db0e9da12b
phibeck <sophie.beck@mat.ethz.ch>
2021-10-22 15:43:30 -0400

    change in license header

--- PW/src/add_dmft_occ.f90
+++ PW/src/add_dmft_occ.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2008 Dmitry Korotin dmitry@korotin.name
+! Copyright (C) 2021 Quantum ESPRESSO Foundation
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
