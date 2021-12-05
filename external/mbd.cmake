c6f79e08f1837877388b9f73f180c4a4632b73a6
Ye Luo <yeluo@anl.gov>
2021-12-04 20:16:16 -0600

    [CMake] Fix mbd when using internal lapack

--- external/mbd.cmake
+++ external/mbd.cmake
@@ -1,9 +1,9 @@
 ###########################################################
 # MBD
 ###########################################################
+add_library(qe_mbd INTERFACE)
+qe_install_targets(qe_mbd)
 if(MBD_ROOT)
-    add_library(qe_mbd INTERFACE)
-    qe_install_targets(qe_mbd)
     target_link_libraries(qe_mbd INTERFACE "-L${MBD_ROOT}/lib;-lmbd")
     target_include_directories(qe_mbd INTERFACE ${MBD_ROOT}/include)
 else()
@@ -19,8 +19,7 @@ else()
     if(FORCE_BUILD_STATIC_LIBS)
         unset(BUILD_SHARED_LIBS)
     endif()
-    add_library(qe_mbd INTERFACE)
     target_link_libraries(qe_mbd INTERFACE Mbd)
-    qe_fix_fortran_modules(Mbd)
-    qe_install_targets(qe_mbd Mbd)
+    # need to fix libmbd CMake https://github.com/libmbd/libmbd/pull/34
+    #install(TARGETS mbd)
 endif()
