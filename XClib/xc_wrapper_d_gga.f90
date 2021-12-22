36f3b98452d69e05949fbeea7c1f66c4731a5426
fabrizio22 <ferrariruffino.fz@gmail.com>
2021-12-22 16:49:45 +0100

    xc_deriv - gga libxc side set with openacc

--- XClib/xc_wrapper_d_gga.f90
+++ XClib/xc_wrapper_d_gga.f90
@@ -125,11 +125,13 @@ SUBROUTINE dgcxc_( length, sp, r_in, g_in, dvxc_rr, dvxc_sr, dvxc_ss )
     IF (sp == 2) length_dlxc = length*3
     !
     ALLOCATE( rho_lxc(length*sp), sigma(length_dlxc) )
+    !$acc data copyout( rho_lxc, sigma )
     !
     ! ... set libxc input
     SELECT CASE( sp )
     CASE( 1 )
       !
+      !$acc parallel loop
       DO k = 1, length
         rho_lxc(k) = r_in(k,1)
         sigma(k) = g_in(k,1,1)**2 + g_in(k,2,1)**2 + g_in(k,3,1)**2
@@ -137,6 +139,7 @@ SUBROUTINE dgcxc_( length, sp, r_in, g_in, dvxc_rr, dvxc_sr, dvxc_ss )
       !
     CASE( 2 )
       !
+      !$acc parallel loop
       DO k = 1, length
         rho_lxc(2*k-1) = r_in(k,1)
         rho_lxc(2*k)   = r_in(k,2)
@@ -158,136 +161,52 @@ SUBROUTINE dgcxc_( length, sp, r_in, g_in, dvxc_rr, dvxc_sr, dvxc_ss )
       !
     END SELECT
     !
+    !$acc end data
+    !
   ENDIF
   !
+  IF (sp==1) THEN
+    !$acc parallel loop
+    DO k = 1, length
+      dvxc_rr(k,1,1) = 0._DP
+      dvxc_sr(k,1,1) = 0._DP
+      dvxc_ss(k,1,1) = 0._DP
+    ENDDO
+  ELSE
+    !$acc parallel loop
+    DO k = 1, length
+      dvxc_rr(k,1,1) = 0._DP ; dvxc_rr(k,1,2) = 0._DP
+      dvxc_rr(k,2,1) = 0._DP ; dvxc_rr(k,2,2) = 0._DP
+      dvxc_sr(k,1,1) = 0._DP ; dvxc_sr(k,1,2) = 0._DP
+      dvxc_sr(k,2,1) = 0._DP ; dvxc_sr(k,2,2) = 0._DP
+      dvxc_ss(k,1,1) = 0._DP ; dvxc_ss(k,1,2) = 0._DP
+      dvxc_ss(k,2,1) = 0._DP ; dvxc_ss(k,2,2) = 0._DP
+    ENDDO
+  ENDIF
   !
-  IF ( is_libxc(3) ) THEN
+  ! ... LIBXC DERIVATIVE FOR EXCHANGE
+  !
+  IF ( is_libxc(3) .AND. igcx/=0 ) THEN
     ALLOCATE( v2rho2_x(length_dlxc), v2rhosigma_x(length_dlxc*sp), &
               v2sigma2_x(length_dlxc*sp) )
-    ! ... DERIVATIVE FOR EXCHANGE
     v2rho2_x = 0._DP ;  v2rhosigma_x = 0._DP ;  v2sigma2_x = 0._DP
-    IF (igcx /= 0) THEN
-      CALL xc_f03_func_set_dens_threshold( xc_func(3), epsr )
-      CALL xc_f03_gga_fxc( xc_func(3), lengthxc, rho_lxc(1), sigma(1), v2rho2_x(1), &
-                           v2rhosigma_x(1), v2sigma2_x(1) )
-    ENDIF
-  ENDIF
-  !
-  IF ( is_libxc(4) ) THEN
-    ALLOCATE( v2rho2_c(length_dlxc), v2rhosigma_c(length_dlxc*sp), &
-              v2sigma2_c(length_dlxc*sp) )
-    ! ... DERIVATIVE FOR CORRELATION
-    v2rho2_c = 0._DP ;  v2rhosigma_c = 0._DP ;  v2sigma2_c = 0._DP
-    IF (igcc /= 0) THEN
-      fkind = xc_f03_func_info_get_kind( xc_info(4) )
-      CALL xc_f03_func_set_dens_threshold( xc_func(4), epsr )
-      CALL xc_f03_gga_fxc( xc_func(4), lengthxc, rho_lxc(1), sigma(1), v2rho2_c(1), &
-                           v2rhosigma_c(1), v2sigma2_c(1) )
-    ENDIF
-  ENDIF
-  !
-  dvxc_rr = 0._DP
-  dvxc_sr = 0._DP
-  dvxc_ss = 0._DP
-  !
-  IF ( ((.NOT.is_libxc(3).AND.igcx/=0) .OR. (.NOT.is_libxc(4).AND.igcc/=0)) &
-        .AND. fkind/=XC_EXCHANGE_CORRELATION ) THEN
+    CALL xc_f03_func_set_dens_threshold( xc_func(3), epsr )
+    CALL xc_f03_gga_fxc( xc_func(3), lengthxc, rho_lxc(1), sigma(1), v2rho2_x(1), &
+                         v2rhosigma_x(1), v2sigma2_x(1) )
+    !$acc data copyin( v2rho2_x, v2rhosigma_x, v2sigma2_x )
     !
-    ALLOCATE( vrrx(length,sp), vsrx(length,sp), vssx(length,sp) )
-    ALLOCATE( vrrc(length,sp), vsrc(length,sp), vssc(length) )
-    !
-    IF ( sp == 1 ) THEN
-       !
-       IF (.NOT. ALLOCATED(sigma)) THEN
-         ALLOCATE( sigma(length) )
-         sigma(:) = g_in(:,1,1)**2 + g_in(:,2,1)**2 + g_in(:,3,1)**2
-       ENDIF
-       !
-       CALL dgcxc_unpol( length, r_in(:,1), sigma, vrrx(:,1), vsrx(:,1), vssx(:,1), &
-                         vrrc(:,1), vsrc(:,1), vssc )
-       !
-    ELSEIF ( sp == 2 ) THEN
-       !
-       ALLOCATE( vrzc(length,sp) )
-       !
-       CALL dgcxc_spin( length, r_in, g_in, vrrx, vsrx, vssx, vrrc, vsrc, vssc, vrzc )
-       !
-    ENDIF
-    !
-  ENDIF
-  !
-  IF ( sp == 1 ) THEN
-    !
-    IF ( ((.NOT.is_libxc(3).AND.igcx/=0) .OR. (.NOT.is_libxc(4).AND.igcc/=0)) &
-        .AND. fkind/=XC_EXCHANGE_CORRELATION ) THEN
+    IF (sp==1) THEN
+      !$acc parallel loop
       DO k = 1, length
-        dvxc_rr(k,1,1) = e2 * (vrrx(k,1) + vrrc(k,1))
-        dvxc_sr(k,1,1) = e2 * (vsrx(k,1) + vsrc(k,1))
-        dvxc_ss(k,1,1) = e2 * (vssx(k,1) + vssc(k)  )
-      ENDDO
-      !
-      DEALLOCATE( vrrx, vsrx, vssx )
-      DEALLOCATE( vrrc, vsrc, vssc )
-      !
-    ENDIF
-    !
-    IF ( is_libxc(3) ) THEN
-      DO k = 1, length
-        IF ( rho_lxc(k) > small .AND. SQRT(ABS(sigma(k))) > small ) CYCLE
+        IF ( rho_lxc(k)>small .AND. SQRT(ABS(sigma(k)))>small ) CYCLE
         IF ( rho_lxc(k)>rho_threshold_lda ) THEN
           dvxc_rr(k,1,1) = dvxc_rr(k,1,1) + e2 * v2rho2_x(k)
           dvxc_sr(k,1,1) = dvxc_sr(k,1,1) + e2 * v2rhosigma_x(k)*2._DP
         ENDIF
         dvxc_ss(k,1,1) = dvxc_ss(k,1,1) + e2 * v2sigma2_x(k)*4._DP
       ENDDO
-    ENDIF
-    IF ( is_libxc(4) ) THEN
-      DO k = 1, length
-        IF ( rho_lxc(k) > small .AND. SQRT(ABS(sigma(k))) > small ) CYCLE
-        IF ( rho_lxc(k)>rho_threshold_lda ) THEN
-          dvxc_rr(k,1,1) = dvxc_rr(k,1,1) + e2 * v2rho2_c(k)
-          dvxc_sr(k,1,1) = dvxc_sr(k,1,1) + e2 * v2rhosigma_c(k)*2._DP
-        ENDIF
-        dvxc_ss(k,1,1) = dvxc_ss(k,1,1) + e2 * v2sigma2_c(k)*4._DP
-      ENDDO
-    ENDIF
-    !
-  ELSEIF ( sp == 2 ) THEN
-    !
-    IF ( ((.NOT.is_libxc(3).AND.igcx/=0) .OR. (.NOT.is_libxc(4).AND.igcc/=0)) &
-        .AND. fkind/=XC_EXCHANGE_CORRELATION ) THEN
-      !
-      DO k = 1, length
-        rht = r_in(k,1) + r_in(k,2)
-        IF (rht > epsr) THEN
-          zeta = (r_in(k,1) - r_in(k,2)) / rht
-          !
-          dvxc_rr(k,1,1) = e2*(vrrx(k,1) + vrrc(k,1) + vrzc(k,1)*(1._DP - zeta)/rht)
-          dvxc_rr(k,1,2) = e2*(vrrc(k,1) - vrzc(k,1)*(1._DP + zeta)/rht)
-          dvxc_rr(k,2,1) = e2*(vrrc(k,2) + vrzc(k,2)*(1._DP - zeta)/rht)
-          dvxc_rr(k,2,2) = e2*(vrrx(k,2) + vrrc(k,2) - vrzc(k,2)*(1._DP + zeta)/rht)
-        ENDIF
-        !
-        dvxc_sr(k,1,1) = e2*(vsrx(k,1) + vsrc(k,1))
-        dvxc_sr(k,1,2) = e2*vsrc(k,1)
-        dvxc_sr(k,2,1) = e2*vsrc(k,2)
-        dvxc_sr(k,2,2) = e2*(vsrx(k,2) + vsrc(k,2))
-        !
-        dvxc_ss(k,1,1) = e2*(vssx(k,1) + vssc(k))
-        dvxc_ss(k,1,2) = e2*vssc(k)
-        dvxc_ss(k,2,1) = e2*vssc(k)
-        dvxc_ss(k,2,2) = e2*(vssx(k,2) + vssc(k))
-        !
-      ENDDO
-      !
-      DEALLOCATE( vrrx, vsrx, vssx )
-      DEALLOCATE( vrrc, vsrc, vssc )
-      DEALLOCATE( vrzc )
-      !
-    ENDIF
-    !
-    IF ( is_libxc(3) ) THEN
-      !
+    ELSEIF (sp==2) THEN
+      !$acc parallel loop
       DO k = 1, length
         thr_up_cond = r_in(k,1)>epsr .AND. SQRT(ABS(sigma(3*k-2)))>epsg
         thr_dw_cond = r_in(k,2)>epsr .AND. SQRT(ABS(sigma(3*k)))>epsg
@@ -301,13 +220,37 @@ SUBROUTINE dgcxc_( length, sp, r_in, g_in, dvxc_rr, dvxc_sr, dvxc_ss )
         dvxc_rr(k,2,1) = dvxc_rr(k,2,1) + e2 * v2rho2_x(3*k-1)
         dvxc_sr(k,2,2) = dvxc_sr(k,2,2) + e2 * v2rhosigma_x(6*k)*2._DP
       ENDDO
-      !
-      DEALLOCATE( v2rho2_x, v2rhosigma_x, v2sigma2_x )
-      !
     ENDIF
+    !$acc end data
+    DEALLOCATE( v2rho2_x, v2rhosigma_x, v2sigma2_x )
     !
-    IF ( is_libxc(4) ) THEN
-      !
+  ENDIF
+  !
+  ! ... LIBXC DERIVATIVE FOR CORRELATION
+  !
+  IF ( is_libxc(4) .AND. igcc/=0 ) THEN
+    ALLOCATE( v2rho2_c(length_dlxc), v2rhosigma_c(length_dlxc*sp), &
+              v2sigma2_c(length_dlxc*sp) )
+    ! ... DERIVATIVE FOR CORRELATION
+    v2rho2_c = 0._DP ;  v2rhosigma_c = 0._DP ;  v2sigma2_c = 0._DP
+    fkind = xc_f03_func_info_get_kind( xc_info(4) )
+    CALL xc_f03_func_set_dens_threshold( xc_func(4), epsr )
+    CALL xc_f03_gga_fxc( xc_func(4), lengthxc, rho_lxc(1), sigma(1), v2rho2_c(1), &
+                           v2rhosigma_c(1), v2sigma2_c(1) )
+    !$acc data copyin( v2rho2_c, v2rhosigma_c, v2sigma2_c )
+    !
+    IF (sp==1) THEN
+      !$acc parallel loop
+      DO k = 1, length
+        IF ( rho_lxc(k)>small .AND. SQRT(ABS(sigma(k)))>small ) CYCLE
+        IF ( rho_lxc(k)>rho_threshold_lda ) THEN
+          dvxc_rr(k,1,1) = dvxc_rr(k,1,1) + e2 * v2rho2_c(k)
+          dvxc_sr(k,1,1) = dvxc_sr(k,1,1) + e2 * v2rhosigma_c(k)*2._DP
+        ENDIF
+        dvxc_ss(k,1,1) = dvxc_ss(k,1,1) + e2 * v2sigma2_c(k)*4._DP
+      ENDDO
+    ELSEIF (sp==2) THEN
+      !$acc parallel loop
       DO k = 1, length
         thr_up_cond = r_in(k,1)>epsr .AND. SQRT(ABS(sigma(3*k-2)))>epsg
         thr_dw_cond = r_in(k,2)>epsr .AND. SQRT(ABS(sigma(3*k)))>epsg
@@ -325,15 +268,88 @@ SUBROUTINE dgcxc_( length, sp, r_in, g_in, dvxc_rr, dvxc_sr, dvxc_ss )
         dvxc_sr(k,2,2) = dvxc_sr(k,2,1) + e2 * v2rhosigma_c(6*k)*2.d0
         dvxc_ss(k,2,2) = dvxc_ss(k,2,2) + e2 * v2sigma2_c(6*k)*4.d0
       ENDDO
-      !
-      DEALLOCATE( v2rho2_c, v2rhosigma_c, v2sigma2_c )
-      !
     ENDIF
     !
-    IF (ANY(is_libxc(3:4))) DEALLOCATE( rho_lxc )
+    !$acc end data
+    DEALLOCATE( v2rho2_c, v2rhosigma_c, v2sigma2_c )
     !
   ENDIF
-  IF ( ALLOCATED(sigma) ) DEALLOCATE( sigma )
+  !
+  ! ... QE DERIVATIVE FOR EXCHANGE AND CORRELATION
+  !
+  IF ( ((.NOT.is_libxc(3).AND.igcx/=0) .OR. (.NOT.is_libxc(4).AND.igcc/=0)) &
+        .AND. fkind/=XC_EXCHANGE_CORRELATION ) THEN
+    !
+    ALLOCATE( vrrx(length,sp), vsrx(length,sp), vssx(length,sp) )
+    ALLOCATE( vrrc(length,sp), vsrc(length,sp), vssc(length) )
+    !$acc data create( vrrx, vsrx, vssx, vrrc, vsrc, vssc )
+    !
+    IF ( sp == 1 ) THEN
+       !
+       IF (.NOT. ALLOCATED(sigma)) THEN
+         ALLOCATE( sigma(length) )
+         sigma(:) = g_in(:,1,1)**2 + g_in(:,2,1)**2 + g_in(:,3,1)**2
+       ENDIF
+       !
+       !$acc data copyin( sigma )
+       !$acc host_data use_device( sigma, vrrx, vsrx, vssx, vrrc, vsrc, vssc )
+       CALL dgcxc_unpol( length, r_in(:,1), sigma, vrrx(:,1), vsrx(:,1), vssx(:,1), &
+                         vrrc(:,1), vsrc(:,1), vssc )
+       !$acc end host_data
+       !$acc end data
+       !
+       !$acc parallel loop
+       DO k = 1, length
+         dvxc_rr(k,1,1) = dvxc_rr(k,1,1) + e2 * (vrrx(k,1) + vrrc(k,1))
+         dvxc_sr(k,1,1) = dvxc_sr(k,1,1) + e2 * (vsrx(k,1) + vsrc(k,1))
+         dvxc_ss(k,1,1) = dvxc_ss(k,1,1) + e2 * (vssx(k,1) + vssc(k)  )
+       ENDDO
+       !
+    ELSEIF ( sp == 2 ) THEN
+       !
+       ALLOCATE( vrzc(length,sp) )
+       !$acc data create( vrzc )
+       !
+       !$acc host_data use_device( vrrx, vsrx, vssx, vrrc, vsrc, vssc, vrzc )
+       CALL dgcxc_spin( length, r_in, g_in, vrrx, vsrx, vssx, vrrc, vsrc, vssc, vrzc )
+       !$acc end host_data
+       !
+       !$acc parallel loop
+       DO k = 1, length
+         rht = r_in(k,1) + r_in(k,2)
+         IF (rht > epsr) THEN
+           zeta = (r_in(k,1) - r_in(k,2))/rht
+           !
+           dvxc_rr(k,1,1) = dvxc_rr(k,1,1) + e2*(vrrx(k,1) + vrrc(k,1) + vrzc(k,1)*(1.d0 - zeta)/rht)
+           dvxc_rr(k,1,2) = dvxc_rr(k,1,2) + e2*(vrrc(k,1) - vrzc(k,1)*(1.d0 + zeta)/rht)
+           dvxc_rr(k,2,1) = dvxc_rr(k,2,1) + e2*(vrrc(k,2) + vrzc(k,2)*(1.d0 - zeta)/rht)
+           dvxc_rr(k,2,2) = dvxc_rr(k,2,2) + e2*(vrrx(k,2) + vrrc(k,2) - vrzc(k,2)*(1.d0 + zeta)/rht)
+         ENDIF
+         !
+         dvxc_sr(k,1,1) = dvxc_sr(k,1,1) + e2 * (vsrx(k,1) + vsrc(k,1))
+         dvxc_sr(k,1,2) = dvxc_sr(k,1,2) + e2 * vsrc(k,1)
+         dvxc_sr(k,2,1) = dvxc_sr(k,2,1) + e2 * vsrc(k,2)
+         dvxc_sr(k,2,2) = dvxc_sr(k,2,2) + e2 * (vsrx(k,2) + vsrc(k,2))
+         !
+         dvxc_ss(k,1,1) = dvxc_ss(k,1,1) + e2 * (vssx(k,1) + vssc(k))
+         dvxc_ss(k,1,2) = dvxc_ss(k,1,2) + e2 * vssc(k)
+         dvxc_ss(k,2,1) = dvxc_ss(k,2,1) + e2 * vssc(k)
+         dvxc_ss(k,2,2) = dvxc_ss(k,2,2) + e2 * (vssx(k,2) + vssc(k))
+       ENDDO
+       !
+       !$acc end data
+       DEALLOCATE( vrzc )
+       !
+    ENDIF
+    !
+    !$acc end data
+    DEALLOCATE( vrrx, vsrx, vssx )
+    DEALLOCATE( vrrc, vsrc, vssc )
+    !
+  ENDIF
+  !
+  IF ( ANY(is_libxc(3:4))) DEALLOCATE( rho_lxc )
+  IF ( ALLOCATED(sigma)  ) DEALLOCATE( sigma )
   !
 #else
   !
