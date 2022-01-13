6fb1e63bc60849731773e0986c2d50efa67642b0
Samuel Ponce <samuel.pon@gmail.com>
2022-01-13 16:11:04 +0100

    2D Coulomb
    Modification needed for the input assume_isolated='2D'
    in pw.x to work with EPW.

--- EPW/src/epw_init.f90
+++ EPW/src/epw_init.f90
@@ -32,7 +32,7 @@
   USE gvect,            ONLY : g, ngm
   USE atom,             ONLY : msh, rgrid
   USE wavefunctions,    ONLY : evc
-  USE noncollin_module, ONLY : noncolin, npol, nspin_mag, lspinorb
+  USE noncollin_module, ONLY : noncolin, npol, nspin_mag
   USE uspp_param,       ONLY : upf, nhm
   USE m_gth,            ONLY : setlocq_gth
   USE units_lr,         ONLY : lrwfc, iuwfc
@@ -42,12 +42,15 @@
   USE elph2,            ONLY : igk_k_all, ngk_all, ngxx, veff, ig_s, ig_e
   USE mp,               ONLY : mp_barrier
   USE mp_global,        ONLY : inter_pool_comm, my_pool_id
+  USE spin_orb,         ONLY : lspinorb
   USE lsda_mod,         ONLY : nspin, lsda, current_spin
   USE phus,             ONLY : int1, int1_nc, int2, int2_so,        &
                                int4, int4_nc, int5, int5_so, alphap
   USE poolgathering,    ONLY : poolgather_int, poolgather_int1
   USE io_epw,           ONLY : readwfc
   USE dvqpsi,           ONLY : dvanqq2
+  USE Coul_cut_2D,      ONLY : do_cutoff_2D
+  USE Coul_cut_2D_ph,   ONLY : cutoff_lr_Vlocq, cutoff_fact_qg
   USE scf,              ONLY : v, vltot
   USE fft_base,         ONLY : dfftp
   USE fft_interfaces,   ONLY : fwfft
@@ -172,6 +175,19 @@
     !
   END DO
   !
+  ! From PHonon/PH/phq_init.f90
+  ! SP: For 2d calculations, we need to initialize the fact for the q+G
+  ! component of the cutoff of the Coulomb interaction
+  IF (do_cutoff_2D) call cutoff_fact_qg()
+  !
+  ! In 2D calculations the long range part of vlocq(g) (erf/r part)
+  ! was not re-added in g-space because everything is caclulated in
+  ! radial coordinates, which is not compatible with 2D cutoff.
+  ! It will be re-added each time vlocq(g) is used in the code.
+  ! Here, this cutoff long-range part of vlocq(g) is computed only once
+  ! by the routine below and stored
+  IF (do_cutoff_2D) call cutoff_lr_Vlocq()
+  ! 
   IF (first_run) THEN
     ALLOCATE(igk_k_all(npwx, nkstot), STAT = ierr)
     IF (ierr /= 0) CALL errore('epw_init', 'Error allocating igk_k_all', 1)
