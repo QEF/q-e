4a5f34622c7a8f3104a7a4284ae0bf9da6ca8f4c
Ivan Carnimeo <icarnimeo@sissa.it>
2023-04-01 15:09:40 +0200

    simplify data movements

--- PW/src/pwcom.f90
+++ PW/src/pwcom.f90
@@ -398,6 +398,7 @@ MODULE force_mod
   COMPLEX(DP), ALLOCATABLE :: overlap_inv(:,:)
   !! overlap matrix (transposed): (O^{-1/2})^T
   COMPLEX(DP), ALLOCATABLE :: doverlap_inv(:,:)
+  !$acc declare device_resident(doverlap_inv)
   !! derivative of the overlap matrix (not transposed): d(O^{-1/2})
   COMPLEX (DP), ALLOCATABLE :: at_dy(:,:), at_dj(:,:)
   !! derivatives of spherical harmonics and spherical Bessel functions (for atomic functions)
