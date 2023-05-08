e9242e01a578c53de19c86af63b184b37fe9708b
Ye Luo <yeluo@anl.gov>
2023-05-08 17:37:00 -0400

    Introduce Cray compiler (CCE ftn) customization.

---
+++ cmake/CrayFortranCompiler.cmake
@@ -0,0 +1,21 @@
+# Check compiler version
+if(CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 14.0.0)
+  message(FATAL_ERROR "Requires CCE 14.0.0 or higher ")
+endif()
+
+qe_add_global_compile_definitions(__CRAY)
+
+# set preprocessor specific flag
+set(Fortran_PREPROCESSOR_FLAGS "-eZ")
+
+if(QE_ENABLE_OPENACC)
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -hacc")
+else()
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -hnoacc")
+endif()
+
+if(QE_ENABLE_OPENMP)
+  #
+else()
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -hnoomp")
+endif()
