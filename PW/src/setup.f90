925f4d7f7bdeed847cec19fa8a51d6b984e0b69c
Paolo Giannozzi <p.giannozzi@gmail.com>
2023-07-03 15:35:53 +0200

    [skip-CI] There is somethng very wrong in the stres with exact exchange

--- PW/src/setup.f90
+++ PW/src/setup.f90
@@ -685,6 +685,8 @@ SUBROUTINE setup()
           & // ' not allowed for hybrid functionals',1)
      IF ( tstress .and. npool > 1 )  CALL errore('setup', &
          'stress for hybrid functionals not available with pools', 1)
+     IF ( tstress )  CALL errore('setup', &
+         'stress for hybrid functionals not available', 1)
      !
      CALL setup_exx  ()
      !
