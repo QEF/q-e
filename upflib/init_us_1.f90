2e767801b874663117a7abd0edd33a9469482fa7
Paolo Giannozzi <p.giannozzi@gmail.com>
2024-01-11 11:23:30 +0100

    Cleanup

--- upflib/init_us_1.f90
+++ upflib/init_us_1.f90
@@ -23,7 +23,6 @@ subroutine init_us_1( nat, ityp, omega, qmax, intra_bgrp_comm )
   !      spherical harmonics in the Q expansion
   !   f) It computes the interpolation table "qrad" for Q(G)
   !   g) It computes the qq terms which define the S matrix.
-  !   h) It fills the interpolation table "tab_beta" for the beta functions
   !
   USE upf_kinds,    ONLY : DP
   USE upf_const,    ONLY : fpi, sqrt2
@@ -52,7 +51,7 @@ subroutine init_us_1( nat, ityp, omega, qmax, intra_bgrp_comm )
   real(DP) ::  j
   ! J=L+S (noninteger!)
   integer :: n1, m0, m1, n, li, mi, vi, vj, ijs, is1, is2, &
-             lk, mk, vk, kh, lh, ijkb0, na
+             lk, mk, kh, lh, ijkb0, na
   integer, external :: sph_ind
   complex(DP) :: coeff
   real(DP) :: ji, jk
@@ -170,12 +169,10 @@ subroutine init_us_1( nat, ityp, omega, qmax, intra_bgrp_comm )
           li = nhtol(ih, nt)
           ji = nhtoj(ih, nt)
           mi = nhtolm(ih, nt)-li*li
-          vi = indv (ih, nt)
           do kh=1,nh(nt)
             lk = nhtol(kh, nt)
             jk = nhtoj(kh, nt)
             mk = nhtolm(kh, nt)-lk*lk
-            vk = indv (kh, nt)
             if (li == lk .and. abs(ji-jk) < 1.d-7) then
               do is1=1,2
                 do is2=1,2
