f4a9b2d6e1c8156f39638641b81e15270ceb6db4
Jae-Mo Lihm <jaemolim96@gmail.com>
2024-10-19 20:17:48 +0200

    Remove comments, cleanup ph_twochem

--- LR_Modules/sym_dmag.f90
+++ LR_Modules/sym_dmag.f90
@@ -84,69 +84,9 @@ subroutine sym_dmag (dmagtosym)
   in1 = tpi / DBLE (dfftp%nr1)
   in2 = tpi / DBLE (dfftp%nr2)
   in3 = tpi / DBLE (dfftp%nr3)
-
+  !
   CALL scale_sym_ops( nsymq, s, ft, dfftp%nr1, dfftp%nr2, dfftp%nr3, &
        s_scaled, ftau )
-  
-!   if (minus_q) then
-!      g1 (1) = 0.d0
-!      g2 (1) = 0.d0
-!      g3 (1) = 0.d0
-!      do ipol = 1, 3
-!         g1 (1) = g1 (1) + gimq (ipol) * in1 * at (ipol, 1)
-!         g2 (1) = g2 (1) + gimq (ipol) * in2 * at (ipol, 2)
-!         g3 (1) = g3 (1) + gimq (ipol) * in3 * at (ipol, 3)
-!      enddo
-!      term (1, 1) = CMPLX(cos (g1 (1) ), sin (g1 (1) ) ,kind=DP)
-!      term (2, 1) = CMPLX(cos (g2 (1) ), sin (g2 (1) ) ,kind=DP)
-!      term (3, 1) = CMPLX(cos (g3 (1) ), sin (g3 (1) ) ,kind=DP)
-!      phase (1) = (1.d0, 0.d0)
-!      do k = 1, dfftp%nr3
-!         do j = 1, dfftp%nr2
-!            do i = 1, dfftp%nr1
-!               CALL rotate_grid_point(s_scaled(1,1,irotmq), ftau(1,irotmq), &
-!                    i, j, k, dfftp%nr1, dfftp%nr2, dfftp%nr3, ri, rj, rk)
-!               do ipert = 1, lr_npert
-!                  aux2 = (0.d0, 0.d0)
-!                  do jpert = 1, lr_npert
-!                     do is=2,4
-!                        aux2(is-1) = aux2(is-1) + upert_mq(jpert, ipert) * &
-!                            dmagtosym (ri, rj, rk, is, jpert) * phase (1)
-!                     enddo
-!                  enddo
-!                  do kpol = 1, 3
-!                     mag(kpol)=bg(1,kpol)*aux2(1) + bg(2,kpol)*aux2(2) + &
-!                               bg(3,kpol)*aux2(3)
-!                  enddo
-! ! rotate the magnetic moment
-!                  do kpol = 1, 3
-!                     magrot(kpol) = s(1,kpol,invs(irotmq))*mag(1) + &
-!                                    s(2,kpol,invs(irotmq))*mag(2) + &
-!                                    s(3,kpol,invs(irotmq))*mag(3)
-!                  enddo
-!                  if (sname(irotmq)(1:3)=='inv') magrot=-magrot
-!                  if(t_rev(irotmq).eq.1) magrot=-magrot
-! ! go back to cartesian coordinates
-!                  do kpol = 1, 3
-!                     mag(kpol)=at(kpol,1)*magrot(1) + &
-!                               at(kpol,2)*magrot(2) + &
-!                               at(kpol,3)*magrot(3)
-!                     dmagsym(i,j,k,kpol,ipert)=(dmagtosym(i,j,k,kpol+1,ipert)+&
-!                          CONJG(mag(kpol)) ) * 0.5d0
-!                  enddo
-!               enddo
-!               phase (1) = phase (1) * term (1, 1)
-!            enddo
-!            phase (1) = phase (1) * term (2, 1)
-!         enddo
-!         phase (1) = phase (1) * term (3, 1)
-!      enddo
-!      do ipert = 1, lr_npert
-!         do is=2,4
-!            dmagtosym(:, :, :, is, ipert) = dmagsym (:, :, :, is-1, ipert)
-!         end do
-!      enddo
-!   endif ! minus_q
   !
   ! Here we symmetrize with respect to the small group of q
   !
