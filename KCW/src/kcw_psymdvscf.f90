a04929ef3b41d7ae0f753902fe7c3acf57c91e77
nscolonna <nicola.colonna@psi.ch>
2024-12-13 12:03:03 +0100

    Cleanup kcw_symdvscf

--- KCW/src/kcw_psymdvscf.f90
+++ KCW/src/kcw_psymdvscf.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2001-2008 Quantum ESPRESSO group
+! Copyright (C) 2001-2018 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -8,70 +8,58 @@
 !
 !adapted from PH code
 !-----------------------------------------------------------------------
-SUBROUTINE kcw_psymdvscf (dvtosym)!(nper, irr, dvtosym)
+SUBROUTINE kcw_psymdvscf (dvtosym)
   !-----------------------------------------------------------------------
-  !! p-symmetrize the potential.
+  ! Symmetrization of the response charge density.
   !
   USE kinds,     ONLY : DP
   USE noncollin_module, ONLY : nspin_mag
-  USE mp_bands,  ONLY : me_bgrp
   USE fft_base,  ONLY : dfftp
   USE scatter_mod,  ONLY : cgather_sym
-
   USE lr_symm_base, ONLY : nsymq, minus_q
   USE control_kcw,  ONLY : irr_bz
   !
   IMPLICIT NONE
   !
-  !INTEGER :: nper
-  !! the number of perturbations
-  !INTEGER :: irr
-  !! the representation under consideration
-  COMPLEX(DP) :: dvtosym(dfftp%nnr,nspin_mag)!,nper)
-  !! the potential to symmetrize
-  !
-  ! ... local variable
+  COMPLEX(DP) :: dvtosym (dfftp%nnr, nspin_mag)
   !
 #if defined (__MPI)
   !
-  INTEGER :: i, is, iper, ir3, ioff, ioff_tg, nxyp
-
-  COMPLEX(DP), ALLOCATABLE :: ddvtosym (:,:)!,:)
-  ! the potential to symm
-
+  INTEGER :: i, is, ir3, ioff, ioff_tg, nxyp
+  COMPLEX(DP), ALLOCATABLE :: ddvtosym (:,:)
+  !
   if ( .not. irr_bz ) RETURN
-  IF (nsymq.EQ.1.AND. (.NOT.minus_q) ) RETURN
-  CALL start_clock ('psymdvscf')
-
-  ALLOCATE (ddvtosym ( dfftp%nr1x * dfftp%nr2x * dfftp%nr3x, nspin_mag) )!, nper))
-
-  !DO iper = 1, nper
-     DO is = 1, nspin_mag
-        CALL cgather_sym (dfftp, dvtosym (:, is), ddvtosym(:, is) ) !, iper), ddvtosym (:, is) )!, iper) )
-     ENDDO
-
-  !ENDDO
-
+  !
+  IF (nsymq == 1 .AND. (.NOT.minus_q) ) RETURN
+  !
+  CALL start_clock ('hp_psymdvscf')
+  !
+  ALLOCATE (ddvtosym ( dfftp%nr1x * dfftp%nr2x * dfftp%nr3x, nspin_mag))
+  !
+  DO is = 1, nspin_mag
+     CALL cgather_sym (dfftp, dvtosym(:,is), ddvtosym(:,is))
+  ENDDO
+  !
   CALL kcw_symdvscf (ddvtosym) !(nper, irr, ddvtosym)
-
+  !
   nxyp = dfftp%nr1x * dfftp%my_nr2p
-  !DO iper = 1, nper
-     DO is = 1, nspin_mag
-        DO ir3 = 1, dfftp%my_nr3p
-           ioff    = dfftp%nr1x * dfftp%my_nr2p * (ir3-1)
-           ioff_tg = dfftp%nr1x * dfftp%nr2x    * (dfftp%my_i0r3p+ir3-1) + dfftp%nr1x * dfftp%my_i0r2p 
-           CALL zcopy (nxyp, ddvtosym (ioff_tg+1, is), 1, dvtosym (ioff+1, is), 1)
-        END DO
+  DO is = 1, nspin_mag
+     DO ir3 = 1, dfftp%my_nr3p
+        ioff    = dfftp%nr1x * dfftp%my_nr2p * (ir3-1)
+        ioff_tg = dfftp%nr1x * dfftp%nr2x    * (dfftp%my_i0r3p+ir3-1) + dfftp%nr1x * dfftp%my_i0r2p
+        CALL zcopy (nxyp, ddvtosym (ioff_tg+1, is), 1, dvtosym (ioff+1, is), 1)
      ENDDO
-  !ENDDO
+  ENDDO
+  !
   DEALLOCATE (ddvtosym)
-
+  !
   CALL stop_clock ('psymdvscf')
+  !
 #else
-   if ( .not. irr_bz ) RETURN
-  CALL kcw_symdvscf (dvtosym)!(nper, irr, dvtosym)
+  if ( .not. irr_bz ) RETURN
+  CALL kcw_symdvscf (dvtosym)
 #endif
-
+  !
   RETURN
-
+  !
 END SUBROUTINE kcw_psymdvscf
--- KCW/src/kcw_symdvscf.f90
+++ KCW/src/kcw_symdvscf.f90
@@ -8,228 +8,188 @@
 !
 !---------------------------------------------------------------------
 SUBROUTINE kcw_symdvscf (dvtosym)
-    !---------------------------------------------------------------------
-    !
-    ! Symmetrize the self-consistent potential of the perturbations
-    !
-    USE kinds,            ONLY : DP
-    USE constants,        ONLY : tpi
-    USE fft_base,         ONLY : dfftp
-    USE cell_base,        ONLY : at
-    USE symm_base,        ONLY : s, ft, t_rev
-    USE noncollin_module, ONLY : nspin_lsda, nspin_mag
-    USE ions_base,        ONLY : tau
-    USE qpoint,           ONLY : xq
-    USE lr_symm_base,     ONLY : minus_q, irotmq, nsymq, gi, gimq
-    USE qpoint,           ONLY : xq
-    USE symm_base,        ONLY : s
-    USE cell_base,        ONLY : bg, at
-    USE noncollin_module, ONLY : noncolin
-    USE qpoint,            ONLY : xq
-    implicit none
-  
-    complex(DP) :: dvtosym (dfftp%nr1x, dfftp%nr2x, dfftp%nr3x, nspin_mag)
-    ! the potential to be symmetrized
-    integer :: ftau(3,48), s_scaled(3,3,48)
-    integer :: is, ri, rj, rk, i, j, k, ipol, isym, irot
-    !  counters
-    real(DP) :: gf(3), gf2, n(3), gi_t(3, 48), aq (3), raq (3), wrk (3)
-    !  temp variables
-    complex(DP), allocatable :: dvsym (:,:,:)
-    ! the symmetrized potential
-    complex(DP) ::  aux2, term(3,48), phase(48), phase2(48)
-    ! auxiliary space
-    ! the multiplication factor
-    ! the phase factor
-    COMPLEX(DP)    :: phase_ft
-    COMPLEX(DP)    :: imag
-    REAL(DP)       :: xq_cryst(3)
-    !
-    if (nsymq == 1 .and. (.not.minus_q) ) return
-    !
-    call start_clock ('kcw_symdvscf')
-    !
-    allocate ( dvsym(dfftp%nr1x, dfftp%nr2x, dfftp%nr3x) )
-    !
-    n(1) = tpi / DBLE(dfftp%nr1)
-    n(2) = tpi / DBLE(dfftp%nr2)
-    n(3) = tpi / DBLE(dfftp%nr3)
-    !
-    xq_cryst = xq
-    imag = (0.D0, 1.D0)
-    CALL cryst_to_cart(1,xq_cryst,at,-1)
-    !
-    CALL scale_sym_ops( nsymq, s, ft, dfftp%nr1, dfftp%nr2, dfftp%nr3, &
-         s_scaled, ftau )
-    !
-    ! Symmetrize with -q if present (Sq = -q + G)
-    !
-    IF (minus_q) THEN
-       !
-       ! Compute the phase factor exp(iG*r)
-       ! where G = Sq + q
-       !
-       gf(:) =  gimq(1) * at(1,:) * n(:) + &
-                gimq(2) * at(2,:) * n(:) + &
-                gimq(3) * at(3,:) * n(:)
-       term(:,1) = CMPLX(cos(gf(:)), sin(gf(:)), kind=DP)
-       !
-       do is = 1, nspin_lsda
-          !
-          phase(1) = (1.d0, 0.d0)
-          !
-          do k = 1, dfftp%nr3
-             !
-             do j = 1, dfftp%nr2
+  !---------------------------------------------------------------------
+  !
+  ! Symmetrize the self-consistent potential of the perturbations
+  !
+  USE kinds,            ONLY : DP
+  USE constants,        ONLY : tpi
+  USE fft_base,         ONLY : dfftp
+  USE cell_base,        ONLY : at
+  USE symm_base,        ONLY : s, ft, t_rev
+  USE noncollin_module, ONLY : nspin_lsda, nspin_mag
+  USE ions_base,        ONLY : tau
+  USE qpoint,           ONLY : xq
+  USE lr_symm_base,     ONLY : minus_q, irotmq, nsymq, gi, gimq
+  USE qpoint,           ONLY : xq
+  USE symm_base,        ONLY : s
+  USE cell_base,        ONLY : bg, at
+  USE noncollin_module, ONLY : noncolin
+  USE qpoint,            ONLY : xq
+  implicit none
+
+  complex(DP) :: dvtosym (dfftp%nr1x, dfftp%nr2x, dfftp%nr3x, nspin_mag)
+  ! the potential to be symmetrized
+  integer :: ftau(3,48), s_scaled(3,3,48)
+  integer :: is, ri, rj, rk, i, j, k, ipol, isym, irot
+  !  counters
+  real(DP) :: gf(3), gf2, n(3), gi_t(3, 48), aq (3), raq (3), wrk (3)
+  !  temp variables
+  complex(DP), allocatable :: dvsym (:,:,:)
+  ! the symmetrized potential
+  complex(DP) ::  aux2, term(3,48), phase(48), phase2(48)
+  ! auxiliary space
+  ! the multiplication factor
+  ! the phase factor
+  COMPLEX(DP)    :: phase_ft
+  COMPLEX(DP)    :: imag
+  REAL(DP)       :: xq_cryst(3)
+  !
+  if (nsymq == 1 .and. (.not.minus_q) ) return
+  !
+  call start_clock ('kcw_symdvscf')
+  !
+  allocate ( dvsym(dfftp%nr1x, dfftp%nr2x, dfftp%nr3x) )
+  !
+  n(1) = tpi / DBLE(dfftp%nr1)
+  n(2) = tpi / DBLE(dfftp%nr2)
+  n(3) = tpi / DBLE(dfftp%nr3)
+  !
+  xq_cryst = xq
+  imag = (0.D0, 1.D0)
+  CALL cryst_to_cart(1,xq_cryst,at,-1)
+  !
+  CALL scale_sym_ops( nsymq, s, ft, dfftp%nr1, dfftp%nr2, dfftp%nr3, &
+       s_scaled, ftau )
+  !
+  ! Symmetrize with -q if present (Sq = -q + G)
+  !
+  IF (minus_q) THEN
+     !
+     ! Compute the phase factor exp(iG*r)
+     ! where G = Sq + q
+     !
+     gf(:) =  gimq(1) * at(1,:) * n(:) + &
+              gimq(2) * at(2,:) * n(:) + &
+              gimq(3) * at(3,:) * n(:)
+     term(:,1) = CMPLX(cos(gf(:)), sin(gf(:)), kind=DP)
+     !
+     do is = 1, nspin_lsda
+        !
+        phase(1) = (1.d0, 0.d0)
+        !
+        do k = 1, dfftp%nr3
+           !
+           do j = 1, dfftp%nr2
+              !
+              do i = 1, dfftp%nr1
+                 !
+                 ! Rotation and fractional translation: S^-1 * r - ftau
+                 !
+                 CALL rotate_grid_point( s_scaled(1,1,irotmq), ftau(1,irotmq),&
+                      i,j,k,dfftp%nr1,dfftp%nr2,dfftp%nr3,ri,rj,rk)
+                 !
+                 aux2 = (0.d0, 0.d0)
+                 aux2 = aux2 + dvtosym(ri,rj,rk,is) * phase(1) 
+                 !
+                 dvsym(i,j,k) = ( dvtosym(i,j,k,is) + CONJG(aux2) ) * 0.5d0
+                 !
+                 phase(1) = phase(1) * term(1,1)
+                 !
+              enddo
+              !
+              phase(1) = phase(1) * term(2,1)
+              !
+           enddo
+           !
+           phase(1) = phase(1) * term(3,1)
+           !
+        enddo
+        !
+        !WRITE(*, *) SUM( ABS( dvtosym(:,:,:,is) - dvsym(:,:,:) ) )
+        dvtosym(:,:,:,is) = dvsym(:,:,:)
+        !
+     enddo
+     !
+  ENDIF
+  !
+  ! Here we symmetrize with respect to the small group of q (Sq = q + G)
+  !
+  ! Compute the phase factor exp(iG*r)
+  ! where G = Sq - q
+  !
+  DO isym = 1, nsymq
+     gf(:) = gi(1,isym) * at(1,:) * n(:) + &
+             gi(2,isym) * at(2,:) * n(:) + &
+             gi(3,isym) * at(3,:) * n(:)
+     term(:,isym) = CMPLX( cos(gf(:)), sin(gf(:)), kind=DP)
+  ENDDO
+  !
+  DO is = 1, nspin_lsda
+     !
+     dvsym(:,:,:) = (0.d0, 0.d0)
+     !
+     do isym = 1, nsymq
+        phase(isym) = (1.d0, 0.d0)
+     enddo
+     !
+     do k = 1, dfftp%nr3
+        !
+        do j = 1, dfftp%nr2
+           !
+           do i = 1, dfftp%nr1
+              !
+              do isym = 1, nsymq
+                phase_ft = EXP(- imag * tpi * dot_product( xq_cryst(:), ft(:, isym) ) ) 
                 !
-                do i = 1, dfftp%nr1
-                   !
-                   ! Rotation and fractional translation: S^-1 * r - ftau
-                   !
-                   CALL rotate_grid_point( s_scaled(1,1,irotmq), ftau(1,irotmq),&
-                        i,j,k,dfftp%nr1,dfftp%nr2,dfftp%nr3,ri,rj,rk)
-                   !
-                   aux2 = (0.d0, 0.d0)
-                   aux2 = aux2 + dvtosym(ri,rj,rk,is) * phase(1) 
-                   !
-                   dvsym(i,j,k) = ( dvtosym(i,j,k,is) + CONJG(aux2) ) * 0.5d0
-                   !
-                   phase(1) = phase(1) * term(1,1)
-                   !
-                enddo
-                !
-                phase(1) = phase(1) * term(2,1)
-                !
-             enddo
-             !
-             phase(1) = phase(1) * term(3,1)
-             !
-          enddo
-          !
-          !WRITE(*, *) SUM( ABS( dvtosym(:,:,:,is) - dvsym(:,:,:) ) )
-          dvtosym(:,:,:,is) = dvsym(:,:,:)
-          !
-       enddo
-       !
-    ENDIF
-    !
-    ! Here we symmetrize with respect to the small group of q (Sq = q + G)
-    !
-    ! Compute the phase factor exp(iG*r)
-    ! where G = Sq - q
-    !
-    DO isym = 1, nsymq
-       gf(:) = gi(1,isym) * at(1,:) * n(:) + &
-               gi(2,isym) * at(2,:) * n(:) + &
-               gi(3,isym) * at(3,:) * n(:)
-       term(:,isym) = CMPLX( cos(gf(:)), sin(gf(:)), kind=DP)
-    ENDDO
-    !
-    ! Compute the phase factor exp(-iG*tau_pert)
-    ! where G = Sq - q, and tau_pert is the position
-    ! of the perturbed atom
-    !
-    !IF (noncolin) THEN
-    !   gi_t = 0.d0 
-    !   aq   = xq
-    !   call cryst_to_cart (1, aq, at, - 1)
-    !   do isym = 1, nsymq
-    !      raq = 0.d0
-    !      do i = 1, 3
-    !         do j = 1, 3
-    !            raq (i) = raq (i) + DBLE (s (i, j, isym) ) * &
-    !                aq (j)
-    !         enddo
-    !      enddo
-    !      do i = 1, 3
-    !         IF (t_rev(isym)==1) wrk (i) = aq (i) - raq (i)
-    !      enddo
-    !      call cryst_to_cart (1, wrk, bg, 1)
-    !      gi_t (:, isym) = wrk (:)
-    !   enddo
-    !ENDIF
-    !
-    !gf2 = 0
-    !DO isym = 1, nsymq
-    !   IF (t_rev(isym) == 1) then
-    !      gf2 = ( gi_t(1,isym) * tau(1,nah_pert) + &
-    !              gi_t(2,isym) * tau(2,nah_pert) + &
-    !              gi_t(3,isym) * tau(3,nah_pert) ) * tpi
-    !       phase2(isym) = CMPLX( cos(gf2), -sin(gf2), kind=DP)
-    !   ELSE
-    !      gf2 = ( gi(1,isym) * tau(1,nah_pert) + &
-    !              gi(2,isym) * tau(2,nah_pert) + &
-    !              gi(3,isym) * tau(3,nah_pert) ) * tpi
-    !      phase2(isym) = CMPLX( cos(gf2), -sin(gf2), kind=DP)
-    !   ENDIF
-    !ENDDO
-    !
-    DO is = 1, nspin_lsda
-       !
-       dvsym(:,:,:) = (0.d0, 0.d0)
-       !
-       do isym = 1, nsymq
-          phase(isym) = (1.d0, 0.d0)
-       enddo
-       !
-       do k = 1, dfftp%nr3
-          !
-          do j = 1, dfftp%nr2
-             !
-             do i = 1, dfftp%nr1
-                !
-                do isym = 1, nsymq
-                  phase_ft = EXP(- imag * tpi * dot_product( xq_cryst(:), ft(:, isym) ) ) 
-                  !
-                   irot = isym
-                   !
-                   ! Rotation and fractional translation: S^-1 * r - ftau
-                   !
-                   CALL rotate_grid_point( s_scaled(1,1,irot), ftau(1,irot),&
-                        i,j,k,dfftp%nr1,dfftp%nr2,dfftp%nr3,ri,rj,rk)
-                   !
-                   ! Calculate drho(S^-1 * r - ftau) * exp(iG*(r-tau_pert))
-                   !
-                   IF ( t_rev(isym) == 0 ) THEN
-                      dvsym(i,j,k) = dvsym(i,j,k) + & 
-                                dvtosym(ri,rj,rk,is) * phase(isym) * phase_ft
-                   ELSE
-                      dvsym(i,j,k) = dvsym(i,j,k) + & 
-                         conjg( dvtosym(ri,rj,rk,is) * phase(isym) * phase_ft )  
-                   ENDIF
-                   !
-                enddo
-                !
-                do isym = 1, nsymq
-                   phase(isym) = phase(isym) * term(1,isym)
-                enddo
-                !
-             enddo
-             !
-             do isym = 1, nsymq
-                phase(isym) = phase(isym) * term(2,isym)
-             enddo
-             !
-          enddo
-          !
-          do isym = 1, nsymq
-             phase(isym) = phase(isym) * term(3,isym)
-          enddo
-          !
-       enddo
-       !
-       ! Final normalization on the number of the symmetry operations
-       ! in the small group of q.
-       !
-       dvtosym(:,:,:,is) = dvsym(:,:,:) / DBLE(nsymq)
-       !
-    ENDDO
-    !
-    deallocate (dvsym)
-    !
-    call stop_clock ('kcw_symdvscf')
-    !
-    RETURN
-    !
-  END SUBROUTINE kcw_symdvscf
-  
+                 irot = isym
+                 !
+                 ! Rotation and fractional translation: S^-1 * r - ftau
+                 !
+                 CALL rotate_grid_point( s_scaled(1,1,irot), ftau(1,irot),&
+                      i,j,k,dfftp%nr1,dfftp%nr2,dfftp%nr3,ri,rj,rk)
+                 !
+                 ! Calculate drho(S^-1 * r - ftau) * exp(iG*(r-tau_pert))
+                 !
+                 IF ( t_rev(isym) == 0 ) THEN
+                    dvsym(i,j,k) = dvsym(i,j,k) + & 
+                              dvtosym(ri,rj,rk,is) * phase(isym) * phase_ft
+                 ELSE
+                    dvsym(i,j,k) = dvsym(i,j,k) + & 
+                       conjg( dvtosym(ri,rj,rk,is) * phase(isym) * phase_ft )  
+                 ENDIF
+                 !
+              enddo
+              !
+              do isym = 1, nsymq
+                 phase(isym) = phase(isym) * term(1,isym)
+              enddo
+              !
+           enddo
+           !
+           do isym = 1, nsymq
+              phase(isym) = phase(isym) * term(2,isym)
+           enddo
+           !
+        enddo
+        !
+        do isym = 1, nsymq
+           phase(isym) = phase(isym) * term(3,isym)
+        enddo
+        !
+     enddo
+     !
+     ! Final normalization on the number of the symmetry operations
+     ! in the small group of q.
+     !
+     dvtosym(:,:,:,is) = dvsym(:,:,:) / DBLE(nsymq)
+     !
+  ENDDO
+  !
+  deallocate (dvsym)
+  !
+  call stop_clock ('kcw_symdvscf')
+  !
+  RETURN
+  !
+END SUBROUTINE kcw_symdvscf
